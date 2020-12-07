718cb36144da76adbc4b66b84fa8c91271aab48e
Andreas Skomedal <andreas.skomedal@nav.no>
2020-12-07 13:18:12 +0100

    Behandling security opening

--- apps/backend/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
+++ apps/backend/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
@@ -46,6 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 "/krav/**",
                 "/etterlevelse/**",
+                "/behandling/**",
 
                 "/team/**"
         );
