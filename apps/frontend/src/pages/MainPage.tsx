203b499b3d01d4a4c57f88d6b8efac786f3ef0ed
Andreas Skomedal <andreas.skomedal@nav.no>
2020-12-14 12:36:45 +0100

    Forsideillustrasjon

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,8 +1,9 @@
 import {Block} from 'baseui/block'
 import React from 'react'
+import image from '../resources/undraw_processing_thoughts_d8ha.svg'
 
 export const MainPage = () => {
   return (
-    <Block>Mainpage</Block>
+    <Block><img width='90%' src={image} alt='Forsideillustrasjon'/> </Block>
   )
 }
