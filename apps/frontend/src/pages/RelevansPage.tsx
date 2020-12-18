67a563e9576b43ddcc6146f39651c90cf5cae191
Andreas Skomedal <andreas.skomedal@nav.no>
2020-12-18 16:12:32 +0100

    Minor krav tabell fiks

--- apps/frontend/src/pages/RelevansPage.tsx
+++ apps/frontend/src/pages/RelevansPage.tsx
@@ -1,7 +1,7 @@
 import {useParams} from 'react-router-dom'
 import {Block} from 'baseui/block'
 import React from 'react'
-import {HeadingMedium, ParagraphMedium} from 'baseui/typography'
+import {HeadingMedium, HeadingSmall, ParagraphMedium} from 'baseui/typography'
 import {codelist, codelistCompareField, ListName} from '../services/Codelist'
 import RouteLink from '../components/common/RouteLink'
 import {theme} from '../util'
@@ -37,44 +37,48 @@ export const RelevansPage = () => {
       <Block marginTop={theme.sizing.scale1200}>
         {loading && <Spinner size={theme.sizing.scale2400}/>}
         {!loading &&
-        <Table
-          data={data}
-          emptyText='krav'
-          headers={[
-            {title: 'Nummer', column: 'kravNummer', small: true},
-            {title: 'Krav', column: 'navn'},
-            {title: 'Etterlevelser', column: 'etterlevelser'},
-            {title: 'Avdeling', column: 'avdeling'},
-            {title: 'Underavdeling', column: 'underavdeling'},
-            {title: 'Status', column: 'status'},
-          ]}
-          config={{
-            initialSortColumn: 'kravNummer',
-            useDefaultStringCompare: true,
-            sorting: {
-              kravNummer: (a, b) => a.kravNummer === b.kravNummer ? a.kravVersjon - b.kravVersjon : a.kravNummer - b.kravNummer,
-              avdeling: codelistCompareField('avdeling'),
-              underavdeling: codelistCompareField('underavdeling')
+        <Block>
+          <HeadingSmall marginBottom={theme.sizing.scale200}>Krav</HeadingSmall>
+          <Table
+            data={data}
+            emptyText='krav'
+            headers={[
+              {title: 'Nummer', column: 'kravNummer', small: true},
+              {title: 'Navn', column: 'navn'},
+              {title: 'Etterlevelser', column: 'etterlevelser'},
+              {title: 'Avdeling', column: 'avdeling'},
+              {title: 'Underavdeling', column: 'underavdeling'},
+              {title: 'Status', column: 'status'},
+            ]}
+            config={{
+              initialSortColumn: 'kravNummer',
+              useDefaultStringCompare: true,
+              sorting: {
+                kravNummer: (a, b) => a.kravNummer === b.kravNummer ? a.kravVersjon - b.kravVersjon : a.kravNummer - b.kravNummer,
+                avdeling: codelistCompareField('avdeling'),
+                underavdeling: codelistCompareField('underavdeling')
 
-            }
-          }}
-          render={state => {
-            return state.data.map((krav, i) => {
-              return (
-                <Row key={i}>
-                  <Cell small>{krav.kravNummer}.{krav.kravVersjon}</Cell>
-                  <Cell>
-                    <RouteLink href={`/krav/${krav.kravNummer}/${krav.kravVersjon}`}>{krav.navn}</RouteLink>
-                  </Cell>
-                  <Cell>{krav.etterlevelser.length}</Cell>
-                  <Cell>{krav.avdeling?.shortName}</Cell>
-                  <Cell>{krav.underavdeling?.shortName}</Cell>
-                  <Cell>{kravStatus(krav.status)}</Cell>
-                </Row>
-              )
-            })
-          }}
-        />}
+              }
+            }}
+            render={state => {
+              return state.data.map((krav, i) => {
+                return (
+                  <Row key={i}>
+                    <Cell small>{krav.kravNummer}.{krav.kravVersjon}</Cell>
+                    <Cell>
+                      <RouteLink href={`/krav/${krav.kravNummer}/${krav.kravVersjon}`}>{krav.navn}</RouteLink>
+                    </Cell>
+                    <Cell>{krav.etterlevelser.length}</Cell>
+                    <Cell>{krav.avdeling?.shortName}</Cell>
+                    <Cell>{krav.underavdeling?.shortName}</Cell>
+                    <Cell>{kravStatus(krav.status)}</Cell>
+                  </Row>
+                )
+              })
+            }}
+          />
+        </Block>
+        }
       </Block>
     </Block>
   )
