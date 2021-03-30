08c6df186148b2b515cedae41c447676c80ef8b8
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-03-30 12:19:13 +0200

    ADD: landmark roles and aria label to multiple landmarks (navigation role)

--- apps/frontend/src/components/Navigation/Footer.tsx
+++ apps/frontend/src/components/Navigation/Footer.tsx
@@ -1,39 +1,41 @@
-import {Block} from 'baseui/block'
-import {theme} from '../../util'
-import {datajegerSlackLink, documentationLink, githubRepo} from '../../util/config'
+import { Block } from 'baseui/block'
+import { theme } from '../../util'
+import { datajegerSlackLink, documentationLink, githubRepo } from '../../util/config'
 import SlackLogo from '../../resources/Slack_Monochrome_White.svg'
-import {LabelMedium} from 'baseui/typography'
-import {StatefulTooltip} from 'baseui/tooltip'
-import {env} from '../../util/env'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faCodeBranch} from '@fortawesome/free-solid-svg-icons'
+import { LabelMedium } from 'baseui/typography'
+import { StatefulTooltip } from 'baseui/tooltip'
+import { env } from '../../util/env'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons'
 import * as React from 'react'
-import {ExternalLink} from '../common/RouteLink'
-import {navImage} from '../Images'
+import { ExternalLink } from '../common/RouteLink'
+import { navImage } from '../Images'
 
 
 export const Footer = () => {
   return (
-    <Block display='flex' justifyContent='space-between'
-           width='100%' maxWidth='600px' marginBottom='100px'
-           alignItems='center'>
+    <Block display='flex'
+      width='100%' maxWidth='600px' marginBottom='100px'
+      alignItems='center' overrides={{ Block: { props: { role: 'contentinfo' } } }}>
 
-      <img src={navImage} alt='NAV logo' width="85px"/>
+      <img src={navImage} alt='NAV logo' width="85px" />
 
-      <ExternalLink href={datajegerSlackLink} hideUnderline>
-        <Block display="flex" alignItems="center">
-          <img src={SlackLogo} width="60px" alt="slack logo"/>
-          <LabelMedium>#datajegerne </LabelMedium>
-        </Block>
-      </ExternalLink>
+      <Block marginLeft="scale1600" width='100%' display="flex" justifyContent='space-between' alignItems='center' overrides={{ Block: { props: { role: 'navigation', 'aria-label': 'Ekstern lenker' } } }}>
+        <ExternalLink href={datajegerSlackLink} hideUnderline>
+          <Block display="flex" alignItems="center">
+            <img src={SlackLogo} width="60px" alt="slack logo" />
+            <LabelMedium>#datajegerne </LabelMedium>
+          </Block>
+        </ExternalLink>
 
-      <StatefulTooltip content={`Versjon: ${env.githubVersion}`}>
-        <span><ExternalLink href={githubRepo} hideUnderline><FontAwesomeIcon icon={faCodeBranch}/> Github</ExternalLink></span>
-      </StatefulTooltip>
+        <StatefulTooltip content={`Versjon: ${env.githubVersion}`}>
+          <span><ExternalLink href={githubRepo} hideUnderline><FontAwesomeIcon icon={faCodeBranch} /> Github</ExternalLink></span>
+        </StatefulTooltip>
 
-      <ExternalLink href={documentationLink} hideUnderline>
-        <LabelMedium marginLeft={theme.sizing.scale200}>Dokumentasjon</LabelMedium>
-      </ExternalLink>
+        <ExternalLink href={documentationLink} hideUnderline>
+          <LabelMedium marginLeft={theme.sizing.scale200}>Dokumentasjon</LabelMedium>
+        </ExternalLink>
+      </Block>
     </Block>
   )
 }
