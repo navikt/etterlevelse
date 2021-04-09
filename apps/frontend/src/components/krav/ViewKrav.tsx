f1e54da342df5c5546a3a284575a13c94a41d7c5
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-04-09 12:21:43 +0200

    ADD: new tabs and design for tabs
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/krav/ViewKrav.tsx
+++ apps/frontend/src/components/krav/ViewKrav.tsx
@@ -7,7 +7,7 @@ import moment from 'moment'
 import Button from '../common/Button'
 import { DotTag, DotTags } from '../common/DotTag'
 import { ListName } from '../../services/Codelist'
-import { Label } from '../common/PropertyLabel'
+import { Label, LabelAboveContent } from '../common/PropertyLabel'
 import { ExternalLink, ObjectLink } from '../common/RouteLink'
 import { StyledLink } from 'baseui/link'
 import { slackLink, slackUserLink, termUrl } from '../../util/config'
@@ -15,7 +15,8 @@ import { user } from '../../services/User'
 import { LovViewList } from '../Lov'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
-import { Tabs, Tab } from "baseui/tabs";
+import { CustomizedTabs, CustomizedTab } from '../common/CustomizedTabs'
+import { LabelLarge } from 'baseui/typography'
 
 
 
@@ -23,7 +24,6 @@ const formatDate = (date?: string) => date && moment(date).format('ll')
 
 export const ViewKrav = ({ krav }: { krav: KravQL }) => {
   const [expand, setExpand] = useState(false)
-  const [activeKey, setActiveKey] = useState('0')
 
   return (
     <Block width='100%'>
@@ -33,28 +33,27 @@ export const ViewKrav = ({ krav }: { krav: KravQL }) => {
         </Block>
       </Block>
 
-      <Block display='flex' justifyContent='center'>
-          <Tabs
-            onChange={({ activeKey}) => {
-              setActiveKey(activeKey.toString())
-            }}
-            activeKey={activeKey}
-          >
-            <Tab title="Tab Link 1">Content 1</Tab>
-            <Tab title="Tab Link 2">Content 2</Tab>
-            <Tab title="Tab Link 3">Content 3</Tab>
-          </Tabs>
+      <Block display='flex' justifyContent='center' width='100%'>
+        <Block backgroundColor='#CCD9D7' flex={1} height='58px'/>
+        <Block width='600px'>
+          <CustomizedTabs>
+            <CustomizedTab title={<LabelLarge>Om kravet</LabelLarge>}>
+              <LabelAboveContent title='Suksesskriterier' markdown={krav.suksesskriterier.map(s => `${s.id}: ${s.navn}`)} vertical />
+              <LabelAboveContent title='Beskrivelse' markdown={krav.beskrivelse} />
+
+              <Block height={theme.sizing.scale800} />
+
+              {expand && <AllInfo krav={krav} />}
+              {!expand && <MediumInfo krav={krav} />}
+              <Block display='flex' justifyContent='flex-end'>
+                <Button onClick={() => setExpand(!expand)}>{`${expand ? 'Skjul' : 'Vis'} detaljer`}</Button>
+              </Block>
+            </CustomizedTab>
+            <CustomizedTab title={<LabelLarge>Spørsmål og svar</LabelLarge>}> test 2</CustomizedTab>
+            <CustomizedTab title={<LabelLarge>Eksempler på etterlevelse</LabelLarge>}> test 3</CustomizedTab>
+          </CustomizedTabs>
         </Block>
-
-      <Label title='Suksesskriterier' markdown={krav.suksesskriterier.map(s => `${s.id}: ${s.navn}`)} vertical />
-      <Label title='Beskrivelse' markdown={krav.beskrivelse} />
-
-      <Block height={theme.sizing.scale800} />
-
-      {expand && <AllInfo krav={krav} />}
-      {!expand && <MediumInfo krav={krav} />}
-      <Block display='flex' justifyContent='flex-end'>
-        <Button onClick={() => setExpand(!expand)}>{`${expand ? 'Skjul' : 'Vis'} detaljer`}</Button>
+        <Block backgroundColor='#CCD9D7' flex={1} height='58px'/>
       </Block>
     </Block>
   )
@@ -69,33 +68,33 @@ const MediumInfo = ({ krav }: { krav: KravQL }) => (
 
 const AllInfo = ({ krav }: { krav: KravQL }) => (
   <>
-    <Label title='Utfyllende beskrivelse' markdown={krav.utdypendeBeskrivelse} />
-    <Label title='Endringer fra forrige versjon' markdown={krav.versjonEndringer} />
-    <Label title='Dokumentasjon' markdown={krav.dokumentasjon} />
-    <Label title='Regelverk' hide={!krav.regelverk.length}>
+    <LabelAboveContent title='Utfyllende beskrivelse' markdown={krav.utdypendeBeskrivelse} />
+    <LabelAboveContent title='Endringer fra forrige versjon' markdown={krav.versjonEndringer} />
+    <LabelAboveContent title='Dokumentasjon' markdown={krav.dokumentasjon} />
+    <LabelAboveContent title='Regelverk' hide={!krav.regelverk.length}>
       <LovViewList regelverk={krav.regelverk} />
-    </Label>
-    <Label title='Rettskilder' markdown={krav.rettskilder} />
+    </LabelAboveContent>
+    <LabelAboveContent title='Rettskilder' markdown={krav.rettskilder} />
 
-    <Label title='Tagger'>{krav.tagger.join(', ')}</Label>
-    <Label title='Kravet er relevant for'><DotTags list={ListName.RELEVANS} codes={krav.relevansFor} linkCodelist /></Label>
-    <Label title='Relevante implementasjoner' markdown={krav.implementasjoner} />
-    <Label title='Begreper'>{krav.begreper.map((b, i) => <BegrepView key={i} begrep={b} />)}</Label>
+    <LabelAboveContent title='Tagger'>{krav.tagger.join(', ')}</LabelAboveContent>
+    <LabelAboveContent title='Kravet er relevant for'><DotTags list={ListName.RELEVANS} codes={krav.relevansFor} linkCodelist /></LabelAboveContent>
+    <LabelAboveContent title='Relevante implementasjoner' markdown={krav.implementasjoner} />
+    <LabelAboveContent title='Begreper'>{krav.begreper.map((b, i) => <BegrepView key={i} begrep={b} />)}</LabelAboveContent>
 
-    {krav.periode?.start && <Label title='Gyldig fom'>{formatDate(krav.periode?.start)}</Label>}
-    {krav.periode?.slutt && <Label title='Gyldig tom'>{formatDate(krav.periode?.slutt)}</Label>}
+    {krav.periode?.start && <LabelAboveContent title='Gyldig fom'>{formatDate(krav.periode?.start)}</LabelAboveContent>}
+    {krav.periode?.slutt && <LabelAboveContent title='Gyldig tom'>{formatDate(krav.periode?.slutt)}</LabelAboveContent>}
 
-    <Label title='Status'>{kravStatus(krav.status)}</Label>
-    <Label title='Varslingsadresser' hide={!user.isKraveier()}>
+    <LabelAboveContent title='Status'>{kravStatus(krav.status)}</LabelAboveContent>
+    <LabelAboveContent title='Varslingsadresser' hide={!user.isKraveier()}>
       <DotTags items={krav.varslingsadresser.map((va, i) => {
         if (va.type === AdresseType.SLACK) return <Block>Slack: <StyledLink href={slackLink(va.adresse)}>#{va.slackChannel?.name || va.adresse}</StyledLink></Block>
         if (va.type === AdresseType.SLACK_USER) return <Block>Slack: <StyledLink href={slackUserLink(va.adresse)}>{va.slackUser?.name || va.adresse}</StyledLink></Block>
         return <Block>Epost: <StyledLink href={`mailto:${va.adresse}`}>{va.adresse}</StyledLink></Block>
       }
       )} />
-    </Label>
-    <Label title='Avdeling'>{krav.avdeling?.shortName}</Label>
-    <Label title='Underavdeling'><ObjectLink id={krav.underavdeling?.code} type={ListName.UNDERAVDELING}>{krav.underavdeling?.shortName}</ObjectLink></Label>
+    </LabelAboveContent>
+    <LabelAboveContent title='Avdeling'>{krav.avdeling?.shortName}</LabelAboveContent>
+    <LabelAboveContent title='Underavdeling'><ObjectLink id={krav.underavdeling?.code} type={ListName.UNDERAVDELING}>{krav.underavdeling?.shortName}</ObjectLink></LabelAboveContent>
   </>
 )
 
