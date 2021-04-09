2c8ecb1671756b674e9bd03df1f99698440aae44
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-04-09 16:28:01 +0200

    UPDATE: footer design with to top button image

--- apps/frontend/src/components/Navigation/Footer.tsx
+++ apps/frontend/src/components/Navigation/Footer.tsx
@@ -14,36 +14,34 @@ import ToTopCircle from '../../resources/ToTopCircle.svg'
 
 export const Footer = () => {
   return (
-    <Block width='100%'>
-      <Block position='fixed' display='flex' width='calc(100% - 80px)' justifyContent='flex-end' $style={{ cursor: 'pointer', top: '-50px' }} onClick={() => window.scrollTo(0, 0)} paddingRight='40px' paddingLeft='40px'>
-        <img src={ToTopCircle} alt='Til toppen av siden' />
-      </Block>
-      <Block width='calc(100% - 80px)' height='100px' backgroundColor='#112724' paddingRight='40px' paddingLeft='40px'>
-        <Block display='flex'
-          width='100%' minHeight='100px'
-          justifyContent='center'
-          alignItems='center' overrides={{ Block: { props: { role: 'contentinfo' } } }}
-        >
-          <Block width='50%'>
-            <Block width='100%' display="flex" justifyContent='space-around' alignItems='center' overrides={{ Block: { props: { role: 'navigation', 'aria-label': 'Ekstern lenker' } } }}>
-              <ExternalLink href={datajegerSlackLink} hideUnderline>
-                <Block display="flex" alignItems="center">
-                  <img src={SlackLogo} width="60px" alt="slack logo" />
-                  <LabelMedium color='#F8F8F8'>#datajegerne </LabelMedium>
-                </Block>
-              </ExternalLink>
-
-              <StatefulTooltip content={`Versjon: ${env.githubVersion}`}>
-                <span><ExternalLink fontColor='#F8F8F8' href={githubRepo} hideUnderline><FontAwesomeIcon color='#F8F8F8' icon={faCodeBranch} /> Github</ExternalLink></span>
-              </StatefulTooltip>
-
-              <ExternalLink href={documentationLink} hideUnderline>
-                <LabelMedium color='#F8F8F8' marginLeft={theme.sizing.scale200}>Dokumentasjon</LabelMedium>
-              </ExternalLink>
+    <Block display='flex' width='calc(100% - 80px)' height='100px' backgroundColor='#112724' paddingRight='40px' paddingLeft='40px'>
+      <Block display='flex'
+        width='100%' minHeight='100px'
+        justifyContent='center'
+        alignItems='center' overrides={{ Block: { props: { role: 'contentinfo' } } }}
+      >
+        <Block width='100%' display="flex" justifyContent='space-around' alignItems='center' overrides={{ Block: { props: { role: 'navigation', 'aria-label': 'Ekstern lenker' } } }}>
+          <ExternalLink href={datajegerSlackLink} hideUnderline>
+            <Block display="flex" alignItems="center">
+              <img src={SlackLogo} width="60px" alt="slack logo" />
+              <LabelMedium color='#F8F8F8'>#datajegerne </LabelMedium>
             </Block>
-          </Block>
+          </ExternalLink>
+
+          <StatefulTooltip content={`Versjon: ${env.githubVersion}`}>
+            <span><ExternalLink fontColor='#F8F8F8' href={githubRepo} hideUnderline><FontAwesomeIcon color='#F8F8F8' icon={faCodeBranch} /> Github</ExternalLink></span>
+          </StatefulTooltip>
+
+          <ExternalLink href={documentationLink} hideUnderline>
+            <LabelMedium color='#F8F8F8' marginLeft={theme.sizing.scale200}>Dokumentasjon</LabelMedium>
+          </ExternalLink>
         </Block>
       </Block>
+
+      <Block position='relative' display='flex' justifyContent='flex-end' $style={{ cursor: 'pointer', top: '-50px' }} onClick={() => window.scrollTo(0, 0)}>
+        <img src={ToTopCircle} alt='Til toppen av siden' />
+      </Block>
+      
     </Block>
   )
 }
