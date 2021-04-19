f3b1f96c1826761ae487e7fb1b5a3c54410fe44a
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-04-19 13:04:41 +0200

    UPDATE: display breakpoints
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -123,7 +123,7 @@ const Header = (props: { noSearchBar?: boolean, noLoginButton?: boolean }) => {
   return (
     <Block paddingLeft='40px' paddingRight='40px' width='calc(100%-80px)' height='76px' overrides={{ Block: { props: { role: 'banner', 'aria-label': 'Header meny' } } }}>
       <HeaderNavigation overrides={{ Root: { style: { paddingBottom: 0, borderBottomStyle: 'none' } } }}>
-        <Block display={['block', 'block', 'none', 'none']}>
+        <Block display={['block', 'block', 'block', 'block', 'none', 'none']}>
           <BurgerMenu />
         </Block>
 
@@ -140,15 +140,15 @@ const Header = (props: { noSearchBar?: boolean, noLoginButton?: boolean }) => {
           </NavigationItem>
         </NavigationList>
 
-        <NavigationList $style={{justifyContent: 'center', marginLeft: '68px'}}>
-          {!props.noSearchBar && (<NavigationItem $style={{ width: pageWidth }}>
-            <Block display={['none', 'none', 'flex', 'flex']} overrides={{ Block: { props: { role: 'search' } } }}>
+        <NavigationList $style={{justifyContent: 'center'}}>
+          {!props.noSearchBar && (<NavigationItem>
+            <Block flex='1' display={['none', 'none','none', 'none', 'none', 'flex']} overrides={{ Block: { props: { role: 'search' } } }}>
               <MainSearch />
             </Block>
           </NavigationItem>)}
         </NavigationList>
 
-        {!props.noLoginButton && (<Block display={['none', 'none', 'flex', 'flex']}>
+        {!props.noLoginButton && (<Block display={['none', 'none','none', 'none', 'flex', 'flex']}>
           <NavigationList $align={ALIGN.right}>
             {user.isAdmin() && (
               <NavigationItem $style={{ paddingLeft: 0 }}>
--- apps/frontend/src/components/search/MainSearch.tsx
+++ apps/frontend/src/components/search/MainSearch.tsx
@@ -34,7 +34,7 @@ type RadioProps = {
   $checked: boolean
 }
 
-const responsiveWidth = ['300px', '400px', '400px', '500px']
+const responsiveWidth = ['300px', '400px']
 
 const SmallRadio = (value: SearchType, label: string) => {
   return (
