8fed14f7cafb6061adc5cd0f608b6a48fc9abd4a
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-04-23 15:37:24 +0200

    UPDATE: label for datatext to label1

--- apps/frontend/src/components/common/DataText.tsx
+++ apps/frontend/src/components/common/DataText.tsx
@@ -1,7 +1,7 @@
 import { default as React, ReactNode } from 'react'
 import { Block } from 'baseui/block'
 import { theme } from '../../util'
-import { Label2, H2, H3 } from 'baseui/typography'
+import { Label1, H2 } from 'baseui/typography'
 
 type DataTextProps = {
   label?: string
@@ -29,7 +29,7 @@ const DataText = (props: DataTextProps) => {
     } else {
       return (
         <Block minWidth={labelWidth} maxWidth={labelWidth} paddingRight={theme.sizing.scale400}>
-          <Label2 $style={{ lineHeight: theme.sizing.scale800 }}>{props.label}</Label2>
+          <Label1 $style={{ lineHeight: theme.sizing.scale800 }}>{props.label}</Label1>
         </Block>
       )
     }
