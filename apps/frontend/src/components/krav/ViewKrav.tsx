dc51bf12aaf075003e332dd3dd2dec4538709d44
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-05-03 15:28:11 +0200

    UPDATE: margin on krav page
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/krav/ViewKrav.tsx
+++ apps/frontend/src/components/krav/ViewKrav.tsx
@@ -1,77 +1,102 @@
-import {AdresseType, Begrep, KravQL} from '../../constants'
-import {Block} from 'baseui/block'
+import { AdresseType, Begrep, KravQL } from '../../constants'
+import { Block } from 'baseui/block'
 import React from 'react'
-import {kravStatus} from '../../pages/KravPage'
-import {theme} from '../../util'
+import { kravStatus } from '../../pages/KravPage'
+import { theme } from '../../util'
 import moment from 'moment'
-import {DotTag, DotTags} from '../common/DotTag'
-import {ListName} from '../../services/Codelist'
-import {Label, LabelAboveContent} from '../common/PropertyLabel'
-import {ExternalLink, ObjectLink} from '../common/RouteLink'
-import {StyledLink} from 'baseui/link'
-import {slackLink, slackUserLink, termUrl} from '../../util/config'
-import {user} from '../../services/User'
-import {LovViewList} from '../Lov'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons'
-import {SuksesskriterieCard} from './Suksesskriterie'
+import { DotTag, DotTags } from '../common/DotTag'
+import { ListName } from '../../services/Codelist'
+import { Label, LabelAboveContent } from '../common/PropertyLabel'
+import { ExternalLink, ObjectLink } from '../common/RouteLink'
+import { StyledLink } from 'baseui/link'
+import { slackLink, slackUserLink, termUrl } from '../../util/config'
+import { user } from '../../services/User'
+import { LovViewList } from '../Lov'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { SuksesskriterieCard } from './Suksesskriterie'
 
 
 const formatDate = (date?: string) => date && moment(date).format('ll')
 
-export const ViewKrav = ({krav}: {krav: KravQL}) => {
+const LabelWrapper = ({ children }: { children: React.ReactNode }) => (
+  <Block marginTop='48px' marginBottom='48px'>
+    {children}
+  </Block>
+)
+
+export const ViewKrav = ({ krav }: { krav: KravQL }) => {
 
   return (
     <Block width='100%'>
 
-      {krav.suksesskriterier.map((s,i) => <SuksesskriterieCard key={s.id} suksesskriterie={s} num={i+1} totalt={krav.suksesskriterier.length}/>)}
-      <Block height={theme.sizing.scale1000}/>
+      {krav.suksesskriterier.map((s, i) => <SuksesskriterieCard key={s.id} suksesskriterie={s} num={i + 1} totalt={krav.suksesskriterier.length} />)}
+      <Block height={theme.sizing.scale1000} />
 
-      <LabelAboveContent header title='Beskrivelse' markdown={krav.beskrivelse}/>
+      <LabelAboveContent header title='Beskrivelse' markdown={krav.beskrivelse} />
 
-      <Block height={theme.sizing.scale800}/>
+      <Block height={theme.sizing.scale800} />
 
-      {<AllInfo krav={krav}/>}
+      {<AllInfo krav={krav} />}
     </Block>
   )
 }
 
-const MediumInfo = ({krav}: {krav: KravQL}) => (
+const MediumInfo = ({ krav }: { krav: KravQL }) => (
   <>
     <Label title='Status'>{kravStatus(krav.status)}</Label>
     <Label title='Underavdeling'><ObjectLink id={krav.underavdeling?.code} type={ListName.UNDERAVDELING}>{krav.underavdeling?.shortName}</ObjectLink></Label>
   </>
 )
 
-const AllInfo = ({krav}: {krav: KravQL}) => (
+const AllInfo = ({ krav }: { krav: KravQL }) => (
   <>
-    <LabelAboveContent header title='Utfyllende beskrivelse' markdown={krav.utdypendeBeskrivelse}/>
-    <LabelAboveContent header title='Endringer fra forrige versjon' markdown={krav.versjonEndringer}/>
-    <LabelAboveContent header title='Dokumentasjon' markdown={krav.dokumentasjon}/>
+    <LabelWrapper>
+      <LabelAboveContent header title='Utfyllende beskrivelse' markdown={krav.utdypendeBeskrivelse} />
+    </LabelWrapper>
 
-    <LabelAboveContent header title='Rettskilder' markdown={krav.rettskilder}/>
+    <LabelWrapper>
+      <LabelAboveContent header title='Endringer fra forrige versjon' markdown={krav.versjonEndringer} />
+    </LabelWrapper>
 
-    <LabelAboveContent header title='Etiketter'>{krav.tagger.join(', ')}</LabelAboveContent>
-    <LabelAboveContent header title='Relevante implementasjoner' markdown={krav.implementasjoner}/>
-    <LabelAboveContent header title='Begreper'>{krav.begreper.map((b, i) => <BegrepView key={i} begrep={b}/>)}</LabelAboveContent>
+    <LabelWrapper>
+      <LabelAboveContent header title='Dokumentasjon' markdown={krav.dokumentasjon} />
+    </LabelWrapper>
 
+    <LabelWrapper>
+      <LabelAboveContent header title='Rettskilder' markdown={krav.rettskilder} />
+    </LabelWrapper>
+
+    <LabelWrapper>
+      <LabelAboveContent header title='Etiketter'>{krav.tagger.join(', ')}</LabelAboveContent>
+    </LabelWrapper>
+
+    <LabelWrapper>
+      <LabelAboveContent header title='Relevante implementasjoner' markdown={krav.implementasjoner} />
+    </LabelWrapper>
+
+    <LabelWrapper>
+      <LabelAboveContent header title='Begreper'>{krav.begreper.map((b, i) => <BegrepView key={i} begrep={b} />)}</LabelAboveContent>
+    </LabelWrapper>
 
     {/* <LabelAboveContent title='Avdeling'>{krav.avdeling?.shortName}</LabelAboveContent> */}
 
-    <LabelAboveContent header title='Kravet er relevant for'><DotTags list={ListName.RELEVANS} codes={krav.relevansFor} linkCodelist/></LabelAboveContent>
+    <LabelWrapper>
+      <LabelAboveContent header title='Kravet er relevant for'><DotTags list={ListName.RELEVANS} codes={krav.relevansFor} linkCodelist /></LabelAboveContent>
+    </LabelWrapper>
 
-    <Block backgroundColor='#F6E8E6' padding='40px'>
+    <Block backgroundColor='#F1F1F1' padding='32px'>
       <Label title='Ansvarlig'><ObjectLink id={krav.underavdeling?.code} type={ListName.UNDERAVDELING}>{krav.underavdeling?.shortName}</ObjectLink></Label>
       <Label title='Regelverk' hide={!krav.regelverk.length}>
-        <LovViewList regelverk={krav.regelverk}/>
+        <LovViewList regelverk={krav.regelverk} />
       </Label>
       <Label title='Varslingsadresser' hide={!user.isKraveier()}>
         <DotTags items={krav.varslingsadresser.map((va, i) => {
-            if (va.type === AdresseType.SLACK) return <Block>Slack: <StyledLink href={slackLink(va.adresse)}>#{va.slackChannel?.name || va.adresse}</StyledLink></Block>
-            if (va.type === AdresseType.SLACK_USER) return <Block>Slack: <StyledLink href={slackUserLink(va.adresse)}>{va.slackUser?.name || va.adresse}</StyledLink></Block>
-            return <Block>Epost: <StyledLink href={`mailto:${va.adresse}`}>{va.adresse}</StyledLink></Block>
-          }
-        )}/>
+          if (va.type === AdresseType.SLACK) return <Block>Slack: <StyledLink href={slackLink(va.adresse)}>#{va.slackChannel?.name || va.adresse}</StyledLink></Block>
+          if (va.type === AdresseType.SLACK_USER) return <Block>Slack: <StyledLink href={slackUserLink(va.adresse)}>{va.slackUser?.name || va.adresse}</StyledLink></Block>
+          return <Block>Epost: <StyledLink href={`mailto:${va.adresse}`}>{va.adresse}</StyledLink></Block>
+        }
+        )} />
       </Label>
       <Label title='Status'>{kravStatus(krav.status)}</Label>
       {krav.periode?.start && <Label title='Gyldig fom'>{formatDate(krav.periode?.start)}</Label>}
@@ -80,10 +105,10 @@ const AllInfo = ({krav}: {krav: KravQL}) => (
   </>
 )
 
-const BegrepView = ({begrep}: {begrep: Begrep}) => (
+const BegrepView = ({ begrep }: { begrep: Begrep }) => (
   <DotTag>
     {begrep.navn} - {begrep.beskrivelse} <ExternalLink href={termUrl(begrep.id)} label={'Link begrepskatalogen'}>
-    <FontAwesomeIcon icon={faExternalLinkAlt}/>
-  </ExternalLink>
+      <FontAwesomeIcon icon={faExternalLinkAlt} />
+    </ExternalLink>
   </DotTag>
 )
