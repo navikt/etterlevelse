ddf9908783bc18dcd04154efda023bd837d3ba82
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-05-03 14:33:47 +0200

    UPDATE: to show search icon for term to be on right side
    Co-authored-by: andregoseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/krav/Edit/KravBegreperEdit.tsx
+++ apps/frontend/src/components/krav/Edit/KravBegreperEdit.tsx
@@ -1,17 +1,16 @@
-import {FieldWrapper} from '../../common/Inputs'
-import {useBegrepSearch} from '../../../api/BegrepApi'
+import { FieldWrapper } from '../../common/Inputs'
+import { useBegrepSearch } from '../../../api/BegrepApi'
 import React from 'react'
-import {FieldArray} from 'formik'
-import {intl} from '../../../util/intl/intl'
-import {Select, TYPE, SIZE as selectSize} from 'baseui/select'
-import {FormControl} from 'baseui/form-control'
-import {Error} from '../../common/ModalSchema'
-import {RenderTagList} from '../../common/TagList'
-import {Begrep} from '../../../constants'
-import {Block} from 'baseui/block'
+import { FieldArray } from 'formik'
+import { intl } from '../../../util/intl/intl'
+import { Select, TYPE, SIZE as selectSize } from 'baseui/select'
+import { FormControl } from 'baseui/form-control'
+import { Error } from '../../common/ModalSchema'
+import { RenderTagList } from '../../common/TagList'
+import { Begrep } from '../../../constants'
+import { Block } from 'baseui/block'
 import LabelWithTooltip from '../../common/LabelWithTooltip'
-import {searchIcon} from '../../Images'
-import { SIZE } from 'baseui/button'
+import { searchIcon } from '../../Images'
 
 export const EditBegreper = () => {
   const [result, setSearch, loading] = useBegrepSearch()
@@ -21,13 +20,35 @@ export const EditBegreper = () => {
       <FieldArray name='begreper'>
         {p => {
           return (
-            <FormControl label={<LabelWithTooltip label={'Begreper'} tooltip={'Legg ved lenke til relevante begrep(er) i Begrepskatalogen.'}/>} >
+            <FormControl label={<LabelWithTooltip label={'Begreper'} tooltip={'Legg ved lenke til relevante begrep(er) i Begrepskatalogen.'} />} >
               <Block>
                 <Select
-                overrides={{
-                  SearchIcon: {
-                    component: () => <img src={searchIcon} alt='search icon' />
-                  }}}
+                  overrides={{
+                    SearchIcon: {
+                      component: () => <img src={searchIcon} alt='search icon' />,
+                      style: {
+                        display: 'flex',
+                        justifyContent: 'flex-end'
+                      }
+                    },
+                    SearchIconContainer: {
+                      style: {
+                        width: 'calc(100% - 20px)',
+                        display: 'flex',
+                        justifyContent: 'flex-end'
+                      }
+                    },
+                    IconsContainer: {
+                      style: {
+                        marginRight: '20px'
+                      }
+                    },
+                    ValueContainer: {
+                      style: {
+                        paddingLeft: '10px'
+                      }
+                    },
+                  }}
                   size={selectSize.compact}
                   labelKey={'navn'}
                   noResultsMsg={intl.emptyTable}
@@ -44,13 +65,13 @@ export const EditBegreper = () => {
                   error={!!p.form.errors.begreper && !!p.form.submitCount}
                   isLoading={loading}
                 />
-                <RenderTagList wide list={p.form.values.begreper.map((b: Begrep) => b.navn)} onRemove={p.remove}/>
+                <RenderTagList wide list={p.form.values.begreper.map((b: Begrep) => b.navn)} onRemove={p.remove} />
               </Block>
             </FormControl>
           )
         }}
       </FieldArray>
-      <Error fieldName="begreper" fullWidth/>
+      <Error fieldName="begreper" fullWidth />
     </FieldWrapper>
   )
 }
