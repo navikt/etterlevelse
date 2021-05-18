b5945ce9a89ef856fffdb654e34e5d1008b5f820
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-05-18 15:51:23 +0200

    UPDATE: to show initial state

--- apps/frontend/src/components/common/TextEditor.tsx
+++ apps/frontend/src/components/common/TextEditor.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { convertToRaw, EditorState } from 'draft-js'
+import { convertToRaw, convertFromRaw, EditorState } from 'draft-js'
 import { Editor } from 'react-draft-wysiwyg'
-import { draftToMarkdown } from 'markdown-draft-js'
+import { draftToMarkdown, markdownToDraft } from 'markdown-draft-js'
 import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import { Block } from 'baseui/block'
 import { ettlevColors } from '../../util/theme'
@@ -17,25 +17,26 @@ type TextEditorProps = {
 const TextEditor = (props: TextEditorProps) => {
   const [val, setVal] = React.useState(EditorState.createEmpty())
 
+  React.useEffect(() => {
+    const rawData = markdownToDraft(props.initialValue)
+    setVal(EditorState.createWithContent(convertFromRaw(rawData)))
+  }, [props.initialValue])
+
   return (
-    <Block backgroundColor={ettlevColors.white} $style={{border: `1px solid ${ettlevColors.textAreaBorder}`}}>
+    <Block backgroundColor={ettlevColors.white} $style={{ border: `1px solid ${ettlevColors.textAreaBorder}` }}>
       <Editor
-        editorStyle={{padding: '10px', height: props.height || '500px'}}
-        toolbarStyle={{backgroundColor: ettlevColors.grey50, borderBottom: `1px solid ${ettlevColors.textAreaBorder}` }}
+        editorStyle={{ padding: '10px', height: props.height || '500px' }}
+        toolbarStyle={{ backgroundColor: ettlevColors.grey50, borderBottom: `1px solid ${ettlevColors.textAreaBorder}` }}
         onEditorStateChange={data => setVal(data)}
+        contentState={convertToRaw(val.getCurrentContent())}
         toolbar={{
           options: ['inline', 'blockType', 'list', 'textAlign', 'link', 'image', 'history'],
-          blockType: {options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'Blockquote']},
-          inline: {options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace']},
-          list: {options: ['unordered', 'ordered']},
-          link: { options: ['link']},
-          image: {uploadCallback: props.onImageUpload},
+          inline: { options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace'] },
+          list: { options: ['unordered', 'ordered'] },
+          link: { options: ['link'] },
+          image: { uploadCallback: props.onImageUpload },
         }}
       />
-      {/* <textarea
-        disabled
-        value={val && draftToMarkdown(convertToRaw(val.getCurrentContent()))}
-      /> */}
     </Block>
   )
 }
