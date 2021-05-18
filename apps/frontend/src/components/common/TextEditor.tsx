02db78aabf3359ecc15e4ae99b23fb25576e5772
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-05-18 15:12:56 +0200

    UPDATE: toolbar options and upload picture via file

--- apps/frontend/src/components/common/TextEditor.tsx
+++ apps/frontend/src/components/common/TextEditor.tsx
@@ -6,19 +6,32 @@ import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import { Block } from 'baseui/block'
 import { ettlevColors } from '../../util/theme'
 
+type TextEditorProps = {
+  onImageUpload?: (file: File) => Promise<string>,
+}
 
-const TextEditor = () => {
+const TextEditor = (props: TextEditorProps) => {
   const [editorState, setEditorState] = React.useState(EditorState.createEmpty())
 
 
   return (
-    <Block backgroundColor={ettlevColors.white}>
+    <Block backgroundColor={ettlevColors.white} $style={{border: `1px solid ${ettlevColors.grey200}`}}>
       <Editor
-        toolbarClassName="rdw-storybook-toolbar"
-        wrapperClassName="rdw-storybook-wrapper"
-        editorClassName="rdw-storybook-editor"
+        editorStyle={{padding: '10px'}}
+        toolbarStyle={{backgroundColor: ettlevColors.grey50, borderBottom: `1px solid ${ettlevColors.grey200}` }}
         onEditorStateChange={(value: any) => {
           setEditorState(value)
+        }}
+        toolbar={{
+          options: ['inline', 'blockType', 'list', 'textAlign', 'link', 'image', 'history'],
+          inline: {options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace']},
+          list: {options: ['unordered', 'ordered']},
+          link: { options: ['link']},
+          image: {
+            uploadEnabled: true,
+            uploadCallback: props.onImageUpload
+          },
+          
         }}
       />
       <textarea
