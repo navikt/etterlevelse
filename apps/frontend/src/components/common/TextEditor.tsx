f5e14c5e3aad90fe2228dd350fc1325c506a7b9a
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-05-19 10:25:03 +0200

    UPDATE: initial state and updating of state
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: danishnav <danish.khan@nav.no>

--- apps/frontend/src/components/common/TextEditor.tsx
+++ apps/frontend/src/components/common/TextEditor.tsx
@@ -5,6 +5,7 @@ import { draftToMarkdown, markdownToDraft } from 'markdown-draft-js'
 import '../../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
 import { Block } from 'baseui/block'
 import { ettlevColors } from '../../util/theme'
+import { useDebouncedState } from '../../util/hooks'
 
 type TextEditorProps = {
   initialValue: string,
@@ -15,28 +16,24 @@ type TextEditorProps = {
 }
 
 const TextEditor = (props: TextEditorProps) => {
-  const [val, setVal] = React.useState(EditorState.createEmpty())
-
-  React.useEffect(() => {
-    const rawData = markdownToDraft(props.initialValue)
-    setVal(EditorState.createWithContent(convertFromRaw(rawData)))
-  }, [props.initialValue])
+  const [val, setVal] = useDebouncedState(props.initialValue, 500, props.setValue)
 
   return (
     <Block backgroundColor={ettlevColors.white} $style={{ border: `1px solid ${ettlevColors.textAreaBorder}` }}>
       <Editor
         editorStyle={{ padding: '10px', height: props.height || '500px' }}
         toolbarStyle={{ backgroundColor: ettlevColors.grey50, borderBottom: `1px solid ${ettlevColors.textAreaBorder}` }}
-        onEditorStateChange={data => setVal(data)}
-        contentState={convertToRaw(val.getCurrentContent())}
+        onEditorStateChange={data => setVal(draftToMarkdown(convertToRaw(data.getCurrentContent())))}
+        initialContentState={markdownToDraft(val)}
         toolbar={{
-          options: ['inline', 'blockType', 'list', 'textAlign', 'link', 'image', 'history'],
+          options: ['inline', 'blockType', 'list', 'link', 'image', 'history'],
           inline: { options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace'] },
           list: { options: ['unordered', 'ordered'] },
           link: { options: ['link'] },
-          image: { uploadCallback: props.onImageUpload },
+          //image: { uploadCallback: props.onImageUpload },
         }}
       />
+      <textarea value={val}/>
     </Block>
   )
 }
