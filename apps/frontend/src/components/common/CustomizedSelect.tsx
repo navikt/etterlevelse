40c35abc84f2c412efea8e3841b425f091e86fdf
andre groseth <andregroseth@hotmail.com>
2021-05-27 10:54:50 +0200

    UPDATE: Font size and add chevron icon.
    Co-authored-by: JeremiaUy <jeremia.augie.uy@nav.no>
    Co-authored-by: danishnav <danish.khan@nav.no>

--- apps/frontend/src/components/common/CustomizedSelect.tsx
+++ apps/frontend/src/components/common/CustomizedSelect.tsx
@@ -1,15 +1,25 @@
 import { Select, SelectOverrides, SelectProps, StatefulSelect, StatefulSelectProps } from 'baseui/select'
 import _ from 'lodash'
 import { ettlevColors } from '../../util/theme'
+import { navChevronDownIcon } from '../Images'
 
 const customOverrides: SelectOverrides = {
   ControlContainer: {
     style: {
+      borderColor: ettlevColors.grey200,
       boxShadow: '0 2px 2px 0 rgba(0, 0, 0, .12), 0 2px 1px -1px rgba(0, 0, 0, .2)',
       ':hover': {
         backgroundColor: ettlevColors.green50,
         boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.12), 0px 1px 1px -1px rgba(0, 0, 0, 0.2)'
       }
+    }
+  },
+  SelectArrow: {
+    component: () => <img src={navChevronDownIcon} alt='Chevron ned' />
+  },
+  DropdownListItem: {
+    style: {
+      fontSize: '18px'
     }
   }
 }
@@ -17,12 +27,12 @@ const customOverrides: SelectOverrides = {
 export const CustomizedStatefulSelect = (props: StatefulSelectProps) => {
   const overrides = _.merge(customOverrides, props.overrides)
 
-  return <StatefulSelect {...props} overrides={overrides}/>
+  return <StatefulSelect {...props} overrides={overrides} />
 }
 
 export const CustomizedSelect = (props: SelectProps) => {
   const overrides = _.merge(customOverrides, props.overrides)
 
-  return <Select {...props} overrides={overrides}/>
+  return <Select {...props} overrides={overrides} />
 }
 export default CustomizedSelect
