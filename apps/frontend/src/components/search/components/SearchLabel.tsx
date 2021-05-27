0771cb5343b26def3249572924fc3740d536e4a8
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-05-27 15:44:52 +0200

    UPDATE: height of tag
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/search/components/SearchLabel.tsx
+++ apps/frontend/src/components/search/components/SearchLabel.tsx
@@ -10,7 +10,7 @@ const SearchLabel = (props: {
   return (
     <Block display="flex" justifyContent="space-between" width="100%">
       <span style={{padding: '5px'}}>{props.name} </span>
-      <Block $style={{backgroundColor: props.backgroundColor, padding: '5px', margin: '5px', borderRadius: '5px'}}>{props.type}</Block>
+      <Block $style={{backgroundColor: props.backgroundColor, padding: '5px', margin: '5px', borderRadius: '5px', maxHeight: '20px'}}>{props.type}</Block>
     </Block>
   )
 }
