8820a6ab5c68b0be6a89e9cc744a2da84f4751da
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-05-28 15:06:18 +0200

    UPDATE: design
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: danishnav <danish.khan@nav.no>

--- apps/frontend/src/components/etterlevelse/ViewEtterlevelse.tsx
+++ apps/frontend/src/components/etterlevelse/ViewEtterlevelse.tsx
@@ -1,19 +1,18 @@
-import {Etterlevelse} from '../../constants'
-import {Block} from 'baseui/block'
+import { Etterlevelse } from '../../constants'
+import { Block } from 'baseui/block'
 import React from 'react'
-import {etterlevelseStatus} from '../../pages/EtterlevelsePage'
-import {theme} from '../../util'
+import { etterlevelseStatus } from '../../pages/EtterlevelsePage'
+import { theme } from '../../util'
 import moment from 'moment'
 import RouteLink from '../common/RouteLink'
-import {behandlingName, useBehandling} from '../../api/BehandlingApi'
-import {Spinner} from '../common/Spinner'
-import {Label} from '../common/PropertyLabel'
-import {kravNumView} from '../../pages/KravPage'
-import { H2 } from 'baseui/typography'
+import { behandlingName, useBehandling } from '../../api/BehandlingApi'
+import { Spinner } from '../common/Spinner'
+import { Label } from '../common/PropertyLabel'
+import { H2, Paragraph2 } from 'baseui/typography'
 
 const formatDate = (date?: string) => date && moment(date).format('ll')
 
-export const ViewEtterlevelse = ({etterlevelse}: {etterlevelse: Etterlevelse}) => {
+export const ViewEtterlevelse = ({ etterlevelse }: { etterlevelse: Etterlevelse }) => {
   const [behandling] = useBehandling(etterlevelse.behandlingId)
 
   return (
@@ -21,27 +20,35 @@ export const ViewEtterlevelse = ({etterlevelse}: {etterlevelse: Etterlevelse}) =
       <H2>
         Kravet etterlevels av
       </H2>
-      <Label title='Behandling'>
-        {behandling ?
-          <RouteLink href={`/behandling/${behandling.id}`}>
+      {behandling ?
+        <Block>
+          <RouteLink
+            href={`/behandling/${behandling.id}`}
+            style={{
+              fontSize: '21px',
+              fontWeight: 700,
+              lineHeight: '40px'
+            }}
+          >
             {behandlingName(behandling)}
           </RouteLink>
-          : etterlevelse.behandlingId && <> <Spinner size={theme.sizing.scale600}/>{etterlevelse.behandlingId}</>}
-      </Label>
-      <Label title='Krav'>
-        <RouteLink href={`/krav/${etterlevelse.kravNummer}/${etterlevelse.kravVersjon}`}>{kravNumView(etterlevelse)}</RouteLink>
-      </Label>
+          <Paragraph2 marginTop='2px'>
+            Overordnet behandlingsaktivitet
+            </Paragraph2>
+        </Block>
+        : etterlevelse.behandlingId && <Block> <Spinner size={theme.sizing.scale600} />{etterlevelse.behandlingId}</Block>
+      }
 
-      <Block height={theme.sizing.scale600}/>
+      <Block height={theme.sizing.scale600} />
 
-      <Label title='Begrunnelse' markdown={etterlevelse.begrunnelse}/>
-      <Label title='Dokumentasjon' markdown={etterlevelse.dokumentasjon}/>
+      <Label title='Begrunnelse' markdown={etterlevelse.begrunnelse} />
+      <Label title='Dokumentasjon' markdown={etterlevelse.dokumentasjon} />
 
-      <Block height={theme.sizing.scale600}/>
+      <Block height={theme.sizing.scale600} />
 
       <Label title='Frist for ferdigstillelse'>{formatDate(etterlevelse.fristForFerdigstillelse)}</Label>
 
-      <Block height={theme.sizing.scale600}/>
+      <Block height={theme.sizing.scale600} />
 
       <Label title='Status'>{etterlevelseStatus(etterlevelse.status)}</Label>
     </Block>
