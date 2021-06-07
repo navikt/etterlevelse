788fab1c5b5a96038fc212cebbefd59a84e2aecb
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-06-07 15:50:20 +0200

    UPDATE: form to be able to save begrunnelse for suksesskriterie
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/frontend/src/components/etterlevelse/Edit/SuksesskriterieBegrunnelseEdit.tsx
+++ apps/frontend/src/components/etterlevelse/Edit/SuksesskriterieBegrunnelseEdit.tsx
@@ -1,13 +1,17 @@
 import { Block } from "baseui/block"
 import { Checkbox } from "baseui/checkbox"
+import { FormControl } from "baseui/form-control"
 import { Paragraph2 } from "baseui/typography"
 import { FieldArray, FieldArrayRenderProps } from "formik"
 import React from "react"
 import { Suksesskriterie, SuksesskriterieBegrunnelse } from "../../../constants"
+import { useDebouncedState } from "../../../util/hooks"
 import { ettlevColors, theme } from "../../../util/theme"
 import { CustomizedAccordion, CustomizedPanel } from "../../common/CustomizedAccordion"
 import { FieldWrapper, TextAreaField } from "../../common/Inputs"
+import LabelWithTooltip from "../../common/LabelWithTooltip"
 import { Markdown } from "../../common/Markdown"
+import TextEditor from "../../common/TextEditor/TextEditor"
 
 const paddingLeft = '30px'
 
@@ -21,6 +25,12 @@ export const SuksesskriterierBegrunnelseEdit = ({ suksesskriterie }: { suksesskr
   )
 }
 
+const getSuksesskriterieBegrunnelse = (suksesskriterieBegrunnelser: SuksesskriterieBegrunnelse[], suksessKriterie: Suksesskriterie) => {
+  const sb = suksesskriterieBegrunnelser.find((item) => { return item.suksesskriterieId === suksessKriterie.id })
+  if (!sb) { return { suksesskriterieId: suksessKriterie.id, begrunnelse: '' } }
+  else { return sb }
+}
+
 const KriterieBegrunnelseList = ({ props, suksesskriterie }: { props: FieldArrayRenderProps, suksesskriterie: Suksesskriterie[] }) => {
   const suksesskriterieBegrunnelser = props.form.values.suksesskriterieBegrunnelser as SuksesskriterieBegrunnelse[]
 
@@ -28,15 +38,38 @@ const KriterieBegrunnelseList = ({ props, suksesskriterie }: { props: FieldArray
     <Block>
       {suksesskriterie.map((s, i) => {
         return (
-          <KriterieBegrunnelse suksesskriterie={s} index={i} />
+          <KriterieBegrunnelse
+            suksesskriterie={s}
+            index={i}
+            suksesskriterieBegrunnelser={suksesskriterieBegrunnelser}
+            update={updated => props.replace(i, updated)}
+          />
         )
       })}
     </Block>
   )
 }
 
-const KriterieBegrunnelse = ({ suksesskriterie, index }: { suksesskriterie: Suksesskriterie, index: number }) => {
-  const [checked, setChecked] = React.useState(false)
+const KriterieBegrunnelse = ({
+  suksesskriterie,
+  index,
+  suksesskriterieBegrunnelser,
+  update
+}: {
+  suksesskriterie: Suksesskriterie,
+  index: number,
+  suksesskriterieBegrunnelser: SuksesskriterieBegrunnelse[],
+  update: (s: SuksesskriterieBegrunnelse) => void
+}) => {
+  const suksesskriterieBegrunnelse = getSuksesskriterieBegrunnelse(suksesskriterieBegrunnelser, suksesskriterie)
+  const debounceDelay = 500
+  const [checked, setChecked] = React.useState(!!suksesskriterieBegrunnelse.begrunnelse)
+  const [begrunnelse, setBegrunnelse] = useDebouncedState(suksesskriterieBegrunnelse.begrunnelse || '', debounceDelay)
+
+  React.useEffect(() => {
+    console.log(suksesskriterieBegrunnelse)
+    update({ suksesskriterieId: suksesskriterie.id, begrunnelse: begrunnelse })
+  }, [begrunnelse])
 
   return (
     <Block key={suksesskriterie.navn + '_' + index} backgroundColor={ettlevColors.white} padding={theme.sizing.scale750} marginBottom={theme.sizing.scale600}>
@@ -51,7 +84,9 @@ const KriterieBegrunnelse = ({ suksesskriterie, index }: { suksesskriterie: Suks
 
       {checked &&
         <Block paddingLeft={paddingLeft} marginTop={theme.sizing.scale1000}>
-          <TextAreaField label='Dokumentasjon' name='begrunnelse' markdown />
+          <FormControl label='Dokumentasjon'>
+            <TextEditor initialValue={begrunnelse} setValue={setBegrunnelse} height={'188px'} />
+          </FormControl>
         </Block>
       }
 
