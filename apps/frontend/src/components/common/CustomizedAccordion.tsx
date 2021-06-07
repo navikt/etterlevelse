0840b31d744b61ca0c30dcf991b5fdbd0f6f9620
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-06-07 12:04:27 +0200

    UPDATE: to be able to override style

--- apps/frontend/src/components/common/CustomizedAccordion.tsx
+++ apps/frontend/src/components/common/CustomizedAccordion.tsx
@@ -1,60 +1,65 @@
-import {Accordion, AccordionProps, Panel, PanelProps} from 'baseui/accordion'
-import {ettlevColors, theme} from '../../util/theme'
-import {Block} from 'baseui/block'
-import {HeadingLarge} from 'baseui/typography'
-import {faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons'
+import { Accordion, AccordionProps, Panel, PanelProps } from 'baseui/accordion'
+import { ettlevColors, theme } from '../../util/theme'
+import { Block } from 'baseui/block'
+import { HeadingLarge } from 'baseui/typography'
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {borderColor, borderRadius, borderStyle, borderWidth} from './Style'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { borderColor, borderRadius, borderStyle, borderWidth } from './Style'
+import _ from 'lodash'
 
 
 export const CustomizedAccordion = (props: AccordionProps) => {
 
   return (
     <Accordion {...props}
-               overrides={{}}/>
+      overrides={{}} />
   )
 }
 
 export const CustomizedPanel = (props: PanelProps) => {
-  const {expanded} = props
-  return (
-    <Panel {...props} overrides={{
-      ToggleIcon: {
-        component: () => expanded ? <FontAwesomeIcon icon={faChevronUp}/> : <FontAwesomeIcon icon={faChevronDown}/>
-      },
-      Header: {
-        style: {
-          backgroundColor: expanded ? ettlevColors.grey50 : ettlevColors.white,
-          ...borderStyle('solid'),
-          ...borderWidth('1px'),
-          ...borderColor(ettlevColors.grey100),
-          ...borderRadius('4px'),
-          ':hover': {
-            textDecoration: 'underline'
-          }
-        }
-      },
-      Content: {
-        style: {
-          backgroundColor: ettlevColors.grey50,
-          paddingLeft: theme.sizing.scale100,
-          paddingRight: theme.sizing.scale100,
-          paddingTop: 0,
-          paddingBottom: expanded ? theme.sizing.scale100 : 0
-        }
-      },
-      PanelContainer: {
-        style: {
-          marginBottom: theme.sizing.scale200,
-          borderStyle: 'hidden'
+  const { expanded } = props
+
+  const customOverrides = {
+    ToggleIcon: {
+      component: () => expanded ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />
+    },
+    Header: {
+      style: {
+        backgroundColor: expanded ? ettlevColors.grey50 : ettlevColors.white,
+        ...borderStyle('solid'),
+        ...borderWidth('1px'),
+        ...borderColor(ettlevColors.grey100),
+        ...borderRadius('4px'),
+        ':hover': {
+          textDecoration: 'underline'
         }
       }
+    },
+    Content: {
+      style: {
+        backgroundColor: ettlevColors.grey50,
+        paddingLeft: theme.sizing.scale100,
+        paddingRight: theme.sizing.scale100,
+        paddingTop: 0,
+        paddingBottom: expanded ? theme.sizing.scale100 : 0
+      }
+    },
+    PanelContainer: {
+      style: {
+        marginBottom: theme.sizing.scale200,
+        borderStyle: 'hidden'
+      }
     }
-    } title={<Block>
+  }
+
+  const overrides = _.merge(customOverrides, props.overrides)
+
+  return (
+    <Panel {...props} overrides={overrides} title={<Block>
       <HeadingLarge marginTop={theme.sizing.scale100} marginBottom={theme.sizing.scale100} color={ettlevColors.green600}>
         {props.title}
       </HeadingLarge>
-    </Block>}/>
+    </Block>} />
   )
 }
