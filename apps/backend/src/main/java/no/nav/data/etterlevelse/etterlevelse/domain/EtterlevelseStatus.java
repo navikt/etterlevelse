f768caedd32aa6e46dce2a336a700208edf2d417
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-06-09 13:29:08 +0200

    ADD: graphql definition for etterlevelse status and etterlevelse suksesskriterie begrunnelse
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelse/domain/EtterlevelseStatus.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelse/domain/EtterlevelseStatus.java
@@ -3,8 +3,6 @@ package no.nav.data.etterlevelse.etterlevelse.domain;
 public enum EtterlevelseStatus {
     UNDER_REDIGERING,
     FERDIG,
-    UNDER_ARBEID,
-    OPPFYLT,
     OPPFYLLES_SENERE,
     IKKE_RELEVANT,
 }
