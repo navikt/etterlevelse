6da01759b7b246e477cc041cb8b94ca96d199ae8
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-06-21 15:23:00 +0200

    FIX: typo

--- apps/frontend/src/pages/MyBehandlingerPage.tsx
+++ apps/frontend/src/pages/MyBehandlingerPage.tsx
@@ -35,7 +35,7 @@ export const MyBehandlingerPage = () => (
           </Button>
         </RouteLink>
 
-        <HeadingXXLarge marginTop={theme.sizing.scale600}>Dokumentere etterlevelsene</HeadingXXLarge>
+        <HeadingXXLarge marginTop={theme.sizing.scale600}>Dokumentere etterlevelse</HeadingXXLarge>
         <BehandlingTabs />
       </Block>
     </Block>
