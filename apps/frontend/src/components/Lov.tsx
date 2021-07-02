c7d72b53b9cdf8f9baa16f60cd4d35f73909d895
Shayan <shayan.yazdanmehr@nav.no>
2021-07-02 10:24:50 +0200

    Update regex to include lov shortname and adding icon
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/Lov.tsx
+++ apps/frontend/src/components/Lov.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { codelist, ListName } from '../services/Codelist'
-import { env } from '../util/env'
-import { Regelverk } from '../constants'
-import { Block } from 'baseui/block'
+import {codelist, ListName} from '../services/Codelist'
+import {env} from '../util/env'
+import {Regelverk} from '../constants'
+import {Block} from 'baseui/block'
 import CustomizedLink from './common/CustomizedLink'
+import {faExternalLinkAlt} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const reactProcessString = require('react-process-string')
 const processString = reactProcessString as (converters: { regex: RegExp; fn: (key: string, result: string[]) => JSX.Element | string }[]) => (input?: string) => JSX.Element[]
 
 export const LovViewList = (props: { regelverk: Regelverk[] }) => {
   return (
-    <Block display="flex" flexDirection="column" $style={{ wordBreak: 'break-all' }}>
+    <Block display="flex" flexDirection="column" $style={{wordBreak: 'break-all'}}>
       {props.regelverk.map((r, i) => (
         <Block key={i}>
-          <LovView regelverk={r} />
+          <LovView regelverk={r}/>
         </Block>
       ))}
     </Block>
@@ -22,16 +24,16 @@ export const LovViewList = (props: { regelverk: Regelverk[] }) => {
 
 export const LovView = (props: { regelverk?: Regelverk }) => {
   if (!props.regelverk) return null
-  const { spesifisering, lov } = props.regelverk
+  const {spesifisering, lov} = props.regelverk
   const lovCode = lov?.code
 
   let lovDisplay = lov && codelist.getShortname(ListName.LOV, lovCode)
 
-  let descriptionText = codelist.valid(ListName.LOV, lovCode) ? legalBasisLinkProcessor(lovCode, spesifisering) : spesifisering
+  let descriptionText = codelist.valid(ListName.LOV, lovCode) ? legalBasisLinkProcessor(lovCode, lovDisplay + ' ' + spesifisering) : spesifisering
 
   return (
     <span>
-      {lovDisplay} {descriptionText}
+      {descriptionText}
     </span>
   )
 }
@@ -51,6 +53,7 @@ export const lovdataBase = (nationalLaw: string) => {
 }
 
 const legalBasisLinkProcessor = (law: string, text?: string) => {
+
   if (!findLovId(law).match(/^\d+.*/)) {
     return text
   }
@@ -58,24 +61,25 @@ const legalBasisLinkProcessor = (law: string, text?: string) => {
   return processString([
     {
       // Replace '§§ 10 og 4' > '§§ 10 og §§§ 4', so that our rewriter picks up the 2nd part
-      regex: /§§\s*(\d+(-\d+)?)\s*og\s*(\d+(-\d+)?)/gi,
-      fn: (key: string, result: string[]) => `§§ ${result[1]} og §§§ ${result[3]}`,
+      regex: /(.*) §§\s*(\d+(-\d+)?)\s*og\s*(\d+(-\d+)?)/gi,
+      fn: (key: string, result: string[]) => `${result[1]} §§ ${result[2]} og §§§ ${result[4]}`,
     },
     {
       // triple '§§§' is hidden, used as a trick in combination with rule 1 above
-      regex: /§(§§)?(§)?\s*(\d+(-\d+)?)/g,
+      regex: /(.*) §(§§)?(§)?\s*(\d+(-\d+)?)/g,
       fn: (key: string, result: string[]) => (
         <CustomizedLink key={key} href={`${lovdataBase(law)}/§${result[3]}`} target="_blank" rel="noopener noreferrer">
-          {!result[1] && !result[2] && '§'} {result[2] && '§§'} {result[3]}
+          {result[1]} {!result[2] && !result[3] && '§'} {result[3] && '§§'} {result[4]} <FontAwesomeIcon size="xs" icon={faExternalLinkAlt} />
         </CustomizedLink>
       ),
     },
     {
-      regex: /kap(ittel)?\s*(\d+)/gi,
+      regex: /(.*) kap(ittel)?\s*(\d+)/gi,
       fn: (key: string, result: string[]) => (
         <CustomizedLink key={key} href={`${lovdataBase(law)}/KAPITTEL_${result[2]}`} target="_blank" rel="noopener noreferrer">
-          Kapittel {result[2]}
+          {result[1]} Kapittel {result[3]} <FontAwesomeIcon size="xs" icon={faExternalLinkAlt} />
         </CustomizedLink>
+
       ),
     },
   ])(text)
