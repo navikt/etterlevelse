2b21401e94c6354ffe04811b706b1b41fac28645
andre groseth <andregroseth@hotmail.com>
2021-07-08 12:55:38 +0200

    Update: on hover color customized accordion
    Co-authored-by: JeremiahUy  <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/common/CustomizedAccordion.tsx
+++ apps/frontend/src/components/common/CustomizedAccordion.tsx
@@ -45,6 +45,7 @@ export const CustomizedPanel = (props: CustomProps) => {
             }),
         ':hover': {
           textDecoration: 'underline',
+          color: ettlevColors.green800
         },
       },
     },
