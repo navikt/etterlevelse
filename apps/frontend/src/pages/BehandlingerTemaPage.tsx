d4c025ccdc6e4186201d374f21fc7e4bd8249bf3
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-07-28 10:56:11 +0200

    UPDATE: to show previously completed etterlevelse
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/pages/BehandlingerTemaPage.tsx
+++ apps/frontend/src/pages/BehandlingerTemaPage.tsx
@@ -73,7 +73,7 @@ export const BehandlingerTemaPage = () => {
     })
 
     for (let index = mapped.length - 1; index > 0; index--) {
-      if (mapped[index].kravNummer === mapped[index - 1].kravNummer) {
+      if (mapped[index].kravNummer === mapped[index - 1].kravNummer && mapped[index -1].etterlevelseStatus !== EtterlevelseStatus.FERDIG_DOKUMENTERT) {
         mapped.splice(index - 1, 1)
       }
     }
