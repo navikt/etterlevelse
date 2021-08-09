b1c640c7b905384a7df6c94604e9c460746c9190
andre groseth <andregroseth@hotmail.com>
2021-08-09 12:17:02 +0200

    ADD: SkipToContent component
    Co-authored-by: JeremiahUy  <jeremiah.augie.uy@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>
    Co-authored-by: danishnav <danish.khan@nav.no>

---
+++ apps/frontend/src/components/common/SkipToContent/SkipToContent.tsx
@@ -0,0 +1,26 @@
+import { Block, BlockProps } from 'baseui/block'
+import { Label3, Paragraph2 } from 'baseui/typography'
+import React from 'react'
+import { $StyleProp } from 'styletron-react'
+import { ettlevColors } from '../../../util/theme'
+import CustomizedLink from '../CustomizedLink'
+import './SkipToContent.css'
+
+const SkipToContent = () => {
+  const style: $StyleProp<BlockProps> = {
+    background: ettlevColors.white,
+  }
+
+  return (
+    <>
+      <Block $style={style} height='100%' id="skip">
+        <CustomizedLink href="#content"><Label3>Hovedinnhold</Label3></CustomizedLink>
+      </Block>
+      <Block $style={style} id="skip">
+        <CustomizedLink href="#search"><Label3>Søk</Label3></CustomizedLink>
+      </Block>
+    </>
+  )
+}
+
+export default SkipToContent
