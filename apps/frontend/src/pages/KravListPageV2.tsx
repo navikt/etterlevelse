24b6cfcc4fdb40ef79692538dfdefec6e12ded38
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-08-31 13:18:18 +0200

    ADD: list of last modified krav

--- apps/frontend/src/pages/KravListPageV2.tsx
+++ apps/frontend/src/pages/KravListPageV2.tsx
@@ -1,15 +1,25 @@
-import { Block } from "baseui/block";
-import { HeadingXXLarge } from "baseui/typography";
-import { useState } from "react";
-import Button from "../components/common/Button";
-import CustomizedBreadcrumbs from "../components/common/CustomizedBreadcrumbs";
-import CustomizedTabs from "../components/common/CustomizedTabs";
-import RouteLink from "../components/common/RouteLink";
-import { user } from "../services/User";
-import { theme } from "../util";
-import { ettlevColors, maxPageWidth } from "../util/theme";
+import { Block } from 'baseui/block'
+import { H2, HeadingXXLarge } from 'baseui/typography'
+import { useState } from 'react'
+import Button from '../components/common/Button'
+import CustomizedBreadcrumbs from '../components/common/CustomizedBreadcrumbs'
+import CustomizedTabs from '../components/common/CustomizedTabs'
+import RouteLink from '../components/common/RouteLink'
+import { user } from '../services/User'
+import { theme } from '../util'
+import moment from 'moment'
+import { ettlevColors, maxPageWidth } from '../util/theme'
+import { plusIcon } from '../components/Images'
+import { useKravFilter } from '../api/KravGraphQLApi'
+import { PanelLink } from '../components/common/PanelLink'
+import { Spinner } from '../components/common/Spinner'
+import { Notification } from 'baseui/notification'
+import { KravQL } from '../constants'
+import { SkeletonPanel } from '../components/common/LoadingSkeleton'
 
 type Section = 'siste' | 'alle'
+
+const tabMarginBottom = '10px'
 
 export const KravListPage = () => (
   <Block width="100%" paddingBottom={'200px'} id="content" overrides={{ Block: { props: { role: 'main' } } }}>
@@ -20,12 +30,12 @@ export const KravListPage = () => (
           <HeadingXXLarge marginTop='0' >Forvalte og opprette krav</HeadingXXLarge>
         </Block>
         <Block paddingLeft={'100px'} paddingRight={'100px'} display="flex" justifyContent="flex-end">
-            {user.isKraveier() && (
-              <RouteLink hideUnderline href={'/krav/ny'}>
-                <Button size="compact">Nytt krav</Button>
-              </RouteLink>
-            )}
-          </Block>
+          {user.isKraveier() && (
+            <RouteLink hideUnderline href={'/krav/ny'}>
+              <Button startEnhancer={<img src={plusIcon} alt="" />} size="compact">Nytt krav</Button>
+            </RouteLink>
+          )}
+        </Block>
       </Block>
     </Block>
 
@@ -41,11 +51,29 @@ export const KravListPage = () => (
         <Block paddingLeft={'100px'} paddingRight={'100px'} paddingTop={theme.sizing.scale800}>
           <KravTabs />
         </Block>
-
       </Block>
     </Block>
   </Block>
 )
+
+const KravPanels = ({ kravene, loading }: { kravene: KravQL[]; loading?: boolean }) => {
+  if (loading) return <SkeletonPanel count={5} />
+  return (
+    <Block marginBottom={tabMarginBottom}>
+      {kravene.map((k) => (
+        <Block key={k.id} marginBottom={'0px'}>
+          <PanelLink
+            useUnderline
+            href={`/krav/${k.kravNummer}/${k.kravVersjon}`}
+            title={`K${k.kravNummer}.${k.kravVersjon}`}
+            beskrivelse={`${k.navn}`}
+            rightBeskrivelse={!!k.changeStamp.lastModifiedDate ? `Sist endret: ${moment(k.changeStamp.lastModifiedDate).format('ll')}` : ''}
+          />
+        </Block>
+      ))}
+    </Block>
+  )
+}
 
 const KravTabs = () => {
   const [tab, setTab] = useState<Section>('siste')
@@ -61,7 +89,7 @@ const KravTabs = () => {
         {
           key: 'siste',
           title: 'Sist endret av meg',
-          content: 'test',
+          content: <SistRedigertKrav />,
         },
         {
           key: 'alle',
@@ -70,5 +98,29 @@ const KravTabs = () => {
         }
       ]}
     />
+  )
+}
+
+const SistRedigertKrav = () => {
+  const res = useKravFilter({
+    sistRedigert: 10,
+    gjeldendeKrav: false,
+    pageNumber: 0,
+    pageSize: 10,
+  })
+
+  const { variables, data, loading, error } = res
+
+  return loading && !data?.krav?.numberOfElements ? (
+    <Spinner size={theme.sizing.scale2400} />
+  ) : error ? (
+    <Notification kind={'negative'}>{JSON.stringify(error, null, 2)}</Notification>
+  ) : (
+    <Block>
+      <Block>
+        <H2>{data?.krav.content && data?.krav.content.length ? data?.krav.content.length : 0} Krav</H2>
+      </Block>
+      <KravPanels kravene={data?.krav?.content || []} loading={loading}/>
+    </Block>
   )
 }
