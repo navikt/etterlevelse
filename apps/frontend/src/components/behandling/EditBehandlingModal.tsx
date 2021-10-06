877d105de4b3aa813c99c35c2cb42084dd287a99
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-10-06 14:36:57 +0200

    UPDATE: width of checkboxes

--- apps/frontend/src/components/behandling/EditBehandlingModal.tsx
+++ apps/frontend/src/components/behandling/EditBehandlingModal.tsx
@@ -212,12 +212,15 @@ const EditBehandlingModal = (props: EditBehandlingModalProps) => {
                                     paddingTop: '8px',
                                     paddingBottom: '10px',
                                     marginRight: '16px',
+                                    marginBottom: '16px',
                                     borderRadius: '4px',
                                     ':hover': { boxShadow: '0px 2px 0px rgba(0, 0, 0, 0.25), inset 0px -1px 0px rgba(0, 0, 0, 0.25);' },
                                     ':focus': {
                                       boxShadow: '0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)',
                                       outline: `3px solid ${ettlevColors.focusOutline}`,
                                     },
+                                    width: '100%',
+                                    maxWidth: '250px'
                                   },
                                 },
                                 Checkmark: {
