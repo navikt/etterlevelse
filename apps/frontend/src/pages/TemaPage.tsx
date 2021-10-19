fed6595263184e5608e59ea230c6a0bacd3c7824
Shayan <shayan.yazdanmehr@nav.no>
2021-10-19 13:16:27 +0200

    Editing label

--- apps/frontend/src/pages/TemaPage.tsx
+++ apps/frontend/src/pages/TemaPage.tsx
@@ -1,28 +1,28 @@
-import { useParams } from 'react-router-dom'
-import { Block, BlockProps } from 'baseui/block'
-import React, { useEffect, useState } from 'react'
-import { H1, H2, Label3, LabelLarge, Paragraph2, Paragraph4, ParagraphSmall } from 'baseui/typography'
-import { codelist, ListName, TemaCode } from '../services/Codelist'
-import { ObjectLink, urlForObject } from '../components/common/RouteLink'
-import { theme } from '../util'
-import { Markdown } from '../components/common/Markdown'
-import { ettlevColors } from '../util/theme'
+import {useParams} from 'react-router-dom'
+import {Block, BlockProps} from 'baseui/block'
+import React, {useEffect, useState} from 'react'
+import {H1, H2, Label3, LabelLarge, Paragraph2, Paragraph4, ParagraphSmall} from 'baseui/typography'
+import {codelist, ListName, TemaCode} from '../services/Codelist'
+import {ObjectLink, urlForObject} from '../components/common/RouteLink'
+import {theme} from '../util'
+import {Markdown} from '../components/common/Markdown'
+import {ettlevColors} from '../util/theme'
 import Button from '../components/common/Button'
-import { KravFilters } from '../api/KravGraphQLApi'
-import { SkeletonPanel } from '../components/common/LoadingSkeleton'
-import { PanelLink, PanelLinkCard, PanelLinkCardOverrides } from '../components/common/PanelLink'
-import { kravNumView } from './KravPage'
+import {KravFilters} from '../api/KravGraphQLApi'
+import {SkeletonPanel} from '../components/common/LoadingSkeleton'
+import {PanelLink, PanelLinkCard, PanelLinkCardOverrides} from '../components/common/PanelLink'
+import {kravNumView} from './KravPage'
 import * as _ from 'lodash'
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
-import { Layout2, Page } from '../components/scaffold/Page'
-import { SimpleTag } from '../components/common/SimpleTag'
-import { KravQL, PageResponse } from '../constants'
-import { useQuery } from '@apollo/client'
-import { QueryHookOptions } from '@apollo/client/react/types/types'
-import { gql } from '@apollo/client/core'
-import { useForceUpdate } from '../util/hooks'
-import { borderRadius } from '../components/common/Style'
-import { breadcrumbPaths } from '../components/common/CustomizedBreadcrumbs'
+import {faChevronDown, faChevronUp} from '@fortawesome/free-solid-svg-icons'
+import {Layout2, Page} from '../components/scaffold/Page'
+import {SimpleTag} from '../components/common/SimpleTag'
+import {KravQL, PageResponse} from '../constants'
+import {useQuery} from '@apollo/client'
+import {QueryHookOptions} from '@apollo/client/react/types/types'
+import {gql} from '@apollo/client/core'
+import {useForceUpdate} from '../util/hooks'
+import {borderRadius} from '../components/common/Style'
+import {breadcrumbPaths} from '../components/common/CustomizedBreadcrumbs'
 
 export const TemaPage = () => {
   const { tema } = useParams<{ tema: string }>()
@@ -203,14 +203,14 @@ const TemaInfo = (props: { kravAntall: number; temaAntall: number }) => (
     width={cardWidth}
   >
     <Block padding={theme.sizing.scale1000}>
-      <H2 color={ettlevColors.white} $style={{ lineHeight: '40px' }}> 
+      <H2 color={ettlevColors.white} $style={{ lineHeight: '40px' }}>
         Vi har totalt &nbsp;
         <span style={{ fontSize: '40px', lineHeight: '40px', color: ettlevColors.warning400}}>{props.kravAntall}</span> &nbsp;
-        krav gruppert i {props.temaAntall} kategorier
+        krav gruppert i {props.temaAntall} tema
       </H2>
 
       <Paragraph2 color={ettlevColors.white}>
-        Alle vi som utvikler digitale produkter i NAV må forholde seg til en del forskjellige lover og regler. 
+        Alle vi som utvikler digitale produkter i NAV må forholde oss til en del forskjellige lover og regler.
         Disse skal bidra til å sikre at den generelle rettsikkerheten til
         brukerne våre ivaretas.
       </Paragraph2>
