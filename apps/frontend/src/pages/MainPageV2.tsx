8ffd512271998e41841fbd2e876caa73cb29c9b1
Shayan <shayan.yazdanmehr@nav.no>
2021-11-08 10:18:42 +0100

    Design changes

--- apps/frontend/src/pages/MainPageV2.tsx
+++ apps/frontend/src/pages/MainPageV2.tsx
@@ -1,16 +1,16 @@
-import { Narrow, Page, Wide } from '../components/scaffold/Page'
-import { ettlevColors, theme } from '../util/theme'
-import { Block } from 'baseui/block'
-import { H2, HeadingXXLarge } from 'baseui/typography'
-import { PanelLinkCard } from '../components/common/PanelLink'
-import { grafIconBg, paperPenIconBg, paragrafIconBg } from '../components/Images'
-import { Card } from 'baseui/card'
-import { borderRadius, margin } from '../components/common/Style'
+import {Narrow, Page, Wide} from '../components/scaffold/Page'
+import {ettlevColors, theme} from '../util/theme'
+import {Block} from 'baseui/block'
+import {H2, HeadingXXLarge, Label4, Paragraph1} from 'baseui/typography'
+import {PanelLinkCard} from '../components/common/PanelLink'
+import {handWithLeaf, paperPenIconBg, paragrafIconBg} from '../components/Images'
+import {Card} from 'baseui/card'
+import {borderRadius, margin} from '../components/common/Style'
 import ReactPlayer from 'react-player'
-import { Button, SIZE } from 'baseui/button'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { buttonBorderStyle } from '../components/common/Button'
+import {Button, SIZE} from 'baseui/button'
+import {faChevronRight, faPlay} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {buttonBorderStyle} from '../components/common/Button'
 import React from 'react'
 
 const cardWidth = ['95%', '95%', '95%', '95%', '31%', '31%']
@@ -27,7 +27,7 @@ export const MainPageV2 = () => {
       rawMain
       header={
         <HeadingXXLarge display={'flex'} flexDirection={'column'} color={ettlevColors.white} marginTop={theme.sizing.scale1400} marginBottom={theme.sizing.scale1400}>
-          <span style={{ fontWeight: 400 }}>Støtte til etterlevelse av krav</span>
+          <span style={{fontWeight: 400}}>Støtte til etterlevelse</span>
           <span>som sikrer rettssikkerheten til brukerne våre</span>
         </HeadingXXLarge>
       }
@@ -35,7 +35,7 @@ export const MainPageV2 = () => {
       <Block display={'flex'} flexDirection={'column'} alignItems={'center'} width={'100%'}>
         <Wide>
           <Block display={'flex'} justifyContent={'center'} flexWrap>
-          <PanelLinkCard
+            <PanelLinkCard
               marginRight={cardMarginRight}
               height={cardHeight}
               width={cardWidth}
@@ -43,7 +43,7 @@ export const MainPageV2 = () => {
               href={'/tema'}
               tittel={'Forstå kravene'}
               icon={paragrafIconBg}
-              beskrivelse={'Få oversikt over kravene, og bli trygg på at du kjenner til alle relevante krav for det du lager'}
+              beskrivelse={'Få oversikt over krav til etterlevelse, og bli trygg på at du kjenner til alle relevante krav for det du lager'}
             />
 
 
@@ -56,29 +56,29 @@ export const MainPageV2 = () => {
               href={'/behandlinger'}
               tittel={'Dokumentere etterlevelse'}
               icon={paperPenIconBg}
-              beskrivelse={'Få oversikt over hvilke krav som gjelder din løsning og dokumenter hvor godt løsningen etterlever kravene.'}
+              beskrivelse={'Se hvilke krav som gjelder din løsning og dokumenter hvordan løsningen etterlever kravene'}
             />
 
-            
-            <PanelLinkCard
-              height={cardHeight}
-              width={cardWidth}
-              verticalMargin={theme.sizing.scale300}
-              href={'/status'}
-              tittel={'Status i organisasjonen'}
-              icon={grafIconBg}
-              beskrivelse={'Følg med på status i dashbord, lag egne oversikter, og se hvor godt NAV sine produktområder  dokumenterer på kravene'}
-            />
+
+            {/*<PanelLinkCard*/}
+            {/*  height={cardHeight}*/}
+            {/*  width={cardWidth}*/}
+            {/*  verticalMargin={theme.sizing.scale300}*/}
+            {/*  href={'/status'}*/}
+            {/*  tittel={'Status i organisasjonen'}*/}
+            {/*  icon={grafIconBg}*/}
+            {/*  beskrivelse={'Følg med på status og se hvor godt NAV sine produktområder  dokumenterer på kravene'}*/}
+            {/*/>*/}
           </Block>
         </Wide>
 
         <Narrow>
           <Block $style={{}} marginTop={theme.sizing.scale1600} marginBottom={theme.sizing.scale900} paddingLeft={theme.sizing.scale800} paddingRight={theme.sizing.scale800}>
-            <H2 $style={{ fontWeight: 300, fontSize: '32px', lineHeight: '42px' }} marginTop="0px" marginBottom="0px">
-              Se introduksjonsfilm
+            <H2 $style={{fontWeight: 300, fontSize: '32px', lineHeight: '42px'}} marginTop="0px" marginBottom="0px">
+              Etterlevelseskravene er
             </H2>
-            <H2 $style={{ wordBreak: 'break-word', fontSize: '32px', lineHeight: '42px' }} marginTop="0px" marginBottom="0px">
-              om etterlevelseskravene
+            <H2 $style={{wordBreak: 'break-word', fontSize: '32px', lineHeight: '42px'}} marginTop="0px" marginBottom="0px">
+              basert på norske lover og regler
             </H2>
           </Block>
 
@@ -103,9 +103,9 @@ export const MainPageV2 = () => {
                 controls={true}
                 playing={true}
                 playIcon={
-                  <Button kind="secondary" size={SIZE.compact} $style={buttonBorderStyle} startEnhancer={<FontAwesomeIcon icon={faPlay} />}>
+                  <Button kind="secondary" size={SIZE.compact} $style={buttonBorderStyle} startEnhancer={<FontAwesomeIcon icon={faPlay}/>}>
                     {' '}
-                    Se video{' '}
+                    Start{' '}
                   </Button>
                 }
                 width="100%"
@@ -115,6 +115,31 @@ export const MainPageV2 = () => {
               />
             </Block>
           </Card>
+
+          <Block
+            $style={{
+              ...margin(theme.sizing.scale1600, theme.sizing.scale600),
+              border: `1px solid ${ettlevColors.grey100}`,
+              borderRadius: '4px'
+            }}
+          >
+            <Block display={"flex"} width={'100%'} height={'115px'}>
+              <Block display={"flex"} width={"20%"} justifyContent={"center"} alignItems={"center"}>
+                <Block><img src={handWithLeaf} alt={''}/></Block>
+              </Block>
+              <Block width={"60%"} display={"flex"} alignItems={"center"}>
+                <Block>
+                  <Paragraph1 marginBottom={0} marginTop={0}>Mer om etterlevelse i NAV</Paragraph1>
+                  <Label4>Hvordan kravene blir til og hvordan jobbe med etterlevelse i produktutviklingen</Label4>
+                </Block>
+              </Block>
+              <Block width={"20%"} display={"flex"} justifyContent={"flex-end"} alignItems={"center"} paddingRight={"20px"}>
+                <Block>
+                  <FontAwesomeIcon icon={faChevronRight} color={ettlevColors.black} style={{fontSize: '16px'}} aria-hidden={true}/>
+                </Block>
+              </Block>
+            </Block>
+          </Block>
 
           <Block
             $style={{
