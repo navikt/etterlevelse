2b3841104283d79d116bd7a9b46467ddb5f9c851
JeremiahUy <jeremiah.augie.uy@nav.no>
2021-11-12 12:40:24 +0100

    FIX: typo in migration script

--- apps/backend/src/main/resources/db/migration/V1.10_krav_status_under_arbeid_changed_to_utkast.sql
+++ apps/backend/src/main/resources/db/migration/V1.10_krav_status_under_arbeid_changed_to_utkast.sql
@@ -1,2 +1,2 @@
 UPDATE GENERIC_STORAGE
-SET DATA = jsonb_set(DATA, '{status}', '"UTKAST"', false ) WHERE TYPE = 'Krav' AND DATA -> 'status' = "UNDER_ARBEID"
+SET DATA = jsonb_set(DATA, '{status}', '"UTKAST"', false ) WHERE TYPE = 'Krav' AND DATA ->> 'status' = 'UNDER_ARBEID'
