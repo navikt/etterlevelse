f10bcc8f40b0d93c2ed0b037fe094ae29ae378c5
Shayan <shayan.yazdanmehr@nav.no>
2021-11-23 08:30:45 +0100

    Links open in a new tab

--- apps/frontend/src/components/common/TeamName.tsx
+++ apps/frontend/src/components/common/TeamName.tsx
@@ -11,7 +11,7 @@ import {faExternalLinkAlt} from "@fortawesome/free-solid-svg-icons";
 export const TeamName = (props: { id: string; link?: boolean }) => {
   const [name] = useTeam()(props.id)
   return props.link ? (
-    <StyledLink rel="noopener noreferrer" href={teamKatTeamLink(props.id)} $style={{ color: ettlevColors.green600 }}>
+    <StyledLink rel="noopener noreferrer" target={'_blank'} href={teamKatTeamLink(props.id)} $style={{ color: ettlevColors.green600 }} >
       {name}
       <FontAwesomeIcon icon={faExternalLinkAlt} style={{marginLeft:'5px'}}/>
     </StyledLink>
