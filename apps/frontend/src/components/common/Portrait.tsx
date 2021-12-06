d180551c12f90ab444142c60377631eb89a7ca4a
Shayan <shayan.yazdanmehr@nav.no>
2021-12-06 12:48:56 +0100

    Refactor default icon for user without a picture
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/common/Portrait.tsx
+++ apps/frontend/src/components/common/Portrait.tsx
@@ -1,31 +1,44 @@
-import React, { useState } from 'react'
-import { personImageLink } from '../../util/config'
-import { Block } from 'baseui/block'
-import { Spinner } from './Spinner'
-import { questionmarkIcon } from '../Images'
+import React, {useState} from 'react'
+import {personImageLink} from '../../util/config'
+import {Block} from 'baseui/block'
+import {Spinner} from './Spinner'
+import {faUser} from '@fortawesome/free-regular-svg-icons'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export const Portrait = (props: { ident: string; size?: string }) => {
   const [loading, setLoading] = useState(true)
   const [image, setImage] = React.useState(personImageLink(props.ident))
+  const [error, setError] = React.useState<boolean>(false)
   const size = props.size || '42px'
   return (
     <Block width={size} height={size}>
-      {loading && <Spinner size="100%" />}
-      <img
-        onLoad={() => setLoading(false)}
-        onError={() => {
-          setImage(questionmarkIcon)
-          setLoading(false)
-        }}
-        src={image}
-        alt=""
-        aria-hidden
-        style={{
-          width: loading ? 0 : '100%',
-          height: loading ? 0 : '100%',
-          borderRadius: '100%',
-        }}
-      />
+      {loading && <Spinner size="100%"/>}
+      {!error ? (<img
+          onLoad={() => {
+            setLoading(false)
+            setError(false)
+          }}
+          onError={() => {
+            setError(true)
+            setLoading(false)
+          }}
+          src={image}
+          alt=""
+          aria-hidden
+          style={{
+            width: loading ? 0 : '100%',
+            height: loading ? 0 : '100%',
+            borderRadius: '100%',
+          }}
+        />) :
+        <FontAwesomeIcon
+          icon={faUser}
+          style={{
+            width: loading ? 0 : '100%',
+            height: loading ? 0 : '100%',
+            borderRadius: '100%',
+          }}/>
+      }
     </Block>
   )
 }
