e883fecd1d8d13bc652dcf790f6c45888ea1cf8e
andre groseth <andregroseth@hotmail.com>
2022-01-31 14:29:28 +0100

    Refactor: Icon and size
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/common/TeamName.tsx
+++ apps/frontend/src/components/common/TeamName.tsx
@@ -1,42 +1,37 @@
-import {useTeam} from '../../api/TeamApi'
+import { useTeam } from '../../api/TeamApi'
 import React from 'react'
-import {teamKatTeamLink} from '../../util/config'
-import {LinkProps, StyledLink} from 'baseui/link'
-import {Block} from 'baseui/block'
-import {theme} from '../../util'
-import {ettlevColors} from '../../util/theme'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons'
+import { teamKatTeamLink } from '../../util/config'
+import { LinkProps, StyledLink } from 'baseui/link'
+import { Block } from 'baseui/block'
+import { theme } from '../../util'
+import { ettlevColors } from '../../util/theme'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import { $StyleProp } from 'styletron-react'
 import _ from 'lodash'
+import { externalLinkIcon } from '../Images'
 
-export const TeamName = (props: { id: string; link?: boolean, fontColor?: string, style?: $StyleProp<LinkProps> }) => {
+export const TeamName = (props: { id: string; link?: boolean; fontColor?: string; style?: $StyleProp<LinkProps> }) => {
   const [name] = useTeam()(props.id)
   const customStyle = { color: props.fontColor ? props.fontColor : ettlevColors.green600 }
   const mergedStyle = _.merge(customStyle, props.style)
   return props.link ? (
-    <StyledLink
-      rel="noopener noreferrer"
-      target={'_blank'}
-      href={teamKatTeamLink(props.id)}
-      $style={mergedStyle}
-    >
-      {name}
-      <FontAwesomeIcon icon={faExternalLinkAlt} style={{marginLeft: '5px'}}/>
+    <StyledLink rel="noopener noreferrer" target={'_blank'} href={teamKatTeamLink(props.id)} $style={mergedStyle}>
+      <Block display="flex" alignItems="center">
+        {name}
+        <img src={externalLinkIcon} width="12.25" height="12.25" style={{ marginLeft: '5px' }} />
+      </Block>
     </StyledLink>
   ) : (
     <>{name}</>
   )
 }
 
-export const Teams = (props: { teams: string[]; link?: boolean; list?: boolean, fontColor?: string, style?: $StyleProp<LinkProps> }) => (
+export const Teams = (props: { teams: string[]; link?: boolean; list?: boolean; fontColor?: string; style?: $StyleProp<LinkProps> }) => (
   <Block display={props.list ? 'block' : 'flex'} flexWrap>
     {props.teams.map((t) => (
-      <Block
-        key={t}
-        marginRight={props.list ? 'none' : theme.sizing.scale600}
-        marginBottom={props.list ? theme.sizing.scale600 : 'none'}>
-        <TeamName id={t} link={props.link} fontColor={props.fontColor} style={props.style}/>
+      <Block key={t} marginRight={props.list ? 'none' : theme.sizing.scale600} marginBottom={props.list ? theme.sizing.scale600 : 'none'}>
+        <TeamName id={t} link={props.link} fontColor={props.fontColor} style={props.style} />
       </Block>
     ))}
   </Block>
