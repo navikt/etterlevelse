cd007f0d1e0aa1f37ff1c343a5a63785509ce12d
Shayan <shayan.yazdanmehr@nav.no>
2022-02-07 11:36:36 +0100

    Making response class
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/tildeling/dto/TildelingResponse.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/tildeling/dto/TildelingResponse.java
@@ -1,4 +1,26 @@
 package no.nav.data.etterlevelse.tildeling.dto;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import no.nav.data.common.rest.ChangeStampResponse;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonPropertyOrder({"id", "kravNummer", "kravVersion", "behandlingId", "tildeltMed"})
 public class TildelingResponse {
+    private UUID id;
+    private Integer version;
+    private Integer kravNummer;
+    private Integer kravVersjon;
+    private String behandlingId;
+    private List<String> tildeltMed;
+    private ChangeStampResponse changeStamp;
 }
