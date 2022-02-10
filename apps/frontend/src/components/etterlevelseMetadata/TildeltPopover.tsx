60a59169cecda61ce593e50cca8a9772a1c71336
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-02-10 13:17:04 +0100

    UPDATE: design for popover body

--- apps/frontend/src/components/etterlevelseMetadata/TildeltPopover.tsx
+++ apps/frontend/src/components/etterlevelseMetadata/TildeltPopover.tsx
@@ -7,6 +7,8 @@ import Button from '../common/Button'
 import { createEtterlevelseMetadata, updateEtterlevelseMetadata } from '../../api/EtterlevelseMetadataApi'
 import { user } from '../../services/User'
 import { ettlevColors } from '../../util/theme'
+import { Label3 } from 'baseui/typography'
+import { borderColor, borderStyle, borderWidth } from '../common/Style'
 
 type TildeltPopoverProps = {
   etterlevelseMetadata: EtterlevelseMetadata
@@ -19,14 +21,31 @@ export const TildeltPopoever = ({ etterlevelseMetadata, setEtterlevelseMetadata 
       overrides={{
         Inner: {
           style: {
-            backgroundColor: ettlevColors.white
+            backgroundColor: ettlevColors.white,
+          }
+        },
+        Body: {
+          style: {
+            borderRadius: '4px',
+            ...borderColor('#E3E3E3'),
+            ...borderStyle('solid'),
+            ...borderWidth('1px'),
+            boxShadow: '0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(38, 38, 38, 0.12), 0px 1px 3px rgba(38, 38, 38, 0.2)'
+          }
+        },
+        Arrow: {
+          style: {
+            backgroundColor: ettlevColors.white,
+            ...borderColor('#E3E3E3'),
+            ...borderStyle('solid'),
+            ...borderWidth('1px'),
           }
         }
       }}
-      content={({close}) => (
+      content={({ close }) => (
         <Block padding={'20px'}>
           <Button
-            kind="tertiary"
+            kind="underline-hover"
             onClick={() => {
               const ident = user.getName()
               if (etterlevelseMetadata.id !== 'ny') {
@@ -47,7 +66,9 @@ export const TildeltPopoever = ({ etterlevelseMetadata, setEtterlevelseMetadata 
                 })
               }
             }}>
-            Tildelt med meg
+            <Label3 $style={{ fontWeight: 600 }}>
+              Tildel meg selv
+            </Label3>
           </Button>
         </Block>
       )}
