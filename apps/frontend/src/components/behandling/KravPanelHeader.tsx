9e7cc1832b7745d0b9aecd22bd550cc7da434e81
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-02-15 11:00:20 +0100

    ADD: new header component for krav
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/frontend/src/components/behandling/KravPanelHeader.tsx
+++ apps/frontend/src/components/behandling/KravPanelHeader.tsx
@@ -4,6 +4,8 @@ import { HeadingLarge, Label3, Paragraph4 } from 'baseui/typography'
 import { Krav, KravEtterlevelseData } from '../../constants'
 import { theme } from '../../util'
 import { ettlevColors } from '../../util/theme'
+import CustomizedSelect from '../common/CustomizedSelect'
+import { Option } from 'baseui/select'
 
 const responsiveBreakPoints: Responsive<Display> = ['block', 'block', 'block', 'flex', 'flex', 'flex']
 
@@ -39,4 +41,44 @@ export const KravPanelHeader = (props: { title: string; kravData: KravEtterlevel
     </Block>
   )
 }
+
+
+export const KravPanelHeaderWithSorting = (props: { kravData: KravEtterlevelseData[] | Krav[], sortingOptions: Option[], sorting: readonly Option[], setSorting: React.Dispatch<React.SetStateAction<readonly Option[]>>}) => {
+  let antallSuksesskriterier = 0
+
+  props.kravData.forEach((k) => {
+    antallSuksesskriterier += k.suksesskriterier.length
+  })
+
+  const responsiveAlignment: Responsive<JustifyContent> = ['flex-start', 'flex-start', 'flex-start', 'flex-end', 'flex-end', 'flex-end']
+
+  return (
+    <Block display={responsiveBreakPoints} width="100%">
+      <Block display="flex" justifyContent="center" alignItems="center">
+        <Label3 $style={{ fontSize: '16px', lineHeight: '18px' }}>
+          Vis:
+        </Label3>
+
+        <Block paddingLeft="20px" paddingRight="20px" width="100%" maxWidth="290px">
+          <CustomizedSelect size="default" clearable={false} options={props.sortingOptions} value={props.sorting} onChange={(params) => props.setSorting(params.value)} />
+        </Block>
+      </Block>
+      <Block display="flex" justifyContent={responsiveAlignment} flex="1" marginRight="26px">
+        <Block>
+          <Block display="flex" justifyContent={responsiveAlignment} alignItems="baseline" flex="1">
+            <Label3 marginRight="4px" $style={{ color: ettlevColors.navOransje, fontSize: '32px', lineHeight: '21px', marginTop: '0px', marginBottom: '0px' }}>
+              {props.kravData.length}
+            </Label3>
+            <Paragraph4 $style={{ lineHeight: '21px', marginTop: '0px', marginBottom: '0px' }}>krav</Paragraph4>
+          </Block>
+          <Block display="flex" justifyContent={responsiveAlignment} flex="1">
+            <Paragraph4 $style={{ lineHeight: '21px', marginTop: '0px', marginBottom: '0px' }}>{antallSuksesskriterier} suksesskriterier</Paragraph4>
+          </Block>
+        </Block>
+      </Block>
+    </Block>
+  )
+
+}
+
 export default KravPanelHeader
