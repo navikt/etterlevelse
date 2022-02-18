50ce6d1c744bbaa3eb1beace8eb5b8ba5c383228
Shayan <shayan.yazdanmehr@nav.no>
2022-02-18 08:37:18 +0100

    Add find by melding type and melding status to repo

--- apps/backend/src/main/java/no/nav/data/etterlevelse/melding/MeldingService.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/melding/MeldingService.java
@@ -5,10 +5,13 @@ import no.nav.data.common.storage.domain.GenericStorage;
 import no.nav.data.etterlevelse.common.domain.DomainService;
 import no.nav.data.etterlevelse.melding.domain.Melding;
 import no.nav.data.etterlevelse.melding.domain.MeldingRepo;
+import no.nav.data.etterlevelse.melding.domain.MeldingStatus;
+import no.nav.data.etterlevelse.melding.domain.MeldingType;
 import no.nav.data.etterlevelse.melding.dto.MeldingRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -28,6 +31,14 @@ public class MeldingService extends DomainService<Melding> {
 
     public Optional<Melding> getById(UUID id) {
         return repo.findById(id).map(GenericStorage::toMelding);
+    }
+
+    public List<Melding> getByMeldingType(MeldingType meldingType) {
+        return GenericStorage.to(repo.findByMeldingtype(meldingType),Melding.class);
+    }
+
+    public List<Melding> getByMeldingStatus(MeldingStatus meldingStatus) {
+        return GenericStorage.to(repo.findByMeldingStatus(meldingStatus),Melding.class);
     }
 
     public Melding save(MeldingRequest request) {
--- apps/backend/src/main/java/no/nav/data/etterlevelse/melding/domain/MeldingRepo.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/melding/domain/MeldingRepo.java
@@ -2,8 +2,16 @@ package no.nav.data.etterlevelse.melding.domain;
 
 import no.nav.data.common.storage.domain.GenericStorage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface MeldingRepo extends JpaRepository<GenericStorage, UUID> {
+
+    @Query(value = "select * from generic_storage where type = 'Melding' and data ->> 'meldingType' = ?1", nativeQuery = true)
+    List<GenericStorage> findByMeldingtype(MeldingType meldingType);
+
+    @Query(value = "select * from generic_storage where type = 'Melding' and data ->> 'meldingStatus' = ?1", nativeQuery = true)
+    List<GenericStorage> findByMeldingStatus(MeldingStatus meldingStatus);
 }
