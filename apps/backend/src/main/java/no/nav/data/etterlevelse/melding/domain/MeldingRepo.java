8b3cc06e8061c1294905033371cb163c40130225
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-02-21 11:03:40 +0100

    UPDATE: find all override

--- apps/backend/src/main/java/no/nav/data/etterlevelse/melding/domain/MeldingRepo.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/melding/domain/MeldingRepo.java
@@ -1,6 +1,8 @@
 package no.nav.data.etterlevelse.melding.domain;
 
 import no.nav.data.common.storage.domain.GenericStorage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,6 +10,12 @@ import java.util.List;
 import java.util.UUID;
 
 public interface MeldingRepo extends JpaRepository<GenericStorage, UUID> {
+
+    @Override
+    @Query(value = "select * from generic_storage where type = 'Melding'",
+            countQuery = "select count(1) from generic_storage where type = 'Melding'",
+            nativeQuery = true)
+    Page<GenericStorage> findAll(Pageable pageable);
 
     @Query(value = "select * from generic_storage where type = 'Melding' and data ->> 'meldingType' = ?1", nativeQuery = true)
     List<GenericStorage> findByMeldingtype(MeldingType meldingType);
