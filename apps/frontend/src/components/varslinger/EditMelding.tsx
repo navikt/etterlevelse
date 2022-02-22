ae57ca90637ed42e62029953fa10a0fab886c88d
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-02-22 14:39:14 +0100

    UPDATE: props,design and formik
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/varslinger/EditMelding.tsx
+++ apps/frontend/src/components/varslinger/EditMelding.tsx
@@ -1,12 +1,43 @@
 import { Block } from 'baseui/block'
-import React from 'react'
-import { MeldingStatus, MeldingType } from '../../constants'
+import { Formik } from 'formik'
+import React, { useEffect, useState } from 'react'
+import { createMelding, getMeldingByType, mapMeldingToFormValue, updateMelding } from '../../api/MeldingApi'
+import { Melding, MeldingStatus, MeldingType } from '../../constants'
 
-export const EditMelding = ({meldingType}: {meldingType?:MeldingType}) => {
-  
+export const EditMelding = ({ meldingType }: { meldingType: MeldingType }) => {
+
+  const [melding, setMelding] = useState<Partial<Melding>>({ melding: '', meldingType: meldingType, meldingStatus: MeldingStatus.DEACTIVE })
+  const [disableEdit, setDisableEdit] = useState<boolean>(false)
+
+  useEffect(() => {
+    (async () => {
+      getMeldingByType(meldingType).then((m) => {
+        if (m.numberOfElements > 0) {
+          setMelding(m.content[0])
+        }
+      })
+    })()
+  }, [])
+
+  const submit = async (melding: Melding) => {
+
+    setDisableEdit(true)
+
+    if (melding.id) {
+      await updateMelding(melding).then(() => setDisableEdit(false))
+    } else {
+      await createMelding(melding).then(() => setDisableEdit(false))
+    }
+  }
+
   return (
     <Block>
-      this is a block
+      <Formik
+        onSubmit={submit}
+        initialValues={mapMeldingToFormValue(melding)}
+      >
+
+      </Formik>
     </Block>
   )
 }
