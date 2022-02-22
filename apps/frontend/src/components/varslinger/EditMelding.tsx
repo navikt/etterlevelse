44e9fd20e153648436a5e5b7a22301ed601dda66
andre groseth <andregroseth@hotmail.com>
2022-02-22 14:05:23 +0100

    CREATE: EditMelding component
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

---
+++ apps/frontend/src/components/varslinger/EditMelding.tsx
@@ -0,0 +1,14 @@
+import { Block } from 'baseui/block'
+import React from 'react'
+import { MeldingStatus, MeldingType } from '../../constants'
+
+export const EditMelding = ({meldingType}: {meldingType?:MeldingType}) => {
+  
+  return (
+    <Block>
+      this is a block
+    </Block>
+  )
+}
+
+export default EditMelding
