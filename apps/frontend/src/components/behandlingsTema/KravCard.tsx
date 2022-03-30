7362976f7edef23ccc8b09571761b62dd297dc0a
Shayan <shayan.yazdanmehr@nav.no>
2022-03-30 11:45:36 +0200

    Adding Bortfiltert label
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/behandlingsTema/KravCard.tsx
+++ apps/frontend/src/components/behandlingsTema/KravCard.tsx
@@ -1,21 +1,21 @@
-import { EtterlevelseMetadata, KRAV_FILTER_TYPE, KravEtterlevelseData } from '../../constants'
-import React, { useEffect, useState } from 'react'
-import { getEtterlevelseMetadataByBehandlingsIdAndKravNummerAndKravVersion, mapEtterlevelseMetadataToFormValue } from '../../api/EtterlevelseMetadataApi'
-import { Block } from 'baseui/block'
+import {EtterlevelseMetadata, KRAV_FILTER_TYPE, KravEtterlevelseData} from '../../constants'
+import React, {useEffect, useState} from 'react'
+import {getEtterlevelseMetadataByBehandlingsIdAndKravNummerAndKravVersion, mapEtterlevelseMetadataToFormValue} from '../../api/EtterlevelseMetadataApi'
+import {Block} from 'baseui/block'
 import Button from '../common/Button'
-import { ettlevColors } from '../../util/theme'
-import { borderStyle } from '../common/Style'
-import { Label3, Paragraph4 } from 'baseui/typography'
+import {ettlevColors} from '../../util/theme'
+import {borderStyle, marginAll} from '../common/Style'
+import {Label3, Paragraph4} from 'baseui/typography'
 import StatusView from '../common/StatusTag'
-import { getEtterlevelseStatus, getEtterlevelseStatusLabelColor } from '../behandling/utils'
+import {getEtterlevelseStatus, getEtterlevelseStatusLabelColor} from '../behandling/utils'
 import moment from 'moment'
 import TildeltPopoever from '../etterlevelseMetadata/TildeltPopover'
-import { isFerdigUtfylt } from '../../pages/BehandlingerTemaPageV2'
-import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
-import { arkCheckIcon, arkPennIcon, warningAlert } from '../Images'
-import { getEtterlevelserByBehandlingsIdKravNumber } from '../../api/EtterlevelseApi'
-import { useLocation, useNavigate } from 'react-router-dom'
-import { getNumberOfDaysBetween } from '../../util/checkAge'
+import {isFerdigUtfylt} from '../../pages/BehandlingerTemaPageV2'
+import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
+import {arkCheckIcon, arkPennIcon, warningAlert} from '../Images'
+import {getEtterlevelserByBehandlingsIdKravNumber} from '../../api/EtterlevelseApi'
+import {useLocation, useNavigate} from 'react-router-dom'
+import {getNumberOfDaysBetween} from '../../util/checkAge'
 
 export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean; behandlingId: string; noVarsling?: boolean; kravFilter: KRAV_FILTER_TYPE }) => {
   const [nyVersionFlag, setNyVersionFlag] = useState<boolean>(false)
@@ -67,8 +67,8 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
       display={'flex'}
       $style={{
         boxShadow: '0 3px 1px -2px rgba(0, 0, 0, .2), 0 2px 2px 0 rgba(0, 0, 0, .14), 0 1px 2px 0 rgba(0, 0, 0, .12)',
-        ':hover': { boxShadow: '0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)' },
-        ':active': { boxShadow: '0 2px 1px -2px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 1px 0 rgba(0, 0, 0, .12)' },
+        ':hover': {boxShadow: '0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)'},
+        ':active': {boxShadow: '0 2px 1px -2px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 1px 0 rgba(0, 0, 0, .12)'},
         ':focus': {
           boxShadow: '0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)',
           outline: `3px solid ${ettlevColors.focusOutline}`,
@@ -88,7 +88,7 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
             justifyContent: 'flex-start',
             backgroundColor: ettlevColors.white,
             ...borderStyle('hidden'),
-            ':hover': { backgroundColor: 'none', boxShadow: '' },
+            ':hover': {backgroundColor: 'none', boxShadow: ''},
             boxShadow: '',
           }}
           onClick={() => {
@@ -97,19 +97,47 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
         >
           <Block display="flex" justifyContent="center" alignItems="center" width="100%" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
             <Block marginLeft="8px">
-              {props.krav.isIrrelevant || isFerdigUtfylt(props.krav.etterlevelseStatus) ? <img src={arkCheckIcon} alt="" /> : <img src={arkPennIcon} alt="" />}
+              {props.krav.isIrrelevant || isFerdigUtfylt(props.krav.etterlevelseStatus) ? <img src={arkCheckIcon} alt=""/> : <img src={arkPennIcon} alt=""/>}
             </Block>
             <Block marginLeft="14px">
               <Block display={'flex'} alignItems={'center'}>
                 <Paragraph4
-                  $style={{ fontSize: '16px', lineHeight: '24px', marginBottom: '0px', marginTop: '0px', width: 'fit-content', textDecoration: hover ? 'underline' : 'none' }}
+                  $style={{fontSize: '16px', lineHeight: '24px', marginBottom: '0px', marginTop: '0px', width: 'fit-content', textDecoration: hover ? 'underline' : 'none'}}
                 >
                   K{props.krav.kravNummer}.{props.krav.kravVersjon}
                 </Paragraph4>
                 {!props.noVarsling && props.krav.kravVersjon === 1 && props.krav.etterlevelseStatus === undefined && kravAge < 30 && showWarningMessage('Nytt krav')}
-                {!props.noVarsling && props.krav.etterlevelseStatus === undefined && nyVersionFlag && showWarningMessage('Ny version')}
+                {!props.noVarsling && props.krav.etterlevelseStatus === undefined && nyVersionFlag && props.kravFilter === KRAV_FILTER_TYPE.RELEVANTE_KRAV && showWarningMessage('Ny version')}
+                {props.kravFilter === KRAV_FILTER_TYPE.BORTFILTTERTE_KRAV && <StatusView
+                  status={'Bortfiltert'}
+                  statusDisplay={{
+                    background: ettlevColors.grey100,
+                    ...borderStyle('solid')
+                  }}
+                  lineHeight={'16px'}
+                  fontSize={'16px'}
+                  fontStyle={'italic'}
+                  overrides={{
+                    Root: {
+                      style: {
+                        marginLeft: '21px',
+                      }
+                    },
+                    Contents: {
+                      style: {
+                        ...marginAll('2px'),
+                      }
+                    },
+                    Body: {
+                      style: {
+                        ...marginAll('2px'),
+                      }
+                    }
+                  }}
+                />
+                }
               </Block>
-              <Label3 $style={{ fontSize: '18px', fontWeight: 600, alignContent: 'flex-start', textAlign: 'left', textDecoration: hover ? 'underline' : 'none' }}>
+              <Label3 $style={{fontSize: '18px', fontWeight: 600, alignContent: 'flex-start', textAlign: 'left', textDecoration: hover ? 'underline' : 'none'}}>
                 {props.krav.navn}
               </Label3>
             </Block>
@@ -117,26 +145,26 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
               <Block width="350px" display="flex" justifyContent="flex-end" marginLeft="32px">
                 <Block display="flex" width="100%" maxWidth="220px" justifyContent="flex-end">
                   {props.krav && props.krav.etterlevelseStatus && (
-                    <StatusView status={getEtterlevelseStatus(props.krav)} statusDisplay={getEtterlevelseStatusLabelColor(props.krav)} />
+                    <StatusView status={getEtterlevelseStatus(props.krav)} statusDisplay={getEtterlevelseStatusLabelColor(props.krav)}/>
                   )}
                 </Block>
                 {!props.krav.isIrrelevant && (
                   <Block marginLeft="31px" maxWidth="140px" width="100%">
                     {etterlevelseMetadata && etterlevelseMetadata.tildeltMed && etterlevelseMetadata.tildeltMed.length >= 1 ? (
                       <Block>
-                        <Label3 $style={{ fontSize: '14px', lineHeight: '14px', textAlign: 'right' }}>
+                        <Label3 $style={{fontSize: '14px', lineHeight: '14px', textAlign: 'right'}}>
                           Tildelt:{' '}
                           {etterlevelseMetadata.tildeltMed[0].length > 12 ? etterlevelseMetadata.tildeltMed[0].substring(0, 11) + '...' : etterlevelseMetadata.tildeltMed[0]}
                         </Label3>
                       </Block>
                     ) : (
                       <Block>
-                        <Label3 $style={{ fontSize: '14px', lineHeight: '14px', textAlign: 'right', fontStyle: 'italic', fontWeight: 400 }}>Ikke tildelt</Label3>
+                        <Label3 $style={{fontSize: '14px', lineHeight: '14px', textAlign: 'right', fontStyle: 'italic', fontWeight: 400}}>Ikke tildelt</Label3>
                       </Block>
                     )}
                     {props.krav.etterlevelseChangeStamp?.lastModifiedDate && (
                       <Block width="100%" display="flex" justifyContent="flex-end">
-                        <Paragraph4 $style={{ lineHeight: '19px', textAlign: 'right', marginTop: '0px', marginBottom: '0px', whiteSpace: 'nowrap' }}>
+                        <Paragraph4 $style={{lineHeight: '19px', textAlign: 'right', marginTop: '0px', marginBottom: '0px', whiteSpace: 'nowrap'}}>
                           {'Sist utfylt: ' + moment(props.krav.etterlevelseChangeStamp?.lastModifiedDate).format('ll')}
                         </Paragraph4>
                       </Block>
@@ -146,7 +174,7 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
 
                 {props.krav.isIrrelevant && (
                   <Block marginLeft="31px" maxWidth="140px" width="100%">
-                    <Label3 $style={{ fontSize: '14px', lineHeight: '14px', textAlign: 'right', fontStyle: 'italic', fontWeight: 400 }}>Bortfiltrert</Label3>
+                    <Label3 $style={{fontSize: '14px', lineHeight: '14px', textAlign: 'right', fontStyle: 'italic', fontWeight: 400}}>Bortfiltrert</Label3>
                   </Block>
                 )}
               </Block>
