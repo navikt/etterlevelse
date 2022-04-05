8e6f44c998e8d83c32e46ddaad281389e61e8f20
Shayan <shayan.yazdanmehr@nav.no>
2022-04-05 13:27:19 +0200

    ReactDOM.render is no longer supported in React 18
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/index.tsx
+++ apps/frontend/src/index.tsx
@@ -1,8 +1,9 @@
 import 'react-app-polyfill/ie11'
 import 'react-app-polyfill/stable'
-
 import React from 'react'
-import ReactDOM from 'react-dom'
 import Main from './main'
 
-ReactDOM.render(<Main />, document.getElementById('root'))
+import {createRoot} from 'react-dom/client';
+
+// @ts-ignore
+createRoot(document.getElementById('root')).render(<Main/>);
