fc48b5f363fa4f2e5fe980a3de26b02c2d1eab19
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-04-25 15:24:35 +0200

    UPDATE: design bug for radio group date picker

--- apps/frontend/src/components/etterlevelse/Edit/EtterlevelseEditFields.tsx
+++ apps/frontend/src/components/etterlevelse/Edit/EtterlevelseEditFields.tsx
@@ -1,31 +1,31 @@
-import {Etterlevelse, EtterlevelseStatus, KravQL, KravStatus} from '../../../constants'
-import {Field, FieldProps, Form, Formik, FormikProps, validateYupSchema, yupToFormErrors} from 'formik'
-import {mapEtterlevelseToFormValue} from '../../../api/EtterlevelseApi'
-import {Block} from 'baseui/block'
+import { Etterlevelse, EtterlevelseStatus, KravQL, KravStatus } from '../../../constants'
+import { Field, FieldProps, Form, Formik, FormikProps, validateYupSchema, yupToFormErrors } from 'formik'
+import { mapEtterlevelseToFormValue } from '../../../api/EtterlevelseApi'
+import { Block } from 'baseui/block'
 import Button from '../../common/Button'
-import React, {useEffect} from 'react'
-import {DateField, FieldWrapper, TextAreaField} from '../../common/Inputs'
-import {theme} from '../../../util'
-import {FormControl} from 'baseui/form-control'
+import React, { useEffect } from 'react'
+import { DateField, FieldWrapper, TextAreaField } from '../../common/Inputs'
+import { theme } from '../../../util'
+import { FormControl } from 'baseui/form-control'
 
-import {LabelSmall, ParagraphMedium, ParagraphXSmall} from 'baseui/typography'
-import {ettlevColors} from '../../../util/theme'
-import {SuksesskriterierBegrunnelseEdit} from './SuksesskriterieBegrunnelseEdit'
-import {Radio, RadioGroup} from 'baseui/radio'
-import {Code} from '../../../services/Codelist'
-import {Error} from '../../common/ModalSchema'
-import {KIND as NKIND, Notification} from 'baseui/notification'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTimesCircle} from '@fortawesome/free-solid-svg-icons'
-import {borderColor, borderRadius, borderStyle, borderWidth, marginAll, paddingZero} from '../../common/Style'
+import { LabelSmall, ParagraphMedium, ParagraphXSmall } from 'baseui/typography'
+import { ettlevColors } from '../../../util/theme'
+import { SuksesskriterierBegrunnelseEdit } from './SuksesskriterieBegrunnelseEdit'
+import { Radio, RadioGroup } from 'baseui/radio'
+import { Code } from '../../../services/Codelist'
+import { Error } from '../../common/ModalSchema'
+import { KIND as NKIND, Notification } from 'baseui/notification'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
+import { borderColor, borderRadius, borderStyle, borderWidth, marginAll, paddingZero } from '../../common/Style'
 import moment from 'moment'
-import {CustomizedAccordion, CustomizedPanel, CustomPanelDivider} from '../../common/CustomizedAccordion'
-import {AllInfo} from '../../krav/ViewKrav'
+import { CustomizedAccordion, CustomizedPanel, CustomPanelDivider } from '../../common/CustomizedAccordion'
+import { AllInfo } from '../../krav/ViewKrav'
 import CustomizedModal from '../../common/CustomizedModal'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import EtterlevelseCard from '../EtterlevelseCard'
-import {ModalHeader} from 'baseui/modal'
-import {etterlevelseSchema} from './etterlevelseSchema'
+import { ModalHeader } from 'baseui/modal'
+import { etterlevelseSchema } from './etterlevelseSchema'
 import _ from 'lodash'
 
 type EditProps = {
@@ -181,8 +181,8 @@ export const EtterlevelseEditFields = ({
                                         etterlevelseStatus === EtterlevelseStatus.FERDIG_DOKUMENTERT || etterlevelseStatus === EtterlevelseStatus.FERDIG
                                           ? EtterlevelseStatus.UNDER_REDIGERING
                                           : etterlevelseStatus === EtterlevelseStatus.IKKE_RELEVANT_FERDIG_DOKUMENTERT
-                                          ? EtterlevelseStatus.IKKE_RELEVANT
-                                          : etterlevelseStatus
+                                            ? EtterlevelseStatus.IKKE_RELEVANT
+                                            : etterlevelseStatus
                                       }
                                       onChange={(event) => {
                                         p.form.setFieldValue('status', event.currentTarget.value)
@@ -203,31 +203,31 @@ export const EtterlevelseEditFields = ({
                                             {getEtterlevelseRadioLabel(EtterlevelseStatus.OPPFYLLES_SENERE)}
                                           </ParagraphMedium>
                                         </Block>
-
-                                        {etterlevelseStatus === EtterlevelseStatus.OPPFYLLES_SENERE && (
-                                          <Block width="100%">
-                                            <Block maxWidth="170px" width="100%">
-                                              <DateField error={!!p.form.errors.fristForFerdigstillelse} label="Frist" name="fristForFerdigstillelse" />
-                                            </Block>
-                                            {p.form.errors.fristForFerdigstillelse && (
-                                              <Block display="flex" width="100%" marginTop=".2rem">
-                                                <Block width="100%">
-                                                  <Notification
-                                                    overrides={{
-                                                      Body: {
-                                                        style: { width: 'auto', ...paddingZero, marginTop: 0, backgroundColor: 'transparent', color: ettlevColors.red600 },
-                                                      },
-                                                    }}
-                                                    kind={NKIND.negative}
-                                                  >
-                                                    {p.form.errors.fristForFerdigstillelse}
-                                                  </Notification>
-                                                </Block>
-                                              </Block>
-                                            )}
-                                          </Block>
-                                        )}
                                       </Radio>
+
+                                      {etterlevelseStatus === EtterlevelseStatus.OPPFYLLES_SENERE && (
+                                        <Block width="100%" marginLeft="33px">
+                                          <Block maxWidth="170px" width="100%">
+                                            <DateField error={!!p.form.errors.fristForFerdigstillelse} label="Frist" name="fristForFerdigstillelse" />
+                                          </Block>
+                                          {p.form.errors.fristForFerdigstillelse && (
+                                            <Block display="flex" width="100%" marginTop=".2rem">
+                                              <Block width="100%">
+                                                <Notification
+                                                  overrides={{
+                                                    Body: {
+                                                      style: { width: 'auto', ...paddingZero, marginTop: 0, backgroundColor: 'transparent', color: ettlevColors.red600 },
+                                                    },
+                                                  }}
+                                                  kind={NKIND.negative}
+                                                >
+                                                  {p.form.errors.fristForFerdigstillelse}
+                                                </Notification>
+                                              </Block>
+                                            </Block>
+                                          )}
+                                        </Block>
+                                      )}
 
                                       <Radio value={EtterlevelseStatus.IKKE_RELEVANT} key={EtterlevelseStatus.IKKE_RELEVANT}>
                                         <Block $style={{ textDecoration: radioHover === EtterlevelseStatus.IKKE_RELEVANT ? 'underline' : 'none' }}>
