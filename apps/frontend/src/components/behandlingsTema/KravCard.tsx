9a8202c434bbaec1f395e380a329fb537db39ac8
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-05-04 11:22:03 +0200

    UPDATE: design of kravlist for bortfiltrert and utgått

--- apps/frontend/src/components/behandlingsTema/KravCard.tsx
+++ apps/frontend/src/components/behandlingsTema/KravCard.tsx
@@ -1,21 +1,21 @@
-import {EtterlevelseMetadata, KRAV_FILTER_TYPE, KravEtterlevelseData} from '../../constants'
-import React, {useEffect, useState} from 'react'
-import {getEtterlevelseMetadataByBehandlingsIdAndKravNummerAndKravVersion, mapEtterlevelseMetadataToFormValue} from '../../api/EtterlevelseMetadataApi'
-import {Block} from 'baseui/block'
+import { EtterlevelseMetadata, KRAV_FILTER_TYPE, KravEtterlevelseData } from '../../constants'
+import React, { useEffect, useState } from 'react'
+import { getEtterlevelseMetadataByBehandlingsIdAndKravNummerAndKravVersion, mapEtterlevelseMetadataToFormValue } from '../../api/EtterlevelseMetadataApi'
+import { Block } from 'baseui/block'
 import Button from '../common/Button'
-import {ettlevColors} from '../../util/theme'
-import {borderStyle, marginAll} from '../common/Style'
-import {LabelSmall, ParagraphXSmall} from 'baseui/typography'
+import { ettlevColors } from '../../util/theme'
+import { borderStyle, marginAll } from '../common/Style'
+import { LabelSmall, ParagraphXSmall } from 'baseui/typography'
 import StatusView from '../common/StatusTag'
-import {getEtterlevelseStatus, getStatusLabelColor} from '../behandling/utils'
+import { getEtterlevelseStatus, getStatusLabelColor } from '../behandling/utils'
 import moment from 'moment'
 import TildeltPopoever from '../etterlevelseMetadata/TildeltPopover'
-import {isFerdigUtfylt} from '../../pages/BehandlingerTemaPageV2'
-import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons'
-import {arkCheckIcon, arkPennIcon, warningAlert} from '../Images'
-import {getEtterlevelserByBehandlingsIdKravNumber} from '../../api/EtterlevelseApi'
-import {useLocation, useNavigate} from 'react-router-dom'
-import {getNumberOfDaysBetween} from '../../util/checkAge'
+import { isFerdigUtfylt } from '../../pages/BehandlingerTemaPageV2'
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { arkCheckIcon, arkPennIcon, warningAlert } from '../Images'
+import { getEtterlevelserByBehandlingsIdKravNumber } from '../../api/EtterlevelseApi'
+import { useLocation, useNavigate } from 'react-router-dom'
+import { getNumberOfDaysBetween } from '../../util/checkAge'
 
 export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean; behandlingId: string; noVarsling?: boolean; kravFilter: KRAV_FILTER_TYPE }) => {
   const [nyVersionFlag, setNyVersionFlag] = useState<boolean>(false)
@@ -54,12 +54,12 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
     const today = new Date()
     const kravCreatedDate = moment(props.krav.changeStamp.createdDate).toDate()
     setKravAge(getNumberOfDaysBetween(kravCreatedDate, today))
-    ;(async () => {
-      getEtterlevelseMetaData()
-      if (props.krav.kravVersjon > 1 && props.krav.etterlevelseStatus === undefined) {
-        setNyVersionFlag((await getEtterlevelserByBehandlingsIdKravNumber(props.behandlingId, props.krav.kravNummer)).content.length >= 1)
-      }
-    })()
+      ; (async () => {
+        getEtterlevelseMetaData()
+        if (props.krav.kravVersjon > 1 && props.krav.etterlevelseStatus === undefined) {
+          setNyVersionFlag((await getEtterlevelserByBehandlingsIdKravNumber(props.behandlingId, props.krav.kravNummer)).content.length >= 1)
+        }
+      })()
   }, [])
 
   return (
@@ -67,8 +67,8 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
       display={'flex'}
       $style={{
         boxShadow: '0 3px 1px -2px rgba(0, 0, 0, .2), 0 2px 2px 0 rgba(0, 0, 0, .14), 0 1px 2px 0 rgba(0, 0, 0, .12)',
-        ':hover': {boxShadow: '0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)'},
-        ':active': {boxShadow: '0 2px 1px -2px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 1px 0 rgba(0, 0, 0, .12)'},
+        ':hover': { boxShadow: '0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)' },
+        ':active': { boxShadow: '0 2px 1px -2px rgba(0, 0, 0, .2), 0 1px 1px 0 rgba(0, 0, 0, .14), 0 1px 1px 0 rgba(0, 0, 0, .12)' },
         ':focus': {
           boxShadow: '0 2px 4px -1px rgba(0, 0, 0, .2), 0 4px 5px 0 rgba(0, 0, 0, .14), 0 1px 3px 0 rgba(0, 0, 0, .12)',
           outline: `3px solid ${ettlevColors.focusOutline}`,
@@ -88,7 +88,7 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
             justifyContent: 'flex-start',
             backgroundColor: ettlevColors.white,
             ...borderStyle('hidden'),
-            ':hover': {backgroundColor: 'none', boxShadow: ''},
+            ':hover': { backgroundColor: 'none', boxShadow: '' },
             boxShadow: '',
           }}
           onClick={() => {
@@ -97,85 +97,34 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
         >
           <Block display="flex" justifyContent="center" alignItems="center" width="100%" onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
             <Block marginLeft="8px">
-              {props.krav.isIrrelevant || isFerdigUtfylt(props.krav.etterlevelseStatus) ? <img src={arkCheckIcon} alt=""/> : <img src={arkPennIcon} alt=""/>}
+              {props.krav.isIrrelevant || isFerdigUtfylt(props.krav.etterlevelseStatus) ? <img src={arkCheckIcon} alt="" /> : <img src={arkPennIcon} alt="" />}
             </Block>
             <Block marginLeft="14px">
               <Block display={'flex'} alignItems={'center'}>
                 <ParagraphXSmall
-                  $style={{fontSize: '16px', lineHeight: '24px', marginBottom: '0px', marginTop: '0px', width: 'fit-content', textDecoration: hover ? 'underline' : 'none'}}
+                  $style={{ fontSize: '16px', lineHeight: '24px', marginBottom: '0px', marginTop: '0px', width: 'fit-content', textDecoration: hover ? 'underline' : 'none' }}
                 >
                   K{props.krav.kravNummer}.{props.krav.kravVersjon}
                 </ParagraphXSmall>
                 {!props.noVarsling && props.krav.kravVersjon === 1 && props.krav.etterlevelseStatus === undefined && kravAge < 30 && showWarningMessage('Nytt krav')}
                 {!props.noVarsling && props.krav.etterlevelseStatus === undefined && nyVersionFlag && props.kravFilter === KRAV_FILTER_TYPE.RELEVANTE_KRAV && showWarningMessage('Ny version')}
-                {props.kravFilter !== KRAV_FILTER_TYPE.RELEVANTE_KRAV && <StatusView
-                  status={props.kravFilter === KRAV_FILTER_TYPE.UTGAATE_KRAV ? 'Utgått' : 'Bortfiltrert'}
-                  statusDisplay={{
-                    background: ettlevColors.grey50,
-                    ...borderStyle('solid')
-                  }}
-                  lineHeight={'16px'}
-                  fontSize={'16px'}
-                  fontStyle={'italic'}
-                  overrides={{
-                    Root: {
-                      style: {
-                        marginLeft: '21px',
-                      }
-                    },
-                    Contents: {
-                      style: {
-                        ...marginAll('2px'),
-                      }
-                    },
-                    Body: {
-                      style: {
-                        ...marginAll('2px'),
-                      }
-                    }
-                  }}
-                />
-                }
               </Block>
-              <LabelSmall $style={{fontSize: '18px', fontWeight: 600, alignContent: 'flex-start', textAlign: 'left', textDecoration: hover ? 'underline' : 'none'}}>
+              <LabelSmall $style={{ fontSize: '18px', fontWeight: 600, alignContent: 'flex-start', textAlign: 'left', textDecoration: hover ? 'underline' : 'none' }}>
                 {props.krav.navn}
               </LabelSmall>
             </Block>
             <Block display="flex" justifyContent="flex-end" flex="1" width="100%">
               <Block width="350px" display="flex" justifyContent="flex-end" marginLeft="32px">
                 <Block display="flex" width="100%" maxWidth="220px" justifyContent="flex-end">
-                  {props.krav && props.krav.etterlevelseStatus && (
-                    <StatusView status={getEtterlevelseStatus(props.krav.etterlevelseStatus, props.krav.frist)} statusDisplay={getStatusLabelColor(props.krav.etterlevelseStatus)}/>
+                  {props.kravFilter === KRAV_FILTER_TYPE.RELEVANTE_KRAV && props.krav && props.krav.etterlevelseStatus && (
+                    <StatusView status={getEtterlevelseStatus(props.krav.etterlevelseStatus, props.krav.frist)} statusDisplay={getStatusLabelColor(props.krav.etterlevelseStatus)} />
                   )}
                 </Block>
-                {!props.krav.isIrrelevant && (
-                  <Block marginLeft="31px" maxWidth="140px" width="100%">
-                    {etterlevelseMetadata && etterlevelseMetadata.tildeltMed && etterlevelseMetadata.tildeltMed.length >= 1 ? (
-                      <Block>
-                        <LabelSmall $style={{fontSize: '14px', lineHeight: '14px', textAlign: 'right'}}>
-                          Tildelt:{' '}
-                          {etterlevelseMetadata.tildeltMed[0].length > 12 ? etterlevelseMetadata.tildeltMed[0].substring(0, 11) + '...' : etterlevelseMetadata.tildeltMed[0]}
-                        </LabelSmall>
-                      </Block>
-                    ) : (
-                      <Block>
-                        <LabelSmall $style={{fontSize: '14px', lineHeight: '14px', textAlign: 'right', fontStyle: 'italic', fontWeight: 400}}>Ikke tildelt</LabelSmall>
-                      </Block>
-                    )}
-                    {props.krav.etterlevelseChangeStamp?.lastModifiedDate && (
-                      <Block width="100%" display="flex" justifyContent="flex-end">
-                        <ParagraphXSmall $style={{lineHeight: '19px', textAlign: 'right', marginTop: '0px', marginBottom: '0px', whiteSpace: 'nowrap'}}>
-                          {'Sist utfylt: ' + moment(props.krav.etterlevelseChangeStamp?.lastModifiedDate).format('ll')}
-                        </ParagraphXSmall>
-                      </Block>
-                    )}
-                  </Block>
-                )}
 
-                {props.krav.isIrrelevant && (
+                {props.kravFilter !== KRAV_FILTER_TYPE.RELEVANTE_KRAV && (
                   <Block marginLeft="31px" maxWidth="140px" width="100%">
                     <StatusView
-                      status={'Ikke relevant'}
+                      status={props.kravFilter === KRAV_FILTER_TYPE.BORTFILTTERTE_KRAV ? 'Bortfiltrert' : 'Utgått'}
                       statusDisplay={{
                         background: ettlevColors.grey50,
                         border: ettlevColors.grey200
@@ -202,12 +151,37 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
                     />
                   </Block>
                 )}
+
+                {!props.krav.isIrrelevant && (
+                  <Block marginLeft="31px" maxWidth="140px" width="100%">
+                    {etterlevelseMetadata && etterlevelseMetadata.tildeltMed && etterlevelseMetadata.tildeltMed.length >= 1 ? (
+                      <Block>
+                        <LabelSmall $style={{ fontSize: '14px', lineHeight: '14px', textAlign: 'right' }}>
+                          Tildelt:{' '}
+                          {etterlevelseMetadata.tildeltMed[0].length > 12 ? etterlevelseMetadata.tildeltMed[0].substring(0, 11) + '...' : etterlevelseMetadata.tildeltMed[0]}
+                        </LabelSmall>
+                      </Block>
+                    ) : (
+                      <Block>
+                        <LabelSmall $style={{ fontSize: '14px', lineHeight: '14px', textAlign: 'right', fontStyle: 'italic', fontWeight: 400 }}>Ikke tildelt</LabelSmall>
+                      </Block>
+                    )}
+                    {props.krav.etterlevelseChangeStamp?.lastModifiedDate && (
+                      <Block width="100%" display="flex" justifyContent="flex-end">
+                        <ParagraphXSmall $style={{ lineHeight: '19px', textAlign: 'right', marginTop: '0px', marginBottom: '0px', whiteSpace: 'nowrap' }}>
+                          {'Sist utfylt: ' + moment(props.krav.etterlevelseChangeStamp?.lastModifiedDate).format('ll')}
+                        </ParagraphXSmall>
+                      </Block>
+                    )}
+                  </Block>
+                )}
               </Block>
             </Block>
           </Block>
         </Button>
       </Block>
-      {!props.krav.isIrrelevant && etterlevelseMetadata && (
+
+      {props.kravFilter === KRAV_FILTER_TYPE.RELEVANTE_KRAV && etterlevelseMetadata && (
         <Block display="flex" alignItems="center" paddingRight={'8px'}>
           <TildeltPopoever
             etterlevelseMetadata={etterlevelseMetadata}
@@ -217,6 +191,7 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
           />
         </Block>
       )}
+
     </Block>
   )
 }
