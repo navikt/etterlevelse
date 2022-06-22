fd050b8609cb41874ffe405239e3cecc93997e81
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-06-22 14:33:15 +0200

    UPDATE: to versjon endringer logic

--- apps/frontend/src/components/krav/EditKrav.tsx
+++ apps/frontend/src/components/krav/EditKrav.tsx
@@ -186,7 +186,7 @@ export const EditKrav = ({ krav, close, formRef, isOpen, setIsOpen, newVersion, 
       >
         <Formik
           onSubmit={submit}
-          initialValues={!newKrav ? kravMapToFormVal({ ...krav, versjonEndringer: '' }) : kravMapToFormVal(krav)}
+          initialValues={!newKrav && newVersion ? kravMapToFormVal({ ...krav, versjonEndringer: '' }) : kravMapToFormVal(krav)}
           validationSchema={kravSchema()}
           innerRef={formRef}
         >
