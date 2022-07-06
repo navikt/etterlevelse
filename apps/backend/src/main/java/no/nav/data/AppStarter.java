80f96f454060e5bc62be8172d9314e5e60b81eba
Shayan <shayan.yazdanmehr@nav.no>
2022-07-06 10:34:39 +0200

    importing UserDetailsServiceAutoConfiguration class
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

--- apps/backend/src/main/java/no/nav/data/AppStarter.java
+++ apps/backend/src/main/java/no/nav/data/AppStarter.java
@@ -3,6 +3,7 @@ package no.nav.data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @Slf4j
