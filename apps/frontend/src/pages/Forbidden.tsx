783d7d53b25ac187fe99f66b010964ea5895a97c
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-08-08 12:10:09 +0200

    ADD: logic for redirecting when logging in from forbidden page
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>

--- apps/frontend/src/pages/Forbidden.tsx
+++ apps/frontend/src/pages/Forbidden.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Block } from 'baseui/block'
 import { theme } from '../util'
 import { ParagraphLarge } from 'baseui/typography'
@@ -8,11 +8,18 @@ import notFound from '../resources/notfound.svg'
 import { maxPageWidth } from '../util/theme'
 import { Helmet } from 'react-helmet'
 import { ampli } from '../services/Amplitude'
+import { user } from '../services/User'
 
 const Forbidden = () => {
-  const location = useLocation()
+  const navigate = useNavigate()
+
+  if (user.isLoggedIn()) {
+    navigate(-1)
+  }
 
   ampli.logEvent('sidevisning', { side: 'Forbidden', sidetittel: '403 forbidden' })
+
+
 
   return (
     <Block id="content" overrides={{ Block: { props: { role: 'main' } } }} maxWidth={maxPageWidth} width="100%">
