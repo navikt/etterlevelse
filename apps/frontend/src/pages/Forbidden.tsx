b368e0fa2289b2b492d70da606022ebf0a241759
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-08-30 10:29:24 +0200

    REMOVE unused import
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/frontend/src/pages/Forbidden.tsx
+++ apps/frontend/src/pages/Forbidden.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Block } from 'baseui/block'
 import { theme } from '../util'
 import { ParagraphLarge } from 'baseui/typography'
