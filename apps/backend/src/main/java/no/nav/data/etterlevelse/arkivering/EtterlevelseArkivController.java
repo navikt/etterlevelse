941d87b5809940a4f910dcc39d0f6efd040f6077
Shayan <shayan.yazdanmehr@nav.no>
2022-09-21 10:32:51 +0200

    Changing the arkivering method signature to PUT
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/arkivering/EtterlevelseArkivController.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/arkivering/EtterlevelseArkivController.java
@@ -20,7 +20,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
@@ -106,7 +114,7 @@ public class EtterlevelseArkivController {
 
     @Operation(summary = "Update status to arkivert")
     @ApiResponse(description = "ok")
-    @GetMapping("/status/arkivert")
+    @PutMapping("/status/arkivert")
     public ResponseEntity<RestResponsePage<EtterlevelseArkivResponse>> arkiver(){
         log.info("Arkivering vellykket, setter status BEHANDLER_ARKIVERING til ARKIVERT");
 
