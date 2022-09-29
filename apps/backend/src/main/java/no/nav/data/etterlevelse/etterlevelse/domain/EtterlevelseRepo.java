018225f549dda19e2123123819c6b1c5b71ed512
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-09-29 14:09:04 +0200

    ADD: repo implementasion for updating behandlings id for  etterlevelse documentations
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelse/domain/EtterlevelseRepo.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelse/domain/EtterlevelseRepo.java
@@ -4,8 +4,10 @@ import no.nav.data.common.storage.domain.GenericStorage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.UUID;
 
@@ -31,4 +33,9 @@ public interface EtterlevelseRepo extends JpaRepository<GenericStorage, UUID> {
 
     @Query(value = "select * from generic_storage where data ->> 'behandlingId' = ?1 and data-> 'kravNummer' = to_jsonb(?2) and type = 'Etterlevelse'", nativeQuery = true)
     List<GenericStorage> findByBehandlingsIdAndKravNummer(String behandlingsId, int nummer);
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query(value= "update generic_storage set DATA = jsonb_set(DATA, '{behandlingId}', to_jsonb(?2), false) where data ->> 'behandlingId' = ?1 and type = 'Etterlevelse'", nativeQuery = true)
+    void updateEtterlevelseToNewBehandling(String oldBehandlingsId, String newBehandlinsId);
 }
