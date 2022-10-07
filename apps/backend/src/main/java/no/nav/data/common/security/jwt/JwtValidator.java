eec533ee4ec54f5dc0d8c6d8e4ba075bd8cec081
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-10-07 16:34:30 +0200

    UPDATE: issuer check and roles check

--- apps/backend/src/main/java/no/nav/data/common/security/jwt/JwtValidator.java
+++ apps/backend/src/main/java/no/nav/data/common/security/jwt/JwtValidator.java
@@ -7,7 +7,6 @@ import com.auth0.jwk.UrlJwkProvider;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,18 +25,20 @@ public class JwtValidator {
             try {
                 log.info("Validating token from: " + jwt.getClaim("azp"));
                 Jwk jwk = provider.get(jwt.getKeyId());
+
                 Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
                 JWTVerifier verifier = JWT.require(algorithm)
-                        .withIssuer("auth0")
+                        .withIssuer(System.getenv("AZURE_OPENID_CONFIG_ISSUER"))
+                        .withArrayClaim("roles", "arkiv-admin", "access_as_application")
                         .build();
                 DecodedJWT decodedJwt = verifier.verify(token);
 
-                Claim roles = decodedJwt.getClaim("roles");
+/*                Claim roles = decodedJwt.getClaim("roles");
                 if(roles.asList(String.class).isEmpty()){
                     throw new RuntimeException("roles are empty");
-                } else if(!roles.asList(String.class).contains("Arkiv-Admin")){
+                } else if(!roles.asList(String.class).contains("arkiv-Admin")){
                     return false;
-                }
+                }*/
 
             }catch (JwkException e){
                 log.error("Invalid token: " + e);
