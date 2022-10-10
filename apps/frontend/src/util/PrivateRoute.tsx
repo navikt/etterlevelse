eeca6b4452996c24332857d0881a90156b372983
JeremiahUy <jeremiah.augie.uy@nav.no>
2022-10-10 11:51:03 +0200

    UPDATE: redirecting and checking if user is logged in with correct access
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

---
+++ apps/frontend/src/util/PrivateRoute.tsx
@@ -0,0 +1,47 @@
+import { IndexRouteProps, LayoutRouteProps, Navigate, PathRouteProps, Route } from 'react-router-dom'
+import React, { useState } from 'react'
+import { user } from '../services/User'
+import { Spinner } from 'baseui/icon'
+import { ettlevColors, theme } from './theme'
+
+interface PrivateRouteProps {
+  component: JSX.Element
+  adminPage: boolean
+}
+
+
+export const PrivateRoute = ({ component, adminPage, ...rest }: PrivateRouteProps) => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  React.useEffect(() => {
+
+    let interval = setInterval(() => {
+      setIsLoading(false)
+    }, 1)
+
+    return () => clearInterval(interval)
+  }, [])
+
+
+  if (isLoading) {
+    return <Spinner $color={ettlevColors.green400} $size={theme.sizing.scale2400} />
+  }
+
+  if (user.isLoggedIn()) {
+    if (adminPage) {
+      if (user.isAdmin()) {
+        return component
+      } else {
+        return <Navigate to={{ pathname: '/forbidden' }} />
+      }
+    } else {
+      return component
+    }
+  } else {
+    return <Navigate to={{ pathname: '/forbidden' }} />
+  }
+}
+
+
+
+export default PrivateRoute
