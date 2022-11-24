9b085ca9f62dc4e57389813606e65dbeca8f22f8
Lord Andre Groseth <andregroseth@hotmail.com>
2022-11-24 13:55:44 +0100

    ADD: BehandlingStatistikk domain
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

---
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/statistikk/domain/BehandlingStatistikk.java
@@ -0,0 +1,44 @@
+package no.nav.data.etterlevelse.statistikk.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import no.nav.data.etterlevelse.statistikk.dto.BehandligStatistikkResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BehandlingStatistikk {
+    private String behandlingId;
+    private String behandlingNavn;
+    private List<String> team;
+    private LocalDateTime opprettetDato;
+    private LocalDateTime endretDato;
+    private Integer totalKrav;
+    private Integer antallIkkeFiltrertKrav;
+    private Integer antallBortfiltrertKrav;
+    private Integer antallIkkePåbegynt;
+    private Integer underArbeid;
+    private Integer antallFerdigDokumentert;
+
+public BehandligStatistikkResponse toResponse() {
+    return BehandligStatistikkResponse.builder()
+            .behandlingId(behandlingId)
+            .behandlingNavn(behandlingNavn)
+            .team(team)
+            .opprettetDato(opprettetDato)
+            .endretDato(endretDato)
+            .totalKrav(totalKrav)
+            .antallIkkeFiltrertKrav(antallIkkeFiltrertKrav)
+            .antallBortfiltrertKrav(antallBortfiltrertKrav)
+            .antallIkkePåbegynt(antallIkkePåbegynt)
+            .underArbeid(underArbeid)
+            .antallFerdigDokumentert(antallFerdigDokumentert)
+            .build();
+}
+}
