55355a4d8151ca069e0e4609803986edb1084eed
Lord Andre Groseth <andregroseth@hotmail.com>
2022-12-07 11:01:10 +0100

    ADD: Krav statestikk response
    Co-authored-by: JeremiahUy  <jeremiah.augie.uy@nav.no>

---
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/statistikk/dto/KravStatistikkResponse.java
@@ -0,0 +1,39 @@
+package no.nav.data.etterlevelse.statistikk.dto;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import no.nav.data.etterlevelse.codelist.dto.CodelistResponse;
+import no.nav.data.etterlevelse.krav.domain.KravStatus;
+import no.nav.data.etterlevelse.krav.dto.RegelverkResponse;
+import no.nav.data.etterlevelse.krav.dto.SuksesskriterieResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonPropertyOrder({"UUID", "navn", "kravNummer", "kravVersjon"})
+public class KravStatistikkResponse {
+    private UUID id;
+    private LocalDateTime lastModifedDate;
+    private LocalDateTime createdDate;
+    private Integer kravNummer;
+    private Integer kravVersjon;
+    private String navn;
+    private List<RegelverkResponse> regelverk;
+    private List<String> tagger;
+    private List<SuksesskriterieResponse> suksesskriterier;
+    private List<String> kravIdRelasjoner;
+    private CodelistResponse avdeling;
+    private CodelistResponse underavdeling;
+    private List<CodelistResponse> relevansFor;
+    private KravStatus status;
+    private LocalDateTime aktivertDato;
+    private String tema;
+}
