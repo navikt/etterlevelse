f1518233d3c5f4131ae920904d7b1a1b08cb714b
Lord Andre Groseth <andregroseth@hotmail.com>
2022-12-13 13:39:13 +0100

    update: Heading size
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: PaulMartinKokseter <59053738+PaulMartinKokseter@users.noreply.github.com>
    Co-authored-by: Shayan <shayan.yazdanmehr@nav.no>

--- apps/frontend/src/pages/Forbidden.tsx
+++ apps/frontend/src/pages/Forbidden.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Block } from 'baseui/block'
 import { theme } from '../util'
-import { ParagraphLarge } from 'baseui/typography'
+import { HeadingXXLarge, ParagraphLarge } from 'baseui/typography'
 import { intl } from '../util/intl/intl'
 import notFound from '../resources/notfound.svg'
 import { maxPageWidth } from '../util/theme'
@@ -28,7 +28,7 @@ const Forbidden = () => {
         <title>403 forbidden</title>
       </Helmet>
       <Block paddingLeft="40px" paddingRight="40px" width="calc(100%-80px)" display="flex" justifyContent="center" alignContent="center" marginTop={theme.sizing.scale2400}>
-        <ParagraphLarge>Du prøvde å komme inn i en side du ikke har tilgang til.</ParagraphLarge>
+        <HeadingXXLarge>Du prøvde å komme inn i en side du ikke har tilgang til.</HeadingXXLarge>
         <img src={notFound} alt={intl.pageNotFound} style={{ maxWidth: '65%' }} />
       </Block>
     </Block>
