939bb83516b2a0f034b689f2fc5aed7a09afeced
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-02-01 13:35:49 +0100

    UPDATE: to open status page in new tab

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -90,10 +90,11 @@ export const MainPage = () => {
               height={cardHeight}
               width={cardWidth}
               verticalMargin={theme.sizing.scale300}
-              href={'//metabase.intern.nav.no/dashboard/116-dashboard-for-etterlevelse'}
+              href={'https://metabase.intern.nav.no/dashboard/116-dashboard-for-etterlevelse'}
               tittel={'Status i organisasjonen'}
               icon={pageWidth && pageWidth >= 768 ? grafIconBg : grafIconBgSmall}
               beskrivelse={'Følg med på status og se hvor godt NAV sine produktområder  dokumenterer på kravene'}
+              openInNewTab
             />
           </Block>
         </Wide>
