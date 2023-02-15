fa244c9b1388a917aba22cdb2e3707c56465bf54
Lord Andre Groseth <andregroseth@hotmail.com>
2023-02-15 09:46:52 +0100

    ADD: getByBehandlingId to EtterlevelseDokumentasjonService
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/EtterlevelseDokumentasjonService.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/EtterlevelseDokumentasjonService.java
@@ -2,10 +2,13 @@ package no.nav.data.etterlevelse.etterlevelseDokumentasjon;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.data.common.auditing.domain.AuditVersionRepository;
+import no.nav.data.common.storage.domain.GenericStorage;
 import no.nav.data.etterlevelse.common.domain.DomainService;
 import no.nav.data.etterlevelse.etterlevelseDokumentasjon.domain.EtterlevelseDokumentasjon;
 import no.nav.data.etterlevelse.etterlevelseDokumentasjon.dto.EtterlevelseDokumentasjonRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -29,4 +32,10 @@ public class EtterlevelseDokumentasjonService extends DomainService<Etterlevelse
 
         return storage.save(etterlevelseDokumentasjon);
     }
+
+    public List<EtterlevelseDokumentasjon> getByBehandlingId(List<String> ids) {
+        return GenericStorage.to(etterlevelseDokumentasjonRepo.findByBehandlingIds(ids), EtterlevelseDokumentasjon.class);
+    }
+
+
 }
