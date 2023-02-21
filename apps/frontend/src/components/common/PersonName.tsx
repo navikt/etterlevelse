c0ad22b3bc293760d61c3d904c3ab9551f371074
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-02-21 11:13:37 +0100

    ADD: option to have title rolle in name
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/common/PersonName.tsx
+++ apps/frontend/src/components/common/PersonName.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 import { teamKatPersonLink } from '../../util/config'
 import { StyledLink } from 'baseui/link'
 
-export const PersonName = (props: { ident: string; link?: boolean }) => {
+export const PersonName = (props: { ident: string; link?: boolean; kraveier?: boolean }) => {
   const name = usePersonName()(props.ident)
   return props.link ? (
     <StyledLink target="_blank" rel="noopener noreferrer" href={teamKatPersonLink(props.ident)}>
-      {name}
+      {name}{props.kraveier ? ' (Kraveier)' : ''}
     </StyledLink>
   ) : (
-    <>{name}</>
+    <>{name}{props.kraveier ? ' (Kraveier)' : ''}</>
   )
 }
