4d776ee1547717424e55c15046c12a9b071784c6
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-02-22 14:52:18 +0100

    UPDATE: design for new tilbakemedling suksesskriterier
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/frontend/src/components/krav/tilbakemelding/edit/NyTilbakemeldingModal.tsx
+++ apps/frontend/src/components/krav/tilbakemelding/edit/NyTilbakemeldingModal.tsx
@@ -1,27 +1,28 @@
-import {faSlackHash} from '@fortawesome/free-brands-svg-icons'
-import {faEnvelope, faThumbsUp, faUser} from '@fortawesome/free-solid-svg-icons'
-import {Block} from 'baseui/block'
+import { faSlackHash } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faThumbsUp, faUser } from '@fortawesome/free-solid-svg-icons'
+import { Block } from 'baseui/block'
 import Button from '../../../common/Button'
-import {FormControl} from 'baseui/form-control'
-import {ModalBody, ModalFooter, ModalHeader} from 'baseui/modal'
-import {HeadingLarge, HeadingXLarge, LabelSmall, ParagraphLarge} from 'baseui/typography'
-import {Field, FieldProps, Form, Formik} from 'formik'
-import {useState} from 'react'
-import {createNewTilbakemelding, CreateTilbakemeldingRequest} from '../../../../api/TilbakemeldingApi'
-import {AdresseType, Krav, Tilbakemelding, TilbakemeldingMeldingStatus, TilbakemeldingType, Varslingsadresse} from '../../../../constants'
-import {theme} from '../../../../util'
+import { FormControl } from 'baseui/form-control'
+import { ModalBody, ModalFooter, ModalHeader } from 'baseui/modal'
+import { HeadingLarge, HeadingXLarge, LabelLarge, LabelSmall, ParagraphLarge, ParagraphMedium } from 'baseui/typography'
+import { Field, FieldProps, Form, Formik } from 'formik'
+import { useState } from 'react'
+import { createNewTilbakemelding, CreateTilbakemeldingRequest } from '../../../../api/TilbakemeldingApi'
+import { AdresseType, Krav, Tilbakemelding, TilbakemeldingMeldingStatus, TilbakemeldingType, Varslingsadresse } from '../../../../constants'
+import { theme } from '../../../../util'
 import CustomizedModal from '../../../common/CustomizedModal'
-import {TextAreaField} from '../../../common/Inputs'
+import { TextAreaField } from '../../../common/Inputs'
 import LabelWithTooltip from '../../../common/LabelWithTooltip'
-import {AddEmail, SlackChannelSearch, SlackUserSearch, VarslingsadresserTagList} from '../../Edit/KravVarslingsadresserEdit'
-import {Notification} from 'baseui/notification'
+import { AddEmail, SlackChannelSearch, SlackUserSearch, VarslingsadresserTagList } from '../../Edit/KravVarslingsadresserEdit'
+import { Notification } from 'baseui/notification'
 import * as yup from 'yup'
-import {Card} from 'baseui/card'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {ettlevColors} from '../../../../util/theme'
-import {borderColor, borderRadius, borderWidth} from '../../../common/Style'
-import {CustomizedAccordion, CustomizedPanel} from '../../../common/CustomizedAccordion'
-import {SuksesskriterieCard} from '../../Suksesskriterie'
+import { Card } from 'baseui/card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { ettlevColors } from '../../../../util/theme'
+import { borderColor, borderRadius, borderWidth } from '../../../common/Style'
+import { CustomizedAccordion, CustomizedPanel } from '../../../common/CustomizedAccordion'
+import { SuksesskriterieCard } from '../../Suksesskriterie'
+import { Markdown } from '../../../common/Markdown'
 
 type NyTilbakemeldingModalProps = {
   open?: boolean
@@ -40,7 +41,7 @@ const getMessageType = (type: AdresseType | undefined) => {
   }
 }
 
-export const NyTilbakemeldingModal = ({open, close, krav}: NyTilbakemeldingModalProps) => {
+export const NyTilbakemeldingModal = ({ open, close, krav }: NyTilbakemeldingModalProps) => {
   const [error, setError] = useState()
   const [adresseType, setAdresseType] = useState<AdresseType>()
   const [showNotification, setShowNotification] = useState<AdresseType>()
@@ -75,7 +76,7 @@ export const NyTilbakemeldingModal = ({open, close, krav}: NyTilbakemeldingModal
         validateOnBlur={false}
         validateOnChange={false}
       >
-        {({isSubmitting, setFieldValue, values, submitForm}) => {
+        {({ isSubmitting, setFieldValue, values, submitForm }) => {
           const setVarslingsadresse = (v?: Varslingsadresse) => {
             setFieldValue('varslingsadresse', v)
             setAdresseType(undefined)
@@ -101,7 +102,7 @@ export const NyTilbakemeldingModal = ({open, close, krav}: NyTilbakemeldingModal
                     }}
                   >
                     <Block display="flex" alignItems="center">
-                      <FontAwesomeIcon icon={faThumbsUp} size="lg"/>
+                      <FontAwesomeIcon icon={faThumbsUp} size="lg" />
                       <HeadingLarge
                         $style={{
                           fontSize: '20px',
@@ -130,50 +131,67 @@ export const NyTilbakemeldingModal = ({open, close, krav}: NyTilbakemeldingModal
                 ) : (
                   <Block>
                     <CustomizedAccordion>
-                      <CustomizedPanel
-                        overrides={{Content: {style: {backgroundColor: ettlevColors.white, paddingLeft: '20px', paddingRight: '20px'}}}}
-                        title={<LabelSmall>Vis suksesskriterier</LabelSmall>}
-                      >
-                        <Block width="100%">
-                          {krav.suksesskriterier.map((s, i) => (
-                            <SuksesskriterieCard key={s.id} suksesskriterie={s} num={i + 1} totalt={krav.suksesskriterier.length} fullWidth/>
-                          ))}
-                        </Block>
-                      </CustomizedPanel>
+                      {krav.suksesskriterier.map((s, i) => {
+                        return (<CustomizedPanel
+                          overrides={{ Content: { style: { backgroundColor: ettlevColors.white, paddingLeft: '20px', paddingRight: '20px' } } }}
+                          title={
+                            <Block>
+                              <ParagraphMedium
+                                $style={{
+                                  fontSize: '16px',
+                                  lineHeight: '18,75',
+                                  marginTop: '3px',
+                                  marginBottom: '5px',
+                                  font: 'roboto',
+                                  color: ettlevColors.grey600,
+                                }}
+                              >
+                                Suksesskriterium {i + 1} av {krav.suksesskriterier.length}
+                              </ParagraphMedium>
+                              <LabelLarge color={ettlevColors.green600}>{s.navn}</LabelLarge>
+                            </Block>
+                          }
+                        >
+                          <Block width="100%">
+                            <Markdown source={s.beskrivelse} maxWidth="100%" />
+                          </Block>
+                        </CustomizedPanel>)
+                      })}
+
                     </CustomizedAccordion>
-                    <TextAreaField tooltip="Skriv ditt spørsmål i tekstfeltet" label="Ditt spørsmål" name="foersteMelding" placeholder="Skriv her.."/>
+                    <TextAreaField tooltip="Skriv ditt spørsmål i tekstfeltet" label="Ditt spørsmål" name="foersteMelding" placeholder="Skriv her.." />
 
                     {/* <OptionField label="Type" name="type" clearable={false} options={Object.values(TilbakemeldingType).map((o) => ({ id: o, label: typeText(o) }))} /> */}
                     <Field name="varslingsadresse.adresse">
                       {(p: FieldProps) => (
                         <FormControl
-                          label={<LabelWithTooltip label="Din varslingsadresse" tooltip="Velg hvilken adresse du vil varsles på når kraveier svarer på spørsmålet"/>}
+                          label={<LabelWithTooltip label="Din varslingsadresse" tooltip="Velg hvilken adresse du vil varsles på når kraveier svarer på spørsmålet" />}
                           error={p.meta.error}
                         >
                           <Block>
                             <Block display="flex" flexDirection="column" marginTop={theme.sizing.scale600}>
-                              {adresseType === AdresseType.SLACK && <SlackChannelSearch add={setVarslingsadresse}/>}
+                              {adresseType === AdresseType.SLACK && <SlackChannelSearch add={setVarslingsadresse} />}
                               {adresseType !== AdresseType.SLACK && !values.varslingsadresse && (
                                 <Button kind="secondary" size="compact" type="button" icon={faSlackHash} onClick={() => setAdresseType(AdresseType.SLACK)}>
                                   Slack-kanal
                                 </Button>
                               )}
-                              <Block marginTop={theme.sizing.scale400}/>
-                              {adresseType === AdresseType.SLACK_USER && <SlackUserSearch add={setVarslingsadresse}/>}
+                              <Block marginTop={theme.sizing.scale400} />
+                              {adresseType === AdresseType.SLACK_USER && <SlackUserSearch add={setVarslingsadresse} />}
                               {adresseType !== AdresseType.SLACK_USER && !values.varslingsadresse && (
                                 <Button kind="secondary" size="compact" marginLeft type="button" icon={faUser} onClick={() => setAdresseType(AdresseType.SLACK_USER)}>
                                   Slack-bruker
                                 </Button>
                               )}
-                              <Block marginTop={theme.sizing.scale400}/>
-                              {adresseType === AdresseType.EPOST && <AddEmail add={setVarslingsadresse}/>}
+                              <Block marginTop={theme.sizing.scale400} />
+                              {adresseType === AdresseType.EPOST && <AddEmail add={setVarslingsadresse} />}
                               {adresseType !== AdresseType.EPOST && !values.varslingsadresse && (
                                 <Button kind="secondary" size="compact" marginLeft type="button" icon={faEnvelope} onClick={() => setAdresseType(AdresseType.EPOST)}>
                                   Epost
                                 </Button>
                               )}
                             </Block>
-                            {values.varslingsadresse && <VarslingsadresserTagList varslingsadresser={[values.varslingsadresse]} remove={() => setVarslingsadresse(undefined)}/>}
+                            {values.varslingsadresse && <VarslingsadresserTagList varslingsadresser={[values.varslingsadresse]} remove={() => setVarslingsadresse(undefined)} />}
                           </Block>
                         </FormControl>
                       )}
@@ -199,7 +217,7 @@ export const NyTilbakemeldingModal = ({open, close, krav}: NyTilbakemeldingModal
                   <Block display="flex" justifyContent="flex-end">
                     <Block>
                       {error && (
-                        <Notification kind="negative" overrides={{Body: {style: {marginBottom: '-25px'}}}}>
+                        <Notification kind="negative" overrides={{ Body: { style: { marginBottom: '-25px' } } }}>
                           {error}
                         </Notification>
                       )}
