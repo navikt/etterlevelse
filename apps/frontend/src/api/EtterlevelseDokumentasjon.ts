71fe65c7731d2d4721d960c58c897a54149e71ab
Shayan <shayan.yazdanmehr@nav.no>
2023-02-23 11:03:56 +0100

    Adding update,delete, create and use to API
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/api/EtterlevelseDokumentasjon.ts
+++ apps/frontend/src/api/EtterlevelseDokumentasjon.ts
@@ -1,10 +1,7 @@
 import axios from "axios"
-import { EtterlevelseDokumentasjon, PageResponse } from "../constants"
-import { env } from "../util/env"
-
-
-
-
+import {EtterlevelseDokumentasjon, PageResponse} from "../constants"
+import {env} from "../util/env"
+import {useEffect, useState} from "react";
 
 
 export const getEtterlevelseDokumentasjon = async (id: string) => {
@@ -37,3 +34,47 @@ export const searchEtterlevelsedokumentasjon = async (searchParam: string) => {
 export const searchEtterlevelsedokumentasjonByBehandlingId = async (behandlingId: string) => {
   return (await axios.get<PageResponse<EtterlevelseDokumentasjon>>(`${env.backendBaseUrl}/etterlevelsedokumentasjon/search/behandling/${behandlingId}`)).data.content
 }
+export const updateEtterlevelseDokumentasjon = async (etterlevelseDokumentasjon: EtterlevelseDokumentasjon) => {
+  const dto = etterlevelseDokumentasjonToDto(etterlevelseDokumentasjon)
+  return (await axios.put<EtterlevelseDokumentasjon>(`${env.backendBaseUrl}/etterlevelsedokumentasjon/${etterlevelseDokumentasjon.id}`, dto)).data
+}
+
+export const createEtterlevelseDokumentasjon = async (etterlevelseDokumentasjon: EtterlevelseDokumentasjon) => {
+  const dto = etterlevelseDokumentasjonToDto(etterlevelseDokumentasjon)
+  return (await axios.post<EtterlevelseDokumentasjon>(`${env.backendBaseUrl}/etterlevelsedokumentasjon/`, dto)).data
+}
+
+export const deleteEtterlevelseDokumentasjon = async (etterlevelseDokumentasjonId: string) => {
+  return (await axios.delete<EtterlevelseDokumentasjon>(`${env.backendBaseUrl}/etterlevelsedokumentasjon/${etterlevelseDokumentasjonId}`)).data
+}
+
+export const useEtterlevelseDokumentasjon = (etterlevelseDokumentasjonId?: string, behandlingId?: string) => {
+  const isCreateNew = etterlevelseDokumentasjonId === 'ny'
+  const [data, setData] = useState<EtterlevelseDokumentasjon | undefined>(isCreateNew ? etterlevelseDokumentasjonMapToFormVal({}) : undefined)
+  useEffect(() => {
+    etterlevelseDokumentasjonId && !isCreateNew && getEtterlevelseDokumentasjon(etterlevelseDokumentasjonId).then(setData)
+  }, [etterlevelseDokumentasjonId])
+
+  return [data, setData] as [EtterlevelseDokumentasjon | undefined, (e: EtterlevelseDokumentasjon) => void]
+}
+
+function etterlevelseDokumentasjonToDto(etterlevelseDokumentasjon: EtterlevelseDokumentasjon): EtterlevelseDokumentasjon {
+  const dto = {
+    ...etterlevelseDokumentasjon,
+    irrelevansFor: etterlevelseDokumentasjon.irrelevansFor.map((c) => c.code)
+  } as any
+  delete dto.changeStamp
+  delete dto.version
+  return dto
+}
+
+export const etterlevelseDokumentasjonMapToFormVal = (etterlevelseDokumentasjon: Partial<EtterlevelseDokumentasjon>): EtterlevelseDokumentasjon => ({
+  id: etterlevelseDokumentasjon.id || '',
+  changeStamp: etterlevelseDokumentasjon.changeStamp || {lastModifiedDate: '', lastModifiedBy: ''},
+  version: -1,
+  title: etterlevelseDokumentasjon.title || '',
+  behandlingId: etterlevelseDokumentasjon.behandlingId || '',
+  irrelevansFor: etterlevelseDokumentasjon.irrelevansFor || [],
+  etterlevelseNummer: etterlevelseDokumentasjon.etterlevelseNummer || 0
+})
+
