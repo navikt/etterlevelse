f42d60e803ba67868ae38a87b6f98901c2f4f03d
Shayan <shayan.yazdanmehr@nav.no>
2023-03-06 12:25:43 +0100

    Adding EtterlevelseDokumentasjonStats to GraphQL
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
@@ -4,11 +4,16 @@ import graphql.kickstart.tools.GraphQLResolver;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.nav.data.etterlevelse.codelist.CodelistService;
+import no.nav.data.etterlevelse.codelist.domain.ListName;
 import no.nav.data.etterlevelse.etterlevelse.EtterlevelseService;
 import no.nav.data.etterlevelse.etterlevelse.domain.Etterlevelse;
 import no.nav.data.etterlevelse.etterlevelse.dto.EtterlevelseResponse;
 import no.nav.data.etterlevelse.etterlevelseDokumentasjon.dto.EtterlevelseDokumentasjonResponse;
+import no.nav.data.etterlevelse.etterlevelseDokumentasjon.dto.EtterlevelseDokumentasjonStats;
 import no.nav.data.etterlevelse.graphql.support.LoaderUtils;
+import no.nav.data.etterlevelse.krav.KravService;
+import no.nav.data.etterlevelse.krav.domain.Krav;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -18,6 +23,8 @@ import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import static no.nav.data.common.utils.StreamUtils.convert;
+import static no.nav.data.common.utils.StreamUtils.filter;
+import static no.nav.data.common.utils.StreamUtils.safeStream;
 import static no.nav.data.etterlevelse.graphql.DataLoaderReg.ETTERLEVELSE_FOR_ETTERLEVELSEDOKUMENTASJON_LOADER;
 
 @Slf4j
@@ -26,13 +33,54 @@ import static no.nav.data.etterlevelse.graphql.DataLoaderReg.ETTERLEVELSE_FOR_ET
 public class EtterlevelseDokumentasjonFieldResolver implements GraphQLResolver<EtterlevelseDokumentasjonResponse> {
 
     private final EtterlevelseService etterlevelseService;
+    private final KravService kravService;
 
     public CompletableFuture<List<EtterlevelseResponse>> etterlevelser(EtterlevelseDokumentasjonResponse etterlevelseDokumentasjon, DataFetchingEnvironment env) {
         return LoaderUtils.get(env, ETTERLEVELSE_FOR_ETTERLEVELSEDOKUMENTASJON_LOADER, etterlevelseDokumentasjon.getId(), (List<Etterlevelse> e) -> convert(e, Etterlevelse::toResponse));
     }
 
     public CompletableFuture<LocalDateTime> sistEndretEtterlevelse(EtterlevelseDokumentasjonResponse etterlevelseDokumentasjon, DataFetchingEnvironment env) {
-        return LoaderUtils.get(env,ETTERLEVELSE_FOR_ETTERLEVELSEDOKUMENTASJON_LOADER,etterlevelseDokumentasjon.getId().toString(),this::sistEndret);
+        return LoaderUtils.get(env, ETTERLEVELSE_FOR_ETTERLEVELSEDOKUMENTASJON_LOADER, etterlevelseDokumentasjon.getId().toString(), this::sistEndret);
+    }
+
+    public EtterlevelseDokumentasjonStats stats(EtterlevelseDokumentasjonResponse etterlevelseDokumentasjon) {
+        var relevans = etterlevelseDokumentasjon.getIrrelevansFor();
+
+        // Temporarily disabled
+        /*
+       if (relevans.isEmpty()) {
+            return BehandlingStats.empty();
+        }
+        */
+
+        var etterlevelser = etterlevelseService.getByEtterlevelseDokumentasjon(etterlevelseDokumentasjon.getId().toString());
+        var krav = convert(kravService.findForBehandling(etterlevelseDokumentasjon.getId().toString()), Krav::toResponse);
+
+        krav.forEach(k -> {
+            if (k.getEtterlevelser() != null) {
+                k.setEtterlevelser(k.getEtterlevelser().stream().filter(e -> e.getEtterlevelseDokumentasjonId().equals(etterlevelseDokumentasjon.getId().toString())).toList());
+            }
+        });
+
+        var irrelevantKrav = convert(kravService.findForBehandlingIrrelevans(etterlevelseDokumentasjon.getId().toString()), Krav::toResponse);
+
+        var fylt = filter(krav, k -> etterlevelser.stream().anyMatch(e -> e.isEtterleves() && e.kravId().equals(k.kravId())));
+        var ikkeFylt = filter(krav, k -> !fylt.contains(k));
+
+        var irrelevant = filter(irrelevantKrav, i -> !fylt.contains(i) && !ikkeFylt.contains(i));
+
+
+        return EtterlevelseDokumentasjonStats.builder()
+                .fyltKrav(fylt)
+                .ikkeFyltKrav(ikkeFylt)
+                .irrelevantKrav(irrelevant)
+                .lovStats(convert(CodelistService.getCodelist(ListName.LOV), c -> EtterlevelseDokumentasjonStats.LovStats.builder()
+                        .lovCode(c.toResponse())
+                        .fyltKrav(filter(fylt, k -> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
+                        .ikkeFyltKrav(filter(ikkeFylt, k -> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
+                        .irrelevantKrav(filter(irrelevant, k -> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
+                        .build()))
+                .build();
     }
 
     private LocalDateTime sistEndret(List<Etterlevelse> etterlevelser) {
