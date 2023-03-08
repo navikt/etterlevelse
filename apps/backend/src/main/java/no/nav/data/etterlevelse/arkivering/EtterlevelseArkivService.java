d148e5d98206ea0d4b89461df965743300afa3ad
Lord Andre Groseth <andregroseth@hotmail.com>
2023-03-08 12:41:59 +0100

    ADD: EtterlevelseArkivService
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: Shayan <shayan.yazdanmehr@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/arkivering/EtterlevelseArkivService.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/arkivering/EtterlevelseArkivService.java
@@ -14,6 +14,9 @@ import no.nav.data.etterlevelse.behandling.BehandlingService;
 import no.nav.data.etterlevelse.behandling.dto.Behandling;
 import no.nav.data.etterlevelse.common.domain.DomainService;
 import no.nav.data.etterlevelse.etterlevelse.domain.EtterlevelseStatus;
+import no.nav.data.etterlevelse.etterlevelseDokumentasjon.EtterlevelseDokumentasjonService;
+import no.nav.data.etterlevelse.etterlevelseDokumentasjon.domain.EtterlevelseDokumentasjon;
+import no.nav.data.etterlevelse.export.EtterlevelseDokumentasjonToDoc;
 import no.nav.data.etterlevelse.export.EtterlevelseToDoc;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -43,11 +46,16 @@ public class EtterlevelseArkivService extends DomainService<EtterlevelseArkiv> {
 
     private final BehandlingService behandlingService;
 
-    public EtterlevelseArkivService(EtterlevelseArkivRepo repo, EtterlevelseToDoc etterlevelseToDoc, BehandlingService behandlingService) {
+    private final EtterlevelseDokumentasjonService etterlevelseDokumentasjonService;
+    private final EtterlevelseDokumentasjonToDoc etterlevelseDokumentasjonToDoc;
+
+    public EtterlevelseArkivService(EtterlevelseArkivRepo repo, EtterlevelseToDoc etterlevelseToDoc, BehandlingService behandlingService, EtterlevelseDokumentasjonService etterlevelseDokumentasjonService, EtterlevelseDokumentasjonToDoc etterlevelseDokumentasjonToDoc) {
         super(EtterlevelseArkiv.class);
         this.repo = repo;
         this.etterlevelseToDoc = etterlevelseToDoc;
         this.behandlingService = behandlingService;
+        this.etterlevelseDokumentasjonService = etterlevelseDokumentasjonService;
+        this.etterlevelseDokumentasjonToDoc = etterlevelseDokumentasjonToDoc;
     }
 
     public Page<EtterlevelseArkiv> getAll(PageParameters pageParameters) {
@@ -66,8 +74,11 @@ public class EtterlevelseArkivService extends DomainService<EtterlevelseArkiv> {
         return GenericStorage.to(repo.findByBehandling(behandlingId), EtterlevelseArkiv.class);
     }
 
+    public List<EtterlevelseArkiv> getByEtterlevelseDokumentasjon(String etterlevelseDokumentasjonId) {
+        return GenericStorage.to(repo.findByEtterlevelseDokumentsjonId(etterlevelseDokumentasjonId), EtterlevelseArkiv.class);
+    }
+
     public byte[] getEtterlevelserArchiveZip(List<EtterlevelseArkiv> etterlevelseArkivList) throws IOException {
-        String filename;
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy'-'MM'-'dd'_'HH'-'mm'-'ss");
         SimpleDateFormat xmlDateFormatter = new SimpleDateFormat("YYYYMMdd_HHmmss");
         Date date = new Date();
@@ -87,6 +98,33 @@ public class EtterlevelseArkivService extends DomainService<EtterlevelseArkiv> {
             archiveFiles.add(ArchiveFile.builder()
                     .fileName(xmlDateFormatter.format(date) + "_Etterlevelse_B" + behandling.getNummer() + ".xml")
                     .file(createXml(date, formatter.format(date) + "_Etterlevelse_B" + behandling.getNummer() + ".docx", behandling, etterlevelseArkiv))
+                    .build());
+        }
+        return zipUtils.zipOutputStream(archiveFiles);
+    }
+
+    public byte[] getEtterlevelserArchiveZipForEtterlevelseDokumentasjonId(List<EtterlevelseArkiv> etterlevelseArkivList) throws IOException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy'-'MM'-'dd'_'HH'-'mm'-'ss");
+        SimpleDateFormat xmlDateFormatter = new SimpleDateFormat("YYYYMMdd_HHmmss");
+        Date date = new Date();
+
+        ZipUtils zipUtils = new ZipUtils();
+        List<ArchiveFile> archiveFiles = new ArrayList<>();
+
+        for (EtterlevelseArkiv etterlevelseArkiv : etterlevelseArkivList) {
+            EtterlevelseDokumentasjon etterlevelseDokumentasjon = etterlevelseDokumentasjonService.get(UUID.fromString(etterlevelseArkiv.getEtterlevelseDokumentasjonId()));
+
+           // Behandling behandling = behandlingService.getBehandling(etterlevelseArkiv.getBehandlingId());
+            ArrayList<String> statuses = new ArrayList<>();
+            statuses.add(EtterlevelseStatus.FERDIG_DOKUMENTERT.name());
+            statuses.add(EtterlevelseStatus.IKKE_RELEVANT_FERDIG_DOKUMENTERT.name());
+            archiveFiles.add(ArchiveFile.builder()
+                    .fileName(formatter.format(date) + "_Etterlevelse_E" + etterlevelseDokumentasjon.getEtterlevelseNummer() + ".docx")
+                    .file(etterlevelseDokumentasjonToDoc.generateDocFor(etterlevelseDokumentasjon.getId(), statuses, Collections.emptyList(), ""))
+                    .build());
+            archiveFiles.add(ArchiveFile.builder()
+                    .fileName(xmlDateFormatter.format(date) + "_Etterlevelse_E" + etterlevelseDokumentasjon.getEtterlevelseNummer() + ".xml")
+                    .file(createXmlForEtterlevelseDokumentasjon(date, formatter.format(date) + "_Etterlevelse_E" + etterlevelseDokumentasjon.getEtterlevelseNummer() + ".docx", etterlevelseDokumentasjon, etterlevelseArkiv))
                     .build());
         }
         return zipUtils.zipOutputStream(archiveFiles);
@@ -155,6 +193,63 @@ public class EtterlevelseArkivService extends DomainService<EtterlevelseArkiv> {
         return baos.toByteArray();
     }
 
+    @SneakyThrows
+    public byte[] createXmlForEtterlevelseDokumentasjon(Date date, String wordDocFileName, EtterlevelseDokumentasjon etterlevelseDokumentasjon, EtterlevelseArkiv etterlevelseArkiv) {
+
+        String creatorId = etterlevelseArkiv.getChangeStamp().getLastModifiedBy().split(" ")[0];
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+
+        Document document = documentBuilder.newDocument();
+        Element rootElement = document.createElement("NOARK.H");
+        document.appendChild(rootElement);
+
+        Element noarksak = document.createElement("NOARKSAK");
+        rootElement.appendChild(noarksak);
+
+        createElement("SA.ETTERLEVELSEID", "E" + etterlevelseDokumentasjon.getEtterlevelseNummer(), noarksak, document);
+
+        Element journalpostTab = document.createElement("JOURNALPOST.TAB");
+        noarksak.appendChild(journalpostTab);
+
+        Element journalpost = document.createElement("JOURNALPOST");
+        journalpostTab.appendChild(journalpost);
+
+        createElement("JP.INNHOLD",
+                "Etterlevelse for E" + etterlevelseDokumentasjon.getEtterlevelseNummer() + " - " + etterlevelseDokumentasjon.getTitle(),
+                journalpost, document);
+
+        SimpleDateFormat dateTime = new SimpleDateFormat("yyyyMMdd");
+        createElement("JP.DOKDATO", dateTime.format(date), journalpost, document);
+
+        createElement("JP.DOKTYPE", "X", journalpost, document);
+
+        createElement("JP.STATUS", "J", journalpost, document);
+
+        createElement("JP.SB", creatorId, journalpost, document);
+
+        Element dokVersjonTab = document.createElement("DOKVERSJON.TAB");
+        journalpost.appendChild(dokVersjonTab);
+
+        Element dokVersjon = document.createElement("DOKVERSJON");
+        dokVersjonTab.appendChild(dokVersjon);
+
+        createElement("VE.DOKFORMAT", "DOCX", dokVersjon, document);
+
+        createElement("VE.FILREF", wordDocFileName, dokVersjon, document);
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domSource = new DOMSource(document);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        StreamResult result = new StreamResult(baos);
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+        transformer.transform(domSource, result);
+        return baos.toByteArray();
+    }
+
     public List<EtterlevelseArkiv> setStatusToArkivert() {
         return GenericStorage.to(repo.updateStatus(EtterlevelseArkivStatus.BEHANDLER_ARKIVERING.name(), EtterlevelseArkivStatus.ARKIVERT.name()), EtterlevelseArkiv.class);
     }
@@ -169,6 +264,10 @@ public class EtterlevelseArkivService extends DomainService<EtterlevelseArkiv> {
 
     public List<EtterlevelseArkiv> setStatusWithBehandlingsId(String newStatus, String behandlingsId) {
         return GenericStorage.to(repo.updateStatusWithBehandlingsId(newStatus, behandlingsId), EtterlevelseArkiv.class);
+    }
+
+    public List<EtterlevelseArkiv> setStatusWithEtterlevelseDokumentasjonId(String newStatus, String etterlevelseDokumentasjonId) {
+        return GenericStorage.to(repo.updateStatusWithEtterlevelseDokumentasjonId(newStatus, etterlevelseDokumentasjonId), EtterlevelseArkiv.class);
     }
 
     public EtterlevelseArkiv save(EtterlevelseArkivRequest request) {
