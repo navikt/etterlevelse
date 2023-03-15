ffe6377ac15ea783235df2da17d69adb9c84ae7f
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-03-15 12:27:36 +0100

    ADD: empty behandling placeholder

--- apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseFieldResolver.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseFieldResolver.java
@@ -5,11 +5,14 @@ import graphql.schema.DataFetchingEnvironment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.data.etterlevelse.behandling.dto.Behandling;
+import no.nav.data.etterlevelse.common.domain.ExternalCode;
 import no.nav.data.etterlevelse.etterlevelse.dto.EtterlevelseResponse;
 import no.nav.data.etterlevelse.graphql.DataLoaderReg;
+import no.nav.data.integration.team.dto.TeamResponse;
 import org.dataloader.DataLoader;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
@@ -21,7 +24,17 @@ public class EtterlevelseFieldResolver implements GraphQLResolver<EtterlevelseRe
         if(etterlevelse.getBehandling() != null && !etterlevelse.getBehandlingId().equals("")) {
             return loader.load(etterlevelse.getBehandlingId());
         }else {
-            return null;
+            CompletableFuture<Behandling> legacy_data = new CompletableFuture<>();
+            legacy_data.complete(Behandling.builder()
+                    .id("LEGACY_DATA")
+                    .nummer(0)
+                    .navn("LEGACY_DATA")
+                    .overordnetFormaal(ExternalCode.builder().shortName("LEGACY_DATA").build())
+                    .system(ExternalCode.builder().code("LEGACY_DATA").shortName("LEGACY_DATA").build())
+                    .avdeling(ExternalCode.builder().code("LEGACY_DATA").shortName("LEGACY_DATA").build())
+                    .teamsData(List.of(TeamResponse.builder().id("LEGACY_DATA").name("LEGACY_DATA").build()))
+                    .build());
+            return legacy_data;
         }
     }
 }
