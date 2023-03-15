35242b47142a90ae7ba45126885d769cbe3d39ef
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-03-15 11:58:09 +0100

    UPDATE: add check for behandling id
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseFieldResolver.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseFieldResolver.java
@@ -4,7 +4,6 @@ import graphql.kickstart.tools.GraphQLResolver;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import no.nav.data.etterlevelse.behandling.BehandlingService;
 import no.nav.data.etterlevelse.behandling.dto.Behandling;
 import no.nav.data.etterlevelse.etterlevelse.dto.EtterlevelseResponse;
 import no.nav.data.etterlevelse.graphql.DataLoaderReg;
@@ -17,9 +16,12 @@ import java.util.concurrent.CompletableFuture;
 @Component
 @RequiredArgsConstructor
 public class EtterlevelseFieldResolver implements GraphQLResolver<EtterlevelseResponse> {
-
     public CompletableFuture<Behandling> behandling(EtterlevelseResponse etterlevelse, DataFetchingEnvironment env) {
         DataLoader<String, Behandling> loader = env.getDataLoader(DataLoaderReg.BEHANDLING);
-        return loader.load(etterlevelse.getBehandlingId());
+        if(etterlevelse.getBehandling() != null || !etterlevelse.getBehandlingId().equals("")) {
+            return loader.load(etterlevelse.getBehandlingId());
+        }else {
+            return null;
+        }
     }
 }
