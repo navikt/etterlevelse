d5b252f1f5b7aed2144a37b473155aa4616ccfc9
Shayan <shayan.yazdanmehr@nav.no>
2023-04-18 14:23:01 +0200

    Adding logic for utgått kraver
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
@@ -60,7 +60,29 @@ public class EtterlevelseDokumentasjonFieldResolver implements GraphQLResolver<E
 
         List<KravResponse> krav;
         List<KravResponse> irrelevantKrav;
-        List<KravResponse> utgaattKrav = convert(kravService.getByFilter(KravFilter.builder().status(List.of("UTGAATT")).build()), Krav::toResponse);
+        List<KravResponse> utgaattKrav = convert(kravService.getByFilter(KravFilter.builder()
+                .status(List.of("UTGAATT"))
+                .etterlevelseDokumentasjonId(etterlevelseDokumentasjon.getId().toString())
+                .gjeldendeKrav(false)
+                .build()), Krav::toResponse);
+
+//        if (utgaateKraverGraphQLResponse && utgaateKraverGraphQLResponse.krav.content.length > 0) {
+//            utgaatKravData = utgaateKraverGraphQLResponse.krav.content
+//            for (let x = utgaatKravData.length - 1; x > 0; x--) {
+//                if (utgaatKravData[x].kravNummer === utgaatKravData[x - 1].kravNummer && utgaatKravData[x].kravVersjon > utgaatKravData[x - 1].kravVersjon) {
+//                    utgaatKravData.splice(x - 1, 1)
+//                }
+//            }
+//        }
+
+        if (!utgaattKrav.isEmpty()) {
+            for (int x = utgaattKrav.size() - 1; x > 0; x--) {
+                if (utgaattKrav.get(x).getKravNummer() == utgaattKrav.get(x - 1).getKravNummer() && utgaattKrav.get(x).getKravVersjon() > utgaattKrav.get(x - 1).getKravVersjon()) {
+                    utgaattKrav.subList(x - 1, 1);
+                }
+            }
+        }
+
         if (etterlevelseDokumentasjon.isKnyttetTilVirkemiddel() && (etterlevelseDokumentasjon.getVirkemiddelId() != null)) {
             krav = convert(kravService.findForEtterlevelseDokumentasjon(etterlevelseDokumentasjon.getId().toString(), etterlevelseDokumentasjon.getVirkemiddelId()), Krav::toResponse);
             irrelevantKrav = convert(kravService.findForEtterlevelseDokumentasjonIrrelevans(etterlevelseDokumentasjon.getId().toString(), etterlevelseDokumentasjon.getVirkemiddelId()), Krav::toResponse);
@@ -84,7 +106,6 @@ public class EtterlevelseDokumentasjonFieldResolver implements GraphQLResolver<E
         //filtering for only newest version for utgaatt krav
 
 
-
         return EtterlevelseDokumentasjonStats.builder()
                 .fyltKrav(fylt)
                 .ikkeFyltKrav(ikkeFylt)
@@ -95,7 +116,7 @@ public class EtterlevelseDokumentasjonFieldResolver implements GraphQLResolver<E
                         .fyltKrav(filter(fylt, k -> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
                         .ikkeFyltKrav(filter(ikkeFylt, k -> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
                         .irrelevantKrav(filter(irrelevant, k -> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
-                        .utgaattKrav(filter(utgaattKrav, k-> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
+                        .utgaattKrav(filter(utgaattKrav, k -> safeStream(k.getRegelverk()).anyMatch(r -> r.getLov().getCode().equals(c.getCode()))))
                         .build()))
                 .build();
     }
