e28b54540c6f67ae4584db2250919e0885a57d48
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-06-05 13:50:20 +0200

    DEBUG: testing web security config allow all

--- apps/backend/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
+++ apps/backend/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
@@ -54,8 +54,6 @@ public class WebSecurityConfig {
                     "/graphql*/**"
             );
 
-            http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
-
             allowGetAndOptions(http,
                     "/settings/**",
                     "/codelist/**",
@@ -75,35 +73,36 @@ public class WebSecurityConfig {
                     "/etterlevelsearkiv/**",
                     "/virkemiddel/**"
             );
-//
-//            adminOnly(http,
-//                    "/audit/**",
-//                    "/settings/**",
-//                    "/codelist/**",
-//                    "/export/codelist/**",
-//                    "/etterlevelse/update/behandlingid/**",
-//                    "/etterlevelsearkiv/status/arkivert",
-//                    "/etterlevelsearkiv/admin/update"
-//            );
-//
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/krav/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
-//
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/tilbakemelding/**").hasAnyRole(AppRole.WRITE.name(), AppRole.ADMIN.name(), AppRole.KRAVEIER.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/tilbakemelding/status/**").hasAnyRole(AppRole.ADMIN.name(), AppRole.KRAVEIER.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/export/**").hasAnyRole(AppRole.WRITE.name(), AppRole.ADMIN.name(), AppRole.KRAVEIER.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/kravprioritering/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelse/**").hasAnyRole(AppRole.WRITE.name(), AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelsedokumentasjon/**").hasAnyRole(AppRole.WRITE.name(), AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelsemetadata/**").hasAnyRole(AppRole.ADMIN.name(), AppRole.WRITE.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/behandling/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name(), AppRole.WRITE.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/melding/**").hasAnyRole(AppRole.ADMIN.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelsearkiv/**").hasAnyRole(AppRole.WRITE.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelse/update/behandlingid/**").hasAnyRole(AppRole.ADMIN.name()));
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/virkemiddel/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
-//
-//            http.authorizeHttpRequests(auth -> auth.requestMatchers("/logout").authenticated());
-//
-//            http.authorizeHttpRequests(auth -> auth.anyRequest().hasRole(AppRole.WRITE.name()));
+
+            adminOnly(http,
+                    "/audit/**",
+                    "/settings/**",
+                    "/codelist/**",
+                    "/export/codelist/**",
+                    "/etterlevelse/update/behandlingid/**",
+                    "/etterlevelsearkiv/status/arkivert",
+                    "/etterlevelsearkiv/admin/update"
+            );
+
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/krav/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
+
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/tilbakemelding/**").hasAnyRole(AppRole.WRITE.name(), AppRole.ADMIN.name(), AppRole.KRAVEIER.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/tilbakemelding/status/**").hasAnyRole(AppRole.ADMIN.name(), AppRole.KRAVEIER.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/export/**").hasAnyRole(AppRole.WRITE.name(), AppRole.ADMIN.name(), AppRole.KRAVEIER.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/kravprioritering/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelse/**").hasAnyRole(AppRole.WRITE.name(), AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelsedokumentasjon/**").hasAnyRole(AppRole.WRITE.name(), AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelsemetadata/**").hasAnyRole(AppRole.ADMIN.name(), AppRole.WRITE.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/behandling/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name(), AppRole.WRITE.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/melding/**").hasAnyRole(AppRole.ADMIN.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelsearkiv/**").hasAnyRole(AppRole.WRITE.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/etterlevelse/update/behandlingid/**").hasAnyRole(AppRole.ADMIN.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/virkemiddel/**").hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
+
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/logout").authenticated());
+
+            http.authorizeHttpRequests(auth -> auth.anyRequest().hasRole(AppRole.WRITE.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers("/graphql**/**").permitAll());
         }
         return http.build();
     }
