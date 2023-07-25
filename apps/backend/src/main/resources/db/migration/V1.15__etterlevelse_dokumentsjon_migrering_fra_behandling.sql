65b4e5db276b879324d52af496f8537886741e08
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-07-25 11:13:18 +0200

    UPDATE: migration script to include required fields
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/src/main/resources/db/migration/V1.15__etterlevelse_dokumentsjon_migrering_fra_behandling.sql
+++ apps/backend/src/main/resources/db/migration/V1.15__etterlevelse_dokumentsjon_migrering_fra_behandling.sql
@@ -1,5 +1,12 @@
-INSERT INTO GENERIC_STORAGE(TYPE, DATA)
-  SELECT 'EtterlevelseDokumentasjon' , new_data from (
+INSERT INTO GENERIC_STORAGE(ID, TYPE, CREATED_BY, CREATED_DATE, LAST_MODIFIED_BY, LAST_MODIFIED_DATE, VERSION, DATA)
+  SELECT uuid_in(overlay(overlay(md5(random()::text || ':' || clock_timestamp()::text) placing '4' from 13) placing to_hex(floor(random()*(11-8+1) + 8)::int)::text from 17)::cstring) ,
+  'EtterlevelseDokumentasjon',
+  'MIGRATION(ADMIN)',
+   now() ,
+  'MIGRATION(ADMIN)',
+   now() ,
+   0 ,
+   new_data from (
   SELECT DISTINCT DATA ->> 'behandlingId' AS behandlingId,
   jsonb_build_object('behandlingIds', ARRAY[DATA -> 'behandlingId'], 'title', '', 'irrelevansFor', array[]::VARCHAR[]) AS new_data
   FROM GENERIC_STORAGE
