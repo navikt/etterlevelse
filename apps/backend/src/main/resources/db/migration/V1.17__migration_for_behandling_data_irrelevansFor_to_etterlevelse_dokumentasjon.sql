3ba1b21f9130e20b7c69d8e7965682d857b90742
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-07-25 12:12:18 +0200

    UPDATE: migration script

--- apps/backend/src/main/resources/db/migration/V1.17__migration_for_behandling_data_irrelevansFor_to_etterlevelse_dokumentasjon.sql
+++ apps/backend/src/main/resources/db/migration/V1.17__migration_for_behandling_data_irrelevansFor_to_etterlevelse_dokumentasjon.sql
@@ -1,7 +1,7 @@
 UPDATE GENERIC_STORAGE
-SET DATA = jsonb_set(GENERIC_STORAGE.data, '{irrelevansFor}', temp_table.new_data::jsonb, false)
+SET DATA = jsonb_set(data, '{irrelevansFor}', temp_table.new_data::jsonb, true)
     FROM (
-      SELECT to_jsonb(data->'irrelevansFor') as new_data, * FROM GENERIC_STORAGE where type = 'BehandlingData'
+      SELECT to_jsonb(data->'irrelevansFor') as new_data, data as temp_data FROM GENERIC_STORAGE where type = 'BehandlingData'
     ) temp_table
 
-WHERE GENERIC_STORAGE.data->'behandlingIds' ?| array[temp_table.data->>'behandlingId'] and GENERIC_STORAGE.type = 'EtterlevelseDokumentasjon';
+WHERE GENERIC_STORAGE.data->'behandlingIds' ?| array[temp_table.temp_data->>'behandlingId'] and GENERIC_STORAGE.type = 'EtterlevelseDokumentasjon';
