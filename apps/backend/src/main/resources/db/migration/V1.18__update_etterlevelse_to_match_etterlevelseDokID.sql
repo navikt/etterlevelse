ecdf20ac9bdd2eb4f84ba1b78ac4d16204447013
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-07-26 13:04:31 +0200

    UPDATE: migration script

--- apps/backend/src/main/resources/db/migration/V1.18__update_etterlevelse_to_match_etterlevelseDokID.sql
+++ apps/backend/src/main/resources/db/migration/V1.18__update_etterlevelse_to_match_etterlevelseDokID.sql
@@ -4,4 +4,4 @@ SET DATA = jsonb_set(GENERIC_STORAGE.data, '{etterlevelseDokumentasjonId}', temp
       SELECT to_jsonb(id) as new_id, * FROM GENERIC_STORAGE where type = 'EtterlevelseDokumentasjon'
     ) temp_data
 
-WHERE temp_data.data->'behandlingIds' ?| array[GENERIC_STORAGE.data->>'behandlingId'] and GENERIC_STORAGE.type = 'etterlevelse';
+WHERE temp_data.data->'behandlingIds' ?| array[GENERIC_STORAGE.data->>'behandlingId'] and type = 'Etterlevelse';
