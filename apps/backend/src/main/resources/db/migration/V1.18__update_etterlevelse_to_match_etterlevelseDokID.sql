5a135385d16df41e2b5d408ced8ab566f48e0c41
Shayan <shayan.yazdanmehr@nav.no>
2023-07-26 12:53:26 +0200

    create EtterlevelseDokumentasjon from etterlevelse
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>

---
+++ apps/backend/src/main/resources/db/migration/V1.18__update_etterlevelse_to_match_etterlevelseDokID.sql
@@ -0,0 +1,7 @@
+UPDATE GENERIC_STORAGE
+SET DATA = jsonb_set(GENERIC_STORAGE.data, '{etterlevelseDokumentasjonId}', temp_data.new_id::jsonb, true)
+    FROM (
+      SELECT to_jsonb(id) as new_id, * FROM GENERIC_STORAGE where type = 'EtterlevelseDokumentasjon'
+    ) temp_data
+
+WHERE temp_data.data->'behandlingIds' ?| array[GENERIC_STORAGE.data->>'behandlingId'] and GENERIC_STORAGE.type = 'etterlevelse';
