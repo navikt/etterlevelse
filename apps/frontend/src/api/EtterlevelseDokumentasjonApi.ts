a9e835203b6136b70f427b9b1e4c7febc14665da
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-07-31 12:44:54 +0200

    UPDATE: axios request method
    Co-authored-by:  PaulMartinKokseter<Paul.martin.kokseter@nav.no>

--- apps/frontend/src/api/EtterlevelseDokumentasjonApi.ts
+++ apps/frontend/src/api/EtterlevelseDokumentasjonApi.ts
@@ -135,5 +135,5 @@ export const etterlevelseDokumentasjonSchema = () =>
 
 // SKAL SLETTE ETTER MIGRERING BÅDE I DEV OG PROD
 export const oppdatereTittelOgTeams = async () => {
-  return (await axios.get<PageResponse<EtterlevelseDokumentasjon>>(`${env.backendBaseUrl}/etterlevelsedokumentasjon/admin/update/title/team`)).data.content
+  return (await axios.put<PageResponse<EtterlevelseDokumentasjon>>(`${env.backendBaseUrl}/etterlevelsedokumentasjon/admin/update/title/team`)).data.content
 }
