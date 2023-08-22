6b9f740b334e6afcd462d9ef3c2df04fc05f383d
K153697 <paul.martin.kokseter@nav.no>
2023-08-22 12:49:16 +0200

    BUGFIX: Changed key for etterlevelsemetadata again
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: Lord Andre Groseth <andregroseth@hotmail.com>

--- apps/frontend/src/components/behandlingsTema/KravCard.tsx
+++ apps/frontend/src/components/behandlingsTema/KravCard.tsx
@@ -42,7 +42,7 @@ export const KravCard = (props: { krav: KravEtterlevelseData; noStatus?: boolean
         setEtterlevelseMetadata(
           mapEtterlevelseMetadataToFormValue({
             id: 'ny',
-            etterlevelseDokumentasjonId: props.behandlingId,
+            behandlingId: props.behandlingId,
             kravNummer: props.krav.kravNummer,
             kravVersjon: props.krav.kravVersjon,
           }),
