2e57e9cfd9baf1a0949b03bf485ec385a4e4694c
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-09-22 16:07:46 +0200

    UPDATE: add debugging

--- apps/backend/src/main/java/no/nav/data/etterlevelse/statistikk/StatistikkService.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/statistikk/StatistikkService.java
@@ -216,6 +216,7 @@ public class StatistikkService {
 
         if(aktivertDato == null && krav.getStatus() == KravStatus.AKTIV) {
             List<AuditVersion> kravLog = auditVersionRepository.findByTableIdOrderByTimeDesc(krav.getId().toString());
+            log.info(kravLog.toString());
             List<AuditResponse> kravAudits = new AuditLogResponse(krav.getId().toString(), convert(kravLog, AuditVersion::toResponse))
                     .getAudits().stream().filter(audit -> {
 
