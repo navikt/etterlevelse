d136fa3e4b64054163f38c6aac5a380814fc149a
Kent D <kent.daleng@nav.no>
2023-09-22 09:06:15 +0200

    update some more links

--- apps/frontend/src/components/common/TeamName.tsx
+++ apps/frontend/src/components/common/TeamName.tsx
@@ -1,22 +1,18 @@
 import { useTeam } from '../../api/TeamApi'
-import React from 'react'
 import { teamKatTeamLink } from '../../util/config'
-import { StyledLink } from 'baseui/link'
 import { Block } from 'baseui/block'
 import { theme } from '../../util'
-import { ettlevColors } from '../../util/theme'
 import { StyleObject } from 'styletron-react'
 import _ from 'lodash'
-import { ExternalLinkWrapper } from './RouteLink'
+import { Link } from '@navikt/ds-react'
 
 export const TeamName = (props: { id: string; link?: boolean; fontColor?: string; style?: StyleObject }) => {
   const [name] = useTeam()(props.id)
-  const customStyle = { color: props.fontColor ? props.fontColor : ettlevColors.green600 }
-  const mergedStyle = _.merge(customStyle, props.style)
+  
   return props.link ? (
-    <StyledLink rel="noopener noreferrer" target={'_blank'} href={teamKatTeamLink(props.id)} $style={mergedStyle}>
-      name
-    </StyledLink>
+    <Link rel="noopener noreferrer" target={'_blank'} href={teamKatTeamLink(props.id)}>
+      {name}
+    </Link>
   ) : (
     <>{name}</>
   )
