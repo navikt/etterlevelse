55868dd14f023de6ad1b6673e623da92bac5dd57
Lord Andre Groseth <andregroseth@hotmail.com>
2023-10-16 13:45:17 +0200

    Update to use dss

--- apps/frontend/src/components/common/Portrait.tsx
+++ apps/frontend/src/components/common/Portrait.tsx
@@ -8,9 +8,8 @@ export const Portrait = (props: { ident: string; size?: string }) => {
   const [loading, setLoading] = useState(true)
   const [image] = useState(personImageLink(props.ident))
   const [error, setError] = useState<boolean>(false)
-  const size = props.size || '42px'
   return (
-    <Block width={size} height={size}>
+    <div className="w-11 h-11" >
       {loading && <Spinner size="100%" />}
       {!error ? (
         <img
@@ -43,6 +42,6 @@ export const Portrait = (props: { ident: string; size?: string }) => {
           }}
         />
       )}
-    </Block>
+    </div>
   )
 }
