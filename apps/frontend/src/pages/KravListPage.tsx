9ee3472a83c77d5f21ae61013d63ee0ebae7a3ef
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-10-18 11:29:38 +0200

    UPDATE: styling and default width
    Co-authored-by: Lord Andre Groseth <andregroseth@hotmail.com>
    Co-authored-by: shayany <shayan.yazdanmehr@nav.no>

--- apps/frontend/src/pages/KravListPage.tsx
+++ apps/frontend/src/pages/KravListPage.tsx
@@ -1,21 +1,21 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 import CustomizedBreadcrumbs from '../components/common/CustomizedBreadcrumbs'
 import CustomizedTabs from '../components/common/CustomizedTabs'
 import RouteLink from '../components/common/RouteLink'
-import {user} from '../services/User'
+import { user } from '../services/User'
 import moment from 'moment'
-import {ettlevColors} from '../util/theme'
-import {Krav, KravQL} from '../constants'
-import {codelist, ListName} from '../services/Codelist'
-import {AllKrav} from '../components/kravList/AllKrav'
-import {SistRedigertKrav} from '../components/kravList/SisteRedigertKrav'
-import {TemaList} from '../components/kravList/TemaList'
+import { ettlevColors } from '../util/theme'
+import { Krav, KravQL } from '../constants'
+import { codelist, ListName } from '../services/Codelist'
+import { AllKrav } from '../components/kravList/AllKrav'
+import { SistRedigertKrav } from '../components/kravList/SisteRedigertKrav'
+import { TemaList } from '../components/kravList/TemaList'
 import StatusView from '../components/common/StatusTag'
-import {useNavigate, useParams} from 'react-router-dom'
-import {Helmet} from 'react-helmet'
-import {ampli} from '../services/Amplitude'
-import {BodyLong, BodyShort, Button, Heading, Label, LinkPanel, Skeleton} from "@navikt/ds-react";
-import {PlusIcon} from "@navikt/aksel-icons";
+import { useNavigate, useParams } from 'react-router-dom'
+import { Helmet } from 'react-helmet'
+import { ampli } from '../services/Amplitude'
+import { BodyLong, BodyShort, Button, Heading, Label, LinkPanel, Skeleton, Spacer } from '@navikt/ds-react'
+import { PlusIcon } from '@navikt/aksel-icons'
 
 type Section = 'siste' | 'alle' | 'tema'
 
@@ -33,27 +33,27 @@ export const sortKrav = (kravene: KravQL[]) => {
 }
 
 export const KravListPage = () => {
-  ampli.logEvent('sidevisning', {side: 'Kraveier side', sidetittel: 'Forvalte og opprette krav'})
+  ampli.logEvent('sidevisning', { side: 'Kraveier side', sidetittel: 'Forvalte og opprette krav' })
 
   return (
-    <div className={"w-full pb-52"} id="content">
+    <div className="w-full pb-52" id="content">
       <Helmet>
-        <meta charSet="utf-8"/>
+        <meta charSet="utf-8" />
         <title>Forvalte og opprette krav</title>
       </Helmet>
-      <div className={"w-full flex justify-center"}>
-        <div className={"w-full max-w-7xl"}>
-          <div className={"pt-6"}>
-            <CustomizedBreadcrumbs currentPage="Forvalte og opprette krav"/>
-            <div className={"flex"}>
-              <div className={"flex-1"}>
-                <Heading className={"mt-0"} size="xlarge">Forvalte og opprette krav</Heading>
+      <div className="w-full flex justify-center">
+        <div className="w-full max-w-7xl">
+          <div className="pt-6">
+            <CustomizedBreadcrumbs currentPage="Forvalte og opprette krav" />
+            <div className="flex">
+              <div className="flex-1">
+                <Heading className="mt-0" size="xlarge">Forvalte og opprette krav</Heading>
               </div>
 
-              <div className={"flex justify-end"}>
+              <div className="flex justify-end">
                 {user.isKraveier() && (
-                  <RouteLink hideUnderline href={'/krav/ny'}>
-                    <Button iconPosition={"left"} icon={<PlusIcon/>} size={"medium"}>
+                  <RouteLink hideUnderline href="/krav/ny">
+                    <Button iconPosition="left" icon={<PlusIcon />} size="medium">
                       Nytt krav
                     </Button>
                   </RouteLink>
@@ -64,10 +64,10 @@ export const KravListPage = () => {
         </div>
       </div>
 
-      <div className={"flex justify-center w-full"}>
-        <div className={"w-full max-w-7xl"}>
-          <div className={"pt-6"}>
-            <KravTabs/>
+      <div className="flex justify-center w-full">
+        <div className="w-full max-w-7xl">
+          <div className="pt-6">
+            <KravTabs />
           </div>
         </div>
       </div>
@@ -75,53 +75,33 @@ export const KravListPage = () => {
   )
 }
 
-export const KravPanels = ({kravene, loading}: { kravene?: KravQL[] | Krav[]; loading?: boolean }) => {
-  if (loading) return <Skeleton variant={"rectangle"}/>
+export const KravPanels = ({ kravene, loading }: { kravene?: KravQL[] | Krav[]; loading?: boolean }) => {
+  if (loading) return <Skeleton variant="rectangle" />
   return (
-    <div className={"mb-2.5"}>
+    <div className="mb-2.5">
       {kravene &&
         kravene.map((k, index) => {
           const lov = codelist.getCode(ListName.LOV, k.regelverk[0]?.lov?.code)
           const tema = codelist.getCode(ListName.TEMA, lov?.data?.tema)
           return (
-            <div className={"mb-0"} key={k.id}>
-              {index !== 0 && (
-                <div className={"w-full flex justify-center"}>
-                  <div className={"flex w-11/12 h-px"}/>
-                </div>
-              )}
+            <div className="mb-0" key={k.id}>
               <LinkPanel
-                //useDescriptionUnderline
                 href={`/krav/${k.kravNummer}/${k.kravVersjon}`}
-                /*beskrivelse={<Label>{k.navn}</Label>}
-                rightBeskrivelse={!!k.changeStamp.lastModifiedDate ? `Sist endret: ${moment(k.changeStamp.lastModifiedDate).format('ll')}` : ''}
-                rightTitle={tema && tema.shortName ? tema.shortName : ''}
-                statusText={<StatusView status={k.status} />}
-                overrides={{
-                  Block: {
-                    style: {
-                      ':hover': { boxShadow: 'none' },
-                      ...borderStyle('hidden'),
-                    },
-                  },
-                }}*/
-
               >
-                <LinkPanel.Title className={"flex justify-between bg-blue-400"}>
-                  <div className={"flex justify-between"}>
-                    <div>
-                      <BodyShort size={"small"}>
-                        K{k.kravNummer}.{k.kravVersjon}
-                      </BodyShort>
-                      <BodyLong><Label>{k.navn}</Label></BodyLong>
-                    </div>
-                    <div>
-                      <StatusView status={k.status}/>
-                    </div>
-                    <div className={"justify-items-end"}>
-                      <BodyShort size={"medium"}>{tema && tema.shortName ? tema.shortName : ''}</BodyShort>
-                      <BodyShort size={"small"}>{!!k.changeStamp.lastModifiedDate ? `Sist endret: ${moment(k.changeStamp.lastModifiedDate).format('ll')}` : ''}</BodyShort>
-                    </div>
+                <LinkPanel.Title className="flex items-center">
+                  <div className="max-w-xl">
+                    <BodyShort size="small">
+                      K{k.kravNummer}.{k.kravVersjon}
+                    </BodyShort>
+                    <BodyLong><Label>{k.navn}</Label></BodyLong>
+                  </div>
+                  <Spacer />
+                  <div className="mr-5">
+                    <StatusView status={k.status} />
+                  </div>
+                  <div className="w-44">
+                    <BodyShort size="small" className="break-words">{tema && tema.shortName ? tema.shortName : ''}</BodyShort>
+                    <BodyShort size="small">{!!k.changeStamp.lastModifiedDate ? `Sist endret: ${moment(k.changeStamp.lastModifiedDate).format('ll')}` : ''}</BodyShort>
                   </div>
                 </LinkPanel.Title>
               </LinkPanel>
@@ -155,17 +135,17 @@ const KravTabs = () => {
         {
           key: 'siste',
           title: 'Sist endret av meg',
-          content: <SistRedigertKrav/>,
+          content: <SistRedigertKrav />,
         },
         {
           key: 'tema',
           title: 'Tema',
-          content: <TemaList/>,
+          content: <TemaList />,
         },
         {
           key: 'alle',
           title: 'Alle krav',
-          content: <AllKrav/>,
+          content: <AllKrav />,
         },
       ]}
     />
