73c6271bb9e7d8e93508e214f0027de8527a81d2
K153697 <paul.martin.kokseter@nav.no>
2023-10-19 14:40:58 +0200

    Fjernet baseui

--- apps/frontend/src/pages/NotFound.tsx
+++ apps/frontend/src/pages/NotFound.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react'
 import { useLocation } from 'react-router-dom'
-import { Block } from 'baseui/block'
-import { theme } from '../util'
-import { ParagraphLarge } from 'baseui/typography'
 import { intl } from '../util/intl/intl'
 import notFound from '../resources/notfound.svg'
-import { maxPageWidth } from '../util/theme'
 import { Helmet } from 'react-helmet'
 import { ampli } from '../services/Amplitude'
+import {Heading} from "@navikt/ds-react";
 
 const NotFound = () => {
   const location = useLocation()
@@ -15,18 +12,18 @@ const NotFound = () => {
   ampli.logEvent('sidevisning', { side: 'NotFound', sidetittel: '404 not found' })
 
   return (
-    <Block id="content" overrides={{ Block: { props: { role: 'main' } } }} maxWidth={maxPageWidth} width="100%">
+    <div className="max-w-7xl w-full" role="main" id="content">
       <Helmet>
         <meta charSet="utf-8" />
         <title>404 not found</title>
       </Helmet>
-      <Block paddingLeft="40px" paddingRight="40px" width="calc(100%-80px)" display="flex" justifyContent="center" alignContent="center" marginTop={theme.sizing.scale2400}>
-        <ParagraphLarge>
+      <div className="pl-10 pr-10">
+        <Heading className="pt-10 pb-10" size="medium">
           {intl.pageNotFound} - {location.pathname}
-        </ParagraphLarge>
+        </Heading>
         <img src={notFound} alt={intl.pageNotFound} style={{ maxWidth: '65%' }} />
-      </Block>
-    </Block>
+      </div>
+    </div>
   )
 }
 
