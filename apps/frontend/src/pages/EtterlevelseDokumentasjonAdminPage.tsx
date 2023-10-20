ed75def290df2f9919b93ba4acf45f4d21e66e9c
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-10-20 14:29:52 +0200

    UPDATE: page to use dss

--- apps/frontend/src/pages/EtterlevelseDokumentasjonAdminPage.tsx
+++ apps/frontend/src/pages/EtterlevelseDokumentasjonAdminPage.tsx
@@ -1,71 +1,60 @@
-import { Block } from 'baseui/block'
-import { Button } from 'baseui/button'
-import { HeadingXXLarge, LabelLarge } from 'baseui/typography'
 import { Helmet } from 'react-helmet'
-import { Layout2 } from '../components/scaffold/Page'
-import { ettlevColors, maxPageWidth } from '../util/theme'
 import { deleteEtterlevelseDokumentasjon } from '../api/EtterlevelseDokumentasjonApi'
-import CustomizedInput from '../components/common/CustomizedInput'
 import { useState } from 'react'
-import { borderColor } from '../components/common/Style'
 import { UpdateMessage } from './EtterlevelseAdminPage'
+import CustomizedBreadcrumbs from '../components/common/CustomizedBreadcrumbs'
+import { Button, Heading, TextField } from '@navikt/ds-react'
 
 export const EtterlevelseDokumentasjonAdminPage = () => {
   const [etterlevelseDokumentasjonId, setEtterlevelseDokumentasjonId] = useState('')
   const [updateMessage, setUpdateMessage] = useState('')
+  
   return (
-    <Layout2
-      headerBackgroundColor={ettlevColors.grey25}
-      childrenBackgroundColor={ettlevColors.grey25}
-      currentPage="Administrere Dokumentasjon"
-      mainHeader={
-        <Block maxWidth={maxPageWidth} width="100%" display={'flex'} justifyContent="flex-start">
-          <Helmet>
-            <meta charSet="utf-8" />
-            <title>Administrere Dokumentasjon</title>
-          </Helmet>
-          <HeadingXXLarge marginTop="0">Administrere Dokumentasjon</HeadingXXLarge>
-        </Block>
-      }
-    >
-      <Block marginTop="20px">
-        <LabelLarge>Slett dokumentasjon med uid</LabelLarge>
-        <Block display="flex">
-          <CustomizedInput
-            value={etterlevelseDokumentasjonId}
-            placeholder="Dokumentasjon UID"
-            onChange={(e) => {
-              setEtterlevelseDokumentasjonId(e.target.value)
-            }}
-            overrides={{
-              Root: {
-                style: {
-                  ...borderColor(ettlevColors.grey200),
-                  marginRight: '5px',
-                },
-              },
-            }}
-          />
-          <Button
-            disabled={!etterlevelseDokumentasjonId}
-            onClick={() => {
-              setUpdateMessage('')
-              deleteEtterlevelseDokumentasjon(etterlevelseDokumentasjonId)
-                .then(() => {
-                  setUpdateMessage('Sletting vellykket for dokumentasjon med uid: ' + etterlevelseDokumentasjonId)
-                  setEtterlevelseDokumentasjonId('')
-                })
-                .catch((e) => {
-                  setUpdateMessage('Oppdatering mislykket, error: ' + e)
-                })
-            }}
-          >
-            Slett
-          </Button>
-        </Block>
-        <UpdateMessage message={updateMessage} />
-      </Block>
-    </Layout2>
+    <div className="w-full" id="content" role="main">
+      <div className="w-full flex justify-center items-center flex-col mt-6">
+        <div className="w-full max-w-7xl px-8">
+          <div className="flex-1 justify-start flex">
+            <CustomizedBreadcrumbs currentPage="Administrere Etterlevelse Dokumentasjon" />
+          </div>
+          <div>
+            <Helmet>
+              <meta charSet="utf-8" />
+              <title>Administrere Etterlevelse Dokumentasjon</title>
+            </Helmet>
+            <Heading size="xlarge">Administrere Etterlevelse Dokumentasjon</Heading>
+          </div>
+
+          <div className="mt-5">
+            <div className="flex items-end">
+              <TextField
+                label="Slett etterlevelse dokumentasjon med uid"
+                placeholder='Etterlevelse dokumentasjon UID'
+                onChange={(e) => setEtterlevelseDokumentasjonId(e.target.value)}
+                className="w-full mr-3"
+              />
+              <Button
+                disabled={!etterlevelseDokumentasjonId}
+                variant="secondary"
+                onClick={() => {
+                  setUpdateMessage('')
+                  deleteEtterlevelseDokumentasjon(etterlevelseDokumentasjonId)
+                    .then(() => {
+                      setUpdateMessage('Sletting vellykket for etterlevelses med uid: ' + etterlevelseDokumentasjonId)
+                      setEtterlevelseDokumentasjonId('')
+                    })
+                    .catch((e) => {
+                      setUpdateMessage('Oppdatering mislykket, error: ' + e)
+                    })
+                }}
+              >
+                Slett
+              </Button>
+            </div>
+          </div>
+          <UpdateMessage message={updateMessage} />
+        </div>
+      </div>
+    </div>
   )
 }
 export default EtterlevelseDokumentasjonAdminPage
