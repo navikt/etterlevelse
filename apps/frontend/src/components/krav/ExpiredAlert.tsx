b43dd43f0545a99f4bce54b2f9d3a4e171ae9b6b
Shayan <shayan.yazdanmehr@nav.no>
2023-10-26 15:00:26 +0200

    Work in progress DS
    Co-authored-by: JeremiahUy <jeremiah.augie.uy@nav.no>
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>

--- apps/frontend/src/components/krav/ExpiredAlert.tsx
+++ apps/frontend/src/components/krav/ExpiredAlert.tsx
@@ -1,42 +1,24 @@
-import { Block } from 'baseui/block'
-import { ParagraphMedium } from 'baseui/typography'
-import { KravStatus, KravVersjon } from '../../constants'
-import { theme } from '../../util'
-import { ettlevColors } from '../../util/theme'
-import CustomizedLink from '../common/CustomizedLink'
-import { borderColor, borderRadius, borderStyle, borderWidth, padding } from '../common/Style'
-import { warningAlert } from '../Images'
-import { kravStatus } from '../../pages/KravPage'
+import {KravStatus, KravVersjon} from '../../constants'
+import {kravStatus} from '../../pages/KravPage'
+import {Alert, BodyLong, Link} from "@navikt/ds-react";
 
 const ExpiredAlert = ({ alleKravVersjoner, statusName }: { alleKravVersjoner: KravVersjon[]; statusName?: KravStatus }) => (
-  <Block
-    width="fit-content"
-    display="flex"
-    backgroundColor={ettlevColors.warning50}
-    $style={{
-      ...padding('12px', '16px'),
-      ...borderColor('#D47B00'),
-      ...borderWidth('1px'),
-      ...borderStyle('solid'),
-      ...borderRadius('4px'),
-    }}
-  >
-    <img src={warningAlert} alt="warning icon" />
-    <ParagraphMedium marginLeft={theme.sizing.scale500} marginTop="0px" marginBottom="0px">
+  <Alert variant={"warning"} className={"w-fit"}>
+    <BodyLong className={"ml-3"}>
       Dette er et {statusName ? `${kravStatus(statusName).toLocaleLowerCase()} krav` : 'utgått krav'}.
       {alleKravVersjoner.length > 1 ? (
         <>
           {' '}
           Gjeldende versjon:{' '}
-          <CustomizedLink href={`/krav/${alleKravVersjoner[0].kravNummer}/${alleKravVersjoner[0].kravVersjon}`}>
+          <Link href={`/krav/${alleKravVersjoner[0].kravNummer}/${alleKravVersjoner[0].kravVersjon}`}>
             K{alleKravVersjoner[0].kravNummer}.{alleKravVersjoner[0].kravVersjon}
-          </CustomizedLink>
+          </Link>
         </>
       ) : (
         ''
       )}
-    </ParagraphMedium>
-  </Block>
+    </BodyLong>
+  </Alert>
 )
 
 export default ExpiredAlert
