07d7fbd1b9d63958cf49de051532fcad35709946
Shayan <shayan.yazdanmehr@nav.no>
2023-10-27 10:46:15 +0200

    Refactoring DataText

--- apps/frontend/src/components/common/DataText.tsx
+++ apps/frontend/src/components/common/DataText.tsx
@@ -1,22 +1,17 @@
 import {default as React, ReactNode} from 'react'
-import {Block, Responsive} from 'baseui/block'
-import {Heading, Label} from "@navikt/ds-react";
+import {BodyShort, Heading, Label} from "@navikt/ds-react";
 
 type DataTextProps = {
   label?: string
   text?: false | string | string[]
   children?: ReactNode
   hideComponent?: boolean
-  compact?: boolean
   notFlexed?: boolean
   header?: boolean
-  labelWidth?: string
-  display?: Responsive<any>
 }
 
 const DataText = (props: DataTextProps) => {
   if (props.hideComponent) return null
-  const labelWidth = props.labelWidth ? props.labelWidth : '12rem'
 
   const getLabel = () => {
     if (props.header) {
@@ -35,11 +30,10 @@ const DataText = (props: DataTextProps) => {
   }
 
   return (
-    <Block display={props.display ? props.display : props.notFlexed ? 'block' : 'flex'} marginBottom={props.compact ? '.5rem' : '2rem'} width="100%">
-      {/*<div className={`${props.display ? props.display : props.notFlexed ? 'block' : 'flex'} ${props.compact ? 'mb-2' : 'mb-8'} w-full`}>*/}
+      <div className={`${props.notFlexed ? 'block' : 'flex'} w-full`}>
       {props.label && getLabel()}
-      <Block font="ParagraphMedium">{props.children}</Block>
-    </Block>
+      <BodyShort>{props.children}</BodyShort>
+      </div>
   )
 }
 
