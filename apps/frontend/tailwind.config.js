e733b3da19f96dcfc09501ed974e7c7b362d80a5
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-11-03 11:01:17 +0100

    UPDATE: page breakpoints

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,12 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': '479px',
+      // => @media (min-width: 479px) { ... }
+
+      'md': '767px',
+      // => @media (min-width: 767px) { ... }
+
+      'lg': '1023px',
+      // => @media (min-width: 1023px) { ... }
+
+      'xl': '1279px'
+      // => @media (min-width: 1279px) { ... }
+    },
     extend: {},
   },
   plugins: [],
-  presets: [require("@navikt/ds-tailwind")]
+  presets: [require('@navikt/ds-tailwind')]
 }
 
