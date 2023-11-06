6c18863b712bb937f2c16ccd8d2b9b295361cd42
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-11-06 11:11:37 +0100

    UPDATE: code cleanup

--- apps/frontend/src/pages/KravPage.tsx
+++ apps/frontend/src/pages/KravPage.tsx
@@ -1,26 +1,26 @@
-import {useParams} from 'react-router-dom'
-import {deleteKrav, getKravByKravNummer, KravId as KravIdQueryVariables, KravIdParams, kravMapToFormVal} from '../api/KravApi'
-import React, {useEffect, useRef, useState} from 'react'
-import {Krav, KravId, KravQL, KravStatus, KravVersjon} from '../constants'
-import {ViewKrav} from '../components/krav/ViewKrav'
-import {EditKrav} from '../components/krav/EditKrav'
-import {user} from '../services/User'
-import {FormikProps} from 'formik'
-import {DeleteItem} from '../components/DeleteItem'
-import {useQuery} from '@apollo/client'
-import {Tilbakemeldinger} from '../components/krav/tilbakemelding/Tilbakemelding'
-import {ettlevColors} from '../util/theme'
-import {useLocationState, useQueryParam} from '../util/hooks'
-import {gql} from '@apollo/client/core'
+import { useParams } from 'react-router-dom'
+import { deleteKrav, getKravByKravNummer, KravId as KravIdQueryVariables, KravIdParams, kravMapToFormVal } from '../api/KravApi'
+import React, { useEffect, useRef, useState } from 'react'
+import { Krav, KravId, KravQL, KravStatus, KravVersjon } from '../constants'
+import { ViewKrav } from '../components/krav/ViewKrav'
+import { EditKrav } from '../components/krav/EditKrav'
+import { user } from '../services/User'
+import { FormikProps } from 'formik'
+import { DeleteItem } from '../components/DeleteItem'
+import { useQuery } from '@apollo/client'
+import { Tilbakemeldinger } from '../components/krav/tilbakemelding/Tilbakemelding'
+import { useLocationState, useQueryParam } from '../util/hooks'
+import { gql } from '@apollo/client/core'
 import ExpiredAlert from '../components/krav/ExpiredAlert'
-import CustomizedBreadcrumbs, {breadcrumbPaths} from '../components/common/CustomizedBreadcrumbs'
-import {codelist, ListName, TemaCode} from '../services/Codelist'
-import {Helmet} from 'react-helmet'
+import CustomizedBreadcrumbs, { breadcrumbPaths } from '../components/common/CustomizedBreadcrumbs'
+import { codelist, ListName, TemaCode } from '../services/Codelist'
+import { Helmet } from 'react-helmet'
 import Etterlevelser from '../components/krav/Etterlevelser'
-import {ampli} from '../services/Amplitude'
-import {Markdown} from '../components/common/Markdown'
-import {InformationSquareIcon, PencilIcon, PlusIcon} from '@navikt/aksel-icons'
-import {BodyLong, BodyShort, Button, Heading, Skeleton, Tabs, Tag} from '@navikt/ds-react'
+import { ampli } from '../services/Amplitude'
+import { Markdown } from '../components/common/Markdown'
+import { InformationSquareIcon, PencilIcon, PlusIcon } from '@navikt/aksel-icons'
+import { BodyLong, BodyShort, Button, Detail, Heading, Tabs, Tag } from '@navikt/ds-react'
+import { LoadingSkeleton } from '../components/common/LoadingSkeleton'
 
 export const kravNumView = (it: { kravVersjon: number; kravNummer: number }) => `K${it.kravNummer}.${it.kravVersjon}`
 export const kravName = (krav: Krav) => `${kravNumView(krav)} ${krav.navn}`
@@ -44,7 +44,7 @@ type LocationState = { tab: Section; avdelingOpen?: string }
 
 const getQueryVariableFromParams = (params: Readonly<Partial<KravIdParams>>) => {
   if (params.id) {
-    return {id: params.id}
+    return { id: params.id }
   } else if (params.kravNummer && params.kravVersjon) {
     return {
       kravNummer: parseInt(params.kravNummer),
@@ -69,11 +69,11 @@ export const KravPage = () => {
     fetchPolicy: 'no-cache',
   })
 
-  const {state, navigate, changeState} = useLocationState<LocationState>()
+  const { state, navigate, changeState } = useLocationState<LocationState>()
   const tilbakemeldingId = useQueryParam('tilbakemeldingId')
   const [tab, setTab] = useState<Section>(!!tilbakemeldingId ? 'tilbakemeldinger' : state?.tab || 'krav')
 
-  const [alleKravVersjoner, setAlleKravVersjoner] = React.useState<KravVersjon[]>([{kravNummer: 0, kravVersjon: 0, kravStatus: 'Utkast'}])
+  const [alleKravVersjoner, setAlleKravVersjoner] = React.useState<KravVersjon[]>([{ kravNummer: 0, kravVersjon: 0, kravStatus: 'Utkast' }])
   const [kravTema, setKravTema] = useState<TemaCode>()
   const [newVersionWarning, setNewVersionWarning] = useState<boolean>(false)
   const [newKrav, setNewKrav] = useState<boolean>(false)
@@ -84,7 +84,7 @@ export const KravPage = () => {
         if (resp.content.length) {
           const alleVersjoner = resp.content
             .map((k) => {
-              return {kravVersjon: k.kravVersjon, kravNummer: k.kravNummer, kravStatus: k.status}
+              return { kravVersjon: k.kravVersjon, kravNummer: k.kravNummer, kravStatus: k.status }
             })
             .sort((a, b) => (a.kravVersjon > b.kravVersjon ? -1 : 1))
 
@@ -106,14 +106,14 @@ export const KravPage = () => {
     if (krav && kravTema) {
       ampli.logEvent('sidevisning', {
         side: 'Krav side',
-        sidetittel: `${kravNumView({kravNummer: krav?.kravNummer, kravVersjon: krav?.kravVersjon})} ${krav.navn}`,
+        sidetittel: `${kravNumView({ kravNummer: krav?.kravNummer, kravVersjon: krav?.kravVersjon })} ${krav.navn}`,
         section: kravTema?.shortName.toString(),
       })
     }
   }, [krav, kravTema])
 
   useEffect(() => {
-    if (tab !== state?.tab) changeState({tab})
+    if (tab !== state?.tab) changeState({ tab })
   }, [tab])
 
   useEffect(() => {
@@ -144,8 +144,8 @@ export const KravPage = () => {
 
   const newVersion = () => {
     if (!krav) return
-    setKravId({id: krav.id, kravVersjon: krav.kravVersjon})
-    setKrav({...krav, id: '', kravVersjon: krav.kravVersjon + 1, nyKravVersjon: true})
+    setKravId({ id: krav.id, kravVersjon: krav.kravVersjon })
+    setKrav({ ...krav, id: '', kravVersjon: krav.kravVersjon + 1, nyKravVersjon: true })
     setEdit(true)
     setNewVersionWarning(true)
   }
@@ -174,134 +174,113 @@ export const KravPage = () => {
 
   return (
     <div className="w-full" key={'K' + krav?.kravNummer + '/' + krav?.kravVersjon} id="content">
-      {kravLoading &&
-        <div className="grid gap-2">
-          <Skeleton variant="text" width="60%"/>
-          <Skeleton variant="rectangle" width="100%" height={30}/>
-          <Skeleton variant="rounded" width="100%" height={40}/>
-        </div>
-      }
+      {kravLoading && <LoadingSkeleton header="Krav" />}
       {!kravLoading && (
-        <div className="flex w-full justify-center pb-8">
+        <div className="flex w-full pb-8">
           {krav?.id && (
             <Helmet>
-              <meta charSet="utf-8"/>
+              <meta charSet="utf-8" />
               <title>
-                {kravNumView({kravNummer: krav?.kravNummer, kravVersjon: krav?.kravVersjon})} {krav.navn}
+                {kravNumView({ kravNummer: krav?.kravNummer, kravVersjon: krav?.kravVersjon })} {krav.navn}
               </title>
             </Helmet>
           )}
-          <div className="w-full max-w-7xl">
-            <div className="flex flex-col justify-center mb-10">
-              <div className="flex items-center w-full justify-center mt-5">
-                <div className="w-full flex flex-col">
-                  {krav?.id && (
-                    <CustomizedBreadcrumbs
-                      fontColor={ettlevColors.grey25}
-                      currentPage={kravNumView({kravNummer: krav?.kravNummer, kravVersjon: krav?.kravVersjon})}
-                      paths={getBreadcrumPaths()}
-                    />
-                  )}
-                  <div className="flex gap-2 items-center">
-                    {krav && (
-                      <div className="flex w-full justify-end">
-                        <Tag variant="neutral">
-                          <BodyShort>
-                            Status: {kravStatus(krav.status)}
-                          </BodyShort>
-                        </Tag>
-                      </div>
-                    )}
-                    {krav?.id && ((user.isKraveier() && !hasKravExpired()) || user.isAdmin()) && (
-                      <div className="flex flex-1 justify-end">
-                        {krav.status === KravStatus.AKTIV && (
-                          <Button onClick={newVersion} variant="tertiary">
-                            <div className="flex flex-nowrap items-center whitespace-nowrap gap-1">
-                              <PlusIcon area-label="" aria-hidden className="text-2xl" />
-                              Ny versjon
-                            </div>
-                          </Button>
-                        )}
-                        {(user.isAdmin() || krav.status !== KravStatus.AKTIV) && <DeleteItem fun={() => deleteKrav(krav.id)} redirect={'/kravliste'}/>}
-                        <Button variant="tertiary" onClick={() => setEdit(!edit)}>
-                          <div className="flex flex-nowrap items-center gap-1">
-                            <PencilIcon  area-label="" aria-hidden className="text-2xl" />
-                            Rediger
-                          </div>
-                        </Button>
-                      </div>
-                    )}
+          <div className="flex flex-col mb-10 w-full">
+            <div className="w-full flex flex-col">
+              {krav?.id && (
+                <CustomizedBreadcrumbs
+                  currentPage={kravNumView({ kravNummer: krav?.kravNummer, kravVersjon: krav?.kravVersjon })}
+                  paths={getBreadcrumPaths()}
+                />
+              )}
+              <div className="flex gap-2 items-center justify-end w-full">
+                {krav && (
+                  <div className="flex w-full justify-end">
+                    <Tag variant="neutral">
+                      <BodyShort>
+                        Status: {kravStatus(krav.status)}
+                      </BodyShort>
+                    </Tag>
                   </div>
-                </div>
+                )}
+                {krav?.id && ((user.isKraveier() && !hasKravExpired()) || user.isAdmin()) && (
+                  <div className="flex flex-1 justify-end">
+                    {krav.status === KravStatus.AKTIV && (
+                      <Button onClick={newVersion} variant="tertiary">
+                        <div className="flex flex-nowrap items-center whitespace-nowrap gap-1">
+                          <PlusIcon area-label="" aria-hidden className="text-2xl" />
+                          Ny versjon
+                        </div>
+                      </Button>
+                    )}
+                    {(user.isAdmin() || krav.status !== KravStatus.AKTIV) && <DeleteItem fun={() => deleteKrav(krav.id)} redirect={'/kravliste'} />}
+                    <Button variant="tertiary" onClick={() => setEdit(!edit)}>
+                      <div className="flex flex-nowrap items-center gap-1">
+                        <PencilIcon area-label="" aria-hidden className="text-2xl" />
+                        Rediger
+                      </div>
+                    </Button>
+                  </div>
+                )}
               </div>
             </div>
 
-            <div className="flex">
-              <div className="w-full max-w-7xl">
-                <div>
-                  {krav && krav?.kravNummer !== 0 ? kravNumView(krav) : 'Ny'}
+            <div className="w-full">
+              <BodyShort>
+                {krav && krav?.kravNummer !== 0 ? kravNumView(krav) : 'Ny'}
+              </BodyShort>
+              <Heading size="medium" level="1">
+                {krav && krav?.navn ? krav.navn : 'Ny'}{' '}
+              </Heading>
+
+              {krav?.varselMelding && (
+                <div className="w-fit flex justify-center items-center">
+                  <InformationSquareIcon fontSize="1.5rem" />
+                  <BodyLong className="ml-1">
+                    {krav.varselMelding}
+                  </BodyLong>
                 </div>
-                <Heading size="large" className="mt-4">
-                  {krav && krav?.navn ? krav.navn : 'Ny'}{' '}
-                </Heading>
+              )}
 
-                {krav?.varselMelding && (
-                  <div className="w-fit flex justify-center items-center">
-                    <InformationSquareIcon fontSize="1.5rem"/>
-                    <BodyLong className="ml-1">
-                      {krav.varselMelding}
-                    </BodyLong>
-                  </div>
-                )}
-
-                {hasKravExpired() && krav && <ExpiredAlert alleKravVersjoner={alleKravVersjoner} statusName={krav.status}/>}
-              </div>
+              {hasKravExpired() && krav && <ExpiredAlert alleKravVersjoner={alleKravVersjoner} statusName={krav.status} />}
             </div>
           </div>
         </div>
       )}
 
       {krav && !kravLoading && (
-        <div className="w-full">
-
-          <div className="flex w-full justify-center">
-            <div className="w-full max-w-7xl">
-                  <Heading size="large">Hensikten med kravet</Heading>
-                  <Markdown p1 sources={Array.isArray(krav.hensikt) ? krav.hensikt : [krav.hensikt]}/>
-            </div>
-          </div>
-
-          <div className="flex justify-center">
-            <div className="w-full max-w-7xl">
-              <Tabs
-                defaultValue={tab}
-                onChange={(s) => setTab(s as Section)}
-              >
-                <Tabs.List>
-                  <Tabs.Tab
-                    value="krav"
-                    label="Hvordan etterleve?"
-                  />
-                  <Tabs.Tab
-                    value="etterlevelser"
-                    label="Eksempler på etterlevelse"
-                  />
-                  <Tabs.Tab
-                    value="tilbakemeldinger"
-                    label="Spørsmål og svar"
-                  />
-                </Tabs.List>
-                <Tabs.Panel value="krav">
-                  <ViewKrav krav={krav} alleKravVersjoner={alleKravVersjoner}/>
-                </Tabs.Panel>
-                <Tabs.Panel value="etterlevelser">
-                  <Etterlevelser loading={etterlevelserLoading} krav={krav}/>
-                </Tabs.Panel>
-                <Tabs.Panel value="tilbakemeldinger">
-                  <Tilbakemeldinger krav={krav} hasKravExpired={hasKravExpired()}/>
-                </Tabs.Panel>
-              </Tabs>
-            </div>
+        <div>
+          <Heading size="medium" level="1">Hensikten med kravet</Heading>
+          <Markdown sources={Array.isArray(krav.hensikt) ? krav.hensikt : [krav.hensikt]} />
+          <div className="w-full">
+            <Tabs
+              defaultValue={tab}
+              onChange={(s) => setTab(s as Section)}
+            >
+              <Tabs.List>
+                <Tabs.Tab
+                  value="krav"
+                  label="Hvordan etterleve?"
+                />
+                <Tabs.Tab
+                  value="etterlevelser"
+                  label="Eksempler på etterlevelse"
+                />
+                <Tabs.Tab
+                  value="tilbakemeldinger"
+                  label="Spørsmål og svar"
+                />
+              </Tabs.List>
+              <Tabs.Panel value="krav">
+                <ViewKrav krav={krav} alleKravVersjoner={alleKravVersjoner} />
+              </Tabs.Panel>
+              <Tabs.Panel value="etterlevelser">
+                <Etterlevelser loading={etterlevelserLoading} krav={krav} />
+              </Tabs.Panel>
+              <Tabs.Panel value="tilbakemeldinger">
+                <Tilbakemeldinger krav={krav} hasKravExpired={hasKravExpired()} />
+              </Tabs.Panel>
+            </Tabs>
           </div>
         </div>
       )}
@@ -323,7 +302,7 @@ export const KravPage = () => {
                 reloadKrav()
               }
             } else if (krav.nyKravVersjon) {
-              setKrav({...krav, id: kravId!.id, kravVersjon: kravId!.kravVersjon})
+              setKrav({ ...krav, id: kravId!.id, kravVersjon: kravId!.kravVersjon })
             }
             setEdit(false)
             setNewVersionWarning(false)
