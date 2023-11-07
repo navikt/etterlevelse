60e839b5fb43998b2a292e1c043a219f5041b7f2
Kent D <kent.daleng@nav.no>
2023-11-07 15:15:14 +0100

    new header for dokumentasjon

--- apps/frontend/src/components/common/TeamName.tsx
+++ apps/frontend/src/components/common/TeamName.tsx
@@ -2,7 +2,7 @@ import { useTeam } from '../../api/TeamApi'
 import { teamKatTeamLink } from '../../util/config'
 import { Block } from 'baseui/block'
 import { theme } from '../../util'
-import { Link } from '@navikt/ds-react'
+import { Detail, Link } from '@navikt/ds-react'
 
 export const TeamName = (props: { id: string; link?: boolean }) => {
   const [name] = useTeam()(props.id)
@@ -24,4 +24,26 @@ export const Teams = (props: { teams: string[]; link?: boolean; list?: boolean }
       </Block>
     ))}
   </Block>
+)
+
+
+export const TeamNameV2 = (props: { id: string; link?: boolean }) => {
+  const [name] = useTeam()(props.id)
+
+  return props.link ? (
+    <Link className="text-sm" rel="noopener noreferrer" target={'_blank'} href={teamKatTeamLink(props.id)}>
+      {name} (åpnes i ny fane)
+    </Link>
+  ) : (
+    <>{name}</>
+  )
+}
+
+export const TeamsV2 = (props: { teams: string[]; link?: boolean; list?: boolean }) => (
+  <div className="flex flex-wrap gap-2 items-center">
+    <Detail className="font-bold">Team:</Detail>
+    {props.teams.map((t, idx) => (
+        <TeamNameV2 key={`team_${idx}`} id={t} link={props.link} />
+    ))}
+  </div>
 )
