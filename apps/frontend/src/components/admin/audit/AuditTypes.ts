d1753058008a71e624c1f065e3c1846a4a1f9355
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-11-10 15:02:33 +0100

    ADD: etterlevelse metadata to selectable audit object

--- apps/frontend/src/components/admin/audit/AuditTypes.ts
+++ apps/frontend/src/components/admin/audit/AuditTypes.ts
@@ -30,6 +30,7 @@ export enum ObjectType {
   BehandlingData = 'BehandlingData',
   Melding = 'Melding',
   EtterlevelseArkiv = 'EtterlevelseArkiv',
+  EtterlevelseMetadata = 'EtterlevelseMetadata'
 }
 
 export type NavigableItem = ObjectType | ListName.RELEVANS | ListName.UNDERAVDELING | ListName.TEMA | ListName.LOV
