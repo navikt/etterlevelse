d36ce2a8d1d34292340d8a58d56f13f7fcefd40b
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-11-15 11:57:50 +0100

    update: logging to info

--- apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
@@ -90,8 +90,8 @@ public class EtterlevelseDokumentasjonFieldResolver implements GraphQLResolver<E
             }
         });
 
-        log.debug("etterlevelse data: " + etterlevelser);
-        log.debug("Krav data: " + krav);
+        log.info("etterlevelse data: " + etterlevelser);
+        log.info("Krav data: " + krav);
 
         var fylt = filter(krav, k -> etterlevelser.stream().anyMatch(e -> e.isEtterleves() && e.kravId().equals(k.kravId())));
         var ikkeFylt = filter(krav, k -> !fylt.contains(k));
