8dd9acbc39079be1483bd77537b63a9af58010f3
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-11-15 10:33:02 +0100

    UPDATE: filtering of dokumentasjon stats

--- apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/resolver/EtterlevelseDokumentasjonFieldResolver.java
@@ -66,8 +66,6 @@ public class EtterlevelseDokumentasjonFieldResolver implements GraphQLResolver<E
 
     public EtterlevelseDokumentasjonStats stats(EtterlevelseDokumentasjonResponse etterlevelseDokumentasjon) {
 
-        var etterlevelser = etterlevelseService.getByEtterlevelseDokumentasjon(etterlevelseDokumentasjon.getId().toString());
-
         List<KravResponse> krav;
         List<KravResponse> irrelevantKrav;
         List<KravResponse> utgaattKrav = convert(kravService.getByFilter(KravFilter.builder()
@@ -90,14 +88,10 @@ public class EtterlevelseDokumentasjonFieldResolver implements GraphQLResolver<E
             }
         });
 
-
-        var fylt = filter(krav, k -> etterlevelser.stream().anyMatch(e -> e.isEtterleves() && e.kravId().equals(k.kravId())));
-        var ikkeFylt = filter(krav, k -> !fylt.contains(k));
+        var fylt = filter(krav, k -> !k.getEtterlevelser().isEmpty());
+        var ikkeFylt = filter(krav, k -> k.getEtterlevelser().isEmpty());
 
         var irrelevant = filter(irrelevantKrav, i -> !fylt.contains(i) && !ikkeFylt.contains(i));
-
-        //filtering for only newest version for utgaatt krav
-
 
         return EtterlevelseDokumentasjonStats.builder()
                 .fyltKrav(fylt)
