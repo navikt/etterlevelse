c2cea0fcddcc794868369bffee3ae7e6a31178b2
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-12-07 13:43:27 +0100

    REFACTOR: implement general page layout component

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,4 +1,3 @@
-import { Narrow, Wide } from '../components/scaffold/Page'
 import { useEffect, useState } from 'react'
 import { Markdown } from '../components/common/Markdown'
 import { AlertType, Melding, MeldingStatus, MeldingType } from '../constants'
@@ -8,6 +7,7 @@ import { Button, Heading, Link } from '@navikt/ds-react'
 import EditEtterlevelseDokumentasjonModal from '../components/etterlevelseDokumentasjon/edit/EditEtterlevelseDokumentasjonModal'
 import { TemaPanels } from './TemaoversiktPage'
 import { user } from '../services/User'
+import { PageLayout } from '../components/scaffold/Page'
 
 export const MainPage = () => {
   const [forsideVarsel, setForsideVarsle] = useState<Melding>()
@@ -24,41 +24,44 @@ export const MainPage = () => {
   }, [])
 
   return (
-    <div className="w-full pb-52" id="content" role="main">
-      <div className="bg-blue-50 p-10">
-        <div className="flex flex-col pd">
-          <Heading className="flex justify-center" size="xlarge">
-            Etterlevelse i NAV
-          </Heading>
-          <span className="flex justify-center">Forstå og dokumentér</span>
-        </div>
-        <div className="bg-white mt-8 p-8 shadow-md shadow-slate-900 shadow-[#00000040]">
-          <Heading size="medium">Etterlevelse i NAV</Heading>
-          <span>
-            For å dokumentere etterlevelse må du opprette et etterlevelsesdokument. Du vil da se hvilke krav som gjelder for din løsning og kan dokumentere hvordan løsningen
-            etterlever kravene.
-          </span>
-          <div className="mt-8 flex justify-end">
-            <div className="mr-4">
-              <EditEtterlevelseDokumentasjonModal />
+    <PageLayout noPadding fullWidth>
+      <div className="bg-blue-50 py-10 flex justify-center">
+        <div className="max-w-7xl w-full px-2">
+          <div className="flex flex-col">
+            <Heading className="flex justify-center" size="xlarge">
+              Etterlevelse i NAV
+            </Heading>
+            <span className="flex justify-center">Forstå og dokumentér</span>
+          </div>
+          <div className="bg-white mt-8 p-8 shadow-md shadow-slate-900 shadow-[#00000040]">
+            <Heading size="medium">Etterlevelse i NAV</Heading>
+            <span>
+              For å dokumentere etterlevelse må du opprette et etterlevelsesdokument. Du vil da se hvilke krav som gjelder for din løsning og kan dokumentere hvordan løsningen
+              etterlever kravene.
+            </span>
+            <div className="mt-8 flex justify-end">
+              <div className="mr-4">
+                <EditEtterlevelseDokumentasjonModal />
+              </div>
+              <Link href="/dokumentasjoner">
+                <Button as="a" variant="tertiary">
+                  Alle etterlevelsesdokumenter
+                </Button>
+              </Link>
             </div>
-            <Link href="/dokumentasjoner">
-              <Button as="a" variant="tertiary">
-                Alle etterlevelsesdokumenter
-              </Button>
-            </Link>
           </div>
         </div>
       </div>
 
       <div className="flex flex-col items-center w-full bg-no-repeat bg-[left_4.5rem] bg-[url('/src/resources/icons/main-page-background-icon.svg')]">
-        <div className="mt-8">
-          <TemaPanels />
-        </div>
+        <div className="max-w-7xl w-full px-2 pb-6">
+          <div className="mt-8 w-full px-8">
+            <TemaPanels />
+          </div>
 
-        <div className="mt-16 mb-32" id="forsideVarselMelding">
+
           {forsideVarsel?.meldingStatus === MeldingStatus.ACTIVE && (
-            <>
+            <div className="mt-16 mb-32" id="forsideVarselMelding">
               {forsideVarsel.alertType === AlertType.INFO ? (
                 <div className="border-solid border-1 mt-16 p-8 bg-surface-info-subtle border-surface-info">
                   <Markdown source={forsideVarsel.melding} />
@@ -68,10 +71,11 @@ export const MainPage = () => {
                   <Markdown source={forsideVarsel.melding} />
                 </div>
               )}
-            </>
+            </div>
           )}
+
         </div>
       </div>
-    </div>
+    </PageLayout>
   )
 }
