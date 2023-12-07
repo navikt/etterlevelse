d38bb7c118a5eadb0dd45cc9044d323784faa8a7
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-12-07 15:44:16 +0100

    TEST: style override

--- apps/frontend/src/components/etterlevelseDokumentasjon/edit/EditEtterlevelseDokumentasjonModal.tsx
+++ apps/frontend/src/components/etterlevelseDokumentasjon/edit/EditEtterlevelseDokumentasjonModal.tsx
@@ -247,13 +247,13 @@ export const EditEtterlevelseDokumentasjonModal = (props: EditEtterlevelseDokume
                                   onChange={(value) => {
                                     value && p.push(value)
                                   }}
-                                  // styles={{
-                                  //   control: (base) => ({
-                                  //     ...base,
-                                  //     cursor: 'text',
-                                  //     height: '48px',
-                                  //   }),
-                                  // }}
+                                  styles={{
+                                    control: (base) => ({
+                                      ...base,
+                                      // cursor: 'text',
+                                      // height: '48px',
+                                    }),
+                                  }}
                                 />
                               </div>
                               <RenderTagList list={p.form.values.behandlinger.map((b: Behandling) => b.navn)} onRemove={p.remove} />
@@ -292,13 +292,13 @@ export const EditEtterlevelseDokumentasjonModal = (props: EditEtterlevelseDokume
                                   onChange={(value) => {
                                     value && p.push(value)
                                   }}
-                                  styles={{
-                                    control: (base) => ({
-                                      ...base,
-                                      cursor: 'text',
-                                      height: '48px',
-                                    }),
-                                  }}
+                                  // styles={{
+                                  //   control: (base) => ({
+                                  //     ...base,
+                                  //     cursor: 'text',
+                                  //     height: '48px',
+                                  //   }),
+                                  // }}
                                 />
                               </div>
                               <RenderTagList list={p.form.values.teamsData.map((t: Team) => t.name)} onRemove={p.remove} />
