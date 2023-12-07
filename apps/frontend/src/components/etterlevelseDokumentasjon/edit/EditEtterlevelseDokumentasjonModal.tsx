600d79a467516809b0f9c29ebe5c61d2ec0acbf8
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-12-07 15:40:44 +0100

    UNDO: changes and commented style overrides for react-select

--- apps/frontend/src/components/etterlevelseDokumentasjon/edit/EditEtterlevelseDokumentasjonModal.tsx
+++ apps/frontend/src/components/etterlevelseDokumentasjon/edit/EditEtterlevelseDokumentasjonModal.tsx
@@ -247,13 +247,13 @@ export const EditEtterlevelseDokumentasjonModal = (props: EditEtterlevelseDokume
                                   onChange={(value) => {
                                     value && p.push(value)
                                   }}
-                                  styles={{
-                                    control: (base) => ({
-                                      ...base,
-                                      // cursor: 'text',
-                                      // height: '48px',
-                                    }),
-                                  }}
+                                  // styles={{
+                                  //   control: (base) => ({
+                                  //     ...base,
+                                  //     cursor: 'text',
+                                  //     height: '48px',
+                                  //   }),
+                                  // }}
                                 />
                               </div>
                               <RenderTagList list={p.form.values.behandlinger.map((b: Behandling) => b.navn)} onRemove={p.remove} />
