4f75883d91a71768f4b0c764728abb3a5cefc749
JeremiahUy <jeremiah.augie.uy@nav.no>
2023-12-08 14:27:15 +0100

    update footer layout

--- apps/frontend/src/components/Navigation/Footer.tsx
+++ apps/frontend/src/components/Navigation/Footer.tsx
@@ -24,56 +24,58 @@ export const Footer = () => {
   }, [pageScroll])
 
   return (
-    <div className="bg-purple-400 text-white px-12 py-7 w-full mt-auto">
-      {showButtonToTop && <Button
-        className="text-white"
-        size="xsmall"
-        icon={<ArrowUpIcon aria-label="" aria-hidden />}
-        onClick={() => window.scrollTo(0, 0)}
-        variant="tertiary-neutral"
-      >
-        Til toppen
-      </Button>}
-      <div className="flex mt-11">
-        <div className=" flex flex-col">
-          <BodyShort className="text-2xl mb-5 flex">
-            <div className="mr-2.5">
-              <img src={etterlevelseLogoWhiteIcon} alt="Etterlevlese logo" aria-hidden aria-label="" />
-            </div>
-            Støtte til etterlevelse
-          </BodyShort>
-          <BodyShort>
-            2023 NAV
-          </BodyShort>
-          <BodyShort>
-            Arbeids- og velferdsetaten
-          </BodyShort>
-        </div>
-        <Spacer />
-        <div className="flex flex-col gap-4">
-          <BodyShort>
-            Nyttige lenker
-          </BodyShort>
-          <Link className="text-white" href={statusPageLink}>Status på etterlevelse i NAV</Link>
-          <Link className="text-white" href="/omstottetiletterlevelse">Om etterlevelse på Navet</Link>
-          <Link className="text-white" href={behandlingsKatalogLink}>Behandlingskatalogen</Link>
-          <Link className="text-white" href={VeilederEtterlevelseskrav}>Veileder for å skrive etterlevelseskrav</Link>
-        </div>
-        <Spacer />
-        <div className="flex flex-col gap-4">
-          <BodyShort>
-            Om nettstedet
-          </BodyShort>
-          <Link className="text-white" href={documentationLink}>Hva er Støtte til etterlevelse?</Link>
-        </div>
-        <Spacer />
-        <div className="flex flex-col gap-4">
-          <BodyShort>
-            Finn oss
-          </BodyShort>
-          <Link className="text-white" href={datajegerSlackLink}>Slack</Link>
-          <Link className="text-white" href={githubRepo}>Github</Link>
-          <Link className="text-white" href={teamInfoLink}>Teamkatalogen</Link>
+    <div className="bg-purple-400 text-white px-12 py-7 w-full mt-auto justify-center flex">
+      <div className="max-w-7xl w-full">
+        {showButtonToTop && <Button
+          className="text-white"
+          size="xsmall"
+          icon={<ArrowUpIcon aria-label="" aria-hidden />}
+          onClick={() => window.scrollTo(0, 0)}
+          variant="tertiary-neutral"
+        >
+          Til toppen
+        </Button>}
+        <div className="flex mt-11">
+          <div className=" flex flex-col">
+            <BodyShort className="text-2xl mb-5 flex">
+              <div className="mr-2.5">
+                <img src={etterlevelseLogoWhiteIcon} alt="Etterlevlese logo" aria-hidden aria-label="" />
+              </div>
+              Støtte til etterlevelse
+            </BodyShort>
+            <BodyShort>
+              2023 NAV
+            </BodyShort>
+            <BodyShort>
+              Arbeids- og velferdsetaten
+            </BodyShort>
+          </div>
+          <Spacer />
+          <div className="flex flex-col gap-4">
+            <BodyShort>
+              Nyttige lenker
+            </BodyShort>
+            <Link className="text-white" href={statusPageLink}>Status på etterlevelse i NAV</Link>
+            <Link className="text-white" href="/omstottetiletterlevelse">Om etterlevelse på Navet</Link>
+            <Link className="text-white" href={behandlingsKatalogLink}>Behandlingskatalogen</Link>
+            <Link className="text-white" href={VeilederEtterlevelseskrav}>Veileder for å skrive etterlevelseskrav</Link>
+          </div>
+          <Spacer />
+          <div className="flex flex-col gap-4">
+            <BodyShort>
+              Om nettstedet
+            </BodyShort>
+            <Link className="text-white" href={documentationLink}>Hva er Støtte til etterlevelse?</Link>
+          </div>
+          <Spacer />
+          <div className="flex flex-col gap-4">
+            <BodyShort>
+              Finn oss
+            </BodyShort>
+            <Link className="text-white" href={datajegerSlackLink}>Slack</Link>
+            <Link className="text-white" href={githubRepo}>Github</Link>
+            <Link className="text-white" href={teamInfoLink}>Teamkatalogen</Link>
+          </div>
         </div>
       </div>
     </div>
