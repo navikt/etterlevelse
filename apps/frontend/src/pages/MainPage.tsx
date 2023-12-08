f79fb5c600ff8c2a5d2db1ba5ba66204bda0186e
Lord Andre Groseth <andregroseth@hotmail.com>
2023-12-08 11:12:04 +0100

    ADD: View for mine siste dokumenterte if true
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: Rebecca Gjerstad <rebecca.soraya.gjerstad@nav.no>

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,26 +1,33 @@
 import { useEffect, useState } from 'react'
 import { Markdown } from '../components/common/Markdown'
-import { AlertType, Melding, MeldingStatus, MeldingType } from '../constants'
+import { AlertType, EtterlevelseDokumentasjonQL, Melding, MeldingStatus, MeldingType, PageResponse } from '../constants'
 import { getMeldingByType } from '../api/MeldingApi'
 import { ampli, userRoleEventProp } from '../services/Amplitude'
-import { Button, Heading, Link } from '@navikt/ds-react'
+import { Button, Heading, Link, LinkPanel } from '@navikt/ds-react'
 import EditEtterlevelseDokumentasjonModal from '../components/etterlevelseDokumentasjon/edit/EditEtterlevelseDokumentasjonModal'
 import { TemaPanels } from './TemaoversiktPage'
 import { user } from '../services/User'
 import { PageLayout } from '../components/scaffold/Page'
+import { useQuery } from '@apollo/client'
+import { Variables, query } from './MyEtterlevelseDokumentasjonerPage'
+import moment from 'moment'
 
 export const MainPage = () => {
   const [forsideVarsel, setForsideVarsle] = useState<Melding>()
+  const { data, loading: etterlevelseDokumentasjonLoading } = useQuery<{ etterlevelseDokumentasjoner: PageResponse<EtterlevelseDokumentasjonQL> }, Variables>(query, {
+    variables: { sistRedigert: 5 },
+    skip: !user.isLoggedIn(),
+  })
 
   useEffect(() => {
     ampli.logEvent('sidevisning', { side: 'Hovedside', ...userRoleEventProp })
-      ; (async () => {
-        await getMeldingByType(MeldingType.FORSIDE).then((r) => {
-          if (r.numberOfElements > 0) {
-            setForsideVarsle(r.content[0])
-          }
-        })
-      })()
+    ;(async () => {
+      await getMeldingByType(MeldingType.FORSIDE).then((r) => {
+        if (r.numberOfElements > 0) {
+          setForsideVarsle(r.content[0])
+        }
+      })
+    })()
   }, [])
 
   return (
@@ -28,28 +35,57 @@ export const MainPage = () => {
       <div className="bg-blue-50 py-10 flex justify-center">
         <div className="max-w-7xl w-full px-2">
           <div className="flex flex-col">
-            <Heading className="flex justify-center" size="xlarge">
+            <Heading className="flex justify-center" size="medium" level="1">
               Etterlevelse i NAV
             </Heading>
             <span className="flex justify-center">Forstå og dokumentér</span>
           </div>
-          <div className="bg-white mt-8 p-8 shadow-md shadow-slate-900 shadow-[#00000040]">
-            <Heading size="medium">Etterlevelse i NAV</Heading>
-            <span>
-              For å dokumentere etterlevelse må du opprette et etterlevelsesdokument. Du vil da se hvilke krav som gjelder for din løsning og kan dokumentere hvordan løsningen
-              etterlever kravene.
-            </span>
-            <div className="mt-8 flex justify-end">
-              <div className="mr-4">
-                <EditEtterlevelseDokumentasjonModal />
+          {!etterlevelseDokumentasjonLoading && (
+            <div className="bg-white mt-8 p-8 shadow-md shadow-slate-900 shadow-[#00000040]">
+              {!data?.etterlevelseDokumentasjoner.content.length && (
+                <div>
+                  <Heading size="small" level="2">
+                    Etterlevelse i NAV
+                  </Heading>
+                  <span>
+                    For å dokumentere etterlevelse må du opprette et etterlevelsesdokument. Du vil da se hvilke krav som gjelder for din løsning og kan dokumentere hvordan
+                    løsningen etterlever kravene.
+                  </span>
+                </div>
+              )}
+              {data?.etterlevelseDokumentasjoner.content.length && (
+                <div>
+                  <Heading size="small" level="2">
+                    Mine sist dokumenterte
+                  </Heading>
+                  <div className="mt-6 flex flex-col gap-2">
+                    {data.etterlevelseDokumentasjoner.content.map((etterlevelseDokumentasjon, index) => {
+                      return (
+                        <LinkPanel href={'/dokumentasjon/' + etterlevelseDokumentasjon.id} key={etterlevelseDokumentasjon.title + '_' + index}>
+                          <LinkPanel.Title>
+                            E{etterlevelseDokumentasjon.etterlevelseNummer} {etterlevelseDokumentasjon.title}
+                          </LinkPanel.Title>
+                          <LinkPanel.Description>
+                            {!!etterlevelseDokumentasjon.sistEndretEtterlevelse ? `Sist endret: ${moment(etterlevelseDokumentasjon.sistEndretEtterlevelse).format('ll')}` : ''}
+                          </LinkPanel.Description>
+                        </LinkPanel>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}
+              <div className="mt-8 flex justify-end">
+                <div className="mr-4">
+                  <EditEtterlevelseDokumentasjonModal />
+                </div>
+                <Link href="/dokumentasjoner">
+                  <Button as="a" variant="tertiary">
+                    Alle etterlevelsesdokumenter
+                  </Button>
+                </Link>
               </div>
-              <Link href="/dokumentasjoner">
-                <Button as="a" variant="tertiary">
-                  Alle etterlevelsesdokumenter
-                </Button>
-              </Link>
             </div>
-          </div>
+          )}
         </div>
       </div>
 
@@ -58,7 +94,6 @@ export const MainPage = () => {
           <div className="mt-8 w-full px-8">
             <TemaPanels />
           </div>
-
 
           {forsideVarsel?.meldingStatus === MeldingStatus.ACTIVE && (
             <div className="mt-16 mb-32" id="forsideVarselMelding">
@@ -73,7 +108,6 @@ export const MainPage = () => {
               )}
             </div>
           )}
-
         </div>
       </div>
     </PageLayout>
