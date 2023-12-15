6f93f34a91d217b72ab12c04a6df9093de38d525
beeccy <beeccy@users.noreply.github.com>
2023-12-15 12:09:16 +0100

    Forside: Fikset styling på Forstå kravene og Status i organisasjoner

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -70,8 +70,8 @@ export const MainPage = () => {
       </div>
 
       <div className="flex flex-col items-center w-full bg-no-repeat bg-contain xl:bg-cover bg-[left_4.5rem] bg-[url('/src/resources/icons/main-page-background-icon.svg')]">
-        <div className="max-w-7xl w-full px-2 pb-6">
-          <div className="flex mt-10">
+        <div className="max-w-7xl w-full px-2">
+          <div className="flex my-10">
             <ForstaKravene />
             <StatusIOrganisasjonen />
           </div>
