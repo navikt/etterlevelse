6692f598f2fab92400f814139d9a7dba9d2a27cb
beeccy <beeccy@users.noreply.github.com>
2023-12-22 10:17:35 +0100

    Test

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -12,7 +12,6 @@ import { useAwait, useAwaitUser } from './util/hooks'
 import { useNetworkStatus } from './util/network'
 import { customTheme } from './util/theme'
 import { Footer } from './components/Navigation/Footer'
-import { ampli } from './services/Amplitude'
 
 const engine = new Styletron()
 
