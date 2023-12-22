2e464db7d94fa90282bf06c341c861048fa51db0
beeccy <beeccy@users.noreply.github.com>
2023-12-22 12:33:51 +0100

    Reset file

--- apps/frontend/src/components/krav/Suksesskriterie.tsx
+++ apps/frontend/src/components/krav/Suksesskriterie.tsx
@@ -2,7 +2,7 @@ import { Suksesskriterie } from '../../constants'
 import { Markdown } from '../common/Markdown'
 import { Box, Heading, Label } from '@navikt/ds-react'
 export const SuksesskriterieCard = (props: { suksesskriterie: Suksesskriterie; num: number; totalt: number; fullWidth?: boolean }) => {
-  const { suksesskriterie, num, totalt } = props
+  const { suksesskriterie, num, totalt, fullWidth } = props
   return (
     <div className="border-b-2 border-gray-200 py-5">
       <Box>
