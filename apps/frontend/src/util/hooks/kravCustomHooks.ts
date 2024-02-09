49b887a34eb3b972815523b1e750ae51ff4b5441
beeccy <beeccy@users.noreply.github.com>
2024-02-09 11:46:09 +0100

    Lagt inn en egen krav hook, fungerer

---
+++ apps/frontend/src/util/hooks/kravCustomHooks.ts
@@ -0,0 +1,53 @@
+import { ApolloQueryResult, useQuery } from '@apollo/client'
+import { TKravId as KravIdQueryVariables, TKravIdParams } from '../../api/KravApi'
+import { TKravQL } from '../../constants'
+import { getKravWithEtterlevelseQuery } from '../../query/KravQuery'
+
+export interface IKravDataProps {
+  kravQuery: {
+    kravById: TKravQL
+  }
+  kravLoading: boolean
+  reloadKrav: Promise<
+    ApolloQueryResult<{
+      kravById: TKravQL
+    }>
+  >
+}
+
+export function GetKrav(params: Readonly<Partial<TKravIdParams>>): IKravDataProps | undefined {
+  return GetKravData(params)
+}
+
+function GetKravData(params: Readonly<Partial<TKravIdParams>>) {
+  const {
+    loading: kravLoading,
+    data: kravQuery,
+    refetch: reloadKrav,
+  } = useQuery<{ kravById: TKravQL }, KravIdQueryVariables>(getKravWithEtterlevelseQuery, {
+    variables: getQueryVariableFromParams(params),
+    skip: (!params.id || params.id === 'ny') && !params.kravNummer,
+    fetchPolicy: 'no-cache',
+  })
+
+  if (kravQuery?.kravById) {
+    const kravData = { kravQuery: kravQuery, kravLoading: kravLoading, reloadKrav: reloadKrav() }
+
+    return kravData
+  } else {
+    return undefined
+  }
+}
+
+function getQueryVariableFromParams(params: Readonly<Partial<TKravIdParams>>) {
+  if (params.id) {
+    return { id: params.id }
+  } else if (params.kravNummer && params.kravVersjon) {
+    return {
+      kravNummer: parseInt(params.kravNummer),
+      kravVersjon: parseInt(params.kravVersjon),
+    }
+  } else {
+    return undefined
+  }
+}
