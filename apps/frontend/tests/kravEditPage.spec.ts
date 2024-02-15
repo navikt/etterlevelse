9219a18b71e95b278e9fbb179e039f39c1b9234f
beeccy <beeccy@users.noreply.github.com>
2024-02-15 12:11:24 +0100

    Playwright lagt in

---
+++ apps/frontend/tests/kravEditPage.spec.ts
@@ -0,0 +1,10 @@
+// import test from '@playwright/test'
+
+// test.describe('KravEditPage', () => {
+//   test.describe('Buttons', () => {
+//     test.describe('Publiser endringer', () => {
+//       test('should submit etterlevelse', () => {})
+//       test('should navigate user to /krav/:kravNummer/:kravVersjon', () => {})
+//     })
+//   })
+// })
