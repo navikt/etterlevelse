4a6b8832e2059a985da82fd627958de192f1e2f8
beeccy <beeccy@users.noreply.github.com>
2024-02-16 12:53:46 +0100

    WIP Playwright test

--- apps/frontend/tests/kravEditPage.spec.ts
+++ apps/frontend/tests/kravEditPage.spec.ts
@@ -1,10 +1,19 @@
-// import test from '@playwright/test'
+import { test } from '@playwright/test'
 
-// test.describe('KravEditPage', () => {
-//   test.describe('Buttons', () => {
-//     test.describe('Publiser endringer', () => {
-//       test('should submit etterlevelse', () => {})
-//       test('should navigate user to /krav/:kravNummer/:kravVersjon', () => {})
-//     })
-//   })
-// })
+test.describe('KravEditPage', () => {
+  test('test', async ({ page }) => {
+    await page.goto('https://etterlevelse.intern.dev.nav.no/')
+    await page.getByText('Logg inn').click()
+    await page.getByPlaceholder('someone@example.com').fill('someone@nav.no')
+    await page.getByText('Next').click()
+    await page.goto('https://etterlevelse.intern.dev.nav.no/')
+
+    await page.goto('https://etterlevelse.intern.dev.nav.no/kravliste')
+
+    await page.getByText('Nytt krav').click()
+    await page.goto('https://etterlevelse.intern.dev.nav.no/kravliste/opprett')
+
+    // await page.goto('https://etterlevelse.intern.dev.nav.no/krav/redigering/6d52e400-69af-4f50-9d2c-cc3302d8211c')
+    // await page.goto('https://etterlevelse.intern.dev.nav.no/krav/redigering/249/1/')
+  })
+})
