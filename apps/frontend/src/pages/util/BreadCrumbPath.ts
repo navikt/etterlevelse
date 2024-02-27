4b54c436d15c0f7283f402a8f7d5b8d773496640
beeccy <beeccy@users.noreply.github.com>
2024-02-27 11:17:43 +0100

    DA BREADCRUMBS are gathered once again united!
    Co-authored-by: andre groseth <andregroseth@hotmail.com>
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

---
+++ apps/frontend/src/pages/util/BreadCrumbPath.ts
@@ -0,0 +1,16 @@
+import { IBreadCrumbPath } from '../../constants'
+
+export const kravBreadCrumbPath: IBreadCrumbPath = {
+  href: '/kravliste',
+  pathName: 'Forvalte og opprette krav',
+}
+
+export const temaBreadCrumbPath: IBreadCrumbPath = {
+  pathName: 'Forstå kravene',
+  href: '/tema',
+}
+
+export const dokumentasjonerBreadCrumbPath: IBreadCrumbPath = {
+  pathName: 'Dokumenter etterlevelse',
+  href: '/dokumentasjoner',
+}
