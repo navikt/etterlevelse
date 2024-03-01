a3821c4db07490f8865d08b8c8bb034a1b7c956f
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-03-01 14:05:48 +0100

    UPDATE: styling

--- apps/frontend/src/components/kravList/edit/components/KravPriorityPanel.tsx
+++ apps/frontend/src/components/kravList/edit/components/KravPriorityPanel.tsx
@@ -25,36 +25,38 @@ export const KravPriorityPanel = (props: IProps) => {
   }
 
   return (
-    <Box className="w-full flex" padding="2">
-      <div className="w-10 flex justify-center items-center">
-        <BodyShort>{index + 1}.</BodyShort>
-      </div>
-      <div className="max-w-lg w-full">
-        <BodyShort>
-          K{krav.kravNummer}.{krav.kravVersjon}
-        </BodyShort>
-        <Label>{krav.navn}</Label>
-      </div>
-      <div className="flex flex-col items-end w-full justify-center max-w-md">
-        <div className="flex items-center">
-          <StatusView status={krav.status} />
-          <div className="w-44 ml-2.5">
-            <BodyShort size="small">
-              {`Sist endret: ${moment(krav.changeStamp.lastModifiedDate).format('ll')}`}
-            </BodyShort>
+    <div className="border-t border-icon-subtle">
+      <Box className="w-full flex" padding="2">
+        <div className="w-8 flex justify-center items-center border-r border-icon-subtle mr-1">
+          <BodyShort>{index + 1}.</BodyShort>
+        </div>
+        <div className="max-w-lg w-full">
+          <BodyShort>
+            K{krav.kravNummer}.{krav.kravVersjon}
+          </BodyShort>
+          <Label>{krav.navn}</Label>
+        </div>
+        <div className="flex flex-col items-end w-full justify-center max-w-md">
+          <div className="flex items-center">
+            <StatusView status={krav.status} />
+            <div className="w-44 ml-2.5">
+              <BodyShort size="small">
+                {`Sist endret: ${moment(krav.changeStamp.lastModifiedDate).format('ll')}`}
+              </BodyShort>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-center items-center">
-        <RearrangeButtons
-          label="krav"
-          plassering={plassering}
-          setPlassering={setPlassering}
-          index={index}
-          arrayLength={arrayLength}
-          updateIndex={updateIndex}
-        />
-      </div>
-    </Box>
+        <div className="flex flex-1 justify-end items-center ">
+          <RearrangeButtons
+            label="krav"
+            plassering={plassering}
+            setPlassering={setPlassering}
+            index={index}
+            arrayLength={arrayLength}
+            updateIndex={updateIndex}
+          />
+        </div>
+      </Box>
+    </div>
   )
 }
