04d845958bc5f24b78678ab52514b87b95699ad8
Lord Andre Groseth <andregroseth@hotmail.com>
2024-03-05 14:49:58 +0100

    Remove: icon,  PanelLink, TeamaCardEtterlevelseDokumentasjon, TemaPage
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/common/Icon.tsx
+++ apps/frontend/src/components/common/Icon.tsx
@@ -1,18 +1,10 @@
-import { Block } from 'baseui/block'
-
 export const IconInCircle = (props: {
   icon: string
   alt: string
   size: string
   backgroundColor: string
 }) => (
-  <Block
-    $style={{
-      backgroundColor: props.backgroundColor,
-      borderRadius: '100%',
-      padding: '7.5%',
-    }}
-  >
+  <div className={`bg-[${props.backgroundColor}] rounded-full p-[7.5%]`}>
     <img src={props.icon} alt={props.alt} width={props.size} />
-  </Block>
+  </div>
 )
