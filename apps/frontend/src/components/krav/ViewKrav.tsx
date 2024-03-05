e79fd3c77ea1b86ee4bffde895e0e9d3c328395a
beeccy <beeccy@users.noreply.github.com>
2024-03-05 13:20:59 +0100

    Slettet SidePanel

--- apps/frontend/src/components/krav/ViewKrav.tsx
+++ apps/frontend/src/components/krav/ViewKrav.tsx
@@ -40,9 +40,6 @@ export const ViewKrav = ({ krav }: { krav: TKravQL }) => {
         {
           //deactivate side panel, waiting for feedback from design
         }
-        {/* <div className={"block fixed right-0"}>
-        <SidePanel />
-      </div> */}
       </div>
     </div>
   )
@@ -95,7 +92,10 @@ export const AllInfo = ({
         <LabelWrapper>
           <LabelAboveContent title={'Tidligere versjoner'} header={header}>
             {alleKravVersjoner.map((kravRelasjon, index) => {
-              if (kravRelasjon.kravVersjon && parseInt(kravRelasjon.kravVersjon.toString()) < krav.kravVersjon) {
+              if (
+                kravRelasjon.kravVersjon &&
+                parseInt(kravRelasjon.kravVersjon.toString()) < krav.kravVersjon
+              ) {
                 return (
                   <BodyShort key={'kravVersjon_list_' + index} className={'break-words'}>
                     <ExternalLink
