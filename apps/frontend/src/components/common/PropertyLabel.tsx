498b394f42053db83d63a035f7480433e8aa5f42
beeccy <beeccy@users.noreply.github.com>
2024-03-05 13:01:26 +0100

    Slettet CustomLabel const

--- apps/frontend/src/components/common/PropertyLabel.tsx
+++ apps/frontend/src/components/common/PropertyLabel.tsx
@@ -14,24 +14,6 @@ type TLabelProps = {
   fullWidth?: boolean
 } & TOr<{ children: React.ReactNode }, { markdown: string | string[]; vertical?: boolean }>
 
-export const CustomLabel = (props: TLabelProps) => {
-  if (props.hide || (empty(props.children) && empty(props.markdown))) return null
-  return (
-    <DataText label={props.title} header={props.header} labelWidth={props.labelWidth}>
-      {props.markdown ? (
-        <Markdown
-          p1={props.p1}
-          sources={Array.isArray(props.markdown) ? props.markdown : [props.markdown]}
-          vertical={props.vertical}
-          shortenLinks
-        />
-      ) : (
-        props.children
-      )}
-    </DataText>
-  )
-}
-
 export const LabelAboveContent = (props: TLabelProps) => {
   if (props.hide || (empty(props.children) && empty(props.markdown))) return null
   return (
