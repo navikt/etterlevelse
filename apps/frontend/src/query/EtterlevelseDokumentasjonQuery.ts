cbba158d2c9f8176821891c0bb49eee4fa4f489e
beeccy <beeccy@users.noreply.github.com>
2024-03-06 10:19:09 +0100

    KravQueries som kan slettes

--- apps/frontend/src/query/EtterlevelseDokumentasjonQuery.ts
+++ apps/frontend/src/query/EtterlevelseDokumentasjonQuery.ts
@@ -39,6 +39,7 @@ export const getEtterlevelseDokumentasjonListQuery = gql`
   }
 `
 
+/* Jerry sa å ikke slette ubrukte Query */
 export const getEtterlevelseDokumentasjonByRelevansQuery = gql`
   query getEtterlevelsedokumentasjon($relevans: [String!]) {
     etterlevelseDokumentasjon(filter: { relevans: $relevans }) {
