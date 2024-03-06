07b49a8dca9c9eeae93422e1e3426a31381d9ae4
beeccy <beeccy@users.noreply.github.com>
2024-03-06 09:27:18 +0100

    Slettet getStatusLabelColor const

--- apps/frontend/src/components/behandling/utils.tsx
+++ apps/frontend/src/components/behandling/utils.tsx
@@ -1,6 +1,5 @@
 import moment from 'moment'
 import { EEtterlevelseStatus } from '../../constants'
-import { ettlevColors } from '../../util/theme'
 
 export const getEtterlevelseStatus = (status?: EEtterlevelseStatus, frist?: string) => {
   switch (status) {
@@ -22,45 +21,5 @@ export const getEtterlevelseStatus = (status?: EEtterlevelseStatus, frist?: stri
       }
     default:
       return ''
-  }
-}
-
-export const getStatusLabelColor = (status: EEtterlevelseStatus) => {
-  switch (status) {
-    case EEtterlevelseStatus.UNDER_REDIGERING:
-      return {
-        background: ettlevColors.white,
-        border: '#0B483F',
-      }
-    case EEtterlevelseStatus.FERDIG:
-      return {
-        background: ettlevColors.white,
-        border: '#0B483F',
-      }
-    case EEtterlevelseStatus.IKKE_RELEVANT:
-      return {
-        background: ettlevColors.grey50,
-        border: ettlevColors.grey200,
-      }
-    case EEtterlevelseStatus.IKKE_RELEVANT_FERDIG_DOKUMENTERT:
-      return {
-        background: ettlevColors.grey50,
-        border: ettlevColors.grey200,
-      }
-    case EEtterlevelseStatus.FERDIG_DOKUMENTERT:
-      return {
-        background: ettlevColors.green50,
-        border: ettlevColors.green400,
-      }
-    case EEtterlevelseStatus.OPPFYLLES_SENERE:
-      return {
-        background: ettlevColors.warning50,
-        border: '#D47B00',
-      }
-    default:
-      return {
-        background: ettlevColors.white,
-        border: ettlevColors.green400,
-      }
   }
 }
