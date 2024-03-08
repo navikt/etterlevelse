1ee861e82565851355480c19824de069f08794ec
beeccy <beeccy@users.noreply.github.com>
2024-03-08 10:35:43 +0100

    Fikset KravEditVirkemidler, skal taes i bruk senere

--- apps/frontend/src/components/krav/Edit/KravEditVirkemidler.tsx
+++ apps/frontend/src/components/krav/Edit/KravEditVirkemidler.tsx
@@ -9,52 +9,14 @@ export const EditVirkemidler = () => {
       {/* <FieldArray name="virkemidler">
         {(fieldArrayRenderProps: FieldArrayRenderProps) => {
           return (
-            <FormControl
-              label={
-                <LabelWithTooltip
+              <div>
+                 <LabelWithTooltip
                   label={'Legg til relevante virkemiddel'}
                   tooltip={'Velg vikemiddel(er) som er relevant for kravet i nedtrekksmenyen. \n'}
                 />
-              }
-            >
-              <div>
                 <CustomizedSelect
-                  overrides={{
-                    SearchIcon: {
-                      component: () => <img src={searchIcon} alt="search icon" />,
-                      style: {
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                      },
-                    },
-                    SearchIconContainer: {
-                      style: {
-                        width: 'calc(100% - 20px)',
-                        display: 'flex',
-                        justifyContent: 'flex-end',
-                      },
-                    },
-                    IconsContainer: {
-                      style: {
-                        marginRight: '20px',
-                      },
-                    },
-                    ValueContainer: {
-                      style: {
-                        paddingLeft: '10px',
-                      },
-                    },
-                    ControlContainer: {
-                      style: {
-                        ...borderWidth('2px'),
-                      },
-                    },
-                  }}
                   labelKey={'navn'}
                   noResultsMsg={intl.emptyTable}
-                  maxDropdownHeight="350px"
-                  searchable={true}
-                  type={TYPE.search}
                   options={result}
                   placeholder={'Virkemidler'}
                   onInputChange={(event) => setSearch(event.currentTarget.value)}
@@ -63,14 +25,12 @@ export const EditVirkemidler = () => {
                     virkemiddel && fieldArrayRenderProps.push(virkemiddel)
                   }}
                   error={!!fieldArrayRenderProps.form.errors.begreper && !!fieldArrayRenderProps.form.submitCount}
-                  isLoading={loading}
                 />
                 <RenderTagList
                   list={fieldArrayRenderProps.form.values.virkemidler.map((virkemiddel: IVirkemiddel) => virkemiddel.navn)}
                   onRemove={fieldArrayRenderProps.remove}
                 />
               </div>
-            </FormControl>
           )
         }}
       </FieldArray> */}
