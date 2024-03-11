69bdc708dd219ee7b9bdcb485e411128d8dcb84c
Lord Andre Groseth <andregroseth@hotmail.com>
2024-03-11 14:21:14 +0100

    Remove: Styletron
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>>

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,8 +1,6 @@
 import { ApolloProvider } from '@apollo/client'
 import { Helmet } from 'react-helmet'
 import { BrowserRouter } from 'react-router-dom'
-import { Client as Styletron } from 'styletron-engine-atomic'
-import { Provider as StyletronProvider } from 'styletron-react'
 import AppRoutes from './AppRoutes'
 import { apolloClient } from './api/ApolloClient'
 import Header from './components/Header'
@@ -11,8 +9,6 @@ import { codelist } from './services/Codelist'
 import { useAwait, useAwaitUser } from './util/hooks/customHooks'
 import { useNetworkStatus } from './util/network'
 
-const engine = new Styletron()
-
 // ampli.logEvent('sidevisning', { sidetittel: 'Etterlevelse' })
 
 const Main = () => {
@@ -20,23 +16,21 @@ const Main = () => {
   useAwait(codelist.wait())
 
   return (
-    <StyletronProvider value={engine}>
-        <ApolloProvider client={apolloClient}>
-          <BrowserRouter window={window}>
-            <Helmet>
-              <meta charSet="utf-8" />
-              <title>Etterlevelse</title>
-            </Helmet>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter window={window}>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Etterlevelse</title>
+        </Helmet>
 
-            <div className="flex flex-col w-full items-center min-h-screen bg-white">
-              <Header />
-              <AppRoutes />
-              <Footer />
-            </div>
-          </BrowserRouter>
-          <ErrorModal />
-        </ApolloProvider>
-    </StyletronProvider>
+        <div className="flex flex-col w-full items-center min-h-screen bg-white">
+          <Header />
+          <AppRoutes />
+          <Footer />
+        </div>
+      </BrowserRouter>
+      <ErrorModal />
+    </ApolloProvider>
   )
 }
 
