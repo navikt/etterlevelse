8db3271a67018c41483860250caa8ec727b9fc8c
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-04-15 15:00:59 +0200

    UPDATE: to redirect with correct url params for dokumentation page

--- apps/frontend/src/components/etterlevelse/EtterlevelseKravView.tsx
+++ apps/frontend/src/components/etterlevelse/EtterlevelseKravView.tsx
@@ -15,7 +15,7 @@ import {
 import { FormikProps } from 'formik'
 import moment from 'moment'
 import React, { useEffect, useRef, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
 import {
   createEtterlevelse,
   getEtterlevelserByEtterlevelseDokumentasjonIdKravNumber,
@@ -92,6 +92,7 @@ export const EtterlevelseKravView = ({
     skip: !kravId.id && !kravId.kravNummer,
     fetchPolicy: 'no-cache',
   })
+  const params = useParams<{ tema?: string }>()
   const etterlevelserLoading = loading
   const [krav, setKrav] = useState<TKravQL>()
   const [nyereKrav, setNyereKrav] = React.useState<IKrav>()
@@ -495,7 +496,7 @@ export const EtterlevelseKravView = ({
                 </Button>
 
                 <Link
-                  href={'/dokumentasjon/' + etterlevelseDokumentasjonId}
+                  href={'/dokumentasjon/' + etterlevelseDokumentasjonId + '/' + params.tema}
                   className="flex w-full"
                   onClick={() => {
                     ampli.logEvent('knapp klikket', {
