38ee9dc7e5ceb963021a7163d04a4e53ab317df7
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-06-17 13:09:06 +0200

    ADD: admin page for document relation
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/api/DocumentRelationApi.ts
+++ apps/frontend/src/api/DocumentRelationApi.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { IDocumentRelation, IPageResponse } from '../constants'
+import { ERelationType, IDocumentRelation, IPageResponse } from '../constants'
 import { env } from '../util/env'
 
 export const getByDocumentRelation = async (id: string) => {
@@ -12,6 +12,23 @@ export const getDocumentRelationPage = async (pageNumber: number, pageSize: numb
       `${env.backendBaseUrl}/documentrelation?pageNumber=${pageNumber}&pageSize=${pageSize}`
     )
   ).data
+}
+
+export const getAllDocumentRelation = async () => {
+  const PAGE_SIZE = 100
+  const firstPage = await getDocumentRelationPage(0, PAGE_SIZE)
+  if (firstPage.pages === 1) {
+    return firstPage.content.length > 0 ? [...firstPage.content] : []
+  } else {
+    let allDocumentRelation: IDocumentRelation[] = [...firstPage.content]
+    for (let currentPage = 1; currentPage < firstPage.pages; currentPage++) {
+      allDocumentRelation = [
+        ...allDocumentRelation,
+        ...(await getDocumentRelationPage(currentPage, PAGE_SIZE)).content,
+      ]
+    }
+    return allDocumentRelation
+  }
 }
 
 export const deleteDocumentRelation = async (id: string) => {
@@ -43,4 +60,24 @@ function documentRelationToDocumentRelationDomainObject(
   delete domainToObject.changeStamp
   delete domainToObject.version
   return domainToObject
+}
+
+export const documentRelationMapToFormVal = (
+  documentRelation: Partial<IDocumentRelation>
+): IDocumentRelation => ({
+  id: documentRelation.id || '',
+  changeStamp: documentRelation.changeStamp || { lastModifiedDate: '', lastModifiedBy: '' },
+  version: documentRelation.version || -1,
+  fromDocument: documentRelation.fromDocument || '',
+  toDocument: documentRelation.toDocument || '',
+  RelationType: documentRelation.RelationType || ERelationType.ARVER,
+})
+
+export const dokumentRelationTypeToString = (relasjonsType: ERelationType): string => {
+  switch (relasjonsType) {
+    case ERelationType.ARVER:
+      return 'Arver fra'
+    case ERelationType.BYGGER:
+      return 'Bygger fra'
+  }
 }
