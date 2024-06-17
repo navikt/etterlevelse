28cef82d0b93943ee0dfbd0da4d820d9800d7e10
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-06-17 15:01:28 +0200

    FIX: builder problem
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/dto/EtterlevelseDokumentasjonWithRelationRequest.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/dto/EtterlevelseDokumentasjonWithRelationRequest.java
@@ -1,13 +1,15 @@
 package no.nav.data.etterlevelse.etterlevelseDokumentasjon.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 import no.nav.data.etterlevelse.documentRelation.domain.RelationType;
+
+
 @Data
-@Builder
+@EqualsAndHashCode(callSuper = true)
 @FieldNameConstants
 @NoArgsConstructor
 @AllArgsConstructor
