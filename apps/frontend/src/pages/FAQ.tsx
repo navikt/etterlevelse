e27795c9eab451ca37c58961362e1678be65f93a
Lord Andre Groseth <andregroseth@hotmail.com>
2024-07-01 14:00:05 +0200

    ADD: Etterlevelse video to FAQ page.
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/pages/FAQ.tsx
+++ apps/frontend/src/pages/FAQ.tsx
@@ -27,12 +27,16 @@ export const FAQ = () => {
     sidetittel: 'Om Støtte til etterlevelse',
     ...userRoleEventProp,
   })
-
+  /* eslint-disable jsx-a11y/media-has-caption */
   return (
     <PageLayout pageTitle="Om Støtte til etterlevelse" currentPage="Om Støtte til etterlevelse">
+      <div>
+        <video controls src="videos/EtterlevelseskravMedTeksting.mp4"></video>
+      </div>
+
       <div className="flex justify-center w-full">
         <div>
-          <div className="max-w-4xl">
+          <div className="mb-5">
             <Heading size="medium" level="1" className="mt-14 mb-8">
               Om Støtte til etterlevelse
             </Heading>
@@ -40,7 +44,7 @@ export const FAQ = () => {
             <BodyLong className="mt-0">{melding?.melding}</BodyLong>
           </div>
 
-          <div className="max-w-xl">
+          <div>
             {melding?.secondaryTittel && (
               <Heading level="2" size="small" className="mt-14 mb-6">
                 {melding?.secondaryTittel}
