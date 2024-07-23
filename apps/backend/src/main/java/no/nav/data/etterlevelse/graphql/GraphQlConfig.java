2a8a82a46b6d12e0835c936b7d992cd14b5ac688
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-07-23 14:53:05 +0200

    UPDATE: simplified config class

--- apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/GraphQlConfig.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/graphql/GraphQlConfig.java
@@ -1,7 +1,6 @@
 package no.nav.data.etterlevelse.graphql;
 
 import graphql.scalars.ExtendedScalars;
-import graphql.schema.GraphQLScalarType;
 import no.nav.data.etterlevelse.graphql.support.LocalDateTimeCoercing;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,27 +10,11 @@ import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @Configuration
 public class GraphQlConfig {
-
-    private GraphQLScalarType date() {
-        return ExtendedScalars.Date;
-    }
-
-    private GraphQLScalarType dateTime() {
-        return LocalDateTimeCoercing.createScalar();
-    }
-
-    private GraphQLScalarType nonNegativeInt() {
-        return ExtendedScalars.NonNegativeInt;
-    }
-
     @Bean
     public RuntimeWiringConfigurer runtimeWiringConfigurer() {
         return wiringBuilder -> wiringBuilder
-                .scalar(date())
-                .scalar(dateTime())
-                .scalar(nonNegativeInt());
+                .scalar(ExtendedScalars.Date)
+                .scalar(LocalDateTimeCoercing.createScalar())
+                .scalar(ExtendedScalars.NonNegativeInt);
     }
-
-
-
 }
