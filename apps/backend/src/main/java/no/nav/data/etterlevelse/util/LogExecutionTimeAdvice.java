7f759bf217f37dafabb2470155718e6e03255917
Jeremiah Uy <jeremiah.augie.uy@nav.no>
2024-07-31 11:05:22 +0200

    Aop exec time (#938)
    * First go
    
    * UPDATE: execution trigger and profile
    
    * ADD: component annotation and removed unused confic file
    
    * UPDATE: execution trigger
    
    ---------
    
    Co-authored-by: Movafagh <farjam.movafagh@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/util/LogExecutionTimeAdvice.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/util/LogExecutionTimeAdvice.java
@@ -5,13 +5,15 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
 @Aspect
 @Profile("test | local | dev")
 @Slf4j
+@Component
 public class LogExecutionTimeAdvice {
 
-    @Around("execution(public * *..*Repo*.*(..))")
+    @Around("execution(public * *..*Repo.*(..)) || execution(public * *..*Repository.*(..)) || execution(public * *..*RepoImpl.*(..))")
     public Object logExecTimeAdvice(ProceedingJoinPoint point) throws Throwable {
         Throwable thrown = null;
         Object result = null;
@@ -22,7 +24,7 @@ public class LogExecutionTimeAdvice {
             thrown = t;
         }
         execTime = System.currentTimeMillis() - execTime;
-        log.info("Execution time for {}: {}", point.getSignature().toString(), execTime);
+        log.info("Execution time for {}: {} ms", point.getSignature().toString(), execTime);
         if (thrown != null) {
             throw thrown;
         } else {
