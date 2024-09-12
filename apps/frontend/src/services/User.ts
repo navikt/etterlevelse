7a3cc5951a434ef855e4626848a5ca6e0d515c31
beeccy <beeccy@users.noreply.github.com>
2024-09-12 11:30:22 +0200

    Reversed endringer, men bare ved å utkommentere forslag
    Co-authored-by: Andre Groseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/services/User.ts
+++ apps/frontend/src/services/User.ts
@@ -10,229 +10,231 @@ export enum EGroup {
   ADMIN = 'ADMIN',
 }
 
-const UserService = () => {
-  let loaded: boolean
-  let userInfo: IUserInfo = { loggedIn: false, groups: [] }
-  let currentGroups: EGroup[] = [EGroup.READ]
-  let error: string
+// const UserServiceNew = () => {
+//   let loaded: boolean
+//   let userInfo: IUserInfo = { loggedIn: false, groups: [] }
+//   let currentGroups: EGroup[] = [EGroup.READ]
+//   let error: string
 
-  const fetchData = async (): Promise<any> => {
-    return getUserInfo()
-      .then(handleGetResponse)
-      .catch((error) => {
-        error = error.message
-        loaded = true
-      })
-  }
-
-  const promise: Promise<any> = fetchData()
-
-  const handleGetResponse = (response: AxiosResponse<IUserInfo>) => {
-    if (typeof response.data === 'object' && response.data !== null) {
-      const groups =
-        response.data.groups.indexOf(EGroup.ADMIN) >= 0
-          ? (Object.keys(EGroup) as EGroup[])
-          : response.data.groups
-      userInfo = { ...response.data, groups }
-      currentGroups = userInfo.groups
-    } else {
-      error = response.data
-    }
-    loaded = true
-  }
-
-  const isLoggedIn = (): boolean => {
-    return userInfo.loggedIn
-  }
-
-  const getIdent = (): string => {
-    return userInfo.ident ?? ''
-  }
-
-  const getEmail = (): string => {
-    return userInfo.email ?? ''
-  }
-
-  const getName = (): string => {
-    return userInfo.name ?? ''
-  }
-
-  const getFirstNameThenLastName = (): string => {
-    const splittedName = userInfo.name?.split(', ') ?? ''
-
-    return splittedName[1] + ' ' + splittedName[0]
-  }
-
-  const getAvailableGroups = (): { name: string; group: EGroup }[] => {
-    return userInfo.groups
-      .filter((group) => group !== EGroup.READ)
-      .map((group) => ({ name: nameFor(group), group }))
-  }
-
-  const toggleGroup = (group: EGroup, active: boolean) => {
-    if (active && !hasGroup(group) && userInfo.groups.indexOf(group) >= 0) {
-      currentGroups = [...currentGroups, group]
-      updateUser()
-    } else {
-      currentGroups = currentGroups.filter((currentGroup) => currentGroup !== group)
-      updateUser()
-    }
-  }
-
-  const getGroups = (): string[] => {
-    return currentGroups
-  }
-
-  const hasGroup = (group: string): boolean => {
-    return getGroups().indexOf(group) >= 0
-  }
-
-  const canWrite = (): boolean => {
-    return hasGroup(EGroup.WRITE)
-  }
-
-  const isAdmin = (): boolean => {
-    return hasGroup(EGroup.ADMIN)
-  }
-
-  const isKraveier = (): boolean => {
-    return hasGroup(EGroup.KRAVEIER)
-  }
-
-  const getError = (): string => {
-    return error || ''
-  }
-
-  const wait = async (): Promise<any> => {
-    await promise
-  }
-
-  const isLoaded = (): boolean => {
-    return loaded
-  }
-
-  return {
-    isLoggedIn: isLoggedIn(),
-    getIdent: getIdent(),
-    getEmail: getEmail(),
-    getName: getName(),
-    getFirstNameThenLastName: getFirstNameThenLastName(),
-    getAvailableGroups: getAvailableGroups(),
-    toggleGroup: toggleGroup,
-    getGroups: getGroups(),
-    canWrite: canWrite(),
-    isAdmin: isAdmin(),
-    isKraveier: isKraveier(),
-    getError: getError(),
-    wait: wait(),
-    isLoaded: isLoaded(),
-  }
-}
-
-// class UserService {
-//   private loaded = false
-//   private userInfo: IUserInfo = { loggedIn: false, groups: [] }
-//   private currentGroups = [EGroup.READ]
-//   private error?: string
-//   private readonly promise: Promise<any>
-
-//   constructor() {
-//     this.promise = this.fetchData()
-//   }
-
-//   private fetchData = async () => {
+//   const fetchData = async (): Promise<any> => {
 //     return getUserInfo()
-//       .then(this.handleGetResponse)
-//       .catch((err) => {
-//         this.error = err.message
-//         this.loaded = true
+//       .then(handleGetResponse)
+//       .catch((error) => {
+//         error = error.message
+//         loaded = true
 //       })
 //   }
 
-//   handleGetResponse = (response: AxiosResponse<IUserInfo>) => {
+//   const promise: Promise<any> = fetchData()
+
+//   const handleGetResponse = (response: AxiosResponse<IUserInfo>) => {
 //     if (typeof response.data === 'object' && response.data !== null) {
 //       const groups =
 //         response.data.groups.indexOf(EGroup.ADMIN) >= 0
 //           ? (Object.keys(EGroup) as EGroup[])
 //           : response.data.groups
-//       this.userInfo = { ...response.data, groups }
-//       this.currentGroups = this.userInfo.groups
+//       userInfo = { ...response.data, groups }
+//       currentGroups = userInfo.groups
 //     } else {
-//       this.error = response.data
+//       error = response.data
 //     }
-//     this.loaded = true
+//     loaded = true
 //   }
 
-//   isLoggedIn(): boolean {
-//     return this.userInfo.loggedIn
+//   const isLoggedIn = (): boolean => {
+//     return userInfo.loggedIn
 //   }
 
-//   public getIdent(): string {
-//     return this.userInfo.ident ?? ''
+//   const getIdent = (): string => {
+//     return userInfo.ident ?? ''
 //   }
 
-//   public getEmail(): string {
-//     return this.userInfo.email ?? ''
+//   const getEmail = (): string => {
+//     return userInfo.email ?? ''
 //   }
 
-//   public getName(): string {
-//     return this.userInfo.name ?? ''
+//   const getName = (): string => {
+//     return userInfo.name ?? ''
 //   }
 
-//   public getFirstNameThenLastName(): string {
-//     const splittedName = this.userInfo.name?.split(', ') ?? ''
+//   const getFirstNameThenLastName = (): string => {
+//     const splittedName = userInfo.name?.split(', ') ?? ''
 
 //     return splittedName[1] + ' ' + splittedName[0]
 //   }
 
-//   public getAvailableGroups(): { name: string; group: EGroup }[] {
-//     return this.userInfo.groups
-//       .filter((g) => g !== EGroup.READ)
+//   const getAvailableGroups = (): { name: string; group: EGroup }[] => {
+//     return userInfo.groups
+//       .filter((group) => group !== EGroup.READ)
 //       .map((group) => ({ name: nameFor(group), group }))
 //   }
 
-//   public toggleGroup(group: EGroup, active: boolean) {
-//     if (active && !this.hasGroup(group) && this.userInfo.groups.indexOf(group) >= 0) {
-//       this.currentGroups = [...this.currentGroups, group]
+//   const toggleGroup = (group: EGroup, active: boolean) => {
+//     if (active && !hasGroup(group) && userInfo.groups.indexOf(group) >= 0) {
+//       currentGroups = [...currentGroups, group]
 //       updateUser()
 //     } else {
-//       this.currentGroups = this.currentGroups.filter((g) => g !== group)
+//       currentGroups = currentGroups.filter((currentGroup) => currentGroup !== group)
 //       updateUser()
 //     }
 //   }
 
-//   public getGroups(): string[] {
-//     return this.currentGroups
+//   const getGroups = (): string[] => {
+//     return currentGroups
 //   }
 
-//   public hasGroup(group: string): boolean {
-//     return this.getGroups().indexOf(group) >= 0
+//   const hasGroup = (group: string): boolean => {
+//     return getGroups().indexOf(group) >= 0
 //   }
 
-//   public canWrite(): boolean {
-//     return this.hasGroup(EGroup.WRITE)
+//   const canWrite = (): boolean => {
+//     return hasGroup(EGroup.WRITE)
 //   }
 
-//   public isAdmin(): boolean {
-//     return this.hasGroup(EGroup.ADMIN)
+//   const isAdmin = (): boolean => {
+//     return hasGroup(EGroup.ADMIN)
 //   }
 
-//   public isKraveier(): boolean {
-//     return this.hasGroup(EGroup.KRAVEIER)
+//   const isKraveier = (): boolean => {
+//     return hasGroup(EGroup.KRAVEIER)
 //   }
 
-//   public getError(): string {
-//     return this.error || ''
+//   const getError = (): string => {
+//     return error || ''
 //   }
 
-//   async wait() {
-//     await this.promise
+//   const wait = async (): Promise<any> => {
+//     await promise
 //   }
 
-//   isLoaded(): boolean {
-//     return this.loaded
+//   const isLoaded = (): boolean => {
+//     return loaded
+//   }
+
+//   if (isLoggedIn) {
+//     return {
+//       isLoggedIn: isLoggedIn(),
+//       getIdent: getIdent(),
+//       getEmail: getEmail(),
+//       getName: getName(),
+//       getFirstNameThenLastName: getFirstNameThenLastName(),
+//       getAvailableGroups: getAvailableGroups(),
+//       toggleGroup: toggleGroup,
+//       getGroups: getGroups(),
+//       canWrite: canWrite(),
+//       isAdmin: isAdmin(),
+//       isKraveier: isKraveier(),
+//       getError: getError(),
+//       wait: wait(),
+//       isLoaded: isLoaded(),
+//     }
 //   }
 // }
+
+class UserService {
+  private loaded = false
+  private userInfo: IUserInfo = { loggedIn: false, groups: [] }
+  private currentGroups = [EGroup.READ]
+  private error?: string
+  private readonly promise: Promise<any>
+
+  constructor() {
+    this.promise = this.fetchData()
+  }
+
+  private fetchData = async () => {
+    return getUserInfo()
+      .then(this.handleGetResponse)
+      .catch((err) => {
+        this.error = err.message
+        this.loaded = true
+      })
+  }
+
+  handleGetResponse = (response: AxiosResponse<IUserInfo>) => {
+    if (typeof response.data === 'object' && response.data !== null) {
+      const groups =
+        response.data.groups.indexOf(EGroup.ADMIN) >= 0
+          ? (Object.keys(EGroup) as EGroup[])
+          : response.data.groups
+      this.userInfo = { ...response.data, groups }
+      this.currentGroups = this.userInfo.groups
+    } else {
+      this.error = response.data
+    }
+    this.loaded = true
+  }
+
+  isLoggedIn(): boolean {
+    return this.userInfo.loggedIn
+  }
+
+  public getIdent(): string {
+    return this.userInfo.ident ?? ''
+  }
+
+  public getEmail(): string {
+    return this.userInfo.email ?? ''
+  }
+
+  public getName(): string {
+    return this.userInfo.name ?? ''
+  }
+
+  public getFirstNameThenLastName(): string {
+    const splittedName = this.userInfo.name?.split(', ') ?? ''
+
+    return splittedName[1] + ' ' + splittedName[0]
+  }
+
+  public getAvailableGroups(): { name: string; group: EGroup }[] {
+    return this.userInfo.groups
+      .filter((g) => g !== EGroup.READ)
+      .map((group) => ({ name: nameFor(group), group }))
+  }
+
+  public toggleGroup(group: EGroup, active: boolean) {
+    if (active && !this.hasGroup(group) && this.userInfo.groups.indexOf(group) >= 0) {
+      this.currentGroups = [...this.currentGroups, group]
+      updateUser()
+    } else {
+      this.currentGroups = this.currentGroups.filter((g) => g !== group)
+      updateUser()
+    }
+  }
+
+  public getGroups(): string[] {
+    return this.currentGroups
+  }
+
+  public hasGroup(group: string): boolean {
+    return this.getGroups().indexOf(group) >= 0
+  }
+
+  public canWrite(): boolean {
+    return this.hasGroup(EGroup.WRITE)
+  }
+
+  public isAdmin(): boolean {
+    return this.hasGroup(EGroup.ADMIN)
+  }
+
+  public isKraveier(): boolean {
+    return this.hasGroup(EGroup.KRAVEIER)
+  }
+
+  public getError(): string {
+    return this.error || ''
+  }
+
+  async wait() {
+    await this.promise
+  }
+
+  isLoaded(): boolean {
+    return this.loaded
+  }
+}
 
 export const user = new UserService()
 
