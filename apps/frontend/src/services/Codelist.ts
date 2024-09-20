6ce77628891568f13d49d5ad96b443de53bd00e1
beeccy <beeccy@users.noreply.github.com>
2024-09-20 10:02:31 +0200

    Fjernet gammel kode

--- apps/frontend/src/services/Codelist.ts
+++ apps/frontend/src/services/Codelist.ts
@@ -122,11 +122,6 @@ const CodelistService = async (): Promise<ICodelistProps> => {
     return lists || error
   }
 
-  // overloads
-  // const getCodes(list: EListName.LOV): TLovCode[]
-  // const getCodes(list: EListName.TEMA): TTemaCode[]
-  // const getCodes(list: EListName): ICode[]
-
   const getCodes = (list: EListName): ICode[] => {
     return lists && lists.codelist[list]
       ? lists.codelist[list].sort((c1: ICode, c2: ICode) =>
@@ -134,11 +129,6 @@ const CodelistService = async (): Promise<ICodelistProps> => {
         )
       : []
   }
-
-  // overloads
-  // getCode(list: EListName.LOV, codeName?: string): TLovCode | undefined
-  // getCode(list: EListName.TEMA, codeName?: string): TTemaCode | undefined
-  // getCode(list: EListName, codeName?: string): ICode | undefined
 
   const getCode = (list: EListName, codeName?: string): ICode | undefined => {
     return getCodes(list).find((getCode: ICode) => getCode.code === codeName)
@@ -264,162 +254,6 @@ export const codelist: ICodelistProps = await CodelistService().then((response: 
 export const fetchCodelistService = async (): Promise<void> => {
   await CodelistService()
 }
-
-// class CodelistServic {
-//   lists?: IAllCodelists
-//   error?: string
-//   promise: Promise<any>
-
-//   constructor() {
-//     console.log('CodelistService')
-
-//     this.promise = this.fetchData()
-//   }
-
-//   private fetchData = async (refresh?: boolean) => {
-//     console.log('CodelistService fetchData')
-
-//     const codeListPromise = getAllCodelists(refresh)
-//       .then(this.handleGetCodelistResponse)
-//       .catch((err) => (this.error = err.message))
-//     return Promise.all([codeListPromise])
-//   }
-
-//   handleGetCodelistResponse = (response: AxiosResponse<IAllCodelists>) => {
-//     if (typeof response.data === 'object' && response.data !== null) {
-//       this.lists = response.data
-//     } else {
-//       this.error = response.data
-//     }
-//   }
-
-//   refreshCodeLists() {
-//     this.promise = this.fetchData(true)
-//     return this.promise
-//   }
-
-//   async wait() {
-//     console.log('codelist wait')
-
-//     await this.promise
-//   }
-
-//   isLoaded() {
-//     return this.lists || this.error
-//   }
-
-//   // overloads
-//   getCodes(list: EListName.LOV): TLovCode[]
-//   getCodes(list: EListName.TEMA): TTemaCode[]
-//   getCodes(list: EListName): ICode[]
-
-//   getCodes(list: EListName): ICode[] {
-//     return this.lists && this.lists.codelist[list]
-//       ? this.lists.codelist[list].sort((c1, c2) => c1.shortName.localeCompare(c2.shortName, 'nb'))
-//       : []
-//   }
-
-//   // overloads
-//   getCode(list: EListName.LOV, codeName?: string): TLovCode | undefined
-//   getCode(list: EListName.TEMA, codeName?: string): TTemaCode | undefined
-//   getCode(list: EListName, codeName?: string): ICode | undefined
-
-//   getCode(list: EListName, codeName?: string): ICode | undefined {
-//     return this.getCodes(list).find((c) => c.code === codeName)
-//   }
-
-//   getCodesForTema(codeName?: string): TLovCode[] {
-//     return this.getCodes(EListName.LOV).filter((c) => c.data?.tema === codeName)
-//   }
-
-//   valid(list: EListName, codeName?: string): boolean {
-//     return !!codeName && !!this.getCode(list, codeName)
-//   }
-
-//   getShortnameForCode(code: ICode) {
-//     return this.getShortname(code.list, code.code)
-//   }
-
-//   getShortnameForCodes(codes: ICode[]) {
-//     return codes.map((code) => this.getShortname(code.list, code.code)).join(', ')
-//   }
-
-//   getShortname(list: EListName, codeName: string) {
-//     const code = this.getCode(list, codeName)
-//     return code ? code.shortName : codeName
-//   }
-
-//   getShortnames(list: EListName, codeNames: string[]) {
-//     return codeNames.map((codeName) => this.getShortname(list, codeName))
-//   }
-
-//   getDescription(list: EListName, codeName: string) {
-//     const code = this.getCode(list, codeName)
-//     return code ? code.description : codeName
-//   }
-
-//   getParsedOptions(listName: EListName): { value: string; label: string; description: string }[] {
-//     return this.getCodes(listName).map((code: ICode) => {
-//       return { value: code.code, label: code.shortName, description: code.description }
-//     })
-//   }
-
-//   getOptionsForCode(codes: ICode[]): { id: string; label: string; description: string }[] {
-//     return codes.map((code: ICode) => {
-//       return { id: code.code, label: code.shortName, description: code.description }
-//     })
-//   }
-
-//   getParsedOptionsForLov(
-//     forVirkemiddel?: boolean
-//   ): { value: string; label: string; description: string }[] {
-//     const lovList = this.getCodes(EListName.LOV)
-//     let filteredLovList = []
-
-//     if (forVirkemiddel) {
-//       filteredLovList = filterLovCodeListForRelevans(lovList, ELovCodeRelevans.VIRKEMIDDEL)
-//     } else {
-//       filteredLovList = filterLovCodeListForRelevans(lovList, ELovCodeRelevans.KRAV)
-//     }
-
-//     return filteredLovList.map((code: TLovCode) => {
-//       return { value: code.code, label: code.shortName, description: code.description }
-//     })
-//   }
-
-//   getParsedOptionsForList(
-//     listName: EListName,
-//     selected: string[]
-//   ): { id: string; label: string }[] {
-//     return selected.map((code) => ({ id: code, label: this.getShortname(listName, code) }))
-//   }
-
-//   getParsedOptionsFilterOutSelected(
-//     listName: EListName,
-//     currentSelected: string[]
-//   ): { value: string; label: string }[] {
-//     const parsedOptions = this.getParsedOptions(listName)
-//     return !currentSelected
-//       ? parsedOptions
-//       : parsedOptions.filter((option) =>
-//           currentSelected.includes(option.value) ? null : option.value
-//         )
-//   }
-
-//   isForskrift(nationalLawCode?: string) {
-//     return nationalLawCode && nationalLawCode.startsWith(LOVDATA_FORSKRIFT_PREFIX)
-//   }
-
-//   makeValueLabelForAllCodeLists() {
-//     return Object.keys(EListName).map((key) => ({ value: key, label: key }))
-//   }
-
-//   gjelderForLov(tema: TTemaCode, lov: TLovCode) {
-//     return !!this.getCodesForTema(tema.code).filter((l) => l.code === lov.code).length
-//   }
-// }
-
-// export const codelist = new CodelistServic()
 
 export interface IAllCodelists {
   codelist: IList
