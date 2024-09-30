5744a00c9ff6397e04e4cec89c3d1593a3dbcf7b
Lord Andre Groseth <andregroseth@hotmail.com>
2024-09-30 15:34:12 +0200

    Wrap Linkpanel in list
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/pages/MainPage.tsx
+++ apps/frontend/src/pages/MainPage.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@apollo/client'
-import { Alert, BodyLong, Button, Heading, LinkPanel, Skeleton } from '@navikt/ds-react'
+import { Alert, BodyLong, Button, Heading, LinkPanel, List, Skeleton } from '@navikt/ds-react'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getMeldingByType } from '../api/MeldingApi'
@@ -186,31 +186,34 @@ const EtterlevelseDokumentasjonList = ({
       <Heading size="medium" level="2">
         Mine sist dokumenterte
       </Heading>
-      <div className="mt-6 flex flex-col gap-2">
-        {filteredEtterlevelsesDokumentasjoner.length !== 0 &&
-          filteredEtterlevelsesDokumentasjoner
-            .slice(0, 2)
-            .map((etterlevelseDokumentasjon, index) => (
-              <EtterlevelseDokumentasjonsPanel
-                key={etterlevelseDokumentasjon.title + '_' + index}
-                etterlevelseDokumentasjon={etterlevelseDokumentasjon}
-                onClick={() =>
-                  ampli.logEvent('navigere', {
-                    app: 'etterlevelse',
-                    kilde: 'forside-panel',
-                    til: `'/dokumentasjon/' + ${etterlevelseDokumentasjon.id}`,
-                    fra: '/',
-                  })
-                }
-              />
-            ))}
+      <div>
+        <List className="mt-6 flex flex-col gap-2">
+          {filteredEtterlevelsesDokumentasjoner.length !== 0 &&
+            filteredEtterlevelsesDokumentasjoner
+              .slice(0, 2)
+              .map((etterlevelseDokumentasjon, index) => (
+                <List.Item icon={<div />} key={etterlevelseDokumentasjon.title + '_' + index}>
+                  <EtterlevelseDokumentasjonsPanel
+                    etterlevelseDokumentasjon={etterlevelseDokumentasjon}
+                    onClick={() =>
+                      ampli.logEvent('navigere', {
+                        app: 'etterlevelse',
+                        kilde: 'forside-panel',
+                        til: `'/dokumentasjon/' + ${etterlevelseDokumentasjon.id}`,
+                        fra: '/',
+                      })
+                    }
+                  />
+                </List.Item>
+              ))}
 
-        {filteredEtterlevelsesDokumentasjoner.length === 0 && (
-          <BodyLong>
-            Ingen etterlevelsesdokument som har blitt opprettet eller endret av deg de siste 6
-            månedene.
-          </BodyLong>
-        )}
+          {filteredEtterlevelsesDokumentasjoner.length === 0 && (
+            <BodyLong>
+              Ingen etterlevelsesdokument som har blitt opprettet eller endret av deg de siste 6
+              månedene.
+            </BodyLong>
+          )}
+        </List>
       </div>
     </div>
   )
