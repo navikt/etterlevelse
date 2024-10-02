ffeb47705dcf2ca62b99e54b52dd31553379d5bf
beeccy <beeccy@users.noreply.github.com>
2024-10-02 12:04:26 +0200

    Fikset typer TemaPage.tsx

--- apps/frontend/src/pages/TemaPage.tsx
+++ apps/frontend/src/pages/TemaPage.tsx
@@ -8,7 +8,7 @@ import { SkeletonPanel } from '../components/common/LoadingSkeleton'
 import { Markdown } from '../components/common/Markdown'
 import { ExternalLink } from '../components/common/RouteLink'
 import { PageLayout } from '../components/scaffold/Page'
-import { IKrav } from '../constants'
+import { IKrav, IKravPriorityList, TKravQL } from '../constants'
 import { useKravCounter } from '../query/KravQuery'
 import { ampli, userRoleEventProp } from '../services/Amplitude'
 import {
@@ -36,42 +36,47 @@ const getTemaMainHeader = (
   lover: TLovCode[],
   codelistUtils: ICodelistProps,
   noHeader?: boolean
-) => {
-  return (
-    <div className="lg:grid lg:grid-flow-col lg:gap-2">
-      <div>
-        {!noHeader && (
-          <Heading level="1" size="medium" spacing>
-            {tema.shortName}
-          </Heading>
-        )}
-        <Markdown source={tema.description} />
-      </div>
-
-      <div className="my-8 lg:border-l-2 lg:pl-2 lg:border-gray-200">
-        <Heading level="2" size="small" spacing>
-          Ansvarlig for lovtolkning
+) => (
+  <div className="lg:grid lg:grid-flow-col lg:gap-2">
+    <div>
+      {!noHeader && (
+        <Heading level="1" size="medium" spacing>
+          {tema.shortName}
         </Heading>
-        {_.uniq(lover.map((lov) => lov.data?.underavdeling)).map((code, index) => (
+      )}
+      <Markdown source={tema.description} />
+    </div>
+
+    <div className="my-8 lg:border-l-2 lg:pl-2 lg:border-gray-200">
+      <Heading level="2" size="small" spacing>
+        Ansvarlig for lovtolkning
+      </Heading>
+      {_.uniq(lover.map((lov: TLovCode) => lov.data?.underavdeling)).map(
+        (code: string | undefined, index: number) => (
           <BodyShort key={code + '_' + index} size="large" spacing>
             {codelistUtils.getCode(EListName.UNDERAVDELING, code)?.shortName}
           </BodyShort>
-        ))}
-        <Heading level="2" size="small" spacing>
-          Lovdata
-        </Heading>
-        {lover.map((lov, index) => (
-          <div key={lov.code + '_' + index} className="mb-1.5">
-            <ExternalLink href={lovdataBase(lov.code, codelistUtils)}>{lov.shortName}</ExternalLink>
-          </div>
-        ))}
-      </div>
+        )
+      )}
+      <Heading level="2" size="small" spacing>
+        Lovdata
+      </Heading>
+      {lover.map((lov: TLovCode, index: number) => (
+        <div key={lov.code + '_' + index} className="mb-1.5">
+          <ExternalLink href={lovdataBase(lov.code, codelistUtils)}>{lov.shortName}</ExternalLink>
+        </div>
+      ))}
     </div>
-  )
+  </div>
+)
+
+interface ITemaViewProps {
+  tema: TTemaCode
+  codelistUtils: ICodelistProps
 }
 
-const TemaView = ({ tema, codelistUtils }: { tema: TTemaCode; codelistUtils: ICodelistProps }) => {
-  const lover = codelistUtils.getCodesForTema(tema.code)
+const TemaView = ({ tema, codelistUtils }: ITemaViewProps) => {
+  const lover: TLovCode[] = codelistUtils.getCodesForTema(tema.code)
   const { data, loading } = useKravCounter(
     { lover: lover.map((lov) => lov.code) },
     { skip: !lover.length }
@@ -89,10 +94,10 @@ const TemaView = ({ tema, codelistUtils }: { tema: TTemaCode; codelistUtils: ICo
   useEffect(() => {
     if (data && data.krav && data.krav.content && data.krav.content.length > 0) {
       ;(async () => {
-        const kravPriorityList = await getKravPriorityListByTemaCode(tema.code)
-        const kravListe = _.cloneDeep(data.krav.content)
-        kravListe.map((krav) => {
-          const priority = kravPriorityList.priorityList.indexOf(krav.kravNummer) + 1
+        const kravPriorityList: IKravPriorityList = await getKravPriorityListByTemaCode(tema.code)
+        const kravListe: TKravQL[] = _.cloneDeep(data.krav.content)
+        kravListe.map((krav: TKravQL) => {
+          const priority: number = kravPriorityList.priorityList.indexOf(krav.kravNummer) + 1
           krav.prioriteringsId = priority
           return krav
         })
