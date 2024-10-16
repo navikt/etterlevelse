245cec47c40c01fb9733f8bb894c4b2a258c9186
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-10-16 14:32:34 +0200

    ADD: script for generating table for pvk document and index
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

---
+++ apps/backend/src/main/resources/db/migration/V1.25__pvk_tabell.sql
@@ -0,0 +1,42 @@
+create table if not exists pvk_dokument
+(
+    id
+    uuid
+    primary
+    key,
+    etterlevelse_dokumentasjon_id
+    text
+    not
+    null,
+    status
+    text
+    not
+    null,
+    data
+    jsonb
+    not
+    null,
+    version
+    integer
+    not
+    null,
+    created_by
+    text
+    not
+    null,
+    created_date
+    timestamp
+    not
+    null,
+    last_modified_by
+    text
+    not
+    null,
+    last_modified_date
+    timestamp
+    not
+    null,
+
+);
+
+create index if not exists idx_pvk_etterlevelse_dokumentasjon_id on pvk_dokument (etterlevelse_dokumentasjon_id);
