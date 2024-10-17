c1613de8aa98f5b44c62dc594ea775c4a785f24b
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-10-17 09:48:02 +0200

    ADD: table and domain object for pvk document files
    Co-authored-by: farjamm <farjam.movafagh@nav.no>

--- apps/backend/src/main/resources/db/migration/V1.25__pvk_tabell.sql
+++ apps/backend/src/main/resources/db/migration/V1.25__pvk_tabell.sql
@@ -39,3 +39,47 @@ create table if not exists pvk_dokument
 );
 
 create index if not exists idx_pvk_etterlevelse_dokumentasjon_id on pvk_dokument (etterlevelse_dokumentasjon_id);
+
+create table if not exists pvk_dokument
+(
+    id
+    uuid
+    primary
+    key,
+    pvk_dokument_id
+    text
+    not
+    null,
+    file_name
+    text
+    not
+    null,
+    file_type
+    text
+    not
+    null,
+    file_content
+    bytea
+    not
+    null,
+    version
+    integer
+    not
+    null,
+    created_by
+    text
+    not
+    null,
+    created_date
+    timestamp
+    not
+    null,
+    last_modified_by
+    text
+    not
+    null,
+    last_modified_date
+    timestamp
+    not
+    null
+);
