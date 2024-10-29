0ef4b079e49fcdb678250cea3ce9173f15d354ac
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-10-29 11:13:37 +0100

    ADD:Stepper and url update when changing stepper

--- apps/frontend/src/pages/PvkDokumentPage.tsx
+++ apps/frontend/src/pages/PvkDokumentPage.tsx
@@ -1,9 +1,10 @@
-import { Loader } from '@navikt/ds-react'
+import { Loader, Stepper } from '@navikt/ds-react'
 import { useState } from 'react'
+import { Helmet } from 'react-helmet'
 import { useParams } from 'react-router-dom'
 import { useEtterlevelseDokumentasjon } from '../api/EtterlevelseDokumentasjonApi'
 import { usePvkDokument } from '../api/PvkDokumentApi'
-import { PageLayout } from '../components/scaffold/Page'
+import CustomizedBreadcrumbs from '../components/common/CustomizedBreadcrumbs'
 import { IBreadCrumbPath } from '../constants'
 import { dokumentasjonerBreadCrumbPath } from './util/BreadCrumbPath'
 
@@ -14,9 +15,14 @@ export const PvkDokumentPage = () => {
       pvkdokumentId?: string
     }>
   > = useParams<{ id?: string }>()
+  const url = new URL(window.location.href)
+  const currentStep = url.searchParams.get('steg')
   const [currentPage] = useState<string>('Oversikt')
   const [etterlevelseDokumentasjon] = useEtterlevelseDokumentasjon(params.id)
   const [pvkDokument] = usePvkDokument(params.pvkdokumentId)
+  const [activeStep, setActiveStep] = useState<number>(
+    currentStep !== null ? parseInt(currentStep) : 1
+  )
   const breadcrumbPaths: IBreadCrumbPath[] = [
     dokumentasjonerBreadCrumbPath,
     {
@@ -34,20 +40,47 @@ export const PvkDokumentPage = () => {
     },
   ]
 
+  const handleStepChange = (step: number) => {
+    url.searchParams.set('steg', step.toString())
+    window.history.pushState({}, '', url)
+  }
+
   return (
-    <>
+    <div id="content" role="main" className="flex flex-col w-full bg-white">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Pvk Dokument</title>
+      </Helmet>
       {!etterlevelseDokumentasjon && <Loader size="large" />}
       {etterlevelseDokumentasjon && pvkDokument && (
-        <PageLayout
-          pageTitle="Pvk Dokument"
-          currentPage={currentPage}
-          breadcrumbPaths={breadcrumbPaths}
-          fullWidth
-        >
-          <div>WIP</div>
-        </PageLayout>
+        <div className="h-48 bg-[#8269A21F] flex flex-col w-full items-center">
+          <div className="w-full max-w-7xl">
+            <div className="px-2 pb-6">
+              <CustomizedBreadcrumbs currentPage={currentPage} paths={breadcrumbPaths} />
+
+              <div>
+                <Stepper
+                  aria-labelledby="stepper-heading"
+                  activeStep={activeStep}
+                  onStepChange={(value) => {
+                    setActiveStep(value)
+                    handleStepChange(value)
+                  }}
+                  orientation="horizontal"
+                >
+                  <Stepper.Step>Oversikt</Stepper.Step>
+                  <Stepper.Step>Behandlingens art og omfang</Stepper.Step>
+                  <Stepper.Step>Innvolvering av eksterne</Stepper.Step>
+                  <Stepper.Step>Risikoscenarioer tilknyttet etterlevelseskrav</Stepper.Step>
+                  <Stepper.Step>Generelle risikoscenarioer</Stepper.Step>
+                  <Stepper.Step>Send inn</Stepper.Step>
+                </Stepper>
+              </div>
+            </div>
+          </div>
+        </div>
       )}
-    </>
+    </div>
   )
 }
 
