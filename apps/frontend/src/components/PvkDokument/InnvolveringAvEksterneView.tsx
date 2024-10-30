054438f9bfa6456746d9c06fb788326e4e94f870
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-10-30 09:31:54 +0100

    ADD: heading

--- apps/frontend/src/components/PvkDokument/InnvolveringAvEksterneView.tsx
+++ apps/frontend/src/components/PvkDokument/InnvolveringAvEksterneView.tsx
@@ -1,4 +1,12 @@
+import { Heading } from '@navikt/ds-react'
+
 export const InnvolveringAvEksterneView = () => {
-  return <div>WIP</div>
+  return (
+    <div>
+      <Heading level="1" size="medium" className="mb-5">
+        Innvolvering av eksterne
+      </Heading>
+    </div>
+  )
 }
 export default InnvolveringAvEksterneView
