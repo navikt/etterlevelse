7006f7e04fc8ddad2cdf06ddea5e6109af7c7e4a
Lord Andre Groseth <andregroseth@hotmail.com>
2024-11-05 12:51:38 +0100

    ADD: whitespace

--- apps/frontend/src/components/PvkDokument/edit/CreateRisikoscenarioTiltak.tsx
+++ apps/frontend/src/components/PvkDokument/edit/CreateRisikoscenarioTiltak.tsx
@@ -29,7 +29,7 @@ export const CreateRisikoscenarioTiltak = () => {
           {/* This is just sample text: WIP*/}
           <div>
             <BodyLong>
-              <b>Beskriv risikoscenarioet:</b>Lorem ipsum dolor sit amet, consectetur adipiscing
+              <b>Beskriv risikoscenarioet:</b> Lorem ipsum dolor sit amet, consectetur adipiscing
               elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
               minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
