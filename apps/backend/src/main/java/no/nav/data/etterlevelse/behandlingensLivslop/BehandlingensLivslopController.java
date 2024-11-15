202e7e5b8674c6da2f6154ef2cf308f6af844cad
JeremiahUy <jeremiah.augie.uy@nav.no>
2024-11-15 09:35:01 +0100

    ADD: delete endpoint

--- apps/backend/src/main/java/no/nav/data/etterlevelse/behandlingensLivslop/BehandlingensLivslopController.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/behandlingensLivslop/BehandlingensLivslopController.java
@@ -1,31 +1,32 @@
  package no.nav.data.etterlevelse.behandlingensLivslop;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import no.nav.data.common.exceptions.ValidationException;
-import no.nav.data.common.rest.PageParameters;
-import no.nav.data.common.rest.RestResponsePage;
-import no.nav.data.etterlevelse.behandlingensLivslop.domain.BehandlingensLivslop;
-import no.nav.data.etterlevelse.behandlingensLivslop.dto.BehandlingensLivslopRequest;
-import no.nav.data.etterlevelse.behandlingensLivslop.dto.BehandlingensLivslopResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+ import io.swagger.v3.oas.annotations.Operation;
+ import io.swagger.v3.oas.annotations.responses.ApiResponse;
+ import io.swagger.v3.oas.annotations.tags.Tag;
+ import jakarta.validation.Valid;
+ import lombok.RequiredArgsConstructor;
+ import lombok.extern.slf4j.Slf4j;
+ import no.nav.data.common.exceptions.ValidationException;
+ import no.nav.data.common.rest.PageParameters;
+ import no.nav.data.common.rest.RestResponsePage;
+ import no.nav.data.etterlevelse.behandlingensLivslop.domain.BehandlingensLivslop;
+ import no.nav.data.etterlevelse.behandlingensLivslop.dto.BehandlingensLivslopRequest;
+ import no.nav.data.etterlevelse.behandlingensLivslop.dto.BehandlingensLivslopResponse;
+ import org.springframework.data.domain.Page;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.DeleteMapping;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PathVariable;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.PutMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
+ import java.util.Objects;
+ import java.util.Optional;
+ import java.util.UUID;
 
  @Slf4j
 @RequiredArgsConstructor
@@ -82,7 +83,7 @@ public class BehandlingensLivslopController {
     @Operation(summary = "Update Behandlingens Livsløp")
     @ApiResponse(description = "Behandlingens Livsløp updated")
     @PutMapping("/{id}")
-    public ResponseEntity<BehandlingensLivslopResponse> updateBehandlingensLivslo(@PathVariable UUID id, @Valid @RequestBody BehandlingensLivslopRequest request) {
+    public ResponseEntity<BehandlingensLivslopResponse> updateBehandlingensLivslop(@PathVariable UUID id, @Valid @RequestBody BehandlingensLivslopRequest request) {
         log.info("Update Behandlingens Livsløp id={}", id);
 
         if (!Objects.equals(id, request.getIdAsUUID())) {
@@ -100,91 +101,13 @@ public class BehandlingensLivslopController {
         return ResponseEntity.ok(BehandlingensLivslopResponse.buildFrom(behandlingensLivslop));
     }
 
-    /* FIXME
-    @Operation(summary = "Delete Behandlingens Livsløp")
-    @ApiResponse(description = "Pvk Document deleted")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<PvkDokumentResponse> deletePvkDokumentById(@PathVariable UUID id) {
-        log.info("Delete Pvk Document id={}", id);
-        var pvkDokument = pvkDokumentService.delete(id);
-        if(pvkDokument == null) {
-            log.warn("Could not find pvk dokument with id = {} to delete", id);
-            return ResponseEntity.ok(null);
-        } else {
-            return ResponseEntity.ok(PvkDokumentResponse.buildFrom(pvkDokument));
-        }
-    }
+     @Operation(summary = "Delete Etterlevelse")
+     @ApiResponse(description = "Etterlevelse deleted")
+     @DeleteMapping("/{id}")
+     public ResponseEntity<BehandlingensLivslopResponse> deleteBehandlingensLivslopById(@PathVariable UUID id) {
+         log.info("Delete Behandlingens Livslop id={}", id);
+         var behandlingensLivslop = service.delete(id);
+         return ResponseEntity.ok(BehandlingensLivslopResponse.buildFrom(behandlingensLivslop));
+     }
 
-    @Operation(summary = "Get Pvk Document files")
-    @ApiResponse(description = "ok")
-    @GetMapping("/{pvkdokumentid}/files")
-    public ResponseEntity<List<PvkDokumentFil>> getFilesByPvkDokumentId(@PathVariable UUID pvkdokumentid) {
-        log.info("Get files for Pvk dokument id={}", pvkdokumentid);
-        List<PvkDokumentFil> files = pvkDokumentService.getPvkDokumentFilByPvkDokumentId(pvkdokumentid.toString());
-
-        return ResponseEntity.ok(files);
-    }
-
-    @Operation(summary = "Upload Pvk Document files")
-    @ApiResponse(responseCode = "201", description = "Files saved")
-    @PostMapping("/{pvkdokumentid}/files")
-    public ResponseEntity<List<String>> uploadFiles(
-            @PathVariable UUID pvkdokumentid,
-            @RequestParam("file") List<MultipartFile> files
-    ) {
-        log.info("Pvk Document {} upload {} files", pvkdokumentid, files.size());
-        var pvkDokument = pvkDokumentService.get(pvkdokumentid);
-
-        if (pvkDokument == null) {
-            log.warn("Unable to save files, no pvk document with id={} found", pvkdokumentid);
-            throw new ValidationException(String.format("Unable to save files, no pvk document with id=%s found", pvkdokumentid));
-        }
-
-        files.forEach(i -> Assert.isTrue(validFile(i), () -> "Invalid file " + i.getName() + " " + i.getContentType()));
-
-        var filesToSave = pvkDokumentService.saveImages(convert(files, f -> {
-            var pvkDokumentFil = pvkDokumentService.getPvkDokumentFilByFileNameAndType(f.getOriginalFilename(), f.getContentType());
-
-            var pvkDokumentFilToSave = PvkDokumentFil.builder()
-                    .pvkDokumetId(pvkDokument.getId().toString())
-                    .filename(f.getOriginalFilename())
-                    .fileType(f.getContentType())
-                    .content(getBytes(f))
-                    .build();
-
-            pvkDokumentFil.ifPresentOrElse(
-                    pvkFil ->  pvkDokumentFilToSave.setId(pvkFil.getId()),
-                    () -> pvkDokumentFilToSave.setId(UUID.randomUUID()));
-
-            return pvkDokumentFilToSave;
-        }));
-
-        return new ResponseEntity<>(convert(filesToSave, i -> i.getId().toString()), HttpStatus.CREATED);
-    }
-
-    @Operation(summary = "Delete Pvk Document file")
-    @ApiResponse(description = "Pvk Document file deleted")
-    @DeleteMapping("/pvkdokumentfil/{id}")
-    public ResponseEntity<PvkDokumentFil> deletePvkDokumentFilById(@PathVariable UUID id) {
-        log.info("Delete Pvk Document file with id={}", id);
-        var pvkDokumentFil = pvkDokumentService.deleteFile(id);
-
-        if(pvkDokumentFil == null) {
-            log.warn("Could not find pvk dokument file with id = {} to delete", id);
-            return ResponseEntity.ok(null);
-        } else {
-            return ResponseEntity.ok(pvkDokumentFil);
-        }
-    }
-
-    private boolean validFile(MultipartFile i) {
-        return List.of(MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE, MediaType.APPLICATION_PDF).contains(i.getContentType())
-                && getBytes(i).length > 0;
-    }
-
-    @SneakyThrows
-    private byte[] getBytes(MultipartFile f) {
-        return f.getBytes();
-    }
-    //*/
 }
