c98c9a396c237e2261498146333a254439f0fd66
Lord Andre Groseth <andregroseth@hotmail.com>
2024-12-04 10:34:29 +0100

    Update: Validation logic
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/PvkDokument/edit/RisikoscenarioSchemaValidation.ts
+++ apps/frontend/src/components/PvkDokument/edit/RisikoscenarioSchemaValidation.ts
@@ -3,18 +3,5 @@ import * as yup from 'yup'
 export const risikoscenarioCreateValidation = () =>
   yup.object({
     navn: yup.string().required('Du må oppgi et navn til risikoscenario'),
-    sannsynlighetsNivaa: yup.number().test({
-      name: 'sannsynlighetsNivaaCheck',
-      message: 'Du må oppgi et sannsynlighetsnivå',
-      test: function (sannsynlighetsNivaa) {
-        return sannsynlighetsNivaa && sannsynlighetsNivaa > 0 ? true : false
-      },
-    }),
-    konsekvensNivaa: yup.number().test({
-      name: 'konsekvensNivaaCheck',
-      message: 'Du må oppgi et konsekvensnivå',
-      test: function (konsekvensNivaa) {
-        return konsekvensNivaa && konsekvensNivaa > 0 ? true : false
-      },
-    }),
+    beskrivelse: yup.string().required('Du må skrive en beskrivelse'),
   })
