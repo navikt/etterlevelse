563d0af30babaf166dec36dd939a7c315621b6c2
Lord Andre Groseth <andregroseth@hotmail.com>
2024-12-05 14:26:24 +0100

    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

---
+++ apps/frontend/src/components/PvkDokument/edit/RisikoscenarioForm.tsx
@@ -0,0 +1,141 @@
+import { Button, Heading, Radio, RadioGroup, ReadMore } from '@navikt/ds-react'
+import { Field, FieldProps, Form, Formik } from 'formik'
+import { mapRisikoscenarioToFormValue } from '../../../api/RisikoscenarioApi'
+import { IRisikoscenario } from '../../../constants'
+import { TextAreaField } from '../../common/Inputs'
+import { FormError } from '../../common/ModalSchema'
+import { risikoscenarioCreateValidation } from './RisikoscenarioSchemaValidation'
+
+interface IProps {
+  initialValues: Partial<IRisikoscenario>
+  submit: (riskoscenario: IRisikoscenario) => void
+}
+
+export const CreateRisikoscenarioForm = (props: IProps) => {
+  const { initialValues, submit } = props
+
+  return (
+    <div>
+      <Formik
+        validateOnBlur={false}
+        validateOnChange={false}
+        onSubmit={submit}
+        validationSchema={risikoscenarioCreateValidation()}
+        initialValues={mapRisikoscenarioToFormValue(initialValues)}
+      >
+        {({ submitForm }) => (
+          <Form>
+            <div>
+              <TextAreaField
+                rows={1}
+                name="navn"
+                label="Navngi risikoscenarioet"
+                caption="Velg et navn som gjør scenarioet lett å skille fra andre "
+              />
+
+              <div className="mt-3">
+                <TextAreaField
+                  rows={3}
+                  noPlaceholder
+                  label="Beskriv risikoscenarioet"
+                  name="beskrivelse"
+                />
+              </div>
+
+              <Heading level="2" size="small" className="my-5">
+                Risikoscenarioets sannsynlighet
+              </Heading>
+
+              <ReadMore header="Hva menes med de ulike sannsynlighetsnivåene?" className="my-5">
+                ???????? WIP
+              </ReadMore>
+
+              <Field name="sannsynlighetsNivaa">
+                {(fieldProps: FieldProps) => (
+                  <RadioGroup
+                    legend="Vurdér risikoscenarioets sannsynlighetsnivå"
+                    value={fieldProps.field.value}
+                    onChange={(value) => {
+                      fieldProps.form.setFieldValue('sannsynlighetsNivaa', value)
+                    }}
+                    error={
+                      fieldProps.form.errors['sannsynlighetsNivaa'] && (
+                        <FormError fieldName={'sannsynlighetsNivaa'} />
+                      )
+                    }
+                  >
+                    <Radio value={1}>Meget lite sannsynlig</Radio>
+                    <Radio value={2}>Lite sannsynlig</Radio>
+                    <Radio value={3}>Moderat sannsynlig</Radio>
+                    <Radio value={4}>Sannsynlig</Radio>
+                    <Radio value={5}>Nesten sikkert</Radio>
+                  </RadioGroup>
+                )}
+              </Field>
+
+              <div className="mt-3">
+                <TextAreaField
+                  rows={3}
+                  noPlaceholder
+                  label="Begrunn sannsynlighetsnivået"
+                  name="sannsynlighetsNivaaBegrunnelse"
+                />
+              </div>
+
+              <Heading level="2" size="small" className="my-5">
+                Risikoscenarioets konsekvensnivå
+              </Heading>
+
+              <ReadMore header="Hva menes med de ulike konsekvensnivåene?" className="my-5">
+                ???????? WIP
+              </ReadMore>
+
+              <Field name="konsekvensNivaa">
+                {(fieldProps: FieldProps) => (
+                  <RadioGroup
+                    legend="Vurdér risikoscenarioets konsekvensnivå"
+                    value={fieldProps.field.value}
+                    onChange={(value) => {
+                      fieldProps.form.setFieldValue('konsekvensNivaa', value)
+                    }}
+                    error={
+                      fieldProps.form.errors['konsekvensNivaa'] && (
+                        <FormError fieldName={'konsekvensNivaa'} />
+                      )
+                    }
+                  >
+                    <Radio value={1}>Ubetydelig</Radio>
+                    <Radio value={2}>Lav konsekvens</Radio>
+                    <Radio value={3}>Moderat konsekvens</Radio>
+                    <Radio value={4}>Alvorlig konsekvens</Radio>
+                    <Radio value={5}>Svært alvorlig konsekvens</Radio>
+                  </RadioGroup>
+                )}
+              </Field>
+
+              <div className="mt-3">
+                <TextAreaField
+                  rows={3}
+                  noPlaceholder
+                  label="Begrunn konsekvensnivået"
+                  name="konsekvensNivaaBegrunnelse"
+                />
+              </div>
+            </div>
+
+            <div className="flex gap-2 mt-5">
+              <Button type="button" onClick={() => submitForm()}>
+                Lagre risikoscenario
+              </Button>
+              <Button onClick={() => {}} type="button" variant="secondary">
+                Avbryt, ikke lagre
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  )
+}
+
+export default CreateRisikoscenarioForm
