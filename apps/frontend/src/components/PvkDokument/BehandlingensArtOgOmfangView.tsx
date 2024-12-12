ba25ef854a024e6a875ac4582deadc68c0ee0a38
Andre Groseth <andregroseth@hotmail.com>
2024-12-12 11:04:07 +0100

    center: content

--- apps/frontend/src/components/PvkDokument/BehandlingensArtOgOmfangView.tsx
+++ apps/frontend/src/components/PvkDokument/BehandlingensArtOgOmfangView.tsx
@@ -52,98 +52,107 @@ export const BehandlingensArtOgOmfangView = (props: IProps) => {
     >
       {({ submitForm }) => (
         <Form>
-          <Heading level="1" size="medium" className="mb-5">
-            Behandlingens art og omfang
-          </Heading>
+          <div className="flex justify-center">
+            <div>
+              <Heading level="1" size="medium" className="mb-5">
+                Behandlingens art og omfang
+              </Heading>
 
-          <List title="I Behandlingskatalogen står det at dere behandler personopplysninger om:">
-            {personkategorier.length === 0 && <List.Item>Ingen</List.Item>}
-            {personkategorier.length > 0 &&
-              personkategorier.map((personkategori) => (
-                <List.Item key={personkategori}>{personkategori}</List.Item>
-              ))}
-          </List>
+              <List title="I Behandlingskatalogen står det at dere behandler personopplysninger om:">
+                {personkategorier.length === 0 && <List.Item>Ingen</List.Item>}
+                {personkategorier.length > 0 &&
+                  personkategorier.map((personkategori) => (
+                    <List.Item key={personkategori}>{personkategori}</List.Item>
+                  ))}
+              </List>
 
-          <BoolField
-            label="Stemmer denne lista over personkategorier?"
-            name="stemmerPersonkategorier"
-            horizontal
-          />
+              <BoolField
+                label="Stemmer denne lista over personkategorier?"
+                name="stemmerPersonkategorier"
+                horizontal
+              />
 
-          <Field>
-            {(fieldProps: FieldProps) => (
-              <>
-                {fieldProps.form.values.stemmerPersonkategorier === false && (
-                  <Alert inline variant="warning" className="mt-5 mb-10">
-                    Dere må oppdatere personkategori(er) i Behandlingskatalogen. Hvis dere ikke
-                    finner riktig personkategori(er), ta kontakt på{' '}
-                    <Link
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      href="https://nav-it.slack.com/archives/CR1B19E6L"
-                    >
-                      #behandlingskatalogen på Slack (åpnes i ny fane)
-                    </Link>
-                    , eller på epost: teamdatajegerne@nav.no.
-                  </Alert>
+              <Field>
+                {(fieldProps: FieldProps) => (
+                  <>
+                    {fieldProps.form.values.stemmerPersonkategorier === false && (
+                      <div className="max-w-[75ch]">
+                        <Alert inline variant="warning" className="mt-5 mb-10">
+                          Dere må oppdatere personkategori(er) i Behandlingskatalogen. Hvis dere
+                          ikke finner riktig personkategori(er), ta kontakt på{' '}
+                          <Link
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://nav-it.slack.com/archives/CR1B19E6L"
+                          >
+                            #behandlingskatalogen på Slack (åpnes i ny fane)
+                          </Link>
+                          , eller på epost: teamdatajegerne@nav.no.
+                        </Alert>
+                      </div>
+                    )}
+                  </>
                 )}
-              </>
-            )}
-          </Field>
+              </Field>
 
-          <ReadMore className="mt-5 max-w-[75ch]" header="Hvordan kan vi komme med gode estimater?">
-            Det blir ofte vanskelig å tallfeste noen personkategorier, for eksempel når det er snakk
-            om antall brukere eller saksbehandlere. Det er tilstrekkelig å oppgi ca. antall. Hvis du
-            er usikker på hvor du faktisk finner tall på hvor mange som kan ha tilgang, kan du …
-            [good advice goes here]
-          </ReadMore>
+              <ReadMore
+                className="mt-5 max-w-[75ch]"
+                header="Hvordan kan vi komme med gode estimater?"
+              >
+                Det blir ofte vanskelig å tallfeste noen personkategorier, for eksempel når det er
+                snakk om antall brukere eller saksbehandlere. Det er tilstrekkelig å oppgi ca.
+                antall. Hvis du er usikker på hvor du faktisk finner tall på hvor mange som kan ha
+                tilgang, kan du … [good advice goes here]
+              </ReadMore>
 
-          <div className="mt-5 max-w-[75ch]">
-            <TextAreaField
-              rows={3}
-              noPlaceholder
-              label="For hver av personkategoriene over, beskriv hvor mange personer dere behandler personopplysninger om."
-              name="personkategoriAntallBeskrivelse"
-            />
+              <div className="mt-5 max-w-[75ch]">
+                <TextAreaField
+                  rows={3}
+                  noPlaceholder
+                  label="For hver av personkategoriene over, beskriv hvor mange personer dere behandler personopplysninger om."
+                  name="personkategoriAntallBeskrivelse"
+                />
+              </div>
+
+              <div className="mt-5 max-w-[75ch]">
+                <TextAreaField
+                  rows={3}
+                  noPlaceholder
+                  label="Beskriv hvilke roller som skal ha tilgang til personopplysningene. For hver av rollene, beskriv hvor mange som har tilgang."
+                  name="tilgangsBeskrivelsePersonopplysningene"
+                />
+              </div>
+
+              <div className="mt-5 max-w-[75ch]">
+                <TextAreaField
+                  rows={3}
+                  noPlaceholder
+                  label="Beskriv hvordan og hvor lenge personopplysningene skal lagres."
+                  name="lagringsBeskrivelsePersonopplysningene"
+                />
+              </div>
+
+              <div className="mt-5">
+                <Button
+                  type="button"
+                  onClick={() => {
+                    if (submitForm) {
+                      submitForm()
+                    }
+                  }}
+                >
+                  Lagre
+                </Button>
+              </div>
+
+              <FormButtons
+                etterlevelseDokumentasjonId={etterlevelseDokumentasjon.id}
+                activeStep={activeStep}
+                setActiveStep={setActiveStep}
+                submitForm={submitForm}
+              />
+            </div>
           </div>
-
-          <div className="mt-5 max-w-[75ch]">
-            <TextAreaField
-              rows={3}
-              noPlaceholder
-              label="Beskriv hvilke roller som skal ha tilgang til personopplysningene. For hver av rollene, beskriv hvor mange som har tilgang."
-              name="tilgangsBeskrivelsePersonopplysningene"
-            />
-          </div>
-
-          <div className="mt-5 max-w-[75ch]">
-            <TextAreaField
-              rows={3}
-              noPlaceholder
-              label="Beskriv hvordan og hvor lenge personopplysningene skal lagres."
-              name="lagringsBeskrivelsePersonopplysningene"
-            />
-          </div>
-
-          <div className="mt-5">
-            <Button
-              type="button"
-              onClick={() => {
-                if (submitForm) {
-                  submitForm()
-                }
-              }}
-            >
-              Lagre
-            </Button>
-          </div>
-
-          <FormButtons
-            etterlevelseDokumentasjonId={etterlevelseDokumentasjon.id}
-            activeStep={activeStep}
-            setActiveStep={setActiveStep}
-            submitForm={submitForm}
-          />
         </Form>
       )}
     </Formik>
