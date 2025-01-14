52181c228564327afd2fc9efc2d5038372e30c65
K153697 <paul.martin.kokseter@nav.no>
2025-01-14 14:47:49 +0100

    Justere grensesnitt

--- apps/frontend/src/components/PvkDokument/ROSEdit.tsx
+++ apps/frontend/src/components/PvkDokument/ROSEdit.tsx
@@ -1,11 +1,9 @@
 import {Button, TextField, Heading} from '@navikt/ds-react'
 import {FieldArray, FieldArrayRenderProps} from 'formik'
 import {ChangeEvent, useState} from 'react'
-//import {IRegelverk} from '../../constants'
-//import {LovView} from '../Lov'
 import {FieldWrapper} from '../common/Inputs'
 import {Error, FormError} from '../common/ModalSchema'
-//import {RenderTagList} from '../common/TagList'
+import {RenderTagList} from '../common/TagList'
 
 export const ROSEdit = () => {
   const [url, setUrl] = useState('')
@@ -34,48 +32,39 @@ export const ROSEdit = () => {
 
           return (
             <div>
-              <div>
+              <Heading level="2" size="small" spacing>
+                Legg til informasjon on gjeldende ROS-dokumentasjon
+              </Heading>
+              <div className="flex">
+                <TextField
+                  className="w-2/5"
+                  label="Legg inn URL til ROS-dokumentasjon"
+                  value={url}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    setUrl((event.target as HTMLInputElement).value)
+                  }
+                />
+                <TextField
+                  className="w-2/5 mx-2.5"
+                  label="Legg inn navnet til ROS-dokumentet"
+                  value={name}
+                  onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                    setName((event.target as HTMLInputElement).value)}
+                />
                 <div className="flex items-end">
-                  <div className="w-full max-w-[25rem] mr-2.5">
-                    <Heading level="2" size="small" spacing>
-                      Legg til informasjon on gjeldende ROS-dokumentasjon
-                    </Heading>
-                  </div>
-                  <div className="w-full">
-                    <TextField
-                      className="max-w-[25rem] mr-2.5"
-                      label="Legg inn URL til ROS-dokumentasjon"
-                      value={url}
-                      onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                        setUrl((event.target as HTMLInputElement).value)
-                      }
-                    />
-
-                  </div>
-                  <TextField
-                    className="w-full"
-                    label="Legg inn navnet til ROS-dokumentet"
-                    value={name}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) =>
-                      setName((event.target as HTMLInputElement).value)}
-                  />
-                  <div className="min-w-[6.688rem] ml-2.5">
-                    <Button type="button" onClick={add} variant="secondary">
-                      Legg til
-                    </Button>
-                  </div>
+                  <Button type="button" onClick={add} variant="secondary">
+                    Legg til
+                  </Button>
                 </div>
-                {error && <Error message={error}/>}
-                {!error && <FormError fieldName="ros" akselStyling/>}
-
               </div>
 
-              {/*              <RenderTagList
-                list={fieldArrayRenderProps.form.values.regelverk.map((regelverk: IRegelverk) => (
-                  <LovView regelverk={regelverk} key={regelverk.lov.code}/>
-                ))}
+              {error && <Error message={error}/>}
+              {!error && <FormError fieldName="ros" akselStyling/>}
+
+              <RenderTagList
+                list={["ros-1", "ros-2"]}
                 onRemove={fieldArrayRenderProps.remove}
-              />*/}
+              />
             </div>
           )
         }}
