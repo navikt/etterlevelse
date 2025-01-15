7c8204f30a8f575359e020dd958b8b039822a9b6
Andre Groseth <andregroseth@hotmail.com>
2025-01-15 13:54:29 +0100

    ADD: accordion header tags
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/risikoscenario/RisikoscenarioAccordionHeader.tsx
+++ apps/frontend/src/components/risikoscenario/RisikoscenarioAccordionHeader.tsx
@@ -1,5 +1,9 @@
 import { Accordion, Tag } from '@navikt/ds-react'
 import { IRisikoscenario } from '../../constants'
+import RisikoscenarioTag, {
+  getKonsekvenssnivaaText,
+  getSannsynlighetsnivaaText,
+} from './RisikoscenarioTag'
 
 interface IProps {
   risikoscenario: IRisikoscenario
@@ -12,13 +16,29 @@ export const RisikoscenarioAccordianHeader = (props: IProps) => {
     risikoscenario.sannsynlighetsNivaa === 0 ||
     risikoscenario.konsekvensNivaaBegrunnelse === '' ||
     risikoscenario.sannsynlighetsNivaaBegrunnelse === ''
+  const WIP_tiltaksListe = []
 
   return (
     <Accordion.Header>
       {risikoscenario.navn}
       <div className="flex gap-2">
         {ikkeFerdigBeskrevet && <Tag variant="alt2">Det er felter som ikke er ferdig utfylt</Tag>}
-        {!ikkeFerdigBeskrevet && 'ferdig'}
+        {risikoscenario.ingenTiltak && <Tag variant="neutral">Tiltak ikke aktuelt</Tag>}
+        {!risikoscenario.ingenTiltak && WIP_tiltaksListe.length === 0 && (
+          <Tag variant="alt1">Savner tiltak</Tag>
+        )}
+        {!ikkeFerdigBeskrevet && (
+          <RisikoscenarioTag
+            level={risikoscenario.konsekvensNivaa}
+            text={getKonsekvenssnivaaText(risikoscenario.konsekvensNivaa)}
+          />
+        )}
+        {!ikkeFerdigBeskrevet && (
+          <RisikoscenarioTag
+            level={risikoscenario.sannsynlighetsNivaa}
+            text={getSannsynlighetsnivaaText(risikoscenario.sannsynlighetsNivaa)}
+          />
+        )}
       </div>
     </Accordion.Header>
   )
