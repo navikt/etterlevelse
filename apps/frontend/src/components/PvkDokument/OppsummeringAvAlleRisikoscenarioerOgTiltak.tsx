8163d64bf1c1d19fe41d7806946b1f01553ce7cb
Andre Groseth <andregroseth@hotmail.com>
2025-01-16 11:10:43 +0100

    ADD: ToogleGroup and adjust width
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/PvkDokument/OppsummeringAvAlleRisikoscenarioerOgTiltak.tsx
+++ apps/frontend/src/components/PvkDokument/OppsummeringAvAlleRisikoscenarioerOgTiltak.tsx
@@ -1,4 +1,4 @@
-import { BodyShort, Heading, List, Tabs } from '@navikt/ds-react'
+import { BodyShort, Heading, List, Tabs, ToggleGroup } from '@navikt/ds-react'
 import FormButtons from './edit/FormButtons'
 
 interface IProps {
@@ -11,43 +11,48 @@ interface IProps {
 export const OppsummeringAvAlleRisikoscenarioerOgTiltak = (props: IProps) => {
   const { etterlevelseDokumentasjonId, activeStep, setActiveStep, setSelectedStep } = props
   return (
-    <div>
-      <div className="flex justify-center">
-        <div>
-          <Heading level="1" size="medium" className="mb-5">
-            Oppsummering av alle risikoscenarioer og tiltak
-          </Heading>
+    <div className="flex justify-center w-full">
+      <div className="w-full">
+        <Heading level="1" size="medium" className="mb-5">
+          Oppsummering av alle risikoscenarioer og tiltak
+        </Heading>
 
-          <BodyShort className="mt-5">
-            Her får dere oversikt over alle risikoscenarioer og tiltak som er lagt inn. Dere kan
-            velge å se på:
-          </BodyShort>
+        <BodyShort className="mt-5">
+          Her får dere oversikt over alle risikoscenarioer og tiltak som er lagt inn. Dere kan velge
+          å se på:
+        </BodyShort>
 
-          <List>
-            <List.Item>
-              Etterlevelseskrav, og hvilke risikoscenarioer og tiltak som finnes ved hvert krav
-            </List.Item>
-            <List.Item>
-              Etterlevelseskrav, og hvilke risikoscenarioer og tiltak som finnes ved hvert krav
-            </List.Item>
-            <List.Item>Tiltak, inkludert hvilke tiltak som savner ansvarlig eller frist.</List.Item>
-          </List>
-        </div>
-      </div>
+        <List>
+          <List.Item>
+            Etterlevelseskrav, og hvilke risikoscenarioer og tiltak som finnes ved hvert krav
+          </List.Item>
+          <List.Item>
+            Etterlevelseskrav, og hvilke risikoscenarioer og tiltak som finnes ved hvert krav
+          </List.Item>
+          <List.Item>Tiltak, inkludert hvilke tiltak som savner ansvarlig eller frist.</List.Item>
+        </List>
 
-      <Tabs defaultValue="etterlevelseskrav" fill>
-        <Tabs.List>
-          <Tabs.Tab value="risikoscenarioer" label="Vis risikoscenarioer" />
-          <Tabs.Tab value="tiltak" label=" Vis tiltak " />
-        </Tabs.List>
-        <Tabs.Panel value="risikoscenarioer" className="h-24 w-full bg-gray-50 p-4">
-          Her skal risikoscenarioer vises
-        </Tabs.Panel>
-        <Tabs.Panel value="tiltak" className="h-24 w-full bg-gray-50 p-4">
-          Her skal tiltak vises
-        </Tabs.Panel>
-      </Tabs>
-      <div className="flex justify-center">
+        <Tabs defaultValue="risikoscenarioer" fill>
+          <Tabs.List>
+            <Tabs.Tab value="risikoscenarioer" label="Vis risikoscenarioer" />
+            <Tabs.Tab value="tiltak" label=" Vis tiltak " />
+          </Tabs.List>
+          <Tabs.Panel value="risikoscenarioer" className="h-24 w-full justify-center">
+            <ToggleGroup defaultValue="Alle risikoscenarioer" onChange={console.debug} fill>
+              <ToggleGroup.Item value="Alle risikoscenarioer" label="Alle risikoscenarioer" />
+              <ToggleGroup.Item value="" label="Effekt ikke vurdert" />
+              <ToggleGroup.Item value="" label="Høy risiko" />
+              <ToggleGroup.Item value="" label="Blir ikke tiltak" />
+              <ToggleGroup.Item value="" label="Ferdig" />
+            </ToggleGroup>
+            <br />
+            Her skal risikoscenarioer vises
+          </Tabs.Panel>
+          <Tabs.Panel value="tiltak" className="h-24 w-full">
+            Her skal tiltak vises
+          </Tabs.Panel>
+        </Tabs>
+
         <div>
           <FormButtons
             etterlevelseDokumentasjonId={etterlevelseDokumentasjonId}
