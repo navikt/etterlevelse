72010dafdf9dd43d02e2eccdbe78e1079222acc9
Movafagh <farjam.movafagh@nav.no>
2025-01-20 10:43:57 +0100

    wip

--- apps/backend/src/main/resources/db/migration/V1.30__tiltak.sql
+++ apps/backend/src/main/resources/db/migration/V1.30__tiltak.sql
@@ -13,12 +13,11 @@ create table if not exists tiltak
 
 create index if not exists idx_tiltak_pvk_dokument_id on tiltak (pvk_dokument_id);
 
-create table if not exists risikoscenario_tiltak
+create table if not exists risikoscenario_tiltak_relation
 (
     risikoscenario      uuid references risikoscenario(id),
     tiltak              uuid references tiltak(id)
 );
 
-create index if not exists idx_risikoscenario_tiltak_risikoscenario on risikoscenario_tiltak (risikoscenario);
-create index if not exists idx_risikoscenario_tiltak_tiltak on risikoscenario_tiltak (tiltak);
-
+create index if not exists idx_risikoscenario_tiltak_risikoscenario on risikoscenario_tiltak_relation (risikoscenario);
+create index if not exists idx_risikoscenario_tiltak_tiltak on risikoscenario_tiltak_relation (tiltak);
