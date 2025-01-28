2aa60215d3fb2a95ee2d25bd8c5b4ab1f2f16b33
JeremiahUy <jeremiah.uy@outlook.com>
2025-01-28 10:02:23 +0100

    UPDATE: link and tab logic for default tabs if pvk document is present
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/components/PvkDokument/IdentifiseringAvRisikoscenarioerOgTiltak.tsx
+++ apps/frontend/src/components/PvkDokument/IdentifiseringAvRisikoscenarioerOgTiltak.tsx
@@ -74,7 +74,7 @@ export const IdentifiseringAvRisikoscenarioerOgTiltak = (props: IProps) => {
           type="button"
           onClick={() => {
             if (etterlevelseDokumentasjonId)
-              navigate('/dokumentasjon/' + etterlevelseDokumentasjonId)
+              navigate('/dokumentasjon/' + etterlevelseDokumentasjonId + '?tab=pvk')
           }}
         >
           Gå til liste over PVK-relaterte krav
