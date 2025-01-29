d91aae8aedb580950dc174317e1dda6af2a3355e
JeremiahUy <jeremiah.uy@outlook.com>
2025-01-29 14:44:15 +0100

    UPDATE: Web security config for tiltak endpoint

--- apps/backend/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
+++ apps/backend/src/main/java/no/nav/data/common/security/WebSecurityConfig.java
@@ -76,6 +76,7 @@ public class WebSecurityConfig {
                     "/documentrelation/**",
                     "/pvkdokument/**",
                     "/risikoscenario/**",
+                    "/tiltak/**",
                     "/virkemiddel/**"
             );
 
@@ -102,6 +103,7 @@ public class WebSecurityConfig {
             http.authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("/etterlevelsearkiv/**")).hasAnyRole(AppRole.WRITE.name()));
             http.authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("/pvkdokument/**")).hasAnyRole(AppRole.WRITE.name(), AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
             http.authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("/risikoscenario/**")).hasAnyRole(AppRole.WRITE.name(), AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
+            http.authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("/tiltak/**")).hasAnyRole(AppRole.WRITE.name(), AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
             http.authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("/virkemiddel/**")).hasAnyRole(AppRole.KRAVEIER.name(), AppRole.ADMIN.name()));
 
             http.authorizeHttpRequests(auth -> auth.requestMatchers(antMatcher("/logout")).authenticated());
