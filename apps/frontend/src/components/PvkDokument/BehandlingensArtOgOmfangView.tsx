64747d0043c7daca8e675dde6c1f83328f9c2ecc
beeccy <beeccy@users.noreply.github.com>
2025-02-25 09:38:21 +0100

    BehandlingensArtOgOmfangView.tsx Lagt inn readonly

--- apps/frontend/src/components/PvkDokument/BehandlingensArtOgOmfangView.tsx
+++ apps/frontend/src/components/PvkDokument/BehandlingensArtOgOmfangView.tsx
@@ -69,128 +69,184 @@ export const BehandlingensArtOgOmfangView = (props: IProps) => {
 
   return (
     <div className="flex w-full">
-      <div className="pt-6 pr-4 flex flex-1 flex-col gap-4 col-span-8">
-        <Formik
-          validateOnChange={false}
-          validateOnBlur={false}
-          onSubmit={submit}
-          initialValues={mapPvkDokumentToFormValue(pvkDokument as IPvkDokument)}
-          innerRef={formRef}
-        >
-          {({ submitForm }) => (
-            <Form>
-              <div className="flex justify-center">
-                <div>
-                  <Heading level="1" size="medium" className="mb-5">
-                    Behandlingens art og omfang
-                  </Heading>
-                  {pvkDokument.changeStamp.lastModifiedBy && (
-                    <div className="mt-5 mb-10">
-                      {'Sist redigert av: ' + pvkDokument.changeStamp.lastModifiedBy}
-                    </div>
-                  )}
-
-                  <List
-                    headingTag="label"
-                    title="I Behandlingskatalogen står det at dere behandler personopplysninger om:"
-                  >
-                    {personkategorier.length === 0 && <List.Item>Ingen</List.Item>}
-                    {personkategorier.length > 0 &&
-                      personkategorier.map((personkategori) => (
-                        <List.Item key={personkategori}>{personkategori}</List.Item>
-                      ))}
-                  </List>
-
-                  <BoolField
-                    label="Stemmer denne lista over personkategorier?"
-                    name="stemmerPersonkategorier"
-                    horizontal
-                  />
-
-                  <Field>
-                    {(fieldProps: FieldProps) => (
-                      <>
-                        {fieldProps.form.values.stemmerPersonkategorier === false && (
-                          <div className="max-w-[75ch]">
-                            <Alert inline variant="warning" className="mt-5 mb-10">
-                              Dere må oppdatere personkategori(er) i Behandlingskatalogen. Hvis dere
-                              ikke finner riktig personkategori(er), ta kontakt på{' '}
-                              <Link
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                href="https://nav-it.slack.com/archives/CR1B19E6L"
-                                className="inline"
-                              >
-                                #behandlingskatalogen på Slack (åpner i en ny fane)
-                              </Link>
-                              , eller på epost: teamdatajegerne@nav.no.
-                            </Alert>
-                          </div>
-                        )}
-                      </>
+      {(!user.isPersonvernombud() || user.isAdmin()) && (
+        <div className="pt-6 pr-4 flex flex-1 flex-col gap-4 col-span-8">
+          <Formik
+            validateOnChange={false}
+            validateOnBlur={false}
+            onSubmit={submit}
+            initialValues={mapPvkDokumentToFormValue(pvkDokument as IPvkDokument)}
+            innerRef={formRef}
+          >
+            {({ submitForm }) => (
+              <Form>
+                <div className="flex justify-center">
+                  <div>
+                    <Heading level="1" size="medium" className="mb-5">
+                      Behandlingens art og omfang
+                    </Heading>
+                    {pvkDokument.changeStamp.lastModifiedBy && (
+                      <div className="mt-5 mb-10">
+                        {'Sist redigert av: ' + pvkDokument.changeStamp.lastModifiedBy}
+                      </div>
                     )}
-                  </Field>
 
-                  <ReadMore
-                    className="mt-5 max-w-[75ch]"
-                    header="Hvordan kan vi komme med gode estimater på art og omfang?"
-                  >
-                    Det blir ofte vanskelig å tallfeste noen personkategorier, for eksempel når det
-                    er snakk om antall brukere eller saksbehandlere. Det er tilstrekkelig å oppgi
-                    ca. antall.
-                  </ReadMore>
-
-                  <div className="mt-5 max-w-[75ch]">
-                    <TextAreaField
-                      rows={3}
-                      noPlaceholder
-                      label="For hver av personkategoriene over, beskriv hvor mange personer dere behandler personopplysninger om."
-                      name="personkategoriAntallBeskrivelse"
-                    />
-                  </div>
-
-                  <div className="mt-5 max-w-[75ch]">
-                    <TextAreaField
-                      rows={3}
-                      noPlaceholder
-                      label="Beskriv hvilke roller som skal ha tilgang til personopplysningene. For hver av rollene, beskriv hvor mange som har tilgang."
-                      name="tilgangsBeskrivelsePersonopplysningene"
-                    />
-                  </div>
-
-                  <div className="mt-5 max-w-[75ch]">
-                    <TextAreaField
-                      rows={3}
-                      noPlaceholder
-                      label="Beskriv hvordan og hvor lenge personopplysningene skal lagres."
-                      name="lagringsBeskrivelsePersonopplysningene"
-                    />
-                  </div>
-
-                  <div className="mt-5">
-                    <Button
-                      type="button"
-                      onClick={() => {
-                        submitForm()
-                      }}
+                    <List
+                      headingTag="label"
+                      title="I Behandlingskatalogen står det at dere behandler personopplysninger om:"
                     >
-                      Lagre
-                    </Button>
-                  </div>
+                      {personkategorier.length === 0 && <List.Item>Ingen</List.Item>}
+                      {personkategorier.length > 0 &&
+                        personkategorier.map((personkategori) => (
+                          <List.Item key={personkategori}>{personkategori}</List.Item>
+                        ))}
+                    </List>
 
-                  <FormButtons
-                    etterlevelseDokumentasjonId={etterlevelseDokumentasjon.id}
-                    activeStep={activeStep}
-                    setActiveStep={setActiveStep}
-                    setSelectedStep={setSelectedStep}
-                    submitForm={submitForm}
-                  />
+                    <BoolField
+                      label="Stemmer denne lista over personkategorier?"
+                      name="stemmerPersonkategorier"
+                      horizontal
+                    />
+
+                    <Field>
+                      {(fieldProps: FieldProps) => (
+                        <>
+                          {fieldProps.form.values.stemmerPersonkategorier === false && (
+                            <div className="max-w-[75ch]">
+                              <Alert inline variant="warning" className="mt-5 mb-10">
+                                Dere må oppdatere personkategori(er) i Behandlingskatalogen. Hvis
+                                dere ikke finner riktig personkategori(er), ta kontakt på{' '}
+                                <Link
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                  href="https://nav-it.slack.com/archives/CR1B19E6L"
+                                  className="inline"
+                                >
+                                  #behandlingskatalogen på Slack (åpner i en ny fane)
+                                </Link>
+                                , eller på epost: teamdatajegerne@nav.no.
+                              </Alert>
+                            </div>
+                          )}
+                        </>
+                      )}
+                    </Field>
+
+                    <ReadMore
+                      className="mt-5 max-w-[75ch]"
+                      header="Hvordan kan vi komme med gode estimater på art og omfang?"
+                    >
+                      Det blir ofte vanskelig å tallfeste noen personkategorier, for eksempel når
+                      det er snakk om antall brukere eller saksbehandlere. Det er tilstrekkelig å
+                      oppgi ca. antall.
+                    </ReadMore>
+
+                    <div className="mt-5 max-w-[75ch]">
+                      <TextAreaField
+                        rows={3}
+                        noPlaceholder
+                        label="For hver av personkategoriene over, beskriv hvor mange personer dere behandler personopplysninger om."
+                        name="personkategoriAntallBeskrivelse"
+                      />
+                    </div>
+
+                    <div className="mt-5 max-w-[75ch]">
+                      <TextAreaField
+                        rows={3}
+                        noPlaceholder
+                        label="Beskriv hvilke roller som skal ha tilgang til personopplysningene. For hver av rollene, beskriv hvor mange som har tilgang."
+                        name="tilgangsBeskrivelsePersonopplysningene"
+                      />
+                    </div>
+
+                    <div className="mt-5 max-w-[75ch]">
+                      <TextAreaField
+                        rows={3}
+                        noPlaceholder
+                        label="Beskriv hvordan og hvor lenge personopplysningene skal lagres."
+                        name="lagringsBeskrivelsePersonopplysningene"
+                      />
+                    </div>
+
+                    <div className="mt-5">
+                      <Button
+                        type="button"
+                        onClick={() => {
+                          submitForm()
+                        }}
+                      >
+                        Lagre
+                      </Button>
+                    </div>
+
+                    <FormButtons
+                      etterlevelseDokumentasjonId={etterlevelseDokumentasjon.id}
+                      activeStep={activeStep}
+                      setActiveStep={setActiveStep}
+                      setSelectedStep={setSelectedStep}
+                      submitForm={submitForm}
+                    />
+                  </div>
                 </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      )}
+
+      {(user.isPersonvernombud() || user.isAdmin()) && (
+        <div className="pt-6 pr-4 flex flex-1 flex-col gap-4 col-span-8">
+          <div className="flex justify-center">
+            <div>
+              <Heading level="1" size="medium" className="mb-5">
+                Behandlingens art og omfang
+              </Heading>
+              {pvkDokument.changeStamp.lastModifiedBy && (
+                <div className="mt-5 mb-10">
+                  {'Sist redigert av: ' + pvkDokument.changeStamp.lastModifiedBy}
+                </div>
+              )}
+
+              <List
+                headingTag="label"
+                title="I Behandlingskatalogen står det at dere behandler personopplysninger om:"
+              >
+                {personkategorier.length === 0 && <List.Item>Ingen</List.Item>}
+                {personkategorier.length > 0 &&
+                  personkategorier.map((personkategori) => (
+                    <List.Item key={personkategori}>{personkategori}</List.Item>
+                  ))}
+              </List>
+
+              <div className="mt-5 mb-3 max-w-[75ch]">
+                <Label>Stemmer denne lista over personkategorier?</Label>
+                <BodyLong>{pvkDokument.stemmerPersonkategorier}</BodyLong>
               </div>
-            </Form>
-          )}
-        </Formik>
-      </div>
+
+              <div className="mt-5 mb-3 max-w-[75ch]">
+                <Label>
+                  For hver av personkategoriene over, beskriv hvor mange personer dere behandler
+                  personopplysninger om.
+                </Label>
+                <BodyLong>{pvkDokument.personkategoriAntallBeskrivelse}</BodyLong>
+              </div>
+
+              <div className="mt-5 mb-3 max-w-[75ch]">
+                <Label>
+                  Beskriv hvilke roller som skal ha tilgang til personopplysningene. For hver av
+                  rollene, beskriv hvor mange som har tilgang.
+                </Label>
+                <BodyLong>{pvkDokument.tilgangsBeskrivelsePersonopplysningene}</BodyLong>
+              </div>
+
+              <div className="mt-5 mb-3 max-w-[75ch]">
+                <Label>Beskriv hvordan og hvor lenge personopplysningene skal lagres.</Label>
+                <BodyLong>{pvkDokument.lagringsBeskrivelsePersonopplysningene}</BodyLong>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
       {/* PVO sidepanel */}
       {(user.isPersonvernombud() || user.isAdmin()) && (
