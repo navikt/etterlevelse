83b35cf85d44efa317521fd4c8724ae773803750
beeccy <beeccy@users.noreply.github.com>
2025-03-05 10:35:52 +0100

    Lagt inn ingentiltak

--- apps/frontend/src/components/risikoscenario/kravRisikoscenario/KravRisikoscenarioAccordionContentReadOnly.tsx
+++ apps/frontend/src/components/risikoscenario/kravRisikoscenario/KravRisikoscenarioAccordionContentReadOnly.tsx
@@ -1,8 +1,8 @@
-import {Heading} from "@navikt/ds-react";
-import {IRisikoscenario, ITiltak} from "../../../constants";
-import TiltakView from "../../tiltak/TiltakView";
-import RisikoscenarioViewReadOnly from "../RisikoscenarioViewReadOnly";
-
+import { Heading } from '@navikt/ds-react'
+import { IRisikoscenario, ITiltak } from '../../../constants'
+import TiltakView from '../../tiltak/TiltakView'
+import RisikoscenarioViewReadOnly from '../RisikoscenarioViewReadOnly'
+import { KravRisikoscenarioIngenTiltak } from './KravRisikoscenarioIngenTiltak/KravRisikoscenarioIngenTiltak'
 
 interface IProps {
   risikoscenario: IRisikoscenario
@@ -10,43 +10,36 @@ interface IProps {
   tiltakList: ITiltak[]
 }
 
-export const KravRisikoscenarioAccordionContentReadOnly = ({risikoscenario, alleRisikoscenarioer, tiltakList}: IProps) => (
+export const KravRisikoscenarioAccordionContentReadOnly = ({
+  risikoscenario,
+  alleRisikoscenarioer,
+  tiltakList,
+}: IProps) => {
+  const filterTiltakId: ITiltak[] = tiltakList.filter((tiltak: ITiltak) =>
+    risikoscenario.tiltakIds.includes(tiltak.id)
+  )
 
-  <div>
-    <RisikoscenarioViewReadOnly risikoscenario={risikoscenario} noCopyButton={true}/>
+  return (
+    <div>
+      <RisikoscenarioViewReadOnly risikoscenario={risikoscenario} noCopyButton={true} />
 
-    <div className="mt-12">
-      <Heading level="3" size="small">
-        Følgende tiltak gjelder for dette risikoscenarioet
-      </Heading>
+      <div className="mt-12">
+        <Heading level="3" size="small">
+          Følgende tiltak gjelder for dette risikoscenarioet
+        </Heading>
 
-      <div>
-        {tiltakList
-          .filter((tiltak) => risikoscenario.tiltakIds.includes(tiltak.id))
-          .map((tiltak, index) => {
-            return (
-              <div className="mt-3" key={risikoscenario.id + '_' + tiltak.id + '_' + index}>
-                {!risikoscenario.ingenTiltak && (
-                  <div>
-                    {risikoscenario.tiltakIds.length !== 0 && (
-                      <TiltakView tiltak={tiltak} risikoscenarioList={alleRisikoscenarioer}/>
-                    )}
+        {filterTiltakId.map((tiltak: ITiltak, index: number) => (
+          <div className="mt-3" key={risikoscenario.id + '_' + tiltak.id + '_' + index}>
+            {!risikoscenario.ingenTiltak && risikoscenario.tiltakIds.length !== 0 && (
+              <TiltakView tiltak={tiltak} risikoscenarioList={alleRisikoscenarioer} />
+            )}
+          </div>
+        ))}
 
-                  </div>
-                )}
-              </div>
-            )
-          })}
+        <KravRisikoscenarioIngenTiltak risikoscenario={risikoscenario} />
       </div>
-
-
-      <div className="mt-3">
-        ingenTiltak
-      </div>
-
     </div>
-  </div>
-)
+  )
+}
 
 export default KravRisikoscenarioAccordionContentReadOnly
-
