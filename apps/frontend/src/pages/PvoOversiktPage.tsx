cf3807a2e51d8200673006cd241b6806a1502c35
beeccy <beeccy@users.noreply.github.com>
2025-03-13 13:14:33 +0100

    Fikset styling oppsett til å match KravList og gjort ListPageHeader children optional
    Co-authored-by: Andre Groseth <lord.andre.groseth@nav.no>

--- apps/frontend/src/pages/PvoOversiktPage.tsx
+++ apps/frontend/src/pages/PvoOversiktPage.tsx
@@ -2,6 +2,7 @@ import { Loader } from '@navikt/ds-react'
 import { useEffect, useState } from 'react'
 import { getAllPvkDokumentListItem } from '../api/PvkDokumentApi'
 import PvoTabs from '../components/PvkDokument/PvoTabs'
+import { ListPageHeader } from '../components/scaffold/ListPageHeader'
 import { PageLayout } from '../components/scaffold/Page'
 import { IPvkDokumentListItem } from '../constants'
 
@@ -24,16 +25,19 @@ export const PvoOversiktPage = () => {
       currentPage="Oversikts side for Personvernsombudet"
     >
       <div className="pb-52 w-full">
+        <ListPageHeader headingText="Oversiktsside for Personvernombudet" />
         {isLoading && (
           <div className="flex w-full justify-center items-center mt-5">
             <Loader size="3xlarge" className="flex justify-self-center" />
           </div>
         )}
         {!isLoading && allPvkDocumentListItem.length !== 0 && (
-          <div className="w-full">
-            {allPvkDocumentListItem[0].title}
-
-            <PvoTabs />
+          <div className="flex justify-center w-full">
+            <div className="w-full">
+              <div className="pt-6">
+                <PvoTabs />
+              </div>
+            </div>
           </div>
         )}
       </div>
