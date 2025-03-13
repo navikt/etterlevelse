7201393d59a0c07de3e83ebea7b8d9067b1ac28d
Andre Groseth <andregroseth@hotmail.com>
2025-03-13 11:19:43 +0100

    ADD: loader and allPvkDocumentListItem
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/pages/PvoOversiktPage.tsx
+++ apps/frontend/src/pages/PvoOversiktPage.tsx
@@ -1,12 +1,37 @@
+import { Loader } from '@navikt/ds-react'
+import { useEffect, useState } from 'react'
+import { getAllPvkDokumentListItem } from '../api/PvkDokumentApi'
 import { PageLayout } from '../components/scaffold/Page'
+import { IPvkDokumentListItem } from '../constants'
 
 export const PvoOversiktPage = () => {
+  const [allPvkDocumentListItem, setAllPvkDocumentListItem] = useState<IPvkDokumentListItem[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    ;(async () => {
+      setIsLoading(true)
+      await getAllPvkDokumentListItem().then((response) => {
+        setAllPvkDocumentListItem(response)
+        setIsLoading(false)
+      })
+    })()
+  }, [])
+
   return (
     <PageLayout
       pageTitle="Oversikts side for Personvernsombudet"
       currentPage="Oversikts side for Personvernsombudet"
     >
-      <div className="pb-52 w-full">test</div>
+      <div className="pb-52 w-full">
+        {isLoading && (
+          <div className="flex w-full justify-center items-center mt-5">
+            <Loader size="3xlarge" className="flex justify-self-center" />
+          </div>
+        )}
+        {!isLoading && allPvkDocumentListItem.length !== 0 && (
+          <div className="w-full">{allPvkDocumentListItem[0].title}</div>
+        )}
+      </div>
     </PageLayout>
   )
 }
