028171286b6546285dbd7e71310ef1113abcc233
beeccy <beeccy@users.noreply.github.com>
2025-03-14 10:58:05 +0100

    Laget en felles ListLayout for PVO og Krav
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/PvkDokument/PvoTilbakemeldingsList.tsx
+++ apps/frontend/src/components/PvkDokument/PvoTilbakemeldingsList.tsx
@@ -1,6 +1,7 @@
-import { BodyLong, BodyShort, Label, LinkPanel, List, Skeleton, Spacer } from '@navikt/ds-react'
+import { List, Skeleton } from '@navikt/ds-react'
 import moment from 'moment'
 import { IPvkDokumentListItem } from '../../constants'
+import { ListLayout } from '../ListLayout'
 import StatusView from '../common/StatusTag'
 
 interface IProps {
@@ -14,41 +15,27 @@ export const PvoTilbakemeldingsList = ({ allPvkDocumentListItem, isLoading }: IP
     {!isLoading && (
       <List className="mb-2.5 flex flex-col gap-2">
         {allPvkDocumentListItem &&
-          allPvkDocumentListItem.map((pvkDokument: IPvkDokumentListItem) => {
-            return (
-              <List.Item icon={<div />} className="mb-0" key={pvkDokument.id}>
-                <LinkPanel href={`/pvo/pvkdokument/${pvkDokument.id}/tilbakemelding/1`}>
-                  <LinkPanel.Title className="flex items-center">
-                    <div className="max-w-xl">
-                      <BodyShort size="small">E{pvkDokument.etterlevelseNummer}</BodyShort>
-                      <BodyLong>
-                        <Label>{pvkDokument.title}</Label>
-                      </BodyLong>
-                    </div>
-                    <Spacer />
-                    <div className="mr-5">
-                      <StatusView
-                        status={
-                          pvkDokument.status === 'AKTIV' ? 'Under arbeid' : pvkDokument.status
-                        }
-                      />
-                    </div>
-                    <div className="w-44">
-                      <BodyShort size="small" className="break-words">
-                        PVK dokument ble
-                      </BodyShort>
-                      <BodyShort size="small">
-                        {pvkDokument.changeStamp.lastModifiedDate !== undefined &&
-                        pvkDokument.changeStamp.lastModifiedDate !== ''
-                          ? `sist endret: ${moment(pvkDokument.changeStamp.lastModifiedDate).format('ll')}`
-                          : ''}
-                      </BodyShort>
-                    </div>
-                  </LinkPanel.Title>
-                </LinkPanel>
-              </List.Item>
-            )
-          })}
+          allPvkDocumentListItem.map((pvkDokument: IPvkDokumentListItem) => (
+            <ListLayout
+              key={pvkDokument.id}
+              id={pvkDokument.id}
+              url={`/pvo/pvkdokument/${pvkDokument.id}/tilbakemelding/1`}
+              documentNumber={`E${pvkDokument.etterlevelseNummer}`}
+              title={pvkDokument.title}
+              status={
+                <StatusView
+                  status={pvkDokument.status === 'AKTIV' ? 'Under arbeid' : pvkDokument.status}
+                />
+              }
+              upperRightField="PVK dokument ble"
+              changeStamp={
+                pvkDokument.changeStamp.lastModifiedDate !== undefined &&
+                pvkDokument.changeStamp.lastModifiedDate !== ''
+                  ? `sist endret: ${moment(pvkDokument.changeStamp.lastModifiedDate).format('ll')}`
+                  : ''
+              }
+            />
+          ))}
       </List>
     )}
   </div>
