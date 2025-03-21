4ede30881fb39832f34a6d03dae321d3abe6a4cf
beeccy <beeccy@users.noreply.github.com>
2025-03-21 09:33:15 +0100

    Endret litt på props

--- apps/frontend/src/components/risikoscenario/kravRisikoscenario/KravRisikoscenarioAccordionContent/KravRisikoscenarioAccordionContentLimitedReadonly.tsx
+++ apps/frontend/src/components/risikoscenario/kravRisikoscenario/KravRisikoscenarioAccordionContent/KravRisikoscenarioAccordionContentLimitedReadonly.tsx
@@ -32,99 +32,94 @@ interface IProps {
   setIsIngenTilgangFormDirty: Dispatch<SetStateAction<boolean>>
 }
 
-export const KravRisikoscenarioAccordionContentLimitedReadonly = (props: IProps) => {
-  const {
-    activeRisikoscenario,
-    isIngenTilgangFormDirty,
-    userHasAccess,
-    setIsEditModalOpen,
-    kravnummer,
-    risikoscenario,
-    risikoscenarioer,
-    setRisikoscenarioer,
-    risikoscenarioForKrav,
-    setRisikoscenarioForKrav,
-    alleRisikoscenarioer,
-    tiltakList,
-    setTiltakList,
-    setIsEditTiltakFormActive,
-    isCreateTiltakFormActive,
-    isAddExistingMode,
-    submitDeleteTiltak,
-    formRef,
-    setIsCreateTiltakFormActive,
-    setIsAddExisitingMode,
-    submitIngenTiltak,
-    setIsIngenTilgangFormDirty,
-  } = props
-  return (
-    <div>
-      <RisikoscenarioView risikoscenario={activeRisikoscenario} noCopyButton={true} />
+export const KravRisikoscenarioAccordionContentLimitedReadonly = ({
+  activeRisikoscenario,
+  isIngenTilgangFormDirty,
+  userHasAccess,
+  setIsEditModalOpen,
+  kravnummer,
+  risikoscenario,
+  risikoscenarioer,
+  setRisikoscenarioer,
+  risikoscenarioForKrav,
+  setRisikoscenarioForKrav,
+  alleRisikoscenarioer,
+  tiltakList,
+  setTiltakList,
+  setIsEditTiltakFormActive,
+  isCreateTiltakFormActive,
+  isAddExistingMode,
+  submitDeleteTiltak,
+  formRef,
+  setIsCreateTiltakFormActive,
+  setIsAddExisitingMode,
+  submitIngenTiltak,
+  setIsIngenTilgangFormDirty,
+}: IProps) => (
+  <div>
+    <RisikoscenarioView risikoscenario={activeRisikoscenario} noCopyButton={true} />
 
-      {!isIngenTilgangFormDirty && userHasAccess() && (
-        <RedigerRisikoscenarioButtons
-          setIsEditModalOpen={setIsEditModalOpen}
-          kravnummer={kravnummer}
-          risikoscenario={risikoscenario}
-          risikoscenarioer={risikoscenarioer}
-          setRisikoscenarioer={setRisikoscenarioer}
-          risikoscenarioForKrav={risikoscenarioForKrav}
-          setRisikoscenarioForKrav={setRisikoscenarioForKrav}
-        />
+    {!isIngenTilgangFormDirty && userHasAccess() && (
+      <RedigerRisikoscenarioButtons
+        setIsEditModalOpen={setIsEditModalOpen}
+        kravnummer={kravnummer}
+        risikoscenario={risikoscenario}
+        risikoscenarioer={risikoscenarioer}
+        setRisikoscenarioer={setRisikoscenarioer}
+        risikoscenarioForKrav={risikoscenarioForKrav}
+        setRisikoscenarioForKrav={setRisikoscenarioForKrav}
+      />
+    )}
+
+    <div className="mt-12">
+      <KravRisikoscenarioAccordionContentHeader />
+
+      {!risikoscenario.ingenTiltak && userHasAccess() && (
+        <div>
+          {risikoscenario.tiltakIds.length !== 0 && (
+            <TiltakReadMoreList
+              risikoscenario={activeRisikoscenario}
+              risikoscenarioList={alleRisikoscenarioer}
+              tiltakList={tiltakList}
+              setTiltakList={setTiltakList}
+              setIsEditTiltakFormActive={setIsEditTiltakFormActive}
+              isCreateTiltakFormActive={isCreateTiltakFormActive}
+              isAddExistingMode={isAddExistingMode}
+              customDelete={submitDeleteTiltak}
+              formRef={formRef}
+            />
+          )}
+
+          {!isIngenTilgangFormDirty && (
+            <div className="mt-5 flex gap-2 lg:flex-row flex-col">
+              <Button size="small" type="button" onClick={() => setIsCreateTiltakFormActive(true)}>
+                Opprett nytt tiltak
+              </Button>
+              <Button
+                type="button"
+                size="small"
+                variant="secondary"
+                onClick={() => setIsAddExisitingMode(true)}
+              >
+                Legg til eksisterende tiltak
+              </Button>
+            </div>
+          )}
+        </div>
       )}
 
-      <div className="mt-12">
-        <KravRisikoscenarioAccordionContentHeader />
-
-        {!risikoscenario.ingenTiltak && userHasAccess() && (
-          <div>
-            {risikoscenario.tiltakIds.length !== 0 && (
-              <TiltakReadMoreList
-                risikoscenario={activeRisikoscenario}
-                risikoscenarioList={alleRisikoscenarioer}
-                tiltakList={tiltakList}
-                setTiltakList={setTiltakList}
-                setIsEditTiltakFormActive={setIsEditTiltakFormActive}
-                isCreateTiltakFormActive={isCreateTiltakFormActive}
-                isAddExistingMode={isAddExistingMode}
-                customDelete={submitDeleteTiltak}
-                formRef={formRef}
-              />
-            )}
-
-            {!isIngenTilgangFormDirty && (
-              <div className="mt-5 flex gap-2 lg:flex-row flex-col">
-                <Button
-                  size="small"
-                  type="button"
-                  onClick={() => setIsCreateTiltakFormActive(true)}
-                >
-                  Opprett nytt tiltak
-                </Button>
-                <Button
-                  type="button"
-                  size="small"
-                  variant="secondary"
-                  onClick={() => setIsAddExisitingMode(true)}
-                >
-                  Legg til eksisterende tiltak
-                </Button>
-              </div>
-            )}
-          </div>
-        )}
-
-        {activeRisikoscenario.tiltakIds.length === 0 && (
-          <div className="mt-3">
-            <IngenTiltakField
-              risikoscenario={activeRisikoscenario}
-              submit={submitIngenTiltak}
-              formRef={formRef}
-              setIsIngenTilgangFormDirty={setIsIngenTilgangFormDirty}
-            />
-          </div>
-        )}
-      </div>
+      {activeRisikoscenario.tiltakIds.length === 0 && (
+        <div className="mt-3">
+          <IngenTiltakField
+            risikoscenario={activeRisikoscenario}
+            submit={submitIngenTiltak}
+            formRef={formRef}
+            setIsIngenTilgangFormDirty={setIsIngenTilgangFormDirty}
+          />
+        </div>
+      )}
     </div>
-  )
-}
+  </div>
+)
+
+export default KravRisikoscenarioAccordionContentLimitedReadonly
