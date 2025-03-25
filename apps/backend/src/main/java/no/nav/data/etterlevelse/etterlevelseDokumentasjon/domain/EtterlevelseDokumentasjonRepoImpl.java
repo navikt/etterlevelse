734bab3fa14792d2848f3008cbb00a06d72c2814
JeremiahUy <jeremiah.uy@outlook.com>
2025-03-25 11:33:47 +0100

    FIX: typos

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/EtterlevelseDokumentasjonRepoImpl.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/EtterlevelseDokumentasjonRepoImpl.java
@@ -3,7 +3,7 @@ package no.nav.data.etterlevelse.etterlevelseDokumentasjon.domain;
 import lombok.RequiredArgsConstructor;
 import no.nav.data.common.security.SecurityUtils;
 import no.nav.data.etterlevelse.etterlevelseDokumentasjon.dto.EtterlevelseDokumentasjonFilter;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,13 +12,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import static no.nav.data.common.utils.StreamUtils.convert;
-
 @Repository
 @RequiredArgsConstructor
 public class EtterlevelseDokumentasjonRepoImpl implements EtterlevelseDokumentasjonRepoCustom {
 
-    private final JpaRepository<EtterlevelseDokumentasjon, UUID> repository;
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public List<EtterlevelseDokumentasjon> findByIrrelevans(List<String> codes) {
@@ -115,6 +112,12 @@ public class EtterlevelseDokumentasjonRepoImpl implements EtterlevelseDokumentas
     }
 
     private List<EtterlevelseDokumentasjon> fetch(List<Map<String, Object>> resp) {
-        return repository.findAllById(convert(resp, i -> (UUID) i.values().iterator().next()));
+        List<UUID> ids = resp.stream().map(i -> ((UUID) i.values().iterator().next())).toList();
+        if (ids.isEmpty()) {
+            return List.of();
+        }
+        String query = "select * from etterlevelse_dokumentasjon where id in ( :ids )";
+        var par = Map.of("ids", ids);
+        return jdbcTemplate.query(query, par, new BeanPropertyRowMapper<>(EtterlevelseDokumentasjon.class));
     }
 }
