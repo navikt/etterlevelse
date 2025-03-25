31d51ef29755a1bae6f6b58a09ab597b84e4ddbd
JeremiahUy <jeremiah.uy@outlook.com>
2025-03-25 12:50:37 +0100

    UPDATE: put checking for current user access into try catch

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/EtterlevelseDokumentasjonController.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/EtterlevelseDokumentasjonController.java
@@ -18,15 +18,7 @@ import no.nav.data.integration.team.dto.MemberResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -67,7 +59,6 @@ public class EtterlevelseDokumentasjonController {
             response.setHasCurrentUserAccess(true);
         } else {
             List<String> memeberList = new ArrayList<>();
-            var currentUser = SecurityUtils.getCurrentIdent();
             if (!resourceIsEmpty) {
                 memeberList.addAll(response.getResources());
             }
@@ -81,7 +72,12 @@ public class EtterlevelseDokumentasjonController {
                     }
                 });
             }
-            response.setHasCurrentUserAccess(memeberList.contains(currentUser));
+            try {
+                String currentUser = SecurityUtils.getCurrentIdent();
+                response.setHasCurrentUserAccess(memeberList.contains(currentUser));
+            } catch (ValidationException e) {
+                response.setHasCurrentUserAccess(false);
+            }
         }
 
         return ResponseEntity.ok(response);
