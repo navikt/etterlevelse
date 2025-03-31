c8b5c156bb906d8e2e6bbfb62e05bd6f536b1c66
Jeremiah Uy <jeremiah.augie.uy@nav.no>
2025-03-31 13:11:50 +0200

    ADD: graphql logci for pvo tilbakemelding (#1152)
    * ADD: graphql logci for pvo tilbakemelding
    
    * FIX: builder annotation
    
    * FIX: service methods
    
    * UPDATE: schema
    
    * FIX: typo
    
    * UPDATE: query to only return id
    
    * ADD: pvo tilbakemelding graphql test
    
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    
    * UPDATE: wrong class type
    
    ---------
    
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

---
+++ apps/backend/src/main/java/no/nav/data/pvk/pvotilbakemelding/dto/PvoTilbakemeldingGraphqlResponse.java
@@ -0,0 +1,43 @@
+package no.nav.data.pvk.pvotilbakemelding.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import no.nav.data.common.rest.ChangeStampResponse;
+import no.nav.data.etterlevelse.etterlevelseDokumentasjon.dto.EtterlevelseDokumentasjonGraphQlResponse;
+import no.nav.data.pvk.pvkdokument.domain.PvkDokumentStatus;
+import no.nav.data.pvk.pvotilbakemelding.domain.PvoTilbakemelding;
+import no.nav.data.pvk.pvotilbakemelding.domain.PvoTilbakemeldingData;
+
+import java.time.LocalDateTime;
+
+@Data
+@SuperBuilder
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class PvoTilbakemeldingGraphqlResponse extends PvoTilbakemeldingResponse {
+    private PvkDokumentStatus pvkDokumentStatus;
+    private String etterlevelseDokumentasjonId;
+    private EtterlevelseDokumentasjonGraphQlResponse etterlevelseDokumentasjonData;
+    private LocalDateTime sistEndretAvMeg;
+
+    public static PvoTilbakemeldingGraphqlResponse buildFrom(PvoTilbakemelding pvoTilbakemelding) {
+        PvoTilbakemeldingData pvoTilbakemeldingData = pvoTilbakemelding.getPvoTilbakemeldingData();
+        return PvoTilbakemeldingGraphqlResponse.builder()
+                .id(pvoTilbakemelding.getId())
+                .changeStamp(ChangeStampResponse.buildFrom(pvoTilbakemelding))
+                .version(pvoTilbakemelding.getVersion())
+                .pvkDokumentId(pvoTilbakemelding.getPvkDokumentId().toString())
+                .status(pvoTilbakemelding.getStatus())
+                .merknadTilEtterleverEllerRisikoeier(pvoTilbakemeldingData.getMerknadTilEtterleverEllerRisikoeier())
+                .sendtDato(pvoTilbakemeldingData.getSendtDato())
+                .behandlingenslivslop(pvoTilbakemeldingData.getBehandlingenslivslop())
+                .behandlingensArtOgOmfang(pvoTilbakemeldingData.getBehandlingensArtOgOmfang())
+                .innvolveringAvEksterne(pvoTilbakemeldingData.getInnvolveringAvEksterne())
+                .risikoscenarioEtterTiltakk(pvoTilbakemeldingData.getRisikoscenarioEtterTiltakk())
+                .build();
+    }
+}
