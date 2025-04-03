90415b017f68ddc0d14d2a8ac00a9f4712ea0c2b
Movafagh <farjam.movafagh@nav.no>
2025-04-03 08:47:34 +0200

    added deletion of duplicate document relations i migration 1.35

--- apps/backend/src/main/resources/db/migration/V1.35__document_relation_fk.sql
+++ apps/backend/src/main/resources/db/migration/V1.35__document_relation_fk.sql
@@ -27,6 +27,16 @@ references etterlevelse_dokumentasjon (id)
 
 -- Not adding indices, since this table will have a low rowcount
 
+-- Delete duplicate relations (if any)...
+delete from document_relation dup
+where exists ( -- newer relation between the documents
+    select 1 from document_relation dr
+    where dr.from_document = dup.from_document
+      and dr.to_document = dup.to_document
+      and dr.last_modified_date > dup.last_modified_date
+)
+;
+
 -- Add missing unique constraint...
 alter table document_relation
 add constraint uq_document_relation
