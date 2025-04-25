39fa4f5bb499cce115eae0da5305a6c4c33370b6
beeccy <beeccy@users.noreply.github.com>
2025-04-25 10:51:52 +0200

    Fikset typos

--- apps/frontend/src/components/PvkDokument/formSummary/BehandlingensLivslopSummary.tsx
+++ apps/frontend/src/components/PvkDokument/formSummary/BehandlingensLivslopSummary.tsx
@@ -17,7 +17,7 @@ export const BehandlingensLivslopSummary: FunctionComponent<TProps> = ({
     <FormSummary className='my-3'>
       <FormSummary.Header>
         <FormSummary.Heading level='2' id='behandlingensLivslop'>
-          Tegn behandlingens livslop
+          Tegn behandlingens livsløp
         </FormSummary.Heading>
         <FormSummary.EditLink
           className='cursor-pointer'
