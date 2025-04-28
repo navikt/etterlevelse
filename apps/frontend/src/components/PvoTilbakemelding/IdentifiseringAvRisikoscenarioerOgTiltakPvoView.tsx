9a22e09f048b0aed3da32fa894ffb4ff57c3a726
beeccy <beeccy@users.noreply.github.com>
2025-04-28 10:41:11 +0200

    Flyttet identisk kode ut i egen felles komponent

--- apps/frontend/src/components/PvoTilbakemelding/IdentifiseringAvRisikoscenarioerOgTiltakPvoView.tsx
+++ apps/frontend/src/components/PvoTilbakemelding/IdentifiseringAvRisikoscenarioerOgTiltakPvoView.tsx
@@ -1,6 +1,4 @@
-import { Alert, BodyLong, Button, Heading } from '@navikt/ds-react'
 import { FunctionComponent, useEffect, useState } from 'react'
-import { NavigateFunction, useNavigate } from 'react-router-dom'
 import { getRisikoscenarioByPvkDokumentId } from '../../api/RisikoscenarioApi'
 import { getTiltakByPvkDokumentId } from '../../api/TiltakApi'
 import {
@@ -10,7 +8,7 @@ import {
   IRisikoscenario,
   ITiltak,
 } from '../../constants'
-import { etterlevelseDokumentasjonPvkTabUrl } from '../common/RouteLinkEtterlevelsesdokumentasjon'
+import { IdentifiseringAvRisikoscenarioerOgTiltakContent } from '../PvkDokument/common/IdentifiseringAvRisikoscenarioerOgTiltakContent'
 import RisikoscenarioAccordianListPvoView from './RisikoscenarioAccordianListPvoView'
 import PvoFormButtons from './edit/PvoFormButtons'
 
@@ -29,8 +27,6 @@ export const IdentifiseringAvRisikoscenarioerOgTiltakPvoView: FunctionComponent<
   setActiveStep,
   setSelectedStep,
 }) => {
-  const navigate: NavigateFunction = useNavigate()
-
   const [risikoscenarioList, setRisikoscenarioList] = useState<IRisikoscenario[]>([])
   const [allRisikoscenarioList, setAllRisikoscenarioList] = useState<IRisikoscenario[]>([])
   const [tiltakList, setTiltakList] = useState<ITiltak[]>([])
@@ -58,58 +54,12 @@ export const IdentifiseringAvRisikoscenarioerOgTiltakPvoView: FunctionComponent<
   return (
     <div className='flex justify-center w-full'>
       <div className='pt-6 flex-col justify-items-center'>
-        <div className='w-4/5'>
-          <Heading level='1' size='medium' className='mb-5'>
-            Identifisering av risikoscenarioer og tiltak
-          </Heading>
+        <IdentifiseringAvRisikoscenarioerOgTiltakContent
+          stylingHeading='mb-5'
+          etterlevelseDokumentasjonId={etterlevelseDokumentasjonId}
+          risikoscenarioList={risikoscenarioList}
+        />
 
-          <BodyLong>
-            I en PVK må dere vurdere sannsynligheten for at personvern ikke ivaretas på
-            tilstrekkelig vis, og konsekvensene det vil føre til. Hvor dette risikoscenarioet er av
-            betydning, må dere identifisere forebyggende tiltak som reduserer risiko.
-          </BodyLong>
-
-          <Alert variant='info' inline className='mt-5'>
-            <strong>Godt å vite:</strong> risikoscenarioer og tiltak som dere har dokumentert et
-            sted, kan dere finne og gjenbruke andre steder hvor det er aktuelt.
-          </Alert>
-
-          <Heading spacing size='small' level='2' className='mb-5 mt-10'>
-            Legg til risikoscenarioer og tiltak med en tilknytning til etterlevelseskrav
-          </Heading>
-
-          <BodyLong className='mb-5'>
-            Disse vil nok utgjøre hovedparten av deres PVK. Slike risikoscenarioer, samt motvirkende
-            tiltak, beskriver dere på den aktuelle kravsiden.
-          </BodyLong>
-
-          <Button
-            variant='secondary'
-            type='button'
-            onClick={() => {
-              if (etterlevelseDokumentasjonId)
-                navigate(etterlevelseDokumentasjonPvkTabUrl(etterlevelseDokumentasjonId))
-            }}
-          >
-            Gå til liste over PVK-relaterte krav
-          </Button>
-
-          <Heading level='2' size='small' className='mb-5 mt-10'>
-            Legg til øvrige risikoscenarioer
-          </Heading>
-
-          <BodyLong>
-            Noen risikoscenarioer vil ikke har en direkte tilknytning til etterlevelseskrav. Disse,
-            samt motvirkende tiltak, legger dere inn på denne siden. Vi anbefaler at dette gjøres
-            etter at dere har vurderert kravspesifikke risikoscenarioer.
-          </BodyLong>
-
-          {risikoscenarioList.length === 0 && (
-            <Alert variant='info' className='my-5 w-fit'>
-              Dere har ikke lagt inn noen øvrige risikoscenarioer.
-            </Alert>
-          )}
-        </div>
         <div className='w-full'>
           {risikoscenarioList.length !== 0 && (
             <div className='my-5'>
