6e7e858f75274678f061774412f8d524f83979f7
Andre Groseth <andregroseth@hotmail.com>
2025-04-29 15:46:50 +0200

    add: Konsekvensnivå to export
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/export/PvkDokumentToDoc.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/export/PvkDokumentToDoc.java
@@ -216,12 +216,46 @@ public class PvkDokumentToDoc {
                 addMarkdownText("**Status**: " );
                 addHeading5("Beskrivelse");
                 addMarkdownText(risikoscenario.getRisikoscenarioData().getBeskrivelse());
-                addMarkdownText("**Sannsynlighetsnivå**: ");
+                addMarkdownText("**Sannsynlighetsnivå**: " + sannsynlighetsNivaaToText(risikoscenario.getRisikoscenarioData().getSannsynlighetsNivaa()));
                 addMarkdownText(risikoscenario.getRisikoscenarioData().getSannsynlighetsNivaaBegrunnelse());
-                addMarkdownText("**Konsekvensnivå**: ");
+                addMarkdownText("**Konsekvensnivå**: " + konsekvensNivaaToText(risikoscenario.getRisikoscenarioData().getKonsekvensNivaa()));
                 addMarkdownText(risikoscenario.getRisikoscenarioData().getKonsekvensNivaaBegrunnelse());
             });
         }
+
+        private String sannsynlighetsNivaaToText(Integer sannsynlighetsnivaa ) {
+            switch (sannsynlighetsnivaa) {
+                case 1:
+                    return "Meget lite sannsynlig";
+                case 2:
+                    return "Lite sannsynlig";
+                case 3:
+                    return "Moderat sannsynlig";
+                case 4:
+                    return "Sannsynlig";
+                case 5:
+                    return "Nesten sikkert";
+                default:
+                    return "Ingen sannsynlighetsnivå satt";
+            }
+        }
+
+        private String konsekvensNivaaToText(Integer konsekvensnivaa) {
+            switch (konsekvensnivaa) {
+                case 1:
+                    return "Ubetydelig konsekvens";
+                case 2:
+                    return "Lav konsekvens";
+                case 3:
+                    return "Moderat konsekvens";
+                case 4:
+                    return "Alvorlig konsekvens";
+                case 5:
+                    return "Svært alvorlig konsekvens";
+                default:
+                    return "Ingen konsekvensnivå satt";
+            }
+        };
 
         private void addBooleanDataText(String label, Boolean value) {
             addHeading4(label);
