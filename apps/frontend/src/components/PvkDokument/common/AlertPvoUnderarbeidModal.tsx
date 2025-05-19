06f19457b95c7b4f0fd524a43b4f80950275ca3a
JeremiahUy <jeremiah.uy@outlook.com>
2025-05-19 12:29:56 +0200

    ADD: modal for alert when pvo has active dokument
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

---
+++ apps/frontend/src/components/PvkDokument/common/AlertPvoUnderarbeidModal.tsx
@@ -0,0 +1,22 @@
+import { Button, Modal } from '@navikt/ds-react'
+import { FunctionComponent } from 'react'
+
+type TProps = {
+  isOpen: boolean
+  onClose: () => void
+}
+
+export const AlertPvoUnderarbeidModal: FunctionComponent<TProps> = ({ isOpen, onClose }) => {
+  return (
+    <Modal open={isOpen} onClose={() => onClose()} header={{ heading: 'Kan ikke redigeres' }}>
+      <Modal.Body>Personvernombudet har påbegynt vurdering</Modal.Body>
+      <Modal.Footer>
+        <Button type='button' onClick={() => onClose()}>
+          Lukk
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
+
+export default AlertPvoUnderarbeidModal
