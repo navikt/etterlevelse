0c84c53657aba93664614d2c47b3c01b0c1e572c
Andre Groseth <andregroseth@hotmail.com>
2025-05-20 14:14:18 +0200

    Create: InvolveringAvEksterneReadOnlyContent componet.
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

---
+++ apps/frontend/src/components/PvkDokument/readOnly/InvolveringAvEksterneReadOnlyContent.tsx
@@ -0,0 +1,85 @@
+import { BodyLong, Heading, Label, List } from '@navikt/ds-react'
+import { FunctionComponent } from 'react'
+import { EPVK, IPvkDokument } from '../../../constants'
+import DataTextWrapper from '../../PvoTilbakemelding/common/DataTextWrapper'
+
+type TProps = {
+  personkategorier: string[]
+  databehandlere: string[]
+  pvkDokument: IPvkDokument
+}
+
+export const InvolveringAvEksterneReadOnlyContent: FunctionComponent<TProps> = ({
+  personkategorier,
+  databehandlere,
+  pvkDokument,
+}) => {
+  return (
+    <div className='pt-6 pr-4 flex flex-1 flex-col gap-4 col-span-8'>
+      <div className='flex justify-center'>
+        <div>
+          <Heading level='1' size='medium' className='mb-5'>
+            Involvering av eksterne deltakere
+          </Heading>
+
+          <Heading level='2' size='small' className='mb-3'>
+            Representanter for de registrerte
+          </Heading>
+
+          <List className='mt-5'>
+            <Label size='medium'>{EPVK.behandlingAvPersonopplysninger}</Label>
+            {personkategorier.length === 0 && <List.Item>Ingen</List.Item>}
+            {personkategorier.length > 0 &&
+              personkategorier.map((personkategori) => (
+                <List.Item key={personkategori}>{personkategori}</List.Item>
+              ))}
+          </List>
+
+          <div className='mt-5 mb-3 max-w-[75ch]'>
+            <Label>Har dere involvert en representant for de registrerte?</Label>
+            <DataTextWrapper>
+              {pvkDokument.harInvolvertRepresentant === null && 'Ikke besvart'}
+              {pvkDokument.harInvolvertRepresentant === true && 'Ja'}
+              {pvkDokument.harInvolvertRepresentant === false && 'Nei'}
+            </DataTextWrapper>
+          </div>
+
+          <div className='mt-5 mb-3 max-w-[75ch]'>
+            <Label>Utdyp hvordan dere har involvert representant(er) for de registrerte</Label>
+            <DataTextWrapper>{pvkDokument.representantInvolveringsBeskrivelse}</DataTextWrapper>
+          </div>
+
+          <List className='mt-10'>
+            <Heading size='medium'>Representanter for databehandlere</Heading>
+            <BodyLong>
+              I Behandlingskatalogen står det at følgende databehandlere benyttes:
+            </BodyLong>
+            {databehandlere.length === 0 && <List.Item>Ingen</List.Item>}
+            {databehandlere.length > 0 &&
+              databehandlere.map((databehandler) => (
+                <List.Item key={databehandler}>{databehandler}</List.Item>
+              ))}
+          </List>
+
+          <div className='mt-5 mb-3 max-w-[75ch]'>
+            <Label>Har dere involvert en representant for databehandlere?</Label>
+            <DataTextWrapper>
+              {pvkDokument.harDatabehandlerRepresentantInvolvering === null && 'Ingen svar'}
+              {pvkDokument.harDatabehandlerRepresentantInvolvering === true && 'Ja'}
+              {pvkDokument.harDatabehandlerRepresentantInvolvering === false && 'Nei'}
+            </DataTextWrapper>
+          </div>
+
+          <div className='mt-5 mb-3 max-w-[75ch]'>
+            <Label>Utdyp hvordan dere har involvert representant(er) for databehandler(e)</Label>
+            <DataTextWrapper>
+              {pvkDokument.dataBehandlerRepresentantInvolveringBeskrivelse}
+            </DataTextWrapper>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InvolveringAvEksterneReadOnlyContent
