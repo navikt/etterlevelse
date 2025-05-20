9c773496287913ef93c4e18cc608223134553c07
Andre Groseth <andregroseth@hotmail.com>
2025-05-20 14:15:09 +0200

    ADD: InvolveringAvEksterneReadOnlyContent component and logic to display content
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

--- apps/frontend/src/components/PvoTilbakemelding/InvolveringAvEksternePvoView.tsx
+++ apps/frontend/src/components/PvoTilbakemelding/InvolveringAvEksternePvoView.tsx
@@ -1,8 +1,7 @@
-import { BodyLong, Heading, Label, List } from '@navikt/ds-react'
 import { FunctionComponent, RefObject } from 'react'
-import { EPVK, EPvoTilbakemeldingStatus, IPvkDokument, IPvoTilbakemelding } from '../../constants'
+import { EPvoTilbakemeldingStatus, IPvkDokument, IPvoTilbakemelding } from '../../constants'
+import InvolveringAvEksterneReadOnlyContent from '../PvkDokument/readOnly/InvolveringAvEksterneReadOnlyContent'
 import { ContentLayout } from '../layout/layout'
-import DataTextWrapper from './common/DataTextWrapper'
 import PvoSidePanelWrapper from './common/PvoSidePanelWrapper'
 import PvoTilbakemeldingReadOnly from './common/PvoTilbakemeldingReadOnly'
 import PvoFormButtons from './edit/PvoFormButtons'
@@ -31,70 +30,11 @@ export const InvolveringAvEksternePvoView: FunctionComponent<TProps> = ({
 }) => (
   <div className='w-full'>
     <ContentLayout>
-      <div className='pt-6 pr-4 flex flex-1 flex-col gap-4 col-span-8'>
-        <div className='flex justify-center'>
-          <div>
-            <Heading level='1' size='medium' className='mb-5'>
-              Involvering av eksterne deltakere
-            </Heading>
-
-            <Heading level='2' size='small' className='mb-3'>
-              Representanter for de registrerte
-            </Heading>
-
-            <List className='mt-5'>
-              <Label size='medium'>{EPVK.behandlingAvPersonopplysninger}</Label>
-              {personkategorier.length === 0 && <List.Item>Ingen</List.Item>}
-              {personkategorier.length > 0 &&
-                personkategorier.map((personkategori) => (
-                  <List.Item key={personkategori}>{personkategori}</List.Item>
-                ))}
-            </List>
-
-            <div className='mt-5 mb-3 max-w-[75ch]'>
-              <Label>Har dere involvert en representant for de registrerte?</Label>
-              <DataTextWrapper>
-                {pvkDokument.harInvolvertRepresentant === null && 'Ikke besvart'}
-                {pvkDokument.harInvolvertRepresentant === true && 'Ja'}
-                {pvkDokument.harInvolvertRepresentant === false && 'Nei'}
-              </DataTextWrapper>
-            </div>
-
-            <div className='mt-5 mb-3 max-w-[75ch]'>
-              <Label>Utdyp hvordan dere har involvert representant(er) for de registrerte</Label>
-              <DataTextWrapper>{pvkDokument.representantInvolveringsBeskrivelse}</DataTextWrapper>
-            </div>
-
-            <List className='mt-10'>
-              <Heading size='medium'>Representanter for databehandlere</Heading>
-              <BodyLong>
-                I Behandlingskatalogen står det at følgende databehandlere benyttes:
-              </BodyLong>
-              {databehandlere.length === 0 && <List.Item>Ingen</List.Item>}
-              {databehandlere.length > 0 &&
-                databehandlere.map((databehandler) => (
-                  <List.Item key={databehandler}>{databehandler}</List.Item>
-                ))}
-            </List>
-
-            <div className='mt-5 mb-3 max-w-[75ch]'>
-              <Label>Har dere involvert en representant for databehandlere?</Label>
-              <DataTextWrapper>
-                {pvkDokument.harDatabehandlerRepresentantInvolvering === null && 'Ingen svar'}
-                {pvkDokument.harDatabehandlerRepresentantInvolvering === true && 'Ja'}
-                {pvkDokument.harDatabehandlerRepresentantInvolvering === false && 'Nei'}
-              </DataTextWrapper>
-            </div>
-
-            <div className='mt-5 mb-3 max-w-[75ch]'>
-              <Label>Utdyp hvordan dere har involvert representant(er) for databehandler(e)</Label>
-              <DataTextWrapper>
-                {pvkDokument.dataBehandlerRepresentantInvolveringBeskrivelse}
-              </DataTextWrapper>
-            </div>
-          </div>
-        </div>
-      </div>
+      <InvolveringAvEksterneReadOnlyContent
+        personkategorier={personkategorier}
+        databehandlere={databehandlere}
+        pvkDokument={pvkDokument}
+      />
       {/* PVO sidepanel */}
       <PvoSidePanelWrapper>
         {pvoTilbakemelding.status === EPvoTilbakemeldingStatus.FERDIG && (
