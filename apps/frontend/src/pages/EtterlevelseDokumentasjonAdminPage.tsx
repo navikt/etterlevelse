a19f9a59d880003208419991c2a205009c98b477
JeremiahUy <jeremiah.uy@outlook.com>
2025-06-11 09:39:13 +0200

    ADD: delete function for etterlevelse metadata

--- apps/frontend/src/pages/EtterlevelseDokumentasjonAdminPage.tsx
+++ apps/frontend/src/pages/EtterlevelseDokumentasjonAdminPage.tsx
@@ -1,11 +1,13 @@
 import { Box, Button, Heading, TextField } from '@navikt/ds-react'
 import { useState } from 'react'
 import { deleteEtterlevelseDokumentasjon } from '../api/EtterlevelseDokumentasjonApi'
+import { deleteEtterlevelseMetadata } from '../api/EtterlevelseMetadataApi'
 import { PageLayout } from '../components/scaffold/Page'
 import { UpdateMessage } from './EtterlevelseAdminPage'
 
 export const EtterlevelseDokumentasjonAdminPage = () => {
   const [etterlevelseDokumentasjonId, setEtterlevelseDokumentasjonId] = useState('')
+  const [etterlevelseMetadataId, setEtterlevelseMetadataId] = useState('')
   const [updateMessage, setUpdateMessage] = useState('')
 
   return (
@@ -40,6 +42,35 @@ export const EtterlevelseDokumentasjonAdminPage = () => {
                     'Sletting vellykket for etterlevelses med uid: ' + etterlevelseDokumentasjonId
                   )
                   setEtterlevelseDokumentasjonId('')
+                })
+                .catch((e) => {
+                  setUpdateMessage('Sletting mislykket, error: ' + e)
+                })
+            }}
+          >
+            Slett
+          </Button>
+        </div>
+
+        <div className='flex items-end pd-5'>
+          <TextField
+            label='Slett etterlevelse metadata med uid'
+            placeholder='Etterlevelse metadata UID'
+            onChange={(e) => setEtterlevelseMetadataId(e.target.value)}
+            className='w-full mr-3'
+          />
+          <Button
+            disabled={!etterlevelseMetadataId}
+            variant='secondary'
+            onClick={() => {
+              setUpdateMessage('')
+              deleteEtterlevelseMetadata(etterlevelseMetadataId)
+                .then(() => {
+                  setUpdateMessage(
+                    'Sletting vellykket for etterlevelse metadata med uid: ' +
+                      etterlevelseMetadataId
+                  )
+                  setEtterlevelseMetadataId('')
                 })
                 .catch((e) => {
                   setUpdateMessage('Sletting mislykket, error: ' + e)
