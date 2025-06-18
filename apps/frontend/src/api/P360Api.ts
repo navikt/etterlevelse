cf62d2fad0c197a1ac6672b9be0bbb719578a26d
JeremiahUy <jeremiah.uy@outlook.com>
2025-06-18 14:01:26 +0200

    ADD: api for public 360
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

---
+++ apps/frontend/src/api/P360Api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import { IEtterlevelseDokumentasjon } from '../constants'
+import { env } from '../util/env'
+
+export const arkiver = async (etterlevelseDokumentasjonId: string, onlyActiveKrav: boolean) => {
+  return (
+    await axios.post<IEtterlevelseDokumentasjon>(
+      `${env.backendBaseUrl}/p360/arkiver?etterlevelseDokumentasjonId=${etterlevelseDokumentasjonId}&onlyActiveKrav=${onlyActiveKrav}`
+    )
+  ).data
+}
