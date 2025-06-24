de613b96854f778937b6ddb2d65a076899de00e3
Andre Groseth <andregroseth@hotmail.com>
2025-06-24 12:30:17 +0200

    ADD: NOM api
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>

---
+++ apps/frontend/src/api/Nom.ts
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { IOrgEnhet } from '../constants'
+import { env } from '../util/env'
+
+export const getAllNomAvdelinger = async () => {
+  return (await axios.get<IOrgEnhet[]>(`${env.backendBaseUrl}/nom/avdelinger`)).data
+}
+
+export const getAvdelingByNomId = async (id: string) => {
+  return (await axios.get<IOrgEnhet>(`${env.backendBaseUrl}/nom/avdeling/${id}`)).data
+}
+
+export const getByNomId = async (id: string) => {
+  return (await axios.get<IOrgEnhet>(`${env.backendBaseUrl}/nom/${id}`)).data
+}
