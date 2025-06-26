fb54abd2a7612e47b6d9d4583b925de0e7d9c95f
JeremiahUy <jeremiah.uy@outlook.com>
2025-06-26 09:35:59 +0200

    ADD: new status

--- apps/backend/src/main/java/no/nav/data/pvk/pvotilbakemelding/domain/PvoTilbakemeldingStatus.java
+++ apps/backend/src/main/java/no/nav/data/pvk/pvotilbakemelding/domain/PvoTilbakemeldingStatus.java
@@ -3,6 +3,7 @@ package no.nav.data.pvk.pvotilbakemelding.domain;
 public enum PvoTilbakemeldingStatus {
     IKKE_PABEGYNT,
     AVVENTER,
+    TRENGER_REVURDERING,
     UNDERARBEID,
     SNART_FERDIG,
     TIL_KONTROL,
