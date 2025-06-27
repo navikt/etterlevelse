6fce02abb5c1eafb3a7fd481bada6e886d419b66
JeremiahUy <jeremiah.uy@outlook.com>
2025-06-27 14:14:59 +0200

    UPDATE: url for p360

--- apps/frontend/src/components/p360/LinkUrlUtils.ts
+++ apps/frontend/src/components/p360/LinkUrlUtils.ts
@@ -1,5 +1,6 @@
+import { isDev } from '../../util/config'
 import { env } from '../../util/env'
 
 export const p360Url = (recno: number) => {
-  return `${env.p360BaseUrl}locator/DMS/Case/Details/Simplified/2?module=Case&subtype=2&recno=${recno}`
+  return `https://nav${isDev ? '-test' : ''}.${env.p360BaseUrl}locator/DMS/Case/Details/Simplified/2?module=Case&subtype=2&recno=${recno}`
 }
