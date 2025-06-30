795473dae2b905276007bea5877f978a4caa2bcc
Andre Groseth <andregroseth@hotmail.com>
2025-06-30 09:54:43 +0200

    UPDATE: TEXT

--- apps/frontend/src/components/PvoTilbakemelding/OversiktPvoView.tsx
+++ apps/frontend/src/components/PvoTilbakemelding/OversiktPvoView.tsx
@@ -198,7 +198,9 @@ export const OversiktPvoView: FunctionComponent<TProps> = ({
             <List.Item>
               Dere beskriver behandlingen slik at det er enklere å identifisere risikoer.
             </List.Item>
-            <List.Item>Dere gjør risikoanalyse og setter tiltak.</List.Item>
+            <List.Item>
+              Dere gjør risikoanalyse og setter tiltak og vurderer så tiltakenes effekt.
+            </List.Item>
             <List.Item>Dere sender inn PVK til vurdering av Personvernombudet (PVO).</List.Item>
             <List.Item>
               Dere gjør eventuelle endringer, og sender til risikoeier for beslutning om
