e1a777cda3b8d3beda5efbe551f1697cec2c2e14
K153697 <paul.martin.kokseter@nav.no>
2025-07-07 14:12:30 +0200

    Lagt til codelist, api og constants for å få det til å kjøre. installert yup.

---
+++ apps/frontend-nextjs/app/api/codelist/api.ts
@@ -0,0 +1,41 @@
+import { env } from '@/components/others/utils/env/env'
+import { EListName, IAllCodelists, ICode, ICodeUsage } from '@/services/codelist'
+import axios from 'axios'
+
+// refresh will force backend to re-read codelists from db, due to caching and multibackend
+export const getAllCodelists = async (refresh?: boolean) =>
+  await axios.get<IAllCodelists>(
+    `${env.backendBaseUrl}/codelist?refresh=${refresh ? 'true' : 'false'}`
+  )
+
+export const getCodelistUsage = async (listname: EListName, code: string) => {
+  return (
+    await axios.get<ICodeUsage>(`${env.backendBaseUrl}/codelist/usage/find/${listname}/${code}`)
+  ).data
+}
+
+export const replaceCodelistUsage = async (
+  listname: EListName,
+  oldCode: string,
+  newCode: string
+) => {
+  return (
+    await axios.post<ICodeUsage>(`${env.backendBaseUrl}/codelist/usage/replace`, {
+      list: listname,
+      oldCode,
+      newCode,
+    })
+  ).data
+}
+
+export const createCodelist = async (code: ICode) => {
+  return axios.post<ICode[]>(`${env.backendBaseUrl}/codelist`, [code])
+}
+
+export const updateCodelist = async (code: ICode) => {
+  return axios.put<ICode[]>(`${env.backendBaseUrl}/codelist`, [code])
+}
+
+export const deleteCodelist = async (list: string, code: string) => {
+  return axios.delete(`${env.backendBaseUrl}/codelist/${list}/${code}`)
+}
