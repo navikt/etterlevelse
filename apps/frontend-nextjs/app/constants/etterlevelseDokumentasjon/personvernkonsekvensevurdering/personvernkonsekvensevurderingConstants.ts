dbf077b9dea12c6066720a2602ffb57ee2399a95
Jeremiah Uy <jeremiah.augie.uy@nav.no>
2025-07-28 12:46:43 +0200

    Admin/pvk (#1307)
    * ADD: constants and api for pvk dokument
    
    * ADD: constants and api for pvk dokument
    
    * ADD: page for pvk dokument admin

--- apps/frontend-nextjs/app/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensevurderingConstants.ts
+++ apps/frontend-nextjs/app/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensevurderingConstants.ts
@@ -1,1 +1,55 @@
-// Api
+import { IChangeStamp } from '@/constants/commonConstants'
+import { ICode } from '@/services/codelist'
+
+export interface IPvkDokument {
+  id: string
+  changeStamp: IChangeStamp
+  version: number
+  etterlevelseDokumentId: string
+  status: EPvkDokumentStatus
+  behandlingensLivslopBeskrivelse: string
+  ytterligereEgenskaper: ICode[]
+  skalUtforePvk?: boolean
+  pvkVurderingsBegrunnelse: string
+
+  stemmerPersonkategorier?: boolean
+  personkategoriAntallBeskrivelse: string
+  tilgangsBeskrivelsePersonopplysningene: string
+  lagringsBeskrivelsePersonopplysningene: string
+
+  harInvolvertRepresentant?: boolean
+  representantInvolveringsBeskrivelse: string
+
+  harDatabehandlerRepresentantInvolvering?: boolean
+  dataBehandlerRepresentantInvolveringBeskrivelse: string
+
+  merknadTilPvoEllerRisikoeier: string
+  merknadTilRisikoeier: string
+  merknadFraRisikoeier: string
+
+  sendtTilPvoDato: string
+  sendtTilPvoAv: string
+}
+
+export interface IPvkDokumentListItem {
+  id: string
+  changeStamp: IChangeStamp
+  etterlevelseDokumentId: string
+  status: EPvkDokumentStatus
+  title: string
+  etterlevelseNummer: number
+  sendtTilPvoDato: string
+  sendtTilPvoAv: string
+}
+
+export enum EPvkDokumentStatus {
+  UNDERARBEID = 'UNDERARBEID',
+  SENDT_TIL_PVO = 'SENDT_TIL_PVO',
+  PVO_UNDERARBEID = 'PVO_UNDERARBEID',
+  VURDERT_AV_PVO = 'VURDERT_AV_PVO',
+  VURDERT_AV_PVO_TRENGER_MER_ARBEID = 'VURDERT_AV_PVO_TRENGER_MER_ARBEID',
+  SENDT_TIL_PVO_FOR_REVURDERING = 'SENDT_TIL_PVO_FOR_REVURDERING',
+  TRENGER_GODKJENNING = 'TRENGER_GODKJENNING',
+  GODKJENT_AV_RISIKOEIER = 'GODKJENT_AV_RISIKOEIER',
+  AKTIV = 'AKTIV',
+}
--- apps/frontend-nextjs/app/routes/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensvurderingRoutes.ts
+++ apps/frontend-nextjs/app/routes/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensvurderingRoutes.ts
@@ -1,1 +1,103 @@
+import { dokumentasjonUrl } from '../etterelevelseDokumentasjonRoutes'
+
 export const personvernKonsekvensvurderingUrl = '/pvkdokument'
+
+export const etterlevelseDokumentasjonPvkTabUrl = (etterlevelseDokumentasjonId: string): string =>
+  `${dokumentasjonUrl}/${etterlevelseDokumentasjonId}?tab=pvk`
+
+export const pvkDokumentasjonStepUrl = (
+  etterlevelseDokumentId: string | undefined,
+  pvkDokumentId: number | string | undefined,
+  step: number | string,
+  filter?: string
+): string => {
+  const url: string = `${dokumentasjonUrl}/${etterlevelseDokumentId}${personvernKonsekvensvurderingUrl}/${pvkDokumentId}/${step}`
+
+  if (filter) {
+    return `${url}/${filter}`
+  }
+
+  return url
+}
+
+export const pvkDokumentasjonPvkTypeStepUrl = (
+  etterlevelseDokumentId: string,
+  pvkType: string,
+  pvkDokumentId: string,
+  step?: number | string
+): string => {
+  const url: string = `${dokumentasjonUrl}/${etterlevelseDokumentId}/${pvkType}/${pvkDokumentId}`
+
+  if (step) {
+    return `${url}/${step}`
+  }
+
+  return url
+}
+export const pvkDokumentasjonCopyUrl = (
+  locationOrigin: string,
+  etterlevelseDokumentasjonId: string,
+  pvkDokumentId: string,
+  stepUrl: string,
+  queryUrl: string
+): string =>
+  `${locationOrigin}${dokumentasjonUrl}/${etterlevelseDokumentasjonId}${personvernKonsekvensvurderingUrl}/${pvkDokumentId}/${stepUrl}?${queryUrl}`
+
+export const pvkDokumentasjonBehandlingsenLivslopUrl = (
+  etterlevelseDokumentId: string,
+  behandlingensLivslopId: string
+): string =>
+  `${dokumentasjonUrl}/${etterlevelseDokumentId}/behandlingens-livslop/${behandlingensLivslopId}`
+
+export const pvkDokumentasjonPvkBehovUrl = (
+  etterlevelseDokumentId: string,
+  pvkId: string
+): string => `${dokumentasjonUrl}/${etterlevelseDokumentId}/pvkbehov/${pvkId}`
+
+export const risikoscenarioUrl = (risikoId: string): string =>
+  `${window.location.pathname}?risikoscenario=${risikoId}`
+
+export const risikoscenarioTiltakUrl = (activeRisikoscenarioId: string, tiltakId: string): string =>
+  `${window.location.pathname}?risikoscenario=${activeRisikoscenarioId}&tiltak=${tiltakId}`
+
+export const pvkDokumenteringPvoTilbakemeldingUrl = (
+  pvkDokumentId: string | undefined,
+  step: number,
+  filter?: string
+): string => {
+  const url: string = `${personvernKonsekvensvurderingUrl}/${pvkDokumentId}/pvotilbakemelding/${step}`
+
+  if (filter) {
+    return `${url}${filter}`
+  }
+
+  return url
+}
+
+export const pvkDokumentasjonTabFilterRisikoscenarioUrl = (
+  tabQuery: string | null,
+  filter: string | null,
+  risikoscenarioId: string | null
+): string => {
+  const url: string = `${window.location.pathname}?tab=${tabQuery}&filter=${filter}`
+
+  if (risikoscenarioId) {
+    return `${url}&risikoscenario=${risikoscenarioId}`
+  }
+
+  return url
+}
+
+export const pvkDokumentasjonTabFilterUrl = (
+  tabQuery: string | null,
+  filter: string | null,
+  risikoscenarioer: string
+): string => {
+  const url: string = `${window.location.pathname}?tab=${tabQuery}`
+
+  if (tabQuery === risikoscenarioer) {
+    return `${url}&filter=${filter}`
+  }
+
+  return url
+}
