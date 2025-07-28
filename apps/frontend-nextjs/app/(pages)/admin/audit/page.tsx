4cc37b55654069b92d499c49d6a42a6601b7c952
Lord André Groseth <andregroseth@users.noreply.github.com>
2025-07-28 15:00:59 +0200

    Versjonering (#1310)
    * UPDATE: package.json
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * add:  aditApi
    
    * add:  aditActionIcon
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * add:  adit constants
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * update: import
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * add: auditButton
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * add: audit label
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * add: auditRecentTable
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * add; Versjonering admin page
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * remoce: empty file
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * Change: name to versjonering in adminPages
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * ADD: empty page util
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * FIX: duplicate ids on modal
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    
    * FIX: count error for pagination
    
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    
    * Co-authored-by: andregroseth <lord.andre.groseth@nav.no>
    
    ---------
    
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: JeremiahUy <jeremiah.uy@outlook.com>

---
+++ apps/frontend-nextjs/app/(pages)/admin/audit/page.tsx
@@ -0,0 +1,5 @@
+import VersjoneringAdminPage from '@/components/admin/versjonering/versjoneringAdminPage'
+
+const Page = () => <VersjoneringAdminPage />
+
+export default Page
--- apps/frontend-nextjs/app/constants/admin/audit/auditConstants.ts
+++ apps/frontend-nextjs/app/constants/admin/audit/auditConstants.ts
@@ -1,3 +1,5 @@
+import { EListName } from '@/constants/kodeverk/kodeverkConstants'
+
 export enum EObjectType {
   Codelist = 'CODELIST',
   Krav = 'Krav',
@@ -10,3 +12,31 @@ export enum EObjectType {
   EtterlevelseArkiv = 'EtterlevelseArkiv',
   EtterlevelseMetadata = 'EtterlevelseMetadata',
 }
+
+export interface IAuditLog {
+  id: string
+  audits: IAuditItem[]
+}
+
+export interface IAuditItem {
+  action: EAuditAction
+  id: string
+  table: EObjectType
+  tableId: string
+  time: string
+  user: string
+  data: object
+}
+
+export enum EAuditAction {
+  CREATE = 'CREATE',
+  UPDATE = 'UPDATE',
+  DELETE = 'DELETE',
+}
+
+export type TNavigableItem =
+  | EObjectType
+  | EListName.RELEVANS
+  | EListName.UNDERAVDELING
+  | EListName.TEMA
+  | EListName.LOV
