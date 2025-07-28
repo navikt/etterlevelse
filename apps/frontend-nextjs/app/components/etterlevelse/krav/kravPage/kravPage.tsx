fb49fd93ab093ce1a8e8a3e0f49723437fb562e3
beeccy <beeccy@users.noreply.github.com>
2025-07-28 11:48:57 +0200

    Fiks PageLoutout

--- apps/frontend-nextjs/app/components/etterlevelse/krav/kravPage/kravPage.tsx
+++ apps/frontend-nextjs/app/components/etterlevelse/krav/kravPage/kravPage.tsx
@@ -4,17 +4,33 @@ import { getBreadcrumbPaths } from '@/components/common/breadcrumbs/breadcrumbs'
 import { KravMainContent } from '@/components/etterlevelse/krav/kravPage/kravMainContent/kravMainContent'
 import { KravOverview } from '@/components/etterlevelse/krav/kravPage/kravOverview/kravOverview'
 import { PageLayout } from '@/components/others/scaffold/page'
+import { TKravQL } from '@/constants/krav/kravConstants'
 import { TTemaCode } from '@/constants/teamkatalogen/teamkatalogConstants'
 import { useState } from 'react'
 
+type TProps = { kravVersjon: number; kravNummer: number }
+
+export const kravNumView = (props: TProps): string => {
+  const { kravNummer, kravVersjon } = props
+
+  return `K${kravNummer}.${kravVersjon}`
+}
+
 export const KravPage = () => {
+  const [krav, setKrav] = useState<TKravQL | undefined>()
   const [kravTema, setKravTema] = useState<TTemaCode>()
 
   return (
     <PageLayout
-      key='NEEDS KEY'
-      pageTitle='NEEDS TITLE'
-      currentPage='NEEDS CUURENT PAGE'
+      key={`K${krav?.kravNummer}/${krav?.kravVersjon}`}
+      pageTitle={`${kravNumView({
+        kravNummer: krav?.kravNummer ? krav.kravNummer : 0,
+        kravVersjon: krav?.kravVersjon ? krav.kravVersjon : 0,
+      })} ${krav?.navn}`}
+      currentPage={kravNumView({
+        kravNummer: krav?.kravNummer ? krav.kravNummer : 0,
+        kravVersjon: krav?.kravVersjon ? krav.kravVersjon : 0,
+      })}
       breadcrumbPaths={getBreadcrumbPaths(kravTema)}
     >
       ergphjsteld
