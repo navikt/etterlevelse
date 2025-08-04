c7d1c42f229789962593bb1c1ed07cf7da03a179
Andre Groseth <andregroseth@hotmail.com>
2025-08-04 14:00:30 +0200

    ADD: varselAdminPage

---
+++ apps/frontend-nextjs/app/components/admin/varslinger/EditOmEtterlevelse.tsx
@@ -0,0 +1,119 @@
+import { createMelding, deleteMelding, mapMeldingToFormValue, updateMelding } from '@/api/meldingApi/meldingApi'
+import { TextAreaField } from '@/components/common/textAreaField/textAreaField'
+import { EAlertType } from '@/constants/commonConstants'
+import { EMeldingStatus, IMelding } from '@/constants/message/messageConstants'
+import { Button, Heading, Loader } from '@navikt/ds-react'
+import { Formik, FormikProps } from 'formik'
+import React, { useState } from 'react'
+
+
+export const EditOmEtterlevelse = ({
+  melding,
+  setMelding,
+  isLoading,
+  maxChar,
+}: {
+  melding: IMelding | undefined
+  setMelding: React.Dispatch<React.SetStateAction<IMelding | undefined>>
+  isLoading: boolean
+  maxChar?: number
+}) => {
+  const [disableEdit, setDisableEdit] = useState<boolean>(false)
+
+  const initialNumberOfRows = 1
+
+  const submit = async (melding: IMelding) => {
+    const newMelding = { ...melding, alertType: EAlertType.INFO }
+    setDisableEdit(true)
+    if (melding.id) {
+      await updateMelding(newMelding).then((m) => {
+        setMelding(m)
+        setDisableEdit(false)
+        window.location.reload()
+      })
+    } else {
+      await createMelding(newMelding).then((m) => {
+        setMelding(m)
+        setDisableEdit(false)
+        window.location.reload()
+      })
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <div className='flex justify-center'>
+        <Loader size='large' />
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      {melding && (
+        <Formik onSubmit={submit} initialValues={mapMeldingToFormValue(melding)}>
+          {({ values, submitForm }: FormikProps<IMelding>) => (
+            <div>
+              <Heading size='small' level='2' className='my-4'>
+                Om støtte til etterlevelse
+              </Heading>
+              {/* Problem med react-draft-wysiwyg Editor komponent, når du setter en custom option som props vil du man få en ' Can't perform a React state update on an unmounted component' */}
+
+              <TextAreaField
+                maxCharacter={maxChar}
+                height='12.5rem'
+                label={'Innledende tekst'}
+                noPlaceholder
+                name='melding'
+              />
+              <TextAreaField
+                rows={initialNumberOfRows}
+                label={'Overskrift'}
+                name={'secondaryTittel'}
+                noPlaceholder
+              />
+
+              <TextAreaField
+                maxCharacter={maxChar}
+                markdown
+                height='12.5rem'
+                label={'Innhold'}
+                noPlaceholder
+                name='secondaryMelding'
+              />
+
+              <div className='flex w-full mt-2.5'>
+                <Button
+                  type='button'
+                  variant='secondary'
+                  disabled={disableEdit}
+                  onClick={() => {
+                    deleteMelding(melding.id).then(() => {
+                      setMelding(undefined)
+                    })
+                  }}
+                >
+                  Slett
+                </Button>
+                <div className='flex justify-end w-full'>
+                  <Button
+                    type='button'
+                    disabled={disableEdit}
+                    onClick={() => {
+                      values.meldingStatus = EMeldingStatus.ACTIVE
+                      submitForm()
+                    }}
+                  >
+                    Publiser
+                  </Button>
+                </div>
+              </div>
+            </div>
+          )}
+        </Formik>
+      )}
+    </div>
+  )
+}
+
+export default EditOmEtterlevelse
---
+++ apps/frontend-nextjs/app/components/admin/varslinger/VarselAdminPage.tsx
@@ -0,0 +1,119 @@
+import { getMeldingByType, mapMeldingToFormValue } from '@/api/meldingApi/meldingApi'
+import { PageLayout } from '@/components/others/scaffold/scaffold'
+import { EObjectType } from '@/constants/admin/audit/auditConstants'
+import { EMeldingType, IMelding } from '@/constants/message/messageConstants'
+import { Heading, Tabs } from '@navikt/ds-react'
+import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { AuditRecentTable } from '../versjonering/AuditRecentTable'
+import EditMelding from './EditMelding'
+import EditOmEtterlevelse from './EditOmEtterlevelse'
+
+type TSection =
+  | 'utsendtMelding'
+  | EMeldingType.SYSTEM
+  | EMeldingType.FORSIDE
+  | EMeldingType.OM_ETTERLEVELSE
+
+export const VarselAdminPage = () => {
+  return (
+    <PageLayout pageTitle='Varslinger' currentPage='Varslinger'>
+      <Heading className='mt-2' size='medium' level='1'>
+        Varslinger
+      </Heading>
+
+      <div className='pt-6'>
+        <VarselTabs />
+      </div>
+    </PageLayout>
+  )
+}
+
+const getMeldingType = (tabName: string) => {
+  switch (tabName) {
+    case 'FORSIDE':
+      return EMeldingType.FORSIDE
+    case 'OM_ETTERLEVELSE':
+      return EMeldingType.OM_ETTERLEVELSE
+    default:
+      return EMeldingType.SYSTEM
+  }
+}
+
+const VarselTabs = () => {
+  const params = useParams<{ tab?: TSection }>()
+
+  const [tab, setTab] = useState<TSection>(params.tab || 'utsendtMelding')
+  const [isLoading, setLoading] = useState<boolean>(false)
+  const [melding, setMelding] = useState<IMelding>()
+
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      if (tab !== 'utsendtMelding') {
+        ampli.logEvent('sidevisning', {
+          side: 'Varsel side for admin',
+          sidetittel: 'Opprett varsel melding for ' + tab,
+        })
+        const response = await getMeldingByType(getMeldingType(tab))
+        if (response.numberOfElements > 0) {
+          setMelding(response.content[0])
+        } else {
+          setMelding(mapMeldingToFormValue({ meldingType: getMeldingType(tab) }))
+        }
+      }
+      setLoading(false)
+    })()
+  }, [tab])
+
+  return (
+    <Tabs defaultValue='utsendtMelding'>
+      <Tabs.List>
+        <Tabs.Tab value='utsendtMelding' label='Utsendte meldinger' />
+        <Tabs.Tab
+          value={EMeldingType.SYSTEM}
+          label='Systemmelding'
+          onClick={() => {
+            setTab(EMeldingType.SYSTEM)
+          }}
+        />
+        <Tabs.Tab
+          value={EMeldingType.FORSIDE}
+          label='Informasjon på forsiden'
+          onClick={() => {
+            setTab(EMeldingType.FORSIDE)
+          }}
+        />
+        <Tabs.Tab
+          value={EMeldingType.OM_ETTERLEVELSE}
+          label='Om etterlevelse'
+          onClick={() => {
+            setTab(EMeldingType.OM_ETTERLEVELSE)
+          }}
+        />
+      </Tabs.List>
+      <Tabs.Panel value='utsendtMelding'>
+        <AuditRecentTable show={true} tableType={EObjectType.Melding} />
+      </Tabs.Panel>
+      <Tabs.Panel value={EMeldingType.SYSTEM}>
+        <EditMelding melding={melding} setMelding={setMelding} isLoading={isLoading} />
+      </Tabs.Panel>
+      <Tabs.Panel value={EMeldingType.FORSIDE}>
+        <EditMelding
+          melding={melding}
+          setMelding={setMelding}
+          isLoading={isLoading}
+          maxChar={500}
+        />
+      </Tabs.Panel>
+      <Tabs.Panel value={EMeldingType.OM_ETTERLEVELSE}>
+        <EditOmEtterlevelse
+          melding={melding}
+          setMelding={setMelding}
+          isLoading={isLoading}
+          maxChar={500}
+        />
+      </Tabs.Panel>
+    </Tabs>
+  )
+}
