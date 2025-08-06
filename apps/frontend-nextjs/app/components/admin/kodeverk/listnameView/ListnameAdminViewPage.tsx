f60f09030911b18dd065a4c66da09e4797c5776a
Jeremiah Uy <jeremiah.augie.uy@nav.no>
2025-08-06 14:40:55 +0200

    Feature/listnameviewpage (#1328)
    * ADD: logic for getting listname
    
    * ADD: logic for generating table for listname data
    
    * UPDATE: imports

--- apps/frontend-nextjs/app/components/admin/kodeverk/listnameView/ListnameAdminViewPage.tsx
+++ apps/frontend-nextjs/app/components/admin/kodeverk/listnameView/ListnameAdminViewPage.tsx
@@ -1,24 +1,56 @@
 'use client'
 
+import { createCodelist } from '@/api/kodeverk/kodeverkApi'
 import { PageLayout } from '@/components/others/scaffold/scaffold'
+import { ICode, ICodeListFormValues } from '@/constants/kodeverk/kodeverkConstants'
 import { adminCodelist, adminUrl } from '@/routes/admin/adminRoutes'
 import { codelist } from '@/services/kodeverk/kodeverkService'
 import { user } from '@/services/user/userService'
-import { Heading, Loader, Select } from '@navikt/ds-react'
+import { useAwait, useForceUpdate } from '@/util/hooks/customHooks/customHooks'
+import { PlusIcon } from '@navikt/aksel-icons'
+import { Button, Heading, Loader, Select } from '@navikt/ds-react'
 import { useParams, useRouter } from 'next/navigation'
 import { ChangeEvent, useEffect, useState } from 'react'
+import CodeListTable from './CodelistStyledTable'
+import ModalCreateCodeList from './ModalCreateCodeList'
 
 export const ListnameAdminViewPage = () => {
   const params = useParams()
   const router = useRouter()
-  const listname = params.listname
-  const [selectedListname, setSelectedListname] = useState<string>(listname as string)
+  const listname = params.listname as string
+  const [selectedListname, setSelectedListname] = useState<string>(listname)
+  const [isLoading, setIsLoading] = useState(true)
+  const [createCodeListModal, setCreateCodeListModal] = useState(false)
+  const [errorOnResponse, setErrorOnResponse] = useState(null)
+  const forceUpdate = useForceUpdate()
+  useAwait(codelist.wait(), setIsLoading)
+
+  const lists = codelist.lists?.codelist
+  const currentCodelist = lists && listname ? lists[listname] : undefined
 
   useEffect(() => {
     if (selectedListname && selectedListname !== listname) {
       router.push(adminCodelist(selectedListname))
     }
   }, [selectedListname])
+
+  const handleCreateCodelist = async (values: ICodeListFormValues) => {
+    setIsLoading(true)
+    try {
+      await createCodelist({ ...values } as ICode)
+      await codelist.refreshCodeLists()
+      setCreateCodeListModal(false)
+    } catch (error: any) {
+      setCreateCodeListModal(true)
+      setErrorOnResponse(error.message)
+    }
+    setIsLoading(false)
+  }
+
+  const update = async () => {
+    await codelist.refreshCodeLists()
+    forceUpdate()
+  }
 
   return (
     <PageLayout
@@ -63,7 +95,39 @@ export const ListnameAdminViewPage = () => {
               }
             )}
           </Select>
+
+          {listname && (
+            <Button
+              icon={<PlusIcon area-label='' aria-hidden />}
+              variant='tertiary'
+              onClick={() => setCreateCodeListModal(!createCodeListModal)}
+            >
+              Opprett ny kode
+            </Button>
+          )}
         </div>
+      )}
+
+      {isLoading && <Loader size='large' />}
+
+      {!isLoading && currentCodelist && (
+        <div className='mt-4'>
+          <CodeListTable tableData={currentCodelist || []} refresh={update} />
+        </div>
+      )}
+
+      {listname && createCodeListModal && (
+        <ModalCreateCodeList
+          title='Ny kode'
+          list={listname}
+          isOpen={createCodeListModal}
+          errorOnCreate={errorOnResponse}
+          onClose={() => {
+            setCreateCodeListModal(false)
+            setErrorOnResponse(null)
+          }}
+          submit={handleCreateCodelist}
+        />
       )}
 
       {!user.isAdmin() && (
