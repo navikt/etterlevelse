ea2f10bafba2223bf6505a82bffffc826304b0da
K153697 <paul.martin.kokseter@nav.no>
2025-08-14 11:16:45 +0200

    Første del av kravEditPage.tsx

--- apps/frontend-nextjs/app/components/krav/kravEditPage/kravEditPage.tsx
+++ apps/frontend-nextjs/app/components/krav/kravEditPage/kravEditPage.tsx
@@ -1,3 +1,40 @@
+'use client'
+
+import { GetKravData, IKravDataProps, TKravById } from '@/api/krav/kravEdit/kravEditApi'
+import { PageLayout } from '@/components/others/scaffold/scaffold'
+import { TKravQL } from '@/constants/krav/kravConstants'
+import { kravBreadCrumbPath } from '@/util/breadCrumbPath/breadCrumbPath'
+import { Box } from '@navikt/ds-react'
+import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
 export const KravEditPage = () => {
-  return <h1>test</h1>
+  const params = useParams()
+  const [krav, setKrav] = useState<TKravQL | undefined>()
+
+  const kravData: IKravDataProps | undefined = GetKravData(params)
+  const kravQuery: TKravById | undefined = kravData?.kravQuery
+
+  useEffect(() => {
+    if (kravQuery?.kravById) {
+      setKrav(kravQuery.kravById)
+
+      //  setIsEditingUtgaattKrav(kravQuery.kravById.status === EKravStatus.UTGAATT ? true : false)
+    }
+  }, [kravQuery])
+
+  return (
+    <>
+      <PageLayout
+        pageTitle='Rediger krav'
+        currentPage='Rediger krav'
+        breadcrumbPaths={[kravBreadCrumbPath]}
+        key={'K' + krav?.kravNummer + '/' + krav?.kravVersjon}
+      >
+        <Box padding='4' background='surface-warning-subtle'>
+          Det er ikke lov å redigere på et utgått krav.
+        </Box>
+      </PageLayout>
+    </>
+  )
 }
