6e1bf29f22c458151a5da4a114f5abe97f8ec080
Andre Groseth <andregroseth@hotmail.com>
2025-08-14 10:08:46 +0200

    revert changes

--- apps/frontend-nextjs/next.config.ts
+++ apps/frontend-nextjs/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // <-- enables static export
   distDir: 'build', // optional, changes output dir from 'out' to 'build'
 }
 
