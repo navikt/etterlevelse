eed5e8bb2284c420dc162c7e9c1ea3d0c1f1ea0f
Andre Groseth <andregroseth@hotmail.com>
2025-08-14 09:50:42 +0200

    update: to export static site

--- apps/frontend-nextjs/next.config.ts
+++ apps/frontend-nextjs/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: 'build',
+  output: 'export', // <-- enables static export
+  distDir: 'build', // optional, changes output dir from 'out' to 'build'
 }
 
 export default nextConfig
