86a7d11d5e02b29bdf18072d9b69cd3a668a05b2
JeremiahUy <jeremiah.uy@outlook.com>
2025-09-01 10:28:39 +0200

    UPDATE: to handle tab state on render

--- apps/frontend-nextjs/app/components/krav/kravlistePage/kravTabs/kravTabs.tsx
+++ apps/frontend-nextjs/app/components/krav/kravlistePage/kravTabs/kravTabs.tsx
@@ -4,18 +4,31 @@ import { ETab } from '@/constants/krav/kravlist/kravlistConstants'
 import { kravlisteUrl } from '@/routes/krav/kravRoutes'
 import { Tabs } from '@navikt/ds-react'
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 import { AllKrav } from './alle/alleKrav'
 import { SistRedigertKrav } from './sisteRedigertKrav/sisteRedigertKrav'
 import { TemaList } from './temaList/temaList'
 
 export const KravTabs = () => {
   const router: AppRouterInstance = useRouter()
+  const queryParams = useSearchParams()
+  const [selectedTab, setSelectedTab] = useState<ETab>(ETab.SISTE)
+
+  useEffect(() => {
+    const tabQuery = queryParams.get('tab')
+
+    if (tabQuery) {
+      setSelectedTab(tabQuery as ETab)
+    }
+  }, [])
 
   return (
     <Tabs
       defaultValue={ETab.SISTE}
+      value={selectedTab}
       onChange={(tabQuery: string) => {
+        setSelectedTab(tabQuery as ETab)
         router.push(`${kravlisteUrl}?tab=${tabQuery}`)
       }}
     >
