303ffc22f7377d9d94e0a0aa1dcc913ec85d985a
JeremiahUy <jeremiah.uy@outlook.com>
2025-09-02 15:11:24 +0200

    REVERT: timer change

--- apps/backend/src/main/java/no/nav/data/integration/slack/SlackService.java
+++ apps/backend/src/main/java/no/nav/data/integration/slack/SlackService.java
@@ -22,9 +22,8 @@ public class SlackService {
         repo.save(sm);
     }
 
-    // @Scheduled(cron = "0 55 12 * * *") // Happens every day at 12:55:00
     @SchedulerLock(name = "sendSlackEnGros")
-    @Scheduled(cron = "15 * * * * *") // Happens every minute (10 seconds past every minute)
+    @Scheduled(cron = "0 55 12 * * *") // Happens every day at 12:55:00
     public void sendAll() {
         log.info("Sending all pending slack messages...");
         int sendCount = 0;
