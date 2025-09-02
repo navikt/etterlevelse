d75fbd286231bdb7800b269951c0b86fe46b144c
JeremiahUy <jeremiah.uy@outlook.com>
2025-09-02 14:16:44 +0200

    DEBUG: type error on archiving

--- apps/backend/src/main/java/no/nav/data/integration/p360/dto/P360Document.java
+++ apps/backend/src/main/java/no/nav/data/integration/p360/dto/P360Document.java
@@ -1,16 +1,19 @@
 package no.nav.data.integration.p360.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldNameConstants;
 
-@Data
 @Builder
-@FieldNameConstants
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class P360Document {
 
-    //public Integer Recno;
+    public Integer Recno;
     public String DocumentNumber;
     public Boolean Successful;
     public String ErrorMessage;
