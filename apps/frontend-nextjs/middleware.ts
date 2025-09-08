5df3c330d967740aeb0b144067d6f74f311b389a
JeremiahUy <jeremiah.uy@outlook.com>
2025-09-08 13:12:22 +0200

    UPDATE: imports

--- apps/frontend-nextjs/middleware.ts
+++ apps/frontend-nextjs/middleware.ts
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { env } from 'process'
 
 export async function middleware(request: NextRequest): Promise<NextResponse<unknown>> {
   const backendUrl: URL = new URL(request.nextUrl)
 
-  if (env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'production') {
     backendUrl.host = 'etterlevelse-backend'
   } else {
     backendUrl.host = 'localhost:8080'
