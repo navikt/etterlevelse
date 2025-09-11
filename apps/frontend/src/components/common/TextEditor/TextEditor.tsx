6c4643190aa1c651e9de31c23bb966ec3125bb44
JeremiahUy <jeremiah.uy@outlook.com>
2025-09-11 14:16:04 +0200

    ADD: null point fallback value and update local connection to prod

--- apps/frontend/src/components/common/TextEditor/TextEditor.tsx
+++ apps/frontend/src/components/common/TextEditor/TextEditor.tsx
@@ -129,7 +129,7 @@ export const TextEditor = (props: TTextEditorProps) => {
     })
 
   const CustomMarkdownToDraft = (data: string) => {
-    const rawData = data
+    const rawData = data || ''
     const noUnderlineAndHighlightData = rawData
       .replaceAll(/<span style='background-color: rgb(.*?)'>/g, '')
       .replaceAll('</span>', '')
