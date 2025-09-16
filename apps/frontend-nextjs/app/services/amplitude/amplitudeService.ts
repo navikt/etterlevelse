61ea8da8086fe3dcb3afab84f9ac91cdc6856612
JeremiahUy <jeremiah.uy@outlook.com>
2025-09-16 12:04:15 +0200

    UPDATE: disabled amplitude

--- apps/frontend-nextjs/app/services/amplitude/amplitudeService.ts
+++ apps/frontend-nextjs/app/services/amplitude/amplitudeService.ts
@@ -1,39 +1,39 @@
-'use client'
+// 'use client'
 
-import { user } from '@/services/user/userService'
-import { env } from '@/util/env/env'
+// import { user } from '@/services/user/userService'
+// import { env } from '@/util/env/env'
 
-export const ampli = () => {
-  if (typeof window === 'undefined') return null
+// export const ampli = () => {
+//   if (typeof window === 'undefined') return null
 
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const amplitude = require('amplitude-js')
-  const ampliInstance = amplitude.getInstance()
+//   // eslint-disable-next-line @typescript-eslint/no-require-imports
+//   const amplitude = require('amplitude-js')
+//   const ampliInstance = amplitude.getInstance()
 
-  const AmplitudeConfig = {
-    apiEndpoint: env.amplitudeEndpoint,
-    saveEvents: false,
-    includeUtm: true,
-    includeReferrer: true,
-    trackingOptions: {
-      city: false,
-      ip_address: false,
-    },
-    platform: window.location.href,
-  }
+//   const AmplitudeConfig = {
+//     apiEndpoint: env.amplitudeEndpoint,
+//     saveEvents: false,
+//     includeUtm: true,
+//     includeReferrer: true,
+//     trackingOptions: {
+//       city: false,
+//       ip_address: false,
+//     },
+//     platform: window.location.href,
+//   }
 
-  ampliInstance.init(env.amplitudeApiKey, undefined, AmplitudeConfig)
-  ampliInstance.setUserId(null)
+//   ampliInstance.init(env.amplitudeApiKey, undefined, AmplitudeConfig)
+//   ampliInstance.setUserId(null)
 
-  return ampliInstance
-}
+//   return ampliInstance
+// }
 
-export const userRoleEventProp = {
-  role: user.isAdmin()
-    ? 'ADMIN'
-    : user.isKraveier()
-      ? 'KRAVEIER'
-      : user.isPersonvernombud()
-        ? 'PERSONVERNOMBUD'
-        : 'ETTERLEVER',
-}
+// export const userRoleEventProp = {
+//   role: user.isAdmin()
+//     ? 'ADMIN'
+//     : user.isKraveier()
+//       ? 'KRAVEIER'
+//       : user.isPersonvernombud()
+//         ? 'PERSONVERNOMBUD'
+//         : 'ETTERLEVER',
+// }
