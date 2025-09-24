e2710e462f6c51aeccafdeea7aac20c0593bbff5
beeccy <beeccy@users.noreply.github.com>
2025-09-24 11:50:21 +0200

    Lagt inn /dokumentasjoner

---
+++ apps/frontend-nextjs/app/(pages)/dokumentasjoner/page.tsx
@@ -0,0 +1,5 @@
+import { MinEtterlevelseDokumentasjonerPage } from '@/components/etterlevelse/minEtterlevelseDokumentasjonerPage/minEtterlevelseDokumentasjonerPage'
+
+const Page = () => <MinEtterlevelseDokumentasjonerPage />
+
+export default Page
