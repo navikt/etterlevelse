d9dc7e6e12ca660b0734e329892826cf80b4a980
JeremiahUy <jeremiah.uy@outlook.com>
2025-10-01 08:55:40 +0200

    UPDATE: env variable to be public

--- apps/frontend-nextjs/app/util/env/env.ts
+++ apps/frontend-nextjs/app/util/env/env.ts
@@ -1,14 +1,14 @@
-// EXPECTED AND SHOULD CRASH APPLICATION IF VALUES ARE NOT DEFINED
+// EXPECTED AND SHOULD CRASH PUBLICLICATION IF VALUES ARE NOT DEFINED
 
 export const env = {
-  backendBaseUrl: process.env.NEXT_APP_BACKEND_ENDPOINT || '/api',
-  amplitudeEndpoint: process.env.NEXT_APP_AMPLITUDE_ENDPOINT!,
-  amplitudeApiKey: process.env.NEXT_APP_AMPLITUDE_API_KEY!,
-  teamKatBaseUrl: process.env.NEXT_APP_TEAMKAT_BASE_URL!,
-  pollyBaseUrl: process.env.NEXT_APP_POLLY_BASE_URL!,
-  githubVersion: process.env.NEXT_APP_GIT_VERSION || 'local',
-  lovdataLovBaseUrl: process.env.NEXT_APP_LOVDATA_LOV_BASE_URL!,
-  lovdataForskriftBaseUrl: process.env.NEXT_APP_LOVDATA_FORSKRIFT_BASE_URL!,
-  lovdataRundskrivBaseUrl: process.env.NEXT_APP_LOVDATA_RUNDSKRIV_BASE_URL!,
-  p360BaseUrl: process.env.NEXT_APP_P360_BASE_URL!,
+  backendBaseUrl: process.env.NEXT_PUBLIC_BACKEND_ENDPOINT || '/api',
+  amplitudeEndpoint: process.env.NEXT_PUBLIC_AMPLITUDE_ENDPOINT!,
+  amplitudeApiKey: process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY!,
+  teamKatBaseUrl: process.env.NEXT_PUBLIC_TEAMKAT_BASE_URL!,
+  pollyBaseUrl: process.env.NEXT_PUBLIC_POLLY_BASE_URL!,
+  githubVersion: process.env.NEXT_PUBLIC_GIT_VERSION || 'local',
+  lovdataLovBaseUrl: process.env.NEXT_PUBLIC_LOVDATA_LOV_BASE_URL!,
+  lovdataForskriftBaseUrl: process.env.NEXT_PUBLIC_LOVDATA_FORSKRIFT_BASE_URL!,
+  lovdataRundskrivBaseUrl: process.env.NEXT_PUBLIC_LOVDATA_RUNDSKRIV_BASE_URL!,
+  p360BaseUrl: process.env.NEXT_PUBLIC_P360_BASE_URL!,
 }
