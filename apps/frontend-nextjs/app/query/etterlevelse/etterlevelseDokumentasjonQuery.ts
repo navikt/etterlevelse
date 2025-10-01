cc943f4aa40fbc44e38c2bcfd4907bb4226a2e7d
Andre Groseth <andregroseth@hotmail.com>
2025-10-01 13:14:31 +0200

    add: getEtterlevelseDokumentasjonByBehandlingIdQuery and getEtterlevelseDokumentasjonStatsQuery

--- apps/frontend-nextjs/app/query/etterlevelse/etterlevelseDokumentasjonQuery.ts
+++ apps/frontend-nextjs/app/query/etterlevelse/etterlevelseDokumentasjonQuery.ts
@@ -42,3 +42,161 @@ export const getEtterlevelseDokumentasjonListQuery = gql`
     }
   }
 `
+export const getEtterlevelseDokumentasjonByBehandlingIdQuery = gql`
+  query getEtterlevelseDokumentasjoner(
+    $pageNumber: NonNegativeInt
+    $pageSize: NonNegativeInt
+    $mineEtterlevelseDokumentasjoner: Boolean
+    $sistRedigert: NonNegativeInt
+    $sok: String
+    $behandlingId: String
+  ) {
+    etterlevelseDokumentasjoner: etterlevelseDokumentasjon(
+      filter: {
+        mineEtterlevelseDokumentasjoner: $mineEtterlevelseDokumentasjoner
+        sistRedigert: $sistRedigert
+        sok: $sok
+        behandlingId: $behandlingId
+      }
+      pageNumber: $pageNumber
+      pageSize: $pageSize
+    ) {
+      pageNumber
+      pageSize
+      pages
+      numberOfElements
+      totalElements
+      content {
+        id
+        title
+        etterlevelseNummer
+        sistEndretEtterlevelse
+        prioritertKravNummer
+        teamsData {
+          id
+          name
+        }
+        behandlinger {
+          id
+          navn
+          nummer
+        }
+      }
+    }
+  }
+`
+
+export const getEtterlevelseDokumentasjonStatsQuery = gql`
+  query getEtterlevelseDokumentasjonStats($etterlevelseDokumentasjonId: ID) {
+    etterlevelseDokumentasjon(filter: { id: $etterlevelseDokumentasjonId }) {
+      content {
+        stats {
+          relevantKrav {
+            kravNummer
+            kravVersjon
+            navn
+            status
+            aktivertDato
+            tagger
+            etterlevelser(
+              onlyForEtterlevelseDokumentasjon: true
+              etterlevelseDokumentasjonId: $etterlevelseDokumentasjonId
+            ) {
+              status
+              etterlevelseDokumentasjonId
+              fristForFerdigstillelse
+              suksesskriterieBegrunnelser {
+                suksesskriterieStatus
+              }
+              changeStamp {
+                lastModifiedBy
+                lastModifiedDate
+                createdDate
+              }
+            }
+            regelverk {
+              lov {
+                code
+                shortName
+              }
+            }
+            changeStamp {
+              lastModifiedBy
+              lastModifiedDate
+              createdDate
+            }
+          }
+          irrelevantKrav {
+            kravNummer
+            kravVersjon
+            navn
+            status
+            aktivertDato
+            tagger
+            etterlevelser(
+              onlyForEtterlevelseDokumentasjon: true
+              etterlevelseDokumentasjonId: $etterlevelseDokumentasjonId
+            ) {
+              status
+              etterlevelseDokumentasjonId
+              fristForFerdigstillelse
+              suksesskriterieBegrunnelser {
+                suksesskriterieStatus
+              }
+              changeStamp {
+                lastModifiedBy
+                lastModifiedDate
+                createdDate
+              }
+            }
+            regelverk {
+              lov {
+                code
+                shortName
+              }
+            }
+            changeStamp {
+              lastModifiedBy
+              lastModifiedDate
+              createdDate
+            }
+          }
+          utgaattKrav {
+            kravNummer
+            kravVersjon
+            navn
+            status
+            aktivertDato
+            etterlevelser(
+              onlyForEtterlevelseDokumentasjon: true
+              etterlevelseDokumentasjonId: $etterlevelseDokumentasjonId
+            ) {
+              behandlingId
+              status
+              etterlevelseDokumentasjonId
+              suksesskriterieBegrunnelser {
+                suksesskriterieStatus
+              }
+              changeStamp {
+                lastModifiedBy
+                lastModifiedDate
+                createdDate
+              }
+            }
+            regelverk {
+              lov {
+                code
+                shortName
+              }
+            }
+            changeStamp {
+              lastModifiedBy
+              lastModifiedDate
+              createdDate
+            }
+          }
+        }
+      }
+    }
+  }
+`
