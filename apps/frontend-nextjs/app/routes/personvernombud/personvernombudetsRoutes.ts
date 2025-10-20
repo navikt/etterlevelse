99d476cbf3ca664e8d482d0e37a2b3c25ebfd918
Andre Groseth <andregroseth@hotmail.com>
2025-10-20 13:22:11 +0200

    add: routes
    Co-authored-by: Jeremiah Uy <jeremiah.augie.uy@nav.no>
    Co-authored-by: K153697 <paul.martin.kokseter@nav.no>

--- apps/frontend-nextjs/app/routes/personvernombud/personvernombudetsRoutes.ts
+++ apps/frontend-nextjs/app/routes/personvernombud/personvernombudetsRoutes.ts
@@ -1,2 +1,5 @@
 export const pvoUrl: string = '/pvo'
 export const pvoOversiktUrl: string = `${pvoUrl}/oversikt`
+export const pvotilbakemeldingUrl: string = `${pvoUrl}tilbakemelding`
+
+export const pvoTabQueryUrl = (tabQuery: string): string => `${pvoOversiktUrl}?tab=${tabQuery}`
