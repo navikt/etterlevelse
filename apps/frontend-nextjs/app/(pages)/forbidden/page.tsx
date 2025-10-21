1fdf73af7f80e259fa813c9e4e0d55d56aa4625c
JeremiahUy <jeremiah.uy@outlook.com>
2025-10-21 14:55:47 +0200

    UPDATE: forbidden page

--- apps/frontend-nextjs/app/(pages)/forbidden/page.tsx
+++ apps/frontend-nextjs/app/(pages)/forbidden/page.tsx
@@ -1,4 +1,6 @@
 import { PageLayout } from '@/components/others/scaffold/scaffold'
+import notFound from '@/resources/notfound.svg'
+import { Heading } from '@navikt/ds-react'
 import Image from 'next/image'
 
 const Forbidden = () => {
@@ -6,13 +8,15 @@ const Forbidden = () => {
 
   return (
     <PageLayout pageTitle='403 forbidden'>
-      <div className='pl-10 pr-10 pt-10'>
-        {/* <Heading className='pt-10 pb-10 text-center' size='medium' level='1'>
-          Du prøvde å komme inn på en side du ikke har tilgang til.
-        </Heading> */}
-        <div>
-          <Image src='/403-forbidden.png' alt='403 Forbidden' width={1536} height={1024} />
-        </div>
+      <div className='pl-10 pr-10'>
+        <Heading className='pt-10 pb-10' size='medium' level='1'>
+          Du prøvde å komme inn i en side du ikke har tilgang til.
+        </Heading>
+        <Image
+          src={notFound}
+          alt='Oida 404! Fant ikke den siden der nei'
+          style={{ maxWidth: '65%' }}
+        />
       </div>
     </PageLayout>
   )
