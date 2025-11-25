b917f14bda805f56682738fb3c99dbf00614d3bf
JeremiahUy <jeremiah.uy@outlook.com>
2025-11-25 14:44:02 +0100

    FIX: typo
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/NomSeksjon.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/NomSeksjon.java
@@ -8,6 +8,6 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class NomSeksjon {
-    private String nomSeksjontId;
+    private String nomSeksjonId;
     private String nomSeksjonName;
 }
