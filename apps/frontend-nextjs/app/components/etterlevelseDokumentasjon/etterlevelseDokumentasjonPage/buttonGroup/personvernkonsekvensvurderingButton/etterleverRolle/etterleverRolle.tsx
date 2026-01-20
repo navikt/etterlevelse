68e2f4fab9e311f121e99ac5774cc738c6b60888
beeccy <beeccy@users.noreply.github.com>
2026-01-20 12:01:01 +0100

    Fikset feil alternativ

--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/personvernkonsekvensvurderingButton/etterleverRolle/etterleverRolle.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/personvernkonsekvensvurderingButton/etterleverRolle/etterleverRolle.tsx
@@ -18,7 +18,8 @@ import {
 //   isRisikoeier: boolean
 // }
 
-const test: string = EPVKTilstandStatus.PVK_SENDT_TIL_PVO_TELLES_SOM_UNDERARBEID
+const test: string =
+  EPVKTilstandStatus.ETTERLEVER_HAR_SENDT_OPPDATERT_PVK_DIREKTE_TIL_RISIKOEIER_TELLES_SOM_UNDER_ARBEID
 
 const EtterleverRolle: FunctionComponent = (
   {
@@ -39,7 +40,7 @@ const EtterleverRolle: FunctionComponent = (
     case EPVKTilstandStatus.PVK_UNDER_ARBEID_FOR_FORSTE_GANG_FOR_INNSENDING:
       return <EtterleverVariantFour />
     case EPVKTilstandStatus.PVK_SENDT_TIL_PVO_TELLES_SOM_UNDERARBEID:
-      return <EtterleverVariantFour />
+      return <EtterleverVariantFive />
     case EPVKTilstandStatus.PVO_HAR_GITT_TILBAKEMELDING_ETTERLEVER_KAN_REDIGERE_TELLES_SOM_UNDER_ARBEID:
       return <EtterleverVariantSix />
     case EPVKTilstandStatus.ETTERLEVER_HAR_SENDT_TILBAKE_TIL_PVO_TELLES_SOM_UNDER_ARBEID:
