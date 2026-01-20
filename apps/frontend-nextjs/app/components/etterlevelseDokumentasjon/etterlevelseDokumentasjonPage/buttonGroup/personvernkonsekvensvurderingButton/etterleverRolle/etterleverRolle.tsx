185ddcc191ff93a543479bbe552ad6ac814dbd10
beeccy <beeccy@users.noreply.github.com>
2026-01-20 10:47:22 +0100

    Rolle etterlever

---
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/personvernkonsekvensvurderingButton/etterleverRolle/etterleverRolle.tsx
@@ -0,0 +1,277 @@
+import { ChevronDownIcon } from '@navikt/aksel-icons'
+import { ActionMenu, Button } from '@navikt/ds-react'
+
+// type TProps = {
+//   etterlevelseDokumentasjon: TEtterlevelseDokumentasjonQL
+//   risikoscenarioList: IRisikoscenario[]
+//   behandlingsLivslop?: IBehandlingensLivslop
+//   pvkDokument?: IPvkDokument
+//   isRisikoeier: boolean
+// }
+
+const EtterleverRolle = (
+  {
+    //   etterlevelseDokumentasjon,
+    //   behandlingsLivslop,
+    //   pvkDokument,
+    //   risikoscenarioList,
+    //   isRisikoeier,
+  }
+) => (
+  <>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Beskriv art og omfang
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Vurder behov for PVK
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Beskriv art og omfang
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Revurder behov for PVK
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Beskriv art og omfang
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Påbegynn PVK
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Revurder behov for PVK
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Group label=''>
+          <ActionMenu.Item as='a' href=''>
+            Beskriv art og omfang
+          </ActionMenu.Item>
+          <ActionMenu.Item as='a' href=''>
+            Fullfør PVK
+          </ActionMenu.Item>
+        </ActionMenu.Group>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          See behandlingens livsløp (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Group label=''>
+          <ActionMenu.Item as='a' href=''>
+            Se art og omfang (read-only)
+          </ActionMenu.Item>
+          <ActionMenu.Item as='a' href=''>
+            Les PVK (read-only)
+          </ActionMenu.Item>
+        </ActionMenu.Group>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Se art og omfang (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Les PVOs tilbakemelding
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          See behandlingens livsløp (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Se art og omfang (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Les PVK (read-only)
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          See behandlingens livsløp (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Se art og omfang (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Les PVK (read-only)
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Beskriv art og omfang
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Les PVK (read-only, godkjent versjon) og Oppdater PVK (åpner neste versjon)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Revurder behov for PVK
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Beskriv art og omfang
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Fullfør PVK
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+    <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        <ActionMenu.Item as='a' href=''>
+          See behandlingens livsløp (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Se art og omfang (read-only)
+        </ActionMenu.Item>
+        <ActionMenu.Item as='a' href=''>
+          Les PVK (read-only)
+        </ActionMenu.Item>
+      </ActionMenu.Content>
+    </ActionMenu>
+  </>
+)
+
+export default EtterleverRolle
--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/personvernkonsekvensvurderingButton/personvernkonsekvensvurderingButton.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/personvernkonsekvensvurderingButton/personvernkonsekvensvurderingButton.tsx
@@ -1,22 +1,9 @@
 import { IBehandlingensLivslop } from '@/constants/etterlevelseDokumentasjon/behandlingensLivslop/behandlingensLivslopConstants'
 import { TEtterlevelseDokumentasjonQL } from '@/constants/etterlevelseDokumentasjon/etterlevelseDokumentasjonConstants'
-import {
-  EPvkVurdering,
-  IPvkDokument,
-} from '@/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensevurderingConstants'
+import { IPvkDokument } from '@/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensevurderingConstants'
 import { IRisikoscenario } from '@/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/risikoscenario/risikoscenarioConstants'
-import {
-  pvkDokumentasjonBehandlingsenLivslopUrl,
-  pvkDokumentasjonPvkBehovUrl,
-  pvkDokumentasjonStepUrl,
-} from '@/routes/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensvurderingRoutes'
-import {
-  getPvkButtonText,
-  isPvkDokumentVurdert,
-} from '@/util/etterlevelseDokumentasjon/pvkDokument/pvkDokumentUtils'
-import { ChevronDownIcon } from '@navikt/aksel-icons'
-import { ActionMenu, Button } from '@navikt/ds-react'
 import { FunctionComponent } from 'react'
+import EtterleverRolle from './etterleverRolle/etterleverRolle'
 
 type TProps = {
   etterlevelseDokumentasjon: TEtterlevelseDokumentasjonQL
@@ -26,57 +13,70 @@ type TProps = {
   isRisikoeier: boolean
 }
 
-export const PersonvernkonsekvensvurderingButton: FunctionComponent<TProps> = ({
-  etterlevelseDokumentasjon,
-  behandlingsLivslop,
-  pvkDokument,
-  risikoscenarioList,
-  isRisikoeier,
-}) => (
-  <ActionMenu>
-    <ActionMenu.Trigger>
-      <Button
-        variant='secondary-neutral'
-        icon={<ChevronDownIcon aria-hidden />}
-        iconPosition='right'
-      >
-        PVK
-      </Button>
-    </ActionMenu.Trigger>
-    <ActionMenu.Content>
-      {/* {behandlingsLivslop ? 'Rediger behandlinges livsløp' : 'Tegn behandlingens livsløp'} */}
-      <ActionMenu.Item
-        as='a'
-        href={pvkDokumentasjonBehandlingsenLivslopUrl(
-          etterlevelseDokumentasjon.id,
-          behandlingsLivslop ? behandlingsLivslop.id : 'ny'
-        )}
-      >
-        Tegn behandlingens livsløp
-      </ActionMenu.Item>
-      <ActionMenu.Group label=''>
-        {pvkDokument && pvkDokument.pvkVurdering === EPvkVurdering.SKAL_UTFORE && (
-          <ActionMenu.Item
-            as='a'
-            href={pvkDokumentasjonStepUrl(etterlevelseDokumentasjon.id, pvkDokument.id, 1)}
-          >
-            {getPvkButtonText(pvkDokument, risikoscenarioList, isRisikoeier)}
-          </ActionMenu.Item>
-        )}
-        {!etterlevelseDokumentasjon.irrelevansFor.some(
-          (irrelevans) => irrelevans.code === 'PERSONOPPLYSNINGER'
-        ) && (
-          <ActionMenu.Item
-            as='a'
-            href={pvkDokumentasjonPvkBehovUrl(
-              etterlevelseDokumentasjon.id,
-              pvkDokument ? pvkDokument.id : 'ny'
-            )}
-          >
-            {isPvkDokumentVurdert(pvkDokument) ? 'Revurder behov for PVK' : 'Vurder behov for PVK'}
-          </ActionMenu.Item>
-        )}
-      </ActionMenu.Group>
-    </ActionMenu.Content>
-  </ActionMenu>
+export const PersonvernkonsekvensvurderingButton: FunctionComponent<TProps> = (
+  {
+    // etterlevelseDokumentasjon,
+    // behandlingsLivslop,
+    // pvkDokument,
+    // risikoscenarioList,
+    // isRisikoeier,
+  }
+) => (
+  <>
+    <EtterleverRolle
+    // etterlevelseDokumentasjon={etterlevelseDokumentasjon}
+    // risikoscenarioList={risikoscenarioList}
+    // behandlingsLivslop={behandlingsLivslop}
+    // pvkDokument={pvkDokument}
+    // isRisikoeier={isRisikoeier}
+    />
+    {/* <ActionMenu>
+      <ActionMenu.Trigger>
+        <Button
+          variant='secondary-neutral'
+          icon={<ChevronDownIcon aria-hidden />}
+          iconPosition='right'
+        >
+          PVK
+        </Button>
+      </ActionMenu.Trigger>
+      <ActionMenu.Content>
+        {/* {behandlingsLivslop ? 'Rediger behandlinges livsløp' : 'Tegn behandlingens livsløp'} */}
+    {/* <ActionMenu.Item
+          as='a'
+          href={pvkDokumentasjonBehandlingsenLivslopUrl(
+            etterlevelseDokumentasjon.id,
+            behandlingsLivslop ? behandlingsLivslop.id : 'ny'
+          )}
+        >
+          Tegn behandlingens livsløp
+        </ActionMenu.Item>
+        <ActionMenu.Group label=''>
+          {pvkDokument && pvkDokument.pvkVurdering === EPvkVurdering.SKAL_UTFORE && (
+            <ActionMenu.Item
+              as='a'
+              href={pvkDokumentasjonStepUrl(etterlevelseDokumentasjon.id, pvkDokument.id, 1)}
+            >
+              {getPvkButtonText(pvkDokument, risikoscenarioList, isRisikoeier)}
+            </ActionMenu.Item>
+          )}
+          {!etterlevelseDokumentasjon.irrelevansFor.some(
+            (irrelevans) => irrelevans.code === 'PERSONOPPLYSNINGER'
+          ) && (
+            <ActionMenu.Item
+              as='a'
+              href={pvkDokumentasjonPvkBehovUrl(
+                etterlevelseDokumentasjon.id,
+                pvkDokument ? pvkDokument.id : 'ny'
+              )}
+            >
+              {isPvkDokumentVurdert(pvkDokument)
+                ? 'Revurder behov for PVK'
+                : 'Vurder behov for PVK'}
+            </ActionMenu.Item>
+          )}
+        </ActionMenu.Group>
+      </ActionMenu.Content>
+    </ActionMenu> */}
+  </>
 )
