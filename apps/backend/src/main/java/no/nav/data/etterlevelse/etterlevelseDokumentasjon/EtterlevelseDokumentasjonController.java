45bb803e5bd7377c71f2b3de6d04d30d4c742bfe
JeremiahUy <jeremiah.uy@outlook.com>
2026-01-26 13:37:10 +0100

    ADD: new endpoint for versjonsokning
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/EtterlevelseDokumentasjonController.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/EtterlevelseDokumentasjonController.java
@@ -152,6 +152,20 @@ public class EtterlevelseDokumentasjonController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Ny versjon av Etterlevelse Dokumentasjon")
+    @ApiResponse(description = "Ny versjon av Etterlevelse Dokumentasjon")
+    @PutMapping("/ny-versjon/{id}")
+    public ResponseEntity<EtterlevelseDokumentasjonResponse> updateVersion(@PathVariable UUID id, @Valid @RequestBody EtterlevelseDokumentasjonRequest request) {
+        log.debug("Ny versjon av Etterlevelse Dokumentasjon id={}", id);
+        if (!Objects.equals(id, request.getId())) {
+            throw new ValidationException(String.format("id mismatch in request %s and path %s", request.getId(), id));
+        }
+        var response = EtterlevelseDokumentasjonResponse.buildFrom(etterlevelseDokumentasjonService.approvedOfRisikoeierAndSave(request));
+        etterlevelseDokumentasjonService.addBehandlingAndTeamsDataAndResourceDataAndRisikoeiereData(response);
+
+        return ResponseEntity.ok(response);
+    }
+
     @Operation(summary = "Godkjenning av Etterlevelse Dokumentasjon")
     @ApiResponse(description = "Etterlevelse Dokumentasjon Godkjent")
     @PutMapping("/godkjenning/{id}")
