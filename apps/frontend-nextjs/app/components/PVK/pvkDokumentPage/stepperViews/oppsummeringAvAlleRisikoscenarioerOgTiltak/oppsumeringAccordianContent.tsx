0d8a5f466b5a703b132cde597d221cd955b223c7
K153697 <paul.martin.kokseter@nav.no>
2026-02-05 10:55:21 +0100

    UPDATE: replaced Alert with LocalAlert and added a list of "krav" links

--- apps/frontend-nextjs/app/components/PVK/pvkDokumentPage/stepperViews/oppsummeringAvAlleRisikoscenarioerOgTiltak/oppsumeringAccordianContent.tsx
+++ apps/frontend-nextjs/app/components/PVK/pvkDokumentPage/stepperViews/oppsummeringAvAlleRisikoscenarioerOgTiltak/oppsumeringAccordianContent.tsx
@@ -6,6 +6,7 @@ import {
   updateRisikoscenario,
 } from '@/api/risikoscenario/risikoscenarioApi'
 import VurdereTiltaksEffekt from '@/components/PVK/edit/vurdereTiltaksEffekt'
+import { ExternalLink } from '@/components/common/externalLink/externalLink'
 import AlertPvoUnderArbeidModal from '@/components/pvoTilbakemelding/common/alertPvoUnderArbeidModal'
 import RisikoscenarioView from '@/components/risikoscenario/common/RisikoscenarioView'
 import { RisikoscenarioTiltakHeader } from '@/components/risikoscenario/common/risikoscenarioTiltakHeader'
@@ -13,10 +14,12 @@ import RisikoscenarioModalForm from '@/components/risikoscenario/form/risikoscen
 import { TiltakReadMoreListModalEdit } from '@/components/tiltak/edit/tiltakReadMoreListModalEdit'
 import { IRisikoscenario } from '@/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/risikoscenario/risikoscenarioConstants'
 import { ITiltak } from '@/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/tiltak/tiltakConstants'
+import { IKravReference } from '@/constants/krav/kravConstants'
 import { pvkDokumentasjonStepUrl } from '@/routes/etterlevelseDokumentasjon/personvernkonsekvensevurdering/personvernkonsekvensvurderingRoutes'
+import { risikoDokumentasjonTemaKravNummerVersjonUrl } from '@/routes/risikoscenario/risikoscenarioRoutes'
 import { isReadOnlyPvkStatus } from '@/util/etterlevelseDokumentasjon/pvkDokument/pvkDokumentUtils'
 import { PencilIcon } from '@navikt/aksel-icons'
-import { Alert, Button, Link } from '@navikt/ds-react'
+import { Button, Link, List, LocalAlert } from '@navikt/ds-react'
 import { FunctionComponent, RefObject, useState } from 'react'
 
 type TProps = {
@@ -114,29 +117,66 @@ export const OppsumeringAccordianContent: FunctionComponent<TProps> = ({
 
         {!risikoscenario.ingenTiltak && risikoscenario.tiltakIds.length === 0 && (
           <div className='mt-5'>
-            <Alert className='mt-3' variant='warning'>
-              <span>
-                Før dere kan vurdere tiltakenes effekt, må dere{' '}
-                {!risikoscenario.generelScenario && 'legge inn tiltak på kravsiden.'}
-                {risikoscenario.generelScenario && (
-                  <Link
-                    href={pvkDokumentasjonStepUrl(
-                      etterlevelseDokumentasjonId,
-                      risikoscenario.pvkDokumentId,
-                      6,
-                      `?risikoscenario=${risikoscenario.id}`
-                    )}
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    aria-label='redigere etterlevelsesdokumentasjon'
-                    className='inline'
-                  >
-                    legge inn tiltak under Identifisering av risikoscenarioer og tiltak. (åpner i en
-                    ny fane).
-                  </Link>
+            <LocalAlert className='mt-3' status='warning'>
+              <LocalAlert.Header>
+                <LocalAlert.Title>Dette risikoscenarioet savner tiltak</LocalAlert.Title>
+              </LocalAlert.Header>
+              <LocalAlert.Content>
+                {!risikoscenario.generelScenario && (
+                  <>
+                    <span>
+                      Dere kan ikke vurdere tiltakenes effekt uten tiltak! Tiltak legger dere til på
+                      en av kravsidene hvor risikoscenarioet brukes: <br />
+                    </span>
+                    <List as='ul' className='mt-5'>
+                      {risikoscenario.relevanteKravNummer.map(
+                        (relevantKrav: IKravReference, index: number) => {
+                          const kravHref: string = risikoDokumentasjonTemaKravNummerVersjonUrl(
+                            etterlevelseDokumentasjonId,
+                            relevantKrav.temaCode || 'PVK',
+                            relevantKrav.kravNummer,
+                            relevantKrav.kravVersjon
+                          )
+                          return (
+                            <List.Item
+                              className='max-w-[75ch]'
+                              key={`${relevantKrav.kravNummer}_${index}`}
+                            >
+                              <ExternalLink href={kravHref}>
+                                K{relevantKrav.kravNummer}.{relevantKrav.kravVersjon}{' '}
+                                {relevantKrav.navn}
+                              </ExternalLink>
+                            </List.Item>
+                          )
+                        }
+                      )}
+                    </List>
+                  </>
                 )}
-              </span>
-            </Alert>
+                {risikoscenario.generelScenario && (
+                  <>
+                    <span>
+                      Dere kan ikke vurdere tiltakenes effekt uten tiltak! <br />
+                    </span>
+                    <Link
+                      href={pvkDokumentasjonStepUrl(
+                        etterlevelseDokumentasjonId,
+                        risikoscenario.pvkDokumentId,
+                        6,
+                        `?risikoscenario=${risikoscenario.id}`
+                      )}
+                      target='_blank'
+                      rel='noopener noreferrer'
+                      aria-label='redigere etterlevelsesdokumentasjon'
+                      className='mt-3'
+                    >
+                      Legg inn tiltak under Identifisering av risikoscenarioer og tiltak. (åpner i
+                      en ny fane).
+                    </Link>
+                  </>
+                )}
+              </LocalAlert.Content>
+            </LocalAlert>
           </div>
         )}
 
