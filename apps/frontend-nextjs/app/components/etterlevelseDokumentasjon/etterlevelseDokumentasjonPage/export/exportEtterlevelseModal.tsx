e4f4ca9e97debc8822b383927bd9929957520a22
Andre Groseth <andregroseth@hotmail.com>
2026-02-17 09:58:25 +0100

    Removed the unnecessary wrapper element so it no longer creates an extra flex child:

--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/export/exportEtterlevelseModal.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/export/exportEtterlevelseModal.tsx
@@ -46,114 +46,112 @@ export const ExportEtterlevelseModal = (props: IExportEtterlevelseModalProps) =>
   const codelist = useContext(CodelistContext)
 
   return (
-    <div>
-      <Modal
-        width='30rem'
-        open={isExportModalOpen}
-        onClose={() => {
-          setValgtTema('')
-          setIsExportModalOpen(false)
-        }}
-        header={{ heading: 'Eksporter etterlevelse', closeButton: false }}
-      >
-        <Modal.Body>
-          {isLoading ? (
-            <div className='flex justify-center w-full'>
-              <Loader size='large' />
-            </div>
-          ) : (
-            <div className='flex flex-col gap-4'>
-              <Select
-                label='Velg et tema for eksportering'
-                onChange={(event: ChangeEvent<HTMLSelectElement>) =>
-                  setValgtTema(event.currentTarget.value)
-                }
-                value={valgtTema}
-              >
-                <option key='' value=''>
-                  Alle tema
-                </option>
-                {codelist.utils
-                  .getParsedOptions(EListName.TEMA)
-                  .map((codeListOption: IGetParsedOptionsProps) => (
-                    <option key={`option_${codeListOption.value}`} value={codeListOption.value}>
-                      {codeListOption.label}
-                    </option>
-                  ))}
-              </Select>
-              <RadioGroup
-                legend='Dokumentet skal inneholde'
-                hideLegend
-                value={onlyActiveKrav}
-                onChange={(value: boolean) => setOnlyActiveKrav(value)}
-              >
-                <Radio value={false}>Eksporter alle krav versjoner</Radio>
-                <Radio value={true}>Eksporter kun gjeldende versjon krav</Radio>
-              </RadioGroup>
-              {errorMessage && (
-                <div className='w-full mt-4'>
-                  <Box className='mb-2.5' padding='space-4' background='warning-soft'>
-                    <div className='flex justify-center'>
-                      <BodyShort>{errorMessage}</BodyShort>
-                    </div>
-                  </Box>
-                </div>
-              )}
-              <div className='flex justify-end gap-2'>
-                <Button
-                  variant='tertiary'
-                  onClick={() => {
-                    setValgtTema('')
-                    setIsExportModalOpen(false)
-                  }}
-                >
-                  Avbryt
-                </Button>
-                <Button
-                  variant='primary'
-                  onClick={() => {
-                    ;(async () => {
-                      setIsLoading(true)
-                      setErrorMessage('')
-                      let exportUrl = `${env.backendBaseUrl}/export/etterlevelsedokumentasjon?etterlevelseDokumentasjonId=${etterlevelseDokumentasjonId}`
-                      if (valgtTema !== '') {
-                        exportUrl += `&temakode=${valgtTema}&onlyActiveKrav=${onlyActiveKrav}`
-                        axios
-                          .get(exportUrl)
-                          .then(() => {
-                            window.location.href = exportUrl
-                            setIsLoading(false)
-                            setIsExportModalOpen(false)
-                          })
-                          .catch((error: any) => {
-                            setErrorMessage(error.response.data.message)
-                            setIsLoading(false)
-                          })
-                      } else {
-                        exportUrl += `&onlyActiveKrav=${onlyActiveKrav}`
-                        axios
-                          .get(exportUrl)
-                          .then(() => {
-                            window.location.href = exportUrl
-                            setIsLoading(false)
-                            setIsExportModalOpen(false)
-                          })
-                          .catch((error: any) => {
-                            setErrorMessage(error.response.data.message)
-                            setIsLoading(false)
-                          })
-                      }
-                    })()
-                  }}
-                >
-                  Eksporter
-                </Button>
+    <Modal
+      width='30rem'
+      open={isExportModalOpen}
+      onClose={() => {
+        setValgtTema('')
+        setIsExportModalOpen(false)
+      }}
+      header={{ heading: 'Eksporter etterlevelse', closeButton: false }}
+    >
+      <Modal.Body>
+        {isLoading ? (
+          <div className='flex justify-center w-full'>
+            <Loader size='large' />
+          </div>
+        ) : (
+          <div className='flex flex-col gap-4'>
+            <Select
+              label='Velg et tema for eksportering'
+              onChange={(event: ChangeEvent<HTMLSelectElement>) =>
+                setValgtTema(event.currentTarget.value)
+              }
+              value={valgtTema}
+            >
+              <option key='' value=''>
+                Alle tema
+              </option>
+              {codelist.utils
+                .getParsedOptions(EListName.TEMA)
+                .map((codeListOption: IGetParsedOptionsProps) => (
+                  <option key={`option_${codeListOption.value}`} value={codeListOption.value}>
+                    {codeListOption.label}
+                  </option>
+                ))}
+            </Select>
+            <RadioGroup
+              legend='Dokumentet skal inneholde'
+              hideLegend
+              value={onlyActiveKrav}
+              onChange={(value: boolean) => setOnlyActiveKrav(value)}
+            >
+              <Radio value={false}>Eksporter alle krav versjoner</Radio>
+              <Radio value={true}>Eksporter kun gjeldende versjon krav</Radio>
+            </RadioGroup>
+            {errorMessage && (
+              <div className='w-full mt-4'>
+                <Box className='mb-2.5' padding='space-4' background='warning-soft'>
+                  <div className='flex justify-center'>
+                    <BodyShort>{errorMessage}</BodyShort>
+                  </div>
+                </Box>
               </div>
+            )}
+            <div className='flex justify-end gap-2'>
+              <Button
+                variant='tertiary'
+                onClick={() => {
+                  setValgtTema('')
+                  setIsExportModalOpen(false)
+                }}
+              >
+                Avbryt
+              </Button>
+              <Button
+                variant='primary'
+                onClick={() => {
+                  ;(async () => {
+                    setIsLoading(true)
+                    setErrorMessage('')
+                    let exportUrl = `${env.backendBaseUrl}/export/etterlevelsedokumentasjon?etterlevelseDokumentasjonId=${etterlevelseDokumentasjonId}`
+                    if (valgtTema !== '') {
+                      exportUrl += `&temakode=${valgtTema}&onlyActiveKrav=${onlyActiveKrav}`
+                      axios
+                        .get(exportUrl)
+                        .then(() => {
+                          window.location.href = exportUrl
+                          setIsLoading(false)
+                          setIsExportModalOpen(false)
+                        })
+                        .catch((error: any) => {
+                          setErrorMessage(error.response.data.message)
+                          setIsLoading(false)
+                        })
+                    } else {
+                      exportUrl += `&onlyActiveKrav=${onlyActiveKrav}`
+                      axios
+                        .get(exportUrl)
+                        .then(() => {
+                          window.location.href = exportUrl
+                          setIsLoading(false)
+                          setIsExportModalOpen(false)
+                        })
+                        .catch((error: any) => {
+                          setErrorMessage(error.response.data.message)
+                          setIsLoading(false)
+                        })
+                    }
+                  })()
+                }}
+              >
+                Eksporter
+              </Button>
             </div>
-          )}
-        </Modal.Body>
-      </Modal>
-    </div>
+          </div>
+        )}
+      </Modal.Body>
+    </Modal>
   )
 }
 
