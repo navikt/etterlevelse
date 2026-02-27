ff6ed73c33a976d279c7d9572c6c6f5cc6a0b106
Andre Groseth <andregroseth@hotmail.com>
2026-02-27 09:53:03 +0100

    remove: comments

--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/gjenbrukButton/etterleverRolleGjenbruk/etterleverRolleGjenbruk.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/gjenbrukButton/etterleverRolleGjenbruk/etterleverRolleGjenbruk.tsx
@@ -1,14 +1,6 @@
 import { ComponentProps, FunctionComponent } from 'react'
 import { CommonVariantOneGjenbruk } from '../commonGjenbruk/commonGjenbruk'
 
-// type TProps = {
-//   etterlevelseDokumentasjon: TEtterlevelseDokumentasjonQL
-//   risikoscenarioList: IRisikoscenario[]
-//   behandlingsLivslop?: IBehandlingensLivslop
-//   pvkDokument?: IPvkDokument
-//   isRisikoeier: boolean
-// }
-
 const EtterleverRolleGjenbruk: FunctionComponent<
   ComponentProps<typeof CommonVariantOneGjenbruk>
 > = (props) => <CommonVariantOneGjenbruk {...props} />
