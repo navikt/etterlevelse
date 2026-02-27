835f3034a17637e9bd0f8588aa212b441b13f0d5
beeccy <beeccy@users.noreply.github.com>
2026-02-27 14:40:46 +0100

    Mangler URL

--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/etterlevelseButton/commonEtterlevelse/etterleverCommon.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/buttonGroup/etterlevelseButton/commonEtterlevelse/etterleverCommon.tsx
@@ -91,7 +91,7 @@ export const EtterleverGodkjentVariant: FunctionComponent<TProps> = ({
             Rediger dokumentegenskaper
           </RedigerEgenskaperActionMenuItem>
 
-          <ActionMenu.Item as='a' href=''>
+          <ActionMenu.Item as='a' href='????'>
             Oppdater etterlevelsen
           </ActionMenu.Item>
           <ActionMenu.Item as='button' onSelect={() => setIsExportModalOpen(true)}>
