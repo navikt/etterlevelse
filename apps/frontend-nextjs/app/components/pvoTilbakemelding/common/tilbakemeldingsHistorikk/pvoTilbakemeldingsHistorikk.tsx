9ddd424493a45bebc101f8f6d85d49e7d5011836
JeremiahUy <jeremiah.uy@outlook.com>
2026-03-03 12:38:38 +0100

    UPDATE: pvo tilbakemelding historikk to be group by version
    Co-authored-by: PaulMartinKokseter <Paul.martin.kokseter@nav.no>

--- apps/frontend-nextjs/app/components/pvoTilbakemelding/common/tilbakemeldingsHistorikk/pvoTilbakemeldingsHistorikk.tsx
+++ apps/frontend-nextjs/app/components/pvoTilbakemelding/common/tilbakemeldingsHistorikk/pvoTilbakemeldingsHistorikk.tsx
@@ -2,7 +2,7 @@ import {
   IPvoTilbakemelding,
   IVurdering,
 } from '@/constants/pvoTilbakemelding/pvoTilbakemeldingConstants'
-import { Accordion, Heading } from '@navikt/ds-react'
+import { Accordion, Heading, Label } from '@navikt/ds-react'
 import moment from 'moment'
 import { FunctionComponent } from 'react'
 import PvoTilbakemeldingsHistorikkContent from './pvoTilbakemeldingsHistorikkContent'
@@ -22,6 +22,11 @@ export const PvoTilbakemeldingsHistorikk: FunctionComponent<TProps> = ({
   fieldName,
   relevantVurderingsInnsendingId,
 }) => {
+  const versjoner = [
+    ...new Set(
+      pvoTilbakemelding.vurderinger.map((vurdering) => vurdering.etterlevelseDokumentVersjon)
+    ),
+  ].sort((a, b) => b - a)
   let vurderingerForVersjon: IVurdering[] = []
   const sortertVurderingerForVersjon: IVurdering[] = pvoTilbakemelding.vurderinger.sort(
     (a: IVurdering, b: IVurdering) => b.innsendingId - a.innsendingId
@@ -38,22 +43,37 @@ export const PvoTilbakemeldingsHistorikk: FunctionComponent<TProps> = ({
       <Heading level='2' size='small' className='mb-5'>
         Tilbakemeldingshistorikk
       </Heading>
-      <Accordion>
-        {vurderingerForVersjon.map((vurdering: IVurdering) => (
-          <Accordion.Item key={`vurdering_${vurdering.innsendingId}`}>
-            <Accordion.Header>
-              {vurdering.innsendingId}. tilbakemelding - {moment(vurdering.sendtDato).format('LL')}
-            </Accordion.Header>
-            <Accordion.Content>
-              <PvoTilbakemeldingsHistorikkContent
-                tilbakemeldingsinnhold={vurdering[fieldName]}
-                forPvo={true}
-                noHeader={true}
-              />
-            </Accordion.Content>
-          </Accordion.Item>
-        ))}
-      </Accordion>
+
+      {versjoner.map((versjon) => (
+        <div key={'tilbakemelding_historik_' + versjon} className='mb-5'>
+          <Label>Versjon {versjon}</Label>
+
+          <Accordion className='mt-3'>
+            {pvoTilbakemelding.vurderinger
+              .filter((vurdering) => vurdering.etterlevelseDokumentVersjon === versjon)
+              .sort((a, b) => b.innsendingId - a.innsendingId)
+              .map((vurdering) => {
+                if (vurdering.innsendingId < relevantVurderingsInnsendingId) {
+                  return (
+                    <Accordion.Item key={`${versjon}_vurdering_${vurdering.innsendingId}`}>
+                      <Accordion.Header>
+                        {vurdering.innsendingId}. tilbakemelding -{' '}
+                        {moment(vurdering.sendtDato).format('LL')}
+                      </Accordion.Header>
+                      <Accordion.Content>
+                        <PvoTilbakemeldingsHistorikkContent
+                          tilbakemeldingsinnhold={vurdering[fieldName]}
+                          forPvo={true}
+                          noHeader={true}
+                        />
+                      </Accordion.Content>
+                    </Accordion.Item>
+                  )
+                }
+              })}
+          </Accordion>
+        </div>
+      ))}
     </div>
   )
 }
