958c4c8cded8f413efc6b1fc1fca4ae3294a41bc
Andre Groseth <andregroseth@hotmail.com>
2026-03-06 13:08:35 +0100

    fix: typo

--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/form/etterlevelseDokumentasjonForm.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/form/etterlevelseDokumentasjonForm.tsx
@@ -776,7 +776,7 @@ export const EtterlevelseDokumentasjonForm: FunctionComponent<
                       <InfoCard data-color='info' size='small' className='mt-2 max-w-[75ch] mb-3'>
                         <InfoCard.Header icon={<InformationSquareIcon aria-hidden />}>
                           <InfoCard.Title as='div'>
-                            Risikoeier kan ikke redigeres når PVK er sent til godkjenning.
+                            Risikoeier kan ikke redigeres når PVK er sendt til godkjenning.
                           </InfoCard.Title>
                         </InfoCard.Header>
                       </InfoCard>
