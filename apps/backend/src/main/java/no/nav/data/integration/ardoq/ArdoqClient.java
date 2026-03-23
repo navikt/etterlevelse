2d67c75874cf6c00fdaa23c7eed186e453fc88d3
JeremiahUy <jeremiah.uy@outlook.com>
2026-03-23 13:26:52 +0100

    UPDATE: return data

--- apps/backend/src/main/java/no/nav/data/integration/ardoq/ArdoqClient.java
+++ apps/backend/src/main/java/no/nav/data/integration/ardoq/ArdoqClient.java
@@ -1,14 +1,17 @@
 package no.nav.data.integration.ardoq;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.data.common.web.TraceHeaderFilter;
 import no.nav.data.integration.ardoq.dto.ArdoqSystem;
 import no.nav.data.integration.ardoq.dto.ArdoqSystemData;
 import no.nav.data.integration.p360.dto.P360Document;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -17,11 +20,16 @@ import java.util.List;
 @Service
 public class ArdoqClient {
 
+    private final WebClient client;
     private final RestTemplate restTemplate;
     private final ArdoqProperties properties;
 
 
-    public ArdoqClient(RestTemplate restTemplate, ArdoqProperties properties) {
+    public ArdoqClient(WebClient.Builder webClientBuilder, RestTemplate restTemplate, ArdoqProperties properties) {
+        this.client = webClientBuilder
+                .baseUrl(properties.getBaseUrl())
+                .filter(new TraceHeaderFilter(true))
+                .build();
         this.restTemplate = restTemplate;
         this.properties = properties;
     }
@@ -32,10 +40,10 @@ public class ArdoqClient {
         try {
             HttpHeaders headers = createHeadersWithAuth();
             HttpEntity<String> entity = new HttpEntity<>(headers);
-            var response = restTemplate.getForEntity(url, ArdoqSystemData.class, entity);
-            if (response.getBody() != null) {
-                log.info("Succesfully recieve ardoq system with status code: {}", response.getStatusCode());
-                return response.getBody().getValues();
+            var response = get(url, ArdoqSystemData.class, entity);
+            if (response.getValues() != null) {
+                log.info("Succesfully recieve ardoq system");
+                return response.getValues();
             } else {
                 throw new RestClientException("Response body is null");
             }
@@ -43,6 +51,17 @@ public class ArdoqClient {
             log.error("Unable to connect to Ardoq, error: {}", String.valueOf(e));
             return null;
         }
+    }
+
+    private <T> T get(String uri, Class<T> response, Object... params) {
+        var res = client.get()
+                .uri(uri, params)
+                .retrieve()
+                .bodyToMono(response)
+                .block();
+        Assert.isTrue(res != null, "response is null");
+
+        return res;
     }
 
 
--- apps/backend/src/main/java/no/nav/data/integration/ardoq/dto/ArdoqSystem.java
+++ apps/backend/src/main/java/no/nav/data/integration/ardoq/dto/ArdoqSystem.java
@@ -1,5 +1,6 @@
 package no.nav.data.integration.ardoq.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,10 +11,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ArdoqSystem {
+    @JsonProperty("Ardoq OID")
     private String Ardoq_OID;
+    @JsonProperty("Ardoq ID")
     private String Ardoq_ID;
+    @JsonProperty("Name")
     private String Name;
+    @JsonProperty("Alias")
     private String Alias;
+    @JsonProperty("Lifecycle Phase")
     private String Lifecycle_Phase;
+    @JsonProperty("Description")
     private String Description;
 }
