f775e054470eab86dcbfde00c75a95043506049b
Jeremiah Uy <jeremiah.augie.uy@nav.no>
2026-03-31 09:03:26 +0200

    UPDATE: component to show saved alert when risikoeier approves (#1847)

--- apps/frontend-nextjs/app/components/PVK/pvkDokumentPage/stepperViews/sendInn/sendInnCoponents/SendInnLagringVellykketAlert.tsx
+++ apps/frontend-nextjs/app/components/PVK/pvkDokumentPage/stepperViews/sendInn/sendInnCoponents/SendInnLagringVellykketAlert.tsx
@@ -3,12 +3,16 @@ import { FunctionComponent } from 'react'
 
 type TProps = {
   setSavedSuccessful: (state: boolean) => void
+  customText?: string
 }
 
-export const SendInnLagringVellykketAlert: FunctionComponent<TProps> = ({ setSavedSuccessful }) => (
+export const SendInnLagringVellykketAlert: FunctionComponent<TProps> = ({
+  setSavedSuccessful,
+  customText,
+}) => (
   <LocalAlert status='success'>
     <LocalAlert.Header>
-      <LocalAlert.Title>Lagring vellykket</LocalAlert.Title>
+      <LocalAlert.Title> {customText ? customText : 'Lagring vellykket'}</LocalAlert.Title>
       <LocalAlert.CloseButton onClick={() => setSavedSuccessful(false)} />
     </LocalAlert.Header>
   </LocalAlert>
