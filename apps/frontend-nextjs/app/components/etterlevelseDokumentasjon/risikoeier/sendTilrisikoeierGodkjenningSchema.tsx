8ecdb60acf090796afd5ef5e01aa5a092a30eefb
JeremiahUy <jeremiah.uy@outlook.com>
2026-04-14 10:13:12 +0200

    UPDATE: validering to be able to send with empty message on lagre og fortsett senere

--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/risikoeier/sendTilrisikoeierGodkjenningSchema.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/risikoeier/sendTilrisikoeierGodkjenningSchema.tsx
@@ -1,8 +1,20 @@
+import { EEtterlevelseDokumentasjonStatus } from '@/constants/etterlevelseDokumentasjon/etterlevelseDokumentasjonConstants'
 import * as yup from 'yup'
 
-const meldingEtterlevelerTilRisikoeierCheck = yup
-  .string()
-  .required('Dere må oppsummere for risikoeier hvorfor det er aktuelt med godkjenning')
+const meldingEtterlevelerTilRisikoeierCheck = yup.string().test({
+  name: 'meldingEtterlevelerTilRisikoeierCheck',
+  message: 'Dere må oppsummere for risikoeier hvorfor det er aktuelt med godkjenning',
+  test: function (meldingEtterlevelerTilRisikoeier) {
+    const { parent } = this
+    if (
+      parent.status !== EEtterlevelseDokumentasjonStatus.UNDER_ARBEID &&
+      ['', null, undefined].includes(meldingEtterlevelerTilRisikoeier)
+    ) {
+      return false
+    }
+    return true
+  },
+})
 
 export const sendTilRisikoGodkjenningSchema = () =>
   yup.object({
