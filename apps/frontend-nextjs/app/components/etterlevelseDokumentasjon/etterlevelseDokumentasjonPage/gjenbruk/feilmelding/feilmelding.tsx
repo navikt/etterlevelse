a7bfa0399244ef9a1e505623171b6668cab87ae4
beeccy <beeccy@users.noreply.github.com>
2026-04-16 09:48:32 +0200

    NAV-28505 Fikset at arrayene feiler riktig pluss lesbarhet

--- apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/gjenbruk/feilmelding/feilmelding.tsx
+++ apps/frontend-nextjs/app/components/etterlevelseDokumentasjon/etterlevelseDokumentasjonPage/gjenbruk/feilmelding/feilmelding.tsx
@@ -19,95 +19,107 @@ const etterlevelseDokumentasjonUrl = (
 export const GjenbrukFeilmelding: FunctionComponent<TProps> = ({
   etterlevelseDokumentasjon,
   values,
-}) => (
-  <>
-    {values.varslingsadresser.toString()}
-    {(values.title ||
-      values.beskrivelse ||
-      values.varslingsadresser ||
-      values.teamsData ||
-      values.resourcesData ||
-      values.nomAvdelingId) && (
-      <InfoCard data-color='warning'>
-        <InfoCard.Header icon={<ExclamationmarkTriangleIcon aria-hidden />}>
-          <InfoCard.Title>
-            Dere må oppdatere følgende felt i dokumentegenskaper før dere kan slå på gjenbruk.
-          </InfoCard.Title>
-        </InfoCard.Header>
-        <InfoCard.Content>
-          <List>
-            {[null, undefined, ''].includes(values.title) && (
-              <List.Item>
-                <Link
-                  href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'title')}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {EEtterlevelseDokumentSchemaMelding.TITLE} (åpner i en ny fane)
-                </Link>
-              </List.Item>
-            )}
-            {[null, undefined, ''].includes(values.beskrivelse) && (
-              <List.Item>
-                <Link
-                  href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'beskrivelse')}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {EEtterlevelseDokumentSchemaMelding.BESKRIVELSE} (åpner i en ny fane)
-                </Link>
-              </List.Item>
-            )}
-            {(!values.varslingsadresser || values.varslingsadresser.length === 0) && (
-              <List.Item>
-                <Link
-                  href={etterlevelseDokumentasjonUrl(
-                    etterlevelseDokumentasjon,
-                    'varslingsadresser'
-                  )}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {EEtterlevelseDokumentSchemaMelding.VARSLINGSADRESSER} (åpner i en ny fane)
-                </Link>
-              </List.Item>
-            )}
-            {(!values.teamsData || values.teamsData.length === 0) && (
-              <List.Item>
-                <Link
-                  href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'teamsData')}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {EEtterlevelseDokumentSchemaMelding.TEAMDATA} (åpner i en ny fane)
-                </Link>
-              </List.Item>
-            )}
-            {(!values.resourcesData || values.resourcesData.length === 0) && (
-              <List.Item>
-                <Link
-                  href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'resourcesData')}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {EEtterlevelseDokumentSchemaMelding.RESOURCEDATA} (åpner i en ny fane)
-                </Link>
-              </List.Item>
-            )}
-            {[null, undefined, ''].includes(values.nomAvdelingId) && (
-              <List.Item>
-                <Link
-                  href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'nomAvdelingId')}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  {EEtterlevelseDokumentSchemaMelding.NOM_AVDELING_ID} (åpner i en ny fane)
-                </Link>
-              </List.Item>
-            )}
-          </List>
-        </InfoCard.Content>
-      </InfoCard>
-    )}
-  </>
-)
+}) => {
+  const missingTitle = [null, undefined, ''].includes(values.title)
+  const missingBeskrivelse = [null, undefined, ''].includes(values.beskrivelse)
+  const missingVarslingsadresser =
+    !values.varslingsadresser || values.varslingsadresser.length === 0
+  const missingTeamsData = !values.teamsData || values.teamsData.length === 0
+  const missingResourcesData = !values.resourcesData || values.resourcesData.length === 0
+  const missingNomAvdelingId = [null, undefined, ''].includes(values.nomAvdelingId)
+
+  const hasMissingRequiredField =
+    missingTitle ||
+    missingBeskrivelse ||
+    missingVarslingsadresser ||
+    missingTeamsData ||
+    missingResourcesData ||
+    missingNomAvdelingId
+
+  return (
+    <>
+      {hasMissingRequiredField && (
+        <InfoCard data-color='warning'>
+          <InfoCard.Header icon={<ExclamationmarkTriangleIcon aria-hidden />}>
+            <InfoCard.Title>
+              Dere må oppdatere følgende felt i dokumentegenskaper før dere kan slå på gjenbruk.
+            </InfoCard.Title>
+          </InfoCard.Header>
+          <InfoCard.Content>
+            <List>
+              {missingTitle && (
+                <List.Item>
+                  <Link
+                    href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'title')}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {EEtterlevelseDokumentSchemaMelding.TITLE} (åpner i en ny fane)
+                  </Link>
+                </List.Item>
+              )}
+              {missingBeskrivelse && (
+                <List.Item>
+                  <Link
+                    href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'beskrivelse')}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {EEtterlevelseDokumentSchemaMelding.BESKRIVELSE} (åpner i en ny fane)
+                  </Link>
+                </List.Item>
+              )}
+              {missingVarslingsadresser && (
+                <List.Item>
+                  <Link
+                    href={etterlevelseDokumentasjonUrl(
+                      etterlevelseDokumentasjon,
+                      'varslingsadresser'
+                    )}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {EEtterlevelseDokumentSchemaMelding.VARSLINGSADRESSER} (åpner i en ny fane)
+                  </Link>
+                </List.Item>
+              )}
+              {missingTeamsData && (
+                <List.Item>
+                  <Link
+                    href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'teamsData')}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {EEtterlevelseDokumentSchemaMelding.TEAMDATA} (åpner i en ny fane)
+                  </Link>
+                </List.Item>
+              )}
+              {missingResourcesData && (
+                <List.Item>
+                  <Link
+                    href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'resourcesData')}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {EEtterlevelseDokumentSchemaMelding.RESOURCEDATA} (åpner i en ny fane)
+                  </Link>
+                </List.Item>
+              )}
+              {missingNomAvdelingId && (
+                <List.Item>
+                  <Link
+                    href={etterlevelseDokumentasjonUrl(etterlevelseDokumentasjon, 'nomAvdelingId')}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    {EEtterlevelseDokumentSchemaMelding.NOM_AVDELING_ID} (åpner i en ny fane)
+                  </Link>
+                </List.Item>
+              )}
+            </List>
+          </InfoCard.Content>
+        </InfoCard>
+      )}
+    </>
+  )
+}
