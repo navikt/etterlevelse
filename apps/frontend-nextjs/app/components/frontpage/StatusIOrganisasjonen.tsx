e6a0bd112ab3ab99a971e5df49093c3ab51f3cfd
JeremiahUy <jeremiah.uy@outlook.com>
2026-04-22 09:38:40 +0200

    UPDATE: link back to metabase for prod testing

--- apps/frontend-nextjs/app/components/frontpage/StatusIOrganisasjonen.tsx
+++ apps/frontend-nextjs/app/components/frontpage/StatusIOrganisasjonen.tsx
@@ -5,7 +5,20 @@ import { LinkPanel } from '@navikt/ds-react'
 export const StatusIOrganisasjonen = () => {
   return (
     <div className='w-full ml-2.5'>
-      <LinkPanel href='/dashboard'>
+      <LinkPanel
+        href='https://metabase.ansatt.nav.no/dashboard/116-dashboard-for-etterlevelse'
+        onClick={() => {
+          // const ampliInstance = ampli()
+          // if (ampliInstance) {
+          //   ampliInstance.logEvent('navigere', {
+          //     kilde: 'forside-panel',
+          //     app: 'etterlevelse',
+          //     til: 'https://metabase.ansatt.nav.no/dashboard/117-dashboard-for-etterlevelse',
+          //     fra: '/',
+          //   })
+          // }
+        }}
+      >
         <LinkPanel.Title>Status i organisasjonen</LinkPanel.Title>
         <LinkPanel.Description>
           Følg med på status og se hvor godt Nav sine områder dokumenterer på kravene
