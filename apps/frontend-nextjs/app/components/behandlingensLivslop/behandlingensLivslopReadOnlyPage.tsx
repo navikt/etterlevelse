e4819db1c15fc007ac8a05bdeb0459d5fe67dbee
JeremiahUy <jeremiah.uy@outlook.com>
2026-05-05 10:19:57 +0200

    FIX: conditional rendering check

--- apps/frontend-nextjs/app/components/behandlingensLivslop/behandlingensLivslopReadOnlyPage.tsx
+++ apps/frontend-nextjs/app/components/behandlingensLivslop/behandlingensLivslopReadOnlyPage.tsx
@@ -90,13 +90,10 @@ export const BehandlingensLivslopReadOnlyPage = () => {
           </MainPanelLayout>
 
           {/* right side */}
-          {etterlevelseDokumentasjon && (
-            <SidePanelLayout>
-              <BehandlingensLivsLopSidePanel
-                etterlevelseDokumentasjon={etterlevelseDokumentasjon}
-              />
-            </SidePanelLayout>
-          )}
+
+          <SidePanelLayout>
+            <BehandlingensLivsLopSidePanel etterlevelseDokumentasjon={etterlevelseDokumentasjon} />
+          </SidePanelLayout>
         </ContentLayout>
       )}
     </PageLayout>
