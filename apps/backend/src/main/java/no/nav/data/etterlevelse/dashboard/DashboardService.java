929110c019c290c00965f8ca3ba9aafcf746fdda
Andre Groseth <andregroseth@hotmail.com>
2026-05-08 12:04:16 +0200

    fix: add missing java.util.Set import in DashboardService

--- apps/backend/src/main/java/no/nav/data/etterlevelse/dashboard/DashboardService.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/dashboard/DashboardService.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
