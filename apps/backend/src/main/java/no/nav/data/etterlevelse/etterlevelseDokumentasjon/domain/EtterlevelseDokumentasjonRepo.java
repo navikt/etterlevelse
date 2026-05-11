92c333c8192ab68395c427de519aa38069b294e3
JeremiahUy <jeremiah.uy@outlook.com>
2026-05-11 13:34:59 +0200

    UPDATE: query to handle old domain data that does not have the nomavdelingId field
    Co-authored-by: andregroseth <lord.andre.groseth@nav.no>

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/EtterlevelseDokumentasjonRepo.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/EtterlevelseDokumentasjonRepo.java
@@ -29,7 +29,7 @@ public interface EtterlevelseDokumentasjonRepo extends JpaRepository<Etterlevels
     @Query(value = "select * from etterlevelse_dokumentasjon", nativeQuery = true )
     List<EtterlevelseDokumentasjon> getAllEtterlevelseDokumentasjoner();
 
-    @Query(value = "select * from etterlevelse_dokumentasjon where data ->> 'nomAvdelingId' = ?1 or (?1 = '' and data -> 'nomAvdelingId' is null)", nativeQuery = true )
+    @Query(value = "select * from etterlevelse_dokumentasjon where data ->> 'nomAvdelingId' = ?1 or (?1 = '' and (data -> 'nomAvdelingId' is null or (data -> 'nomAvdelingId' IS NOT NULL AND data ->> 'nomAvdelingId' IS NULL)))", nativeQuery = true )
     List<EtterlevelseDokumentasjon> getByAvdelingId(String avdelingId);
 
 
