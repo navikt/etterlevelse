fc7d8d7099d3dc2871b5d1238ce039edf0085f9d
Andre Groseth <andregroseth@hotmail.com>
2026-05-26 13:56:43 +0200

    rename: button

--- apps/frontend-nextjs/app/components/dashboard/TemaDashboardPage.tsx
+++ apps/frontend-nextjs/app/components/dashboard/TemaDashboardPage.tsx
@@ -540,7 +540,7 @@ const TemaDashboardPage = () => {
             disabled={isLoading || filteredTemaStats.length === 0}
             className='pr-4'
           >
-            Last ned nøkkeltall utvalg som CSV
+            Last ned nøkkeltall som CSV
           </Button>
         </div>
 
