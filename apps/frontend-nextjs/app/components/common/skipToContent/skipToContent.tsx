b2cadfe4a8bc9fb6b8ad1a313f9479aeaf5df79c
K153697 <paul.martin.kokseter@nav.no>
2026-06-01 11:16:24 +0200

    Fjerne søk fra skip-link

--- apps/frontend-nextjs/app/components/common/skipToContent/skipToContent.tsx
+++ apps/frontend-nextjs/app/components/common/skipToContent/skipToContent.tsx
@@ -7,11 +7,6 @@ const SkipToContent = () => (
         Hovedinnhold
       </Link>
     </div>
-    <div className='focus-within:w-10 bg-gray-900'>
-      <Link className='p-2 absolute -top-10 left-0 focus:top-0 text-white' href='#search'>
-        Søk
-      </Link>
-    </div>
   </>
 )
 
