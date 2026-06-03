6b7b9b5dce6a8e0b525d158bbca0538b49659d42
beeccy <beeccy@users.noreply.github.com>
2026-06-03 12:36:23 +0200

    NAV_28656 Fjernet behandlingskatalogenBehandlingsIdUrl

--- apps/frontend-nextjs/app/routes/behandlingskatalog/behandlingskatalogRoutes.ts
+++ apps/frontend-nextjs/app/routes/behandlingskatalog/behandlingskatalogRoutes.ts
@@ -1,5 +1,3 @@
-import { etterlevelseDokumentasjonerUrl } from '../etterlevelseDokumentasjon/etterlevelseDokumentasjonRoutes'
-
 export const behandlingUrl: string = '/behandling'
 export const behandlingerUrl: string = '/behandlinger'
 
@@ -7,6 +5,3 @@ export const behandlingskatalogenProcessUrl = (
   pollyBaseUrl: string,
   behandlingId: string
 ): string => `${pollyBaseUrl}process/${behandlingId}`
-
-export const behandlingskatalogenBehandlingsIdUrl = (behandlingId: string): string =>
-  `${etterlevelseDokumentasjonerUrl(`/behandlingsok?behandlingId=${behandlingId}`)}`
