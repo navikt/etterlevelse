ee70d8a9c33fea901fb968b679a5d0dd91235a52
Andre Groseth <andregroseth@hotmail.com>
2026-06-03 13:22:38 +0200

    ADD: ReadMore and move link

--- apps/frontend-nextjs/app/components/dashboard/TemaDashboardPage.tsx
+++ apps/frontend-nextjs/app/components/dashboard/TemaDashboardPage.tsx
@@ -125,9 +125,14 @@ const TemaStatsCard = ({ stats }: { stats: ITemaDashboardStats }) => {
 
   return (
     <div className='border border-gray-300 rounded-lg p-6 bg-white'>
-      <Heading size='small' level='3'>
-        {stats.temaName}
-      </Heading>
+      <div className='flex items-center gap-4'>
+        <Heading size='small' level='3'>
+          {stats.temaName}
+        </Heading>
+        <AkselLink href={`/dashboard/tema/${stats.temaCode}`}>
+          Les mer om etterlevelse under {stats.temaName}
+        </AkselLink>
+      </div>
 
       <TemaDokumentCount count={stats.etterlevelseDokumentCount} />
 
@@ -161,9 +166,60 @@ const TemaStatsCard = ({ stats }: { stats: ITemaDashboardStats }) => {
         </div>
       </div>
 
-      <AkselLink href={`/dashboard/tema/${stats.temaCode}`} className='mt-4 inline-block'>
-        Les mer om {stats.temaName}
-      </AkselLink>
+      <ReadMore
+        header='Hjelp til å tolke figurer og nøkkeltall om etterlevelse'
+        className='mt-4 max-w-[75ch]'
+      >
+        <List>
+          <List.Item>
+            <b>Hvorfor er så mange etterlevelsesdokumenter under arbeid?</b>
+            <br />
+            Det at et etterlevelsesdokument er under arbeid er ikke nødvendigvis et dårlig tegn. I
+            noen kontekster er det behov for å oppdatere etterlevelsesdokumentasjon oftere. Et
+            eksempel er smidig produktutvikling, hvor nytt utviklingsarbeid kan tvinge en
+            revurdering av hvordan man etterlever enkelte krav.
+          </List.Item>
+          <List.Item>
+            <b>Hva er forskjellen mellom etterlevelseskrav og suksesskriterier?</b>
+            <br />
+            Hvert etterlevelseskrav består av ett eller flere suksesskriterier. Suksesskriterier er
+            aktive handlinger som viser hva etterleveren konkret må gjøre for å etterleve kravet.
+            Hvorvidt man etterlever kravet beskrives på suksesskriteriumsnivået.{' '}
+            <AkselLink
+              href={'/omstottetiletterlevelse#dette-inneholder-et-etterlevelseskrav'}
+              target='_blank'
+            >
+              Les mer om etterlevelseskrav og suksesskriterier (åpner i en ny fane).
+            </AkselLink>
+          </List.Item>
+          <List.Item>
+            <b>Hvorfor er så mange suksesskriterier ikke relevant?</b>
+            <br />
+            Det er begrenset med hvor mange irrelevante etterlevelseskrav man får filtrert bort før
+            en begynner å dokumentere etterlevelse. Dette gjør at etterleveren selv kan være nødt
+            til å markere noen krav og suksesskriterier som “ikke relevant”
+          </List.Item>
+          <List.Item>
+            <b>
+              Hva er forskjellen mellom de figurene/nøkkeltallene som handler om suksesskriterier?
+            </b>
+            <br />
+            Første figur/nøkkeltall gir et samlet bilde på dokumentering av alle suksesskriterier
+            underordnet dette temaet. Den andre viser statistikk for krav hvor etterleveren har
+            registrert at de ble ferdig med vurderingen sin. Den tredje viser statistikk for krav
+            som ikke ennå er registrert som ferdig vurdert, selv om etterleveren kan ha vurdert
+            hvorvidt enkelte suksesskriterier etterleves.
+          </List.Item>
+        </List>
+
+        <AkselLink
+          className='mt-4'
+          href={'/omstottetiletterlevelse#hvordan-dokumentere-etterlevelse'}
+          target='_blank'
+        >
+          Les mer om dokumentering av etterlevelse (åpner i en ny fane)
+        </AkselLink>
+      </ReadMore>
     </div>
   )
 }
