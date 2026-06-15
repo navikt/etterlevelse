5f029f359b14a65254df9b733b53d4e2c9860f23
JeremiahUy <jeremiah.uy@outlook.com>
2026-06-15 11:26:16 +0200

    REMOVE: unused imports

--- apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/EtterlevelseDokumentasjonRepo.java
+++ apps/backend/src/main/java/no/nav/data/etterlevelse/etterlevelseDokumentasjon/domain/EtterlevelseDokumentasjonRepo.java
@@ -1,15 +1,13 @@
 package no.nav.data.etterlevelse.etterlevelseDokumentasjon.domain;
 
-import java.util.List;
-import java.util.UUID;
-
+import no.nav.data.etterlevelse.common.domain.KravId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import no.nav.data.etterlevelse.common.domain.KravId;
+import java.util.List;
+import java.util.UUID;
 
 public interface EtterlevelseDokumentasjonRepo extends JpaRepository<EtterlevelseDokumentasjon, UUID> {
 
