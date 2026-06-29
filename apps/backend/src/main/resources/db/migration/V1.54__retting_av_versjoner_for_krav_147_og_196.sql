bf0248e56a2d9079b75143a1ca82129a5901d890
JeremiahUy <jeremiah.uy@outlook.com>
2026-06-29 10:21:16 +0200

    FIX: wrong column name in migration script to fix krav versjons

--- apps/backend/src/main/resources/db/migration/V1.54__retting_av_versjoner_for_krav_147_og_196.sql
+++ apps/backend/src/main/resources/db/migration/V1.54__retting_av_versjoner_for_krav_147_og_196.sql
@@ -3,105 +3,105 @@
 
 -- versjon 2 til 1 --
 UPDATE KRAV
-SET kravVersjon = '1'
-WHERE kravNummer = '147' and kravVersjon = '2';
+SET KRAV_VERSJON = '1'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '2';
 
 UPDATE ETTERLEVELSE
-SET kravVersjon = '1'
-WHERE kravNummer = '147' and kravVersjon = '2';
+SET KRAV_VERSJON = '1'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '2';
 
 UPDATE ETTERLEVELSE_METADATA
-SET kravVersjon = '1'
-WHERE kravNummer = '147' and kravVersjon = '2';
+SET KRAV_VERSJON = '1'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '2';
 
 UPDATE TILBAKEMELDING
-SET kravVersjon = '1'
-WHERE kravNummer = '147' and kravVersjon = '2';
+SET KRAV_VERSJON = '1'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '2';
 
 -- versjon 3 til 2 --
 UPDATE KRAV
-SET kravVersjon = '2'
-WHERE kravNummer = '147' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '3';
 
 UPDATE ETTERLEVELSE
-SET kravVersjon = '2'
-WHERE kravNummer = '147' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '3';
 
 UPDATE ETTERLEVELSE_METADATA
-SET kravVersjon = '2'
-WHERE kravNummer = '147' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '3';
 
 UPDATE TILBAKEMELDING
-SET kravVersjon = '2'
-WHERE kravNummer = '147' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '147' and KRAV_VERSJON = '3';
 
 
 -- Krav 196 --
 
 -- versjon 3 til 2 --
 UPDATE KRAV
-SET kravVersjon = '2'
-WHERE kravNummer = '196' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '3';
 
 UPDATE ETTERLEVELSE
-SET kravVersjon = '2'
-WHERE kravNummer = '196' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '3';
 
 UPDATE ETTERLEVELSE_METADATA
-SET kravVersjon = '2'
-WHERE kravNummer = '196' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '3';
 
 UPDATE TILBAKEMELDING
-SET kravVersjon = '2'
-WHERE kravNummer = '196' and kravVersjon = '3';
+SET KRAV_VERSJON = '2'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '3';
 
 -- versjon 4 til 3 --
 UPDATE KRAV
-SET kravVersjon = '3'
-WHERE kravNummer = '196' and kravVersjon = '4';
+SET KRAV_VERSJON = '3'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '4';
 
 UPDATE ETTERLEVELSE
-SET kravVersjon = '3'
-WHERE kravNummer = '196' and kravVersjon = '4';
+SET KRAV_VERSJON = '3'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '4';
 
 UPDATE ETTERLEVELSE_METADATA
-SET kravVersjon = '3'
-WHERE kravNummer = '196' and kravVersjon = '4';
+SET KRAV_VERSJON = '3'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '4';
 
 UPDATE TILBAKEMELDING
-SET kravVersjon = '3'
-WHERE kravNummer = '196' and kravVersjon = '4';
+SET KRAV_VERSJON = '3'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '4';
 
 -- versjon 5 til 4 --
 UPDATE KRAV
-SET kravVersjon = '4'
-WHERE kravNummer = '196' and kravVersjon = '5';
+SET KRAV_VERSJON = '4'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '5';
 
 UPDATE ETTERLEVELSE
-SET kravVersjon = '4'
-WHERE kravNummer = '196' and kravVersjon = '5';
+SET KRAV_VERSJON = '4'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '5';
 
 UPDATE ETTERLEVELSE_METADATA
-SET kravVersjon = '4'
-WHERE kravNummer = '196' and kravVersjon = '5';
+SET KRAV_VERSJON = '4'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '5';
 
 UPDATE TILBAKEMELDING
-SET kravVersjon = '4'
-WHERE kravNummer = '196' and kravVersjon = '5';
+SET KRAV_VERSJON = '4'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '5';
 
 -- versjon 6 til 5 --
 UPDATE KRAV
-SET kravVersjon = '5'
-WHERE kravNummer = '196' and kravVersjon = '6';
+SET KRAV_VERSJON = '5'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '6';
 
 UPDATE ETTERLEVELSE
-SET kravVersjon = '5'
-WHERE kravNummer = '196' and kravVersjon = '6';
+SET KRAV_VERSJON = '5'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '6';
 
 UPDATE ETTERLEVELSE_METADATA
-SET kravVersjon = '5'
-WHERE kravNummer = '196' and kravVersjon = '6';
+SET KRAV_VERSJON = '5'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '6';
 
 UPDATE TILBAKEMELDING
-SET kravVersjon = '5'
-WHERE kravNummer = '196' and kravVersjon = '6';
+SET KRAV_VERSJON = '5'
+WHERE KRAV_NUMMER = '196' and KRAV_VERSJON = '6';
