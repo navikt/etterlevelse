9be02196a01f73bd2321cbe83ff18c4a28a154da
JeremiahUy <jeremiah.uy@outlook.com>
2026-06-29 10:14:50 +0200

    ADD: migration script to fix krav versjons

---
+++ apps/backend/src/main/resources/db/migration/V1.54__retting_av_versjoner_for_krav_147_og_196.sql
@@ -0,0 +1,107 @@
+
+-- Krav 147 --
+
+-- versjon 2 til 1 --
+UPDATE KRAV
+SET kravVersjon = '1'
+WHERE kravNummer = '147' and kravVersjon = '2';
+
+UPDATE ETTERLEVELSE
+SET kravVersjon = '1'
+WHERE kravNummer = '147' and kravVersjon = '2';
+
+UPDATE ETTERLEVELSE_METADATA
+SET kravVersjon = '1'
+WHERE kravNummer = '147' and kravVersjon = '2';
+
+UPDATE TILBAKEMELDING
+SET kravVersjon = '1'
+WHERE kravNummer = '147' and kravVersjon = '2';
+
+-- versjon 3 til 2 --
+UPDATE KRAV
+SET kravVersjon = '2'
+WHERE kravNummer = '147' and kravVersjon = '3';
+
+UPDATE ETTERLEVELSE
+SET kravVersjon = '2'
+WHERE kravNummer = '147' and kravVersjon = '3';
+
+UPDATE ETTERLEVELSE_METADATA
+SET kravVersjon = '2'
+WHERE kravNummer = '147' and kravVersjon = '3';
+
+UPDATE TILBAKEMELDING
+SET kravVersjon = '2'
+WHERE kravNummer = '147' and kravVersjon = '3';
+
+
+-- Krav 196 --
+
+-- versjon 3 til 2 --
+UPDATE KRAV
+SET kravVersjon = '2'
+WHERE kravNummer = '196' and kravVersjon = '3';
+
+UPDATE ETTERLEVELSE
+SET kravVersjon = '2'
+WHERE kravNummer = '196' and kravVersjon = '3';
+
+UPDATE ETTERLEVELSE_METADATA
+SET kravVersjon = '2'
+WHERE kravNummer = '196' and kravVersjon = '3';
+
+UPDATE TILBAKEMELDING
+SET kravVersjon = '2'
+WHERE kravNummer = '196' and kravVersjon = '3';
+
+-- versjon 4 til 3 --
+UPDATE KRAV
+SET kravVersjon = '3'
+WHERE kravNummer = '196' and kravVersjon = '4';
+
+UPDATE ETTERLEVELSE
+SET kravVersjon = '3'
+WHERE kravNummer = '196' and kravVersjon = '4';
+
+UPDATE ETTERLEVELSE_METADATA
+SET kravVersjon = '3'
+WHERE kravNummer = '196' and kravVersjon = '4';
+
+UPDATE TILBAKEMELDING
+SET kravVersjon = '3'
+WHERE kravNummer = '196' and kravVersjon = '4';
+
+-- versjon 5 til 4 --
+UPDATE KRAV
+SET kravVersjon = '4'
+WHERE kravNummer = '196' and kravVersjon = '5';
+
+UPDATE ETTERLEVELSE
+SET kravVersjon = '4'
+WHERE kravNummer = '196' and kravVersjon = '5';
+
+UPDATE ETTERLEVELSE_METADATA
+SET kravVersjon = '4'
+WHERE kravNummer = '196' and kravVersjon = '5';
+
+UPDATE TILBAKEMELDING
+SET kravVersjon = '4'
+WHERE kravNummer = '196' and kravVersjon = '5';
+
+-- versjon 6 til 5 --
+UPDATE KRAV
+SET kravVersjon = '5'
+WHERE kravNummer = '196' and kravVersjon = '6';
+
+UPDATE ETTERLEVELSE
+SET kravVersjon = '5'
+WHERE kravNummer = '196' and kravVersjon = '6';
+
+UPDATE ETTERLEVELSE_METADATA
+SET kravVersjon = '5'
+WHERE kravNummer = '196' and kravVersjon = '6';
+
+UPDATE TILBAKEMELDING
+SET kravVersjon = '5'
+WHERE kravNummer = '196' and kravVersjon = '6';
