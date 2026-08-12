a95a7bc851e49b319555eb0676d7dab3ef85d993
Andre Groseth <andregroseth@hotmail.com>
2026-08-12 12:15:46 +0200

    Fix mobile search background by wrapping it in dark Theme contex and change text color to white

--- apps/frontend-nextjs/app/components/others/layout/header/header.tsx
+++ apps/frontend-nextjs/app/components/others/layout/header/header.tsx
@@ -8,7 +8,7 @@ import { EMeldingStatus, EMeldingType, IMelding } from '@/constants/admin/messag
 import { EAlertType, IPageResponse } from '@/constants/commonConstants'
 import { UserContext } from '@/provider/user/userProvider'
 import { useQueryParam } from '@/util/hooks/customHooks/customHooks'
-import { InternalHeader, Spacer } from '@navikt/ds-react'
+import { InternalHeader, Spacer, Theme } from '@navikt/ds-react'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import { FunctionComponent, useContext, useEffect, useRef, useState } from 'react'
@@ -71,14 +71,18 @@ const Header: FunctionComponent<TProps> = ({ noSearchBar, noLoginButton }) => {
           </div>
         </InternalHeader>
       </div>
-      <div
+      <Theme
+        theme='dark'
+        hasBackground={false}
+        asChild
         className='flex lg:hidden bg-gray-900 py-2 px-1 w-full justify-center items-center'
-        role='search'
       >
-        <div className=' max-w-xl w-full '>
-          <MainSearch />
+        <div role='search'>
+          <div className=' max-w-xl w-full '>
+            <MainSearch />
+          </div>
         </div>
-      </div>
+      </Theme>
       {systemVarsel && systemVarsel.meldingStatus === EMeldingStatus.ACTIVE && (
         <div className='w-full flex justify-center'>
           <div
