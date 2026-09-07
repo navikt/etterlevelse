5e6afa1e1f50c97221fd3a2ad10c89f7e8a6e924
JeremiahUy <jeremiah.uy@outlook.com>
2026-09-07 14:21:28 +0200

    ADD: default values and contrains to new columns, fix typo on new column names

--- apps/backend/src/main/resources/db/migration/V1.55__update_risikoscenario_tiltak_relation_table_with_new_fields.sql
+++ apps/backend/src/main/resources/db/migration/V1.55__update_risikoscenario_tiltak_relation_table_with_new_fields.sql
@@ -1,4 +1,4 @@
 alter table risikoscenario_tiltak_relation
 DROP CONSTRAINT risikoscenario_tiltak_relation_risikoscenario_id_tiltak_id_key
-ADD COLUMN gyldigDatoFra timestamp
-ADD COLUMN gyldigDatoTil timestamp;
+ADD COLUMN gyldig_dato_fra timestamp not null default '1999-01-01 00:00:00'
+ADD COLUMN gyldig_dato_til timestamp not null default '9999-12-31 23:59:59';
