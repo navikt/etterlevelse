2965c287f290d6a95a566fba2ac2d293a8196bbe
JeremiahUy <jeremiah.uy@outlook.com>
2026-09-07 15:14:00 +0200

    UPDATE: migration for risikoscenario tiltak relation to include history

--- apps/backend/src/main/resources/db/migration/V1.55__update_risikoscenario_tiltak_relation_table_with_new_fields.sql
+++ apps/backend/src/main/resources/db/migration/V1.55__update_risikoscenario_tiltak_relation_table_with_new_fields.sql
@@ -1,4 +1,11 @@
 alter table risikoscenario_tiltak_relation
-DROP CONSTRAINT risikoscenario_tiltak_relation_risikoscenario_id_tiltak_id_key
-ADD COLUMN gyldig_dato_fra timestamp not null default '1999-01-01 00:00:00'
-ADD COLUMN gyldig_dato_til timestamp not null default '9999-12-31 23:59:59';
+    add column gyldig_dato_fra timestamp not null default '-infinity',
+    add column gyldig_dato_til timestamp not null default 'infinity',
+    drop constraint if exists risikoscenario_tiltak_relation_risikoscenario_id_tiltak_id_key,
+    drop constraint if exists risikoscenario_tiltak_relation_risikoscenario_id_fkey,
+    drop constraint if exists risikoscenario_tiltak_relation_tiltak_id_fkey;
+
+alter table risikoscenario_tiltak_relation
+    alter column risikoscenario_id set not null,
+alter column tiltak_id set not null,
+    add constraint chk_gyldig_periode check (gyldig_dato_fra < gyldig_dato_til);
