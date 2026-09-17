99e3becbf0cef1533c0aa46adbcd89220170c80e
K153697 <paul.martin.kokseter@nav.no>
2026-09-17 09:44:07 +0200

    Testing

--- apps/frontend-nextjs/app/page.tsx
+++ apps/frontend-nextjs/app/page.tsx
@@ -4,8 +4,6 @@ import StatusIOrganisasjonen from './components/frontpage/StatusIOrganisasjonen'
 import { EtterlevelseDokumentasjon } from './components/frontpage/etterlevelseDokumentasjon/etterlevelseDokumentasjon'
 import { PageLayout } from './components/others/scaffold/scaffold'
 
-console.debug('test')
-// test
 export default function Home() {
   return (
     <PageLayout noPadding fullWidth>
