d3986d6f5c63ab3a1df453257844d5d2814a2b4d
beeccy <beeccy@users.noreply.github.com>
2026-09-22 10:29:25 +0200

    NAV-30031 Unused exports

--- apps/frontend-nextjs/app/routes/krav/kravRoutes.ts
+++ apps/frontend-nextjs/app/routes/krav/kravRoutes.ts
@@ -29,8 +29,6 @@ export const kravNyVersjonIdUrl = (kravId?: string): string => {
 export const kravPathUrl = (currentPath: string, nextKravPath: string): string =>
   `${currentPath}${kravUrl}${nextKravPath}`
 
-export const kravTemaFilterUrl = (tema: string, filter: string): string => `${tema}/${filter}`
-
 export const kravNummerUrl = (kravNummer: string): string => `${kravUrl}/${kravNummer}`
 
 export const kravNummerVersjonUrl = (
