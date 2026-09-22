6a24747a537fc15c8e4333c582509880e919fc93
beeccy <beeccy@users.noreply.github.com>
2026-09-22 12:09:01 +0200

    NAV-30031 Endret rekkefølge av script

--- apps/frontend-nextjs/app/components/risikoscenario/common/risikoscenarioAccordianListReadOnlyWithIverksetting.tsx
+++ apps/frontend-nextjs/app/components/risikoscenario/common/risikoscenarioAccordianListReadOnlyWithIverksetting.tsx
@@ -1,9 +1,6 @@
 'use client'
 
-import NyttInnholdTag from './NyttInnholdTag'
-import RisikoscenarioView from './RisikoscenarioView'
-import { IdentifiseringAvRisikoscenarioAccordianHeader } from './risikoscenarioAccordionHeader'
-import { RisikoscenarioTiltakHeader } from './risikoscenarioTiltakHeader'
+import { TiltakViewWithIverksetting } from '@/components/tiltak/common/tiltakView'
 import { IRisikoscenario } from '@/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/risikoscenario/risikoscenarioConstants'
 import { ITiltak } from '@/constants/etterlevelseDokumentasjon/personvernkonsekvensevurdering/tiltak/tiltakConstants'
 import { IVurdering } from '@/constants/pvoTilbakemelding/pvoTilbakemeldingConstants'
@@ -12,7 +9,10 @@ import { Accordion, BodyLong, LocalAlert, ReadMore } from '@navikt/ds-react'
 import moment from 'moment'
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { FunctionComponent, useEffect, useRef, useState } from 'react'
-import { TiltakViewWithIverksetting } from '@/components/tiltak/common/tiltakView'
+import NyttInnholdTag from './NyttInnholdTag'
+import RisikoscenarioView from './RisikoscenarioView'
+import { IdentifiseringAvRisikoscenarioAccordianHeader } from './risikoscenarioAccordionHeader'
+import { RisikoscenarioTiltakHeader } from './risikoscenarioTiltakHeader'
 
 type TProps = {
   risikoscenarioList: IRisikoscenario[]
