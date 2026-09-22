26f57a46841605033ecb3e39c3b23ef7db9d26df
beeccy <beeccy@users.noreply.github.com>
2026-09-22 12:02:10 +0200

    NAV-30031 Unused exports

--- apps/frontend-nextjs/app/components/varslingsadresse/VarslingsadresserEdit.tsx
+++ apps/frontend-nextjs/app/components/varslingsadresse/VarslingsadresserEdit.tsx
@@ -1,29 +1,20 @@
 'use client'
 
-import {
-  getSlackChannelById,
-  getSlackUserByEmail,
-  getSlackUserById,
-  usePersonSearch,
-} from '@/api/teamkatalogen/teamkatalogenApi'
+import { getSlackChannelById, getSlackUserById } from '@/api/teamkatalogen/teamkatalogenApi'
 import { IEtterlevelseDokumentasjon } from '@/constants/etterlevelseDokumentasjon/etterlevelseDokumentasjonConstants'
 import { TKravQL } from '@/constants/krav/kravConstants'
 import { ISlackChannel, ISlackUser } from '@/constants/teamkatalogen/slack/slackConstants'
-import { ITeamResource } from '@/constants/teamkatalogen/teamkatalogConstants'
 import {
   EAdresseType,
   IVarslingsadresse,
   TVarslingsadresseQL,
 } from '@/constants/teamkatalogen/varslingsadresse/varslingsadresseConstants'
 import { UserContext } from '@/provider/user/userProvider'
-import { noOptionMessage, selectOverrides } from '@/util/search/searchUtil'
 import { EnvelopeClosedIcon, HashtagIcon, PersonIcon, PlusIcon } from '@navikt/aksel-icons'
-import { Alert, Button, Loader, TextField } from '@navikt/ds-react'
+import { Button, TextField } from '@navikt/ds-react'
 import { FieldArray, FieldArrayRenderProps } from 'formik'
 import React, { FunctionComponent, useContext, useEffect, useState } from 'react'
-import AsyncSelect from 'react-select/async'
 import * as yup from 'yup'
-import { DropdownIndicator } from '../common/dropdownIndicator/dropdownIndicator'
 import { FieldWrapper } from '../common/fieldWrapper/fieldWrapper'
 import { LabelWithDescription } from '../common/labelWithoTootip.tsx/LabelWithTooltip'
 import { Error } from '../common/modalSchema/ModalSchema'
@@ -225,69 +216,6 @@ type TAddVarslingsadresseProps = {
   add: (v: IVarslingsadresse) => void
   added?: IVarslingsadresse[]
   close?: () => void
-}
-
-export const SlackUserSearch: FunctionComponent<TAddVarslingsadresseProps> = ({ add, close }) => {
-  const [error, setError] = useState('')
-  const [loadingSlackId, setLoadingSlackId] = useState(false)
-  const user = useContext(UserContext)
-
-  const addEmail = (email: string) => {
-    getSlackUserByEmail(email)
-      .then((user) => {
-        add({ type: EAdresseType.SLACK_USER, adresse: user.id })
-        setLoadingSlackId(false)
-        setError('')
-        if (close) {
-          close()
-        }
-      })
-      .catch((e) => {
-        setError('Fant ikke slack for bruker, error: ' + e.toString())
-        setLoadingSlackId(false)
-      })
-  }
-
-  return (
-    <div className='flex flex-col'>
-      <ContentLayout>
-        <div className='w-full'>
-          <AsyncSelect
-            aria-label='Søk etter slack-bruker'
-            placeholder='Søk etter slack-bruker'
-            tabSelectsValue={false}
-            noOptionsMessage={({ inputValue }) => noOptionMessage(inputValue)}
-            controlShouldRenderValue={false}
-            loadingMessage={() => 'Søker...'}
-            isClearable={false}
-            loadOptions={usePersonSearch}
-            components={{ DropdownIndicator }}
-            onFocus={() => setError('')}
-            onBlur={() => setError('')}
-            onChange={(person) => {
-              const resource = person as ITeamResource
-              if (resource) {
-                setLoadingSlackId(true)
-                addEmail(resource.email)
-              }
-            }}
-            styles={selectOverrides}
-          />
-        </div>
-        <div className='flex justify-end ml-2.5'>
-          <Button type='button' onClick={() => addEmail(user.getEmail())}>
-            Meg
-          </Button>
-        </div>
-      </ContentLayout>
-      {loadingSlackId && <Loader size='large' className='flex justify-self-center' />}
-      {error && (
-        <Alert className='mt-2.5' variant='error'>
-          {error}
-        </Alert>
-      )}
-    </div>
-  )
 }
 
 export const emailValidator = yup
